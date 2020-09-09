16b8ce4f537f31e6536a2a2566de7aa6f2154f44
David Oberholzer <david@maykinmedia.nl>
2020-09-09 14:24:31 +0200

    :sparkles: Added #15 -- Added basic formio.js builder and admin widget saves to form definition.

--- src/openforms/core/admin/form_definition.py
+++ src/openforms/core/admin/form_definition.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
+from openforms.core.forms import FormDefinitionForm
+
 
 class FormDefinitionAdmin(admin.ModelAdmin):
+    form = FormDefinitionForm
     prepopulated_fields = {'slug': ('name',)}
---
+++ src/openforms/core/widgets/__init__.py
@@ -0,0 +1,1 @@
+from .form_builder import FormBuilderWidget
