6f2f124ccd1707336ad3ee87336a691749554d05
Sven van de Scheur <svenvandescheur@gmail.com>
2020-09-10 18:20:45 +0200

    :sparkles: Fixes #8 -- As developer I want an implementation of the design in frontend code

--- src/openforms/sample_app/urls.py
+++ src/openforms/sample_app/urls.py
@@ -5,8 +5,18 @@ app_name = 'sample_app'
 
 urlpatterns = [
     path('digid-login', TemplateView.as_view(template_name='sample_app/views/digid_login/digid_login.html'), name='digid_login'),
+
     path('form-details', TemplateView.as_view(template_name='sample_app/views/form_details/form_details.html'), name='form_details'),
-    path('form-errors', TemplateView.as_view(template_name='sample_app/views/form_details/form_errors.html'), name='form_errors'),
-    path('', RedirectView.as_view(url="form-details")),
+    path('form-details/errors', TemplateView.as_view(template_name='sample_app/views/form_details/form_details_errors.html'), name='form_details_errors'),
+
+    path('form-appointment', TemplateView.as_view(template_name='sample_app/views/form_appointment/form_appointment.html'), name='form_appointment'),
+    path('form-appointment/errors', TemplateView.as_view(template_name='sample_app/views/form_appointment/form_appointment_errors.html'), name='form_appointment_errors'),
+
+    path('form-contact', TemplateView.as_view(template_name='sample_app/views/form_contact/form_contact.html'), name='form_contact'),
+    path('form-contact/errors', TemplateView.as_view(template_name='sample_app/views/form_contact/form_contact_errors.html'), name='form_contact_errors'),
+
+    path('overview', TemplateView.as_view(template_name='sample_app/views/overview/overview.html'), name='overview'),
+
+    path('', RedirectView.as_view(url="digid-login")),
 
 ]
