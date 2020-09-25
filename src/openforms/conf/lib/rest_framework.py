e6f9943f94977322ce5c2dd50cc4be3cc4bb8f25
Joeri Bekker <joeri@maykinmedia.nl>
2020-09-25 15:56:10 +0200

    :tada: Added API token auth.

--- src/openforms/conf/lib/rest_framework.py
+++ src/openforms/conf/lib/rest_framework.py
@@ -1,2 +1,7 @@
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        # TODO: left this in here to not break stuff during hackathon
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
