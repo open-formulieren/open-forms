ebd58c22395e477364db40778869f169344fc0e0
Joeri Bekker <joeri@maykinmedia.nl>
2020-09-25 15:55:52 +0200

    :tada: Added completed action on submission API.

--- src/openforms/submissions/models.py
+++ src/openforms/submissions/models.py
@@ -12,6 +12,7 @@ class Submission(models.Model):
     form = models.ForeignKey('core.Form', on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
     backend_result = JSONField(blank=True, null=True)
+    completed_on = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         verbose_name = 'Submission'
@@ -19,6 +20,10 @@ class Submission(models.Model):
 
     def __str__(self):
         return f'Submission {self.pk}: Form {self.form_id} submitted on {self.created_on}'
+
+    @property
+    def is_completed(self):
+        return bool(self.completed_on)
 
 
 class SubmissionStep(models.Model):
