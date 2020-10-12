c4b531030523f0d82e0716d854958ebf38c065e7
David Oberholzer <david@maykinmedia.nl>
2020-10-12 17:04:35 +0200

    ✅ - 🚧 Selenium test flow added WIP

--- src/openforms/core/tests/base.py
+++ src/openforms/core/tests/base.py
@@ -1,5 +1,3 @@
-import mock
-
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium import webdriver
@@ -9,13 +7,159 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-class SeleniumDisclosureStepTestBase(StaticLiveServerTestCase):
+TEST_FORM_DEFINITION_CONFIG = {
+    "components": [
+        {
+            "label": "Fieldy Field",
+            "key": "fieldyField",
+            "description": "",
+            "conditional": {
+                "show": None,
+                "when": None,
+                "eq": ""
+            },
+            "input": True,
+            "placeholder": "",
+            "prefix": "",
+            "customClass": "",
+            "suffix": "",
+            "multiple": False,
+            "defaultValue": None,
+            "protected": False,
+            "unique": False,
+            "persistent": True,
+            "hidden": False,
+            "clearOnHide": True,
+            "refreshOn": "",
+            "redrawOn": "",
+            "tableView": True,
+            "modalEdit": False,
+            "labelPosition": "top",
+            "errorLabel": "",
+            "tooltip": "",
+            "hideLabel": False,
+            "tabindex": "",
+            "disabled": False,
+            "autofocus": False,
+            "dbIndex": False,
+            "customDefaultValue": "",
+            "calculateValue": "",
+            "calculateServer": False,
+            "widget": {
+                "type": "input"
+            },
+            "attributes": {},
+            "validateOn": "change",
+            "validate": {
+                "required": False,
+                "custom": "",
+                "customPrivate": False,
+                "strictDateValidation": False,
+                "multiple": False,
+                "unique": False,
+                "minLength": "",
+                "maxLength": "",
+                "pattern": ""
+            },
+            "overlay": {
+                "style": "",
+                "left": "",
+                "top": "",
+                "width": "",
+                "height": ""
+            },
+            "allowCalculateOverride": False,
+            "encrypted": False,
+            "showCharCount": False,
+            "showWordCount": False,
+            "properties": {},
+            "allowMultipleMasks": False,
+            "type": "textfield",
+            "mask": False,
+            "inputType": "text",
+            "inputFormat": "plain",
+            "inputMask": "",
+            "spellcheck": True,
+            "id": "e0evwd"
+        },
+        {
+            "label": "Checky",
+            "key": "checky",
+            "description": "",
+            "input": True,
+            "placeholder": "",
+            "prefix": "",
+            "customClass": "",
+            "suffix": "",
+            "multiple": False,
+            "protected": False,
+            "unique": False,
+            "persistent": True,
+            "hidden": False,
+            "clearOnHide": True,
+            "refreshOn": "",
+            "redrawOn": "",
+            "tableView": False,
+            "modalEdit": False,
+            "labelPosition": "right",
+            "errorLabel": "",
+            "tooltip": "",
+            "hideLabel": False,
+            "tabindex": "",
+            "disabled": False,
+            "autofocus": False,
+            "dbIndex": False,
+            "customDefaultValue": "",
+            "calculateValue": "",
+            "calculateServer": False,
+            "widget": None,
+            "attributes": {},
+            "validateOn": "change",
+            "validate": {
+                "required": False,
+                "custom": "",
+                "customPrivate": False,
+                "strictDateValidation": False,
+                "multiple": False,
+                "unique": False
+            },
+            "conditional": {
+                "show": None,
+                "when": None,
+                "eq": ""
+            },
+            "overlay": {
+                "style": "",
+                "left": "",
+                "top": "",
+                "width": "",
+                "height": ""
+            },
+            "allowCalculateOverride": False,
+            "encrypted": False,
+            "showCharCount": False,
+            "showWordCount": False,
+            "properties": {},
+            "allowMultipleMasks": False,
+            "type": "checkbox",
+            "inputType": "checkbox",
+            "dataGridLabel": True,
+            "value": "",
+            "name": "",
+            "id": "ekyj0j",
+            "defaultValue": False
+        }
+    ]
+}
+
+
+class SeleniumStepTestBase(StaticLiveServerTestCase):
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
+        # chrome_options.add_argument("--headless")
         cls.selenium = webdriver.Chrome(chrome_options=chrome_options)
         cls.selenium.implicitly_wait(20)
 
@@ -24,23 +168,15 @@ class SeleniumDisclosureStepTestBase(StaticLiveServerTestCase):
         cls.selenium.quit()
         super().tearDownClass()
 
-    @mock.patch('vips.accounts.gateways.gateway.send')
-    def setUp(self, mocked_gateway) -> None:
+    def setUp(self) -> None:
         super().setUp()
-        self.selenium.get('{}'.format(self.live_server_url))
+        self.selenium.get('{}/admin/'.format(self.live_server_url))
         username = self.selenium.find_element_by_id('id_username')
         password = self.selenium.find_element_by_id('id_password')
 
         username.send_keys(self.user.username)
         password.send_keys('secret')
 
-        submit = self.selenium.find_element_by_name('submit')
-        submit.click()
-
-        sent_token = mocked_gateway.call_args[1]['token']
-
-        token = self.selenium.find_element_by_id('id_token')
-        token.send_keys(sent_token)
         submit = self.selenium.find_element_by_name('submit')
         submit.click()
 
---
+++ src/openforms/core/tests/test_flow.py
@@ -0,0 +1,80 @@
+from openforms.submissions.models import Submission
+
+from .base import SeleniumStepTestBase, TEST_FORM_DEFINITION_CONFIG
+from .factories import FormDefinitionFactory, FormFactory, FormStepFactory
+
+
+class FormFlowTests(SeleniumStepTestBase):
+
+    def setUp(self) -> None:
+        self.form = FormFactory(name='TestForm1')
+        self.definition_1 = FormDefinitionFactory(
+            configuration=TEST_FORM_DEFINITION_CONFIG,
+            login_required=False
+        )
+        self.definition_2 = FormDefinitionFactory(
+            configuration=TEST_FORM_DEFINITION_CONFIG,
+            login_required=False
+        )
+        self.definition_3 = FormDefinitionFactory(
+            configuration=TEST_FORM_DEFINITION_CONFIG,
+            login_required=False
+        )
+        self.step_1 = FormStepFactory(form=self.form, form_definition=self.definition_1)
+        self.step_2 = FormStepFactory(form=self.form, form_definition=self.definition_2)
+        self.step_3 = FormStepFactory(form=self.form, form_definition=self.definition_3)
+
+    def test_submit_flow(self):
+        self.selenium.get(self.live_server_url)
+        self.selenium.find_element_by_xpath(
+            '//a[text()="TestForm1"]'
+        ).click()
+
+        text = self.selenium.find_element_by_xpath(
+            '//input[@name="fieldyField"]'
+        )
+        text.send_keys('some input')
+        checkbox = self.selenium.find_element_by_xpath(
+            '//input[@name="checky"]'
+        )
+        checkbox.click()
+
+        submit = self.selenium.find_element_by_name('submit')
+        submit.click()
+
+        submission = Submission.objects.get()
+
+        submission_steps = submission.submissionstep_set.all()
+        self.assertEqual(len(submission_steps), 1)
+        self.assertEqual(submission_steps[0].form_step, self.step_1)
+        self.assertEqual(submission_steps[0].data, {
+            'fieldyField': 'some input',
+            'checky': True
+        })
+
+        text = self.selenium.find_element_by_xpath(
+            '//input[@name="fieldyField"]'
+        )
+        text.send_keys('some other input')
+        checkbox = self.selenium.find_element_by_xpath(
+            '//input[@name="checky"]'
+        )
+        checkbox.click()
+
+        submit = self.selenium.find_element_by_name('submit')
+        submit.click()
+
+        submission = Submission.objects.get()
+
+        submission_steps = submission.submissionstep_set.all()
+        self.assertEqual(len(submission_steps), 2)
+        self.assertEqual(submission_steps[1].form_step, self.step_1)
+        self.assertEqual(submission_steps[1].data, {
+            'fieldyField': 'some input',
+            'checky': True
+        })
+        self.assertEqual(submission_steps[1].form_step, self.step_2)
+        self.assertEqual(submission_steps[1].data, {
+            'fieldyField': 'some other input',
+            'checky': True
+        })
