380a930d3dd866b5ca57352b1354b862a9cb9cd5
Sergei Maertens <sergei@maykinmedia.nl>
2020-11-13 12:37:12 +0100

    :white_check_mark: Refs. #108 -- added tests for (multiple) submission starts
    * test that a submission can be started for a given form
    * test that a submission start does not require authentication
    * test that multiple submissions can be started for the same form

--- src/openforms/conf/lib/rest_framework.py
+++ src/openforms/conf/lib/rest_framework.py
@@ -1,15 +1,13 @@
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
         # TODO: left this in here to not break stuff during hackathon
-        'rest_framework.authentication.SessionAuthentication',
+        "rest_framework.authentication.SessionAuthentication",
     ],
-    'DEFAULT_THROTTLE_CLASSES': (
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
+    "DEFAULT_THROTTLE_CLASSES": (
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
     ),
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '200/day',
-        'user': '1000/day'
-    },
+    "DEFAULT_THROTTLE_RATES": {"anon": "200/day", "user": "1000/day"},
+    "TEST_REQUEST_DEFAULT_FORMAT": "json",
 }
---
+++ src/openforms/submissions/tests/test_start_submission.py
@@ -0,0 +1,85 @@
+"""
+Tests for the start of a submission.
+
+A submission acts sort of like a session. It is tied to a particular form (which is made
+up of form steps with their definitions).
+
+Functional requirements are:
+
+* multiple submissions for the same flow must be able to exist at the same time
+* data of different submissions should not affect each other
+* "login" makes no sense, as we are usually dealing with anonymous users
+"""
+from rest_framework import status
+from rest_framework.reverse import reverse, reverse_lazy
+from rest_framework.test import APITestCase
+
+from openforms.core.tests.factories import FormFactory, FormStepFactory
+
+from ..constants import SUBMISSIONS_SESSION_KEY
+from ..models import Submission
+
+
+class SubmissionStartTests(APITestCase):
+    endpoint = reverse_lazy("api:submission-list")
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+
+        # ensure there is a form definition
+        cls.form = FormFactory.create()
+        cls.step = FormStepFactory.create(form=cls.form)
+        cls.form_url = reverse("api:form-detail", kwargs={"uuid": cls.form.uuid})
+        cls.formstep_url = reverse(
+            "api:form-steps-detail",
+            kwargs={"form_uuid": cls.form.uuid, "uuid": cls.step.uuid},
+        )
+
+    def test_start_submission(self):
+        body = {
+            "form": f"http://testserver{self.form_url}",
+        }
+
+        response = self.client.post(self.endpoint, body)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        submission = Submission.objects.get()
+        self.assertEqual(
+            response.json(),
+            {
+                "id": submission.uuid,
+                "form": f"http://testserver{self.form_url}",
+                "nextStep": f"http://testserver{self.formstep_url}",
+            },
+        )
+
+        # check that the submission ID is in the session
+        self.assertEqual(
+            response.wsgi_request.session[SUBMISSIONS_SESSION_KEY],
+            [str(submission.uuid)],
+        )
+
+    def test_start_second_submission(self):
+        body = {
+            "form": f"http://testserver{self.form_url}",
+        }
+
+        with self.subTest(state="first submission"):
+            response = self.client.post(self.endpoint, body)
+
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        with self.subTest(state="second submission"):
+            response = self.client.post(self.endpoint, body)
+
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+            submissions = Submission.objects.all()
+            self.assertEqual(submissions.count(), 2)
+
+            ids = submissions.values_list("uuid", flat=True)
+            self.assertEqual(
+                set(response.wsgi_request.session[SUBMISSIONS_SESSION_KEY]),
+                {str(uuid) for uuid in ids},
+            )
