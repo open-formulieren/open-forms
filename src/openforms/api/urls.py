18d4d7d8048b56f5c7693c29dcdcf8fcf9a95fb4
Sergei Maertens <sergei@maykinmedia.nl>
2020-12-03 17:28:58 +0100

    :pencil: Minimal schema documentation

--- src/openforms/api/urls.py
+++ src/openforms/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 from rest_framework import routers
+from rest_framework.schemas import get_schema_view
 from rest_framework_nested.routers import NestedSimpleRouter
 
 from openforms.core.api.viewsets import (
@@ -9,31 +11,50 @@ from openforms.core.api.viewsets import (
     FormViewSet,
 )
 from openforms.submissions.api.viewsets import (
-    FormSubmissionViewSet, SubmissionStepViewSet, SubmissionViewSet
+    FormSubmissionViewSet,
+    SubmissionStepViewSet,
+    SubmissionViewSet,
 )
 
 app_name = "api"
 
 router = routers.DefaultRouter(trailing_slash=False)
 # form definitions, raw Formio output
-router.register(r'form-definitions', FormDefinitionViewSet)
+router.register(r"form-definitions", FormDefinitionViewSet)
 
 # forms & their steps
-router.register(r'forms', FormViewSet)
-forms_router = NestedSimpleRouter(router, r'forms', lookup='form')
-forms_router.register(r'steps', FormStepViewSet, basename='form-steps')
+router.register(r"forms", FormViewSet)
+forms_router = NestedSimpleRouter(router, r"forms", lookup="form")
+forms_router.register(r"steps", FormStepViewSet, basename="form-steps")
 
 # submissions API
-router.register(r'submissions', SubmissionViewSet)
-submissions_router = NestedSimpleRouter(router, r'submissions', lookup='submission')
-submissions_router.register(r'steps', SubmissionStepViewSet, basename='submission-steps')
+router.register(r"submissions", SubmissionViewSet)
+submissions_router = NestedSimpleRouter(router, r"submissions", lookup="submission")
+submissions_router.register(
+    r"steps", SubmissionStepViewSet, basename="submission-steps"
+)
 
-router.register(r'form-submissions', FormSubmissionViewSet, basename='form-submissions')
+router.register(r"form-submissions", FormSubmissionViewSet, basename="form-submissions")
 
 
 urlpatterns = [
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    path("", include(router.urls)),
-    path("", include(forms_router.urls)),
-    path("", include(submissions_router.urls)),
+    path(
+        "v1",
+        get_schema_view(
+            title="Open Forms",
+            description="Open Forms API to manage forms, form steps and submissions",
+            version="1.0.0",
+            public=True,
+        ),
+        name="openapi-schema",
+    ),
+    path(
+        "v1/docs/",
+        TemplateView.as_view(template_name="api_docs.html"),
+        name="api-docs",
+    ),
+    path("v1/api-auth", include("rest_framework.urls", namespace="rest_framework")),
+    path("v1/", include(router.urls)),
+    path("v1/", include(forms_router.urls)),
+    path("v1/", include(submissions_router.urls)),
 ]
--- src/openforms/urls.py
+++ src/openforms/urls.py
@@ -12,16 +12,31 @@ admin.site.site_title = "openforms admin"
 admin.site.index_title = "Welcome to the openforms admin"
 
 urlpatterns = [
-    path("admin/password_reset/", auth_views.PasswordResetView.as_view(), name="admin_password_reset", ),
-    path("admin/password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done", ),
-    path("admin/hijack/", include("hijack.urls")), path("admin/", admin.site.urls),
-    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm", ),
-    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete", ),
-
-    path("api/v1/", include('openforms.api.urls', namespace='api')),
-    path("prototype/", include('openforms.sample_app.urls', namespace='sample_app')),
-    path("", include('openforms.core.urls', namespace='core')),
-
+    path(
+        "admin/password_reset/",
+        auth_views.PasswordResetView.as_view(),
+        name="admin_password_reset",
+    ),
+    path(
+        "admin/password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path("admin/hijack/", include("hijack.urls")),
+    path("admin/", admin.site.urls),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
+    path("api/", include("openforms.api.urls", namespace="api")),
+    path("prototype/", include("openforms.sample_app.urls", namespace="sample_app")),
+    path("", include("openforms.core.urls", namespace="core")),
     # NOTE: we dont use the User creation feature so don't enable all the mock views
     # path("digid/", include("digid_eherkenning.mock.digid_urls")),
     path("digid/idp/", include("digid_eherkenning.mock.idp.digid_urls")),
@@ -36,4 +51,6 @@ urlpatterns += staticfiles_urlpatterns() + static(
 if settings.DEBUG and apps.is_installed("debug_toolbar"):
     import debug_toolbar
 
-    urlpatterns = [path("__debug__/", include(debug_toolbar.urls)),] + urlpatterns
+    urlpatterns = [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ] + urlpatterns
