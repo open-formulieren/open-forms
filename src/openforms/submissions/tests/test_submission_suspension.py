1ee5707c7113330e3bc5af5157a30e727536ebf4
Sergei Maertens <sergei@maykinmedia.nl>
2020-12-30 14:37:53 +0100

    :white_check_mark: Refs. #112 -- added test for submission suspension

---
+++ src/openforms/submissions/tests/test_submission_suspension.py
@@ -0,0 +1,53 @@
+"""
+Test suspending a form.
+
+Suspension of the form is an explicit action (api-wise), by making a POST call on a
+sub-resource.
+
+The backend collects information to send an e-mail to the user for resuming, for
+example.
+"""
+from django.utils import timezone
+
+from freezegun import freeze_time
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+
+from openforms.core.tests.factories import FormFactory, FormStepFactory
+
+from ..constants import SUBMISSIONS_SESSION_KEY
+from .factories import SubmissionFactory, SubmissionStepFactory
+from .mixins import SubmissionsMixin
+
+
+class SubmissionSuspensionTests(SubmissionsMixin, APITestCase):
+    def test_invalid_submission_id(self):
+        submission = SubmissionFactory.create()
+        endpoint = reverse("api:submission-suspend", kwargs={"uuid": submission.uuid})
+
+        response = self.client.post(endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    @freeze_time("2020-12-11T10:53:19+01:00")
+    def test_suspended_submission(self):
+        form = FormFactory.create()
+        FormStepFactory.create(form=form, optional=False)
+        step2 = FormStepFactory.create(form=form, optional=True)  # noqa
+        submission = SubmissionFactory.create(form=form)
+        SubmissionStepFactory.create(
+            submission=submission, form_step=step2, data={"foo": "bar"}
+        )
+        self._add_submission_to_session(submission)
+        endpoint = reverse("api:submission-suspend", kwargs={"uuid": submission.uuid})
+
+        response = self.client.post(endpoint, {"email": "hello@open-forms.nl"})
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        submission.refresh_from_db()
+        self.assertEqual(submission.suspended_on, timezone.now())
+
+        # test that submission ID is not removed from session
+        submissions_in_session = response.wsgi_request.session[SUBMISSIONS_SESSION_KEY]
+        self.assertIn(str(submission.uuid), submissions_in_session)
