55c6782e44c863fd5afcb9caa669bd626ada3db6
Sergei Maertens <sergei@maykinmedia.nl>
2020-12-30 17:53:34 +0100

    :alembic: Scaffold using material-ui

--- src/spa/src/App.js
+++ src/spa/src/App.js
@@ -1,34 +1,74 @@
-import FormList from './FormList';
-import FormDetail from './FormDetail';
+import React from 'react';
+
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
 
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link as RouterLink
 } from "react-router-dom";
+
+import FormList from './FormList';
+import FormDetail from './FormDetail';
+
+
+const useStyles = makeStyles((theme) => ({
+  navLink: {
+    marginRight: theme.spacing(1),
+  }
+}));
 
 
 const App = () => {
+  const classes = useStyles();
   return (
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Forms</Link>
-          </li>
-        </ul>
+      <Container maxWidth="xl">
 
-        <hr />
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+
+            <Link
+              to="/"
+              color="inherit"
+              variant="h5"
+              className={classes.navLink}
+              component={RouterLink} >Forms</Link>
+
+            <Link
+              to="/dummy"
+              color="inherit"
+              variant="h5"
+              className={classes.navLink}
+              component={RouterLink} >Dummy</Link>
+
+          </Toolbar>
+        </AppBar>
+
+        <Box my={4}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Open Forms demo
+          </Typography>
+        </Box>
 
         <Switch>
           <Route exact path="/"> <FormList /> </Route>
-          <Route path="/forms/:id">
-            <FormDetail />
-          </Route>
+          <Route path="/forms/:id"> <FormDetail /> </Route>
         </Switch>
 
-      </div>
+      </Container>
     </Router>
   );
 };
--- src/spa/src/index.js
+++ src/spa/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
