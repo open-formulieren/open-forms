cd8110a640b9cbfcf721f1cc11731d61cbc48096
Sergei Maertens <sergei@maykinmedia.nl>
2021-01-04 18:12:48 +0100

    :children_crossing: Display loader while loading

--- src/spa/src/FormList.js
+++ src/spa/src/FormList.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-import Link from '@material-ui/core/Link';
-import {Link as RouterLink} from 'react-router-dom';
-
+import {Link} from 'react-router-dom';
 import useAsync from 'react-use/esm/useAsync';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Paper from '@material-ui/core/Paper';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import { get } from './api';
 
@@ -18,31 +20,34 @@ const getForms = async () => {
 const FormList = () => {
   const {loading, value, error} = useAsync(getForms, []);
 
-  // TODO: display loader
-  if (loading) return null;
+  if (loading) {
+    return (<CircularProgress />);
+  }
+
   if (error) return (
     <div>Error: {JSON.stringify(error)}</div>
   );
 
   return (
-    <ul>
-    {
-      value.map( form => (
-        <li key={form.uuid}>
-          <Link to={`/forms/${form.uuid}`}
-                color="inherit"
-                component={RouterLink}>
+    <Paper>
+      <List>
+      {
+        value.map( form => (
+          <ListItem
+            key={form.uuid}
+            component={Link}
+            to={`/forms/${form.uuid}`}
+            color="inherit"
+          >
             {form.name}
-          </Link>
-        </li>
-      ))
-    }
-    </ul>
+          </ListItem>
+        ))
+      }
+      </List>
+    </Paper>
   );
 };
 
-FormList.propTypes = {
-};
-
+FormList.propTypes = {};
 
 export default FormList;
