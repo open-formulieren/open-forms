39ab656ea208d30bd9ffba41f444af82a7ff6e74
Sergei Maertens <sergei@maykinmedia.nl>
2021-04-09 11:35:33 +0200

    :fire: Remove dead code

--- src/openforms/forms/static/forms/js/components/formio_form/index.js
+++
@@ -1,8 +0,0 @@
-//jshint ignore:start
-import {FORMIO_FORMS} from './constants';
-
-// Start!
-if (FORMIO_FORMS.length) {
-    import(/* webpackChunkName: 'formio_form' */ './formio_form');
-}
-
--- src/openforms/forms/static/forms/js/components/index.js
+++ src/openforms/forms/static/forms/js/components/index.js
@@ -1,3 +1,2 @@
 import './form';
 import './formio_builder';
-import './formio_form';
