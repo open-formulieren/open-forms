87c6847d71e202b9d629a391a5d8b87174709d4d
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2021-04-12 11:48:06 +0200

    :bulb: Fix comment

--- src/openforms/forms/api/permissions.py
+++ src/openforms/forms/api/permissions.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsStaffOrReadOnly(BasePermission):
     """
-    The request is authenticated as a user, or is a read-only request.
+    The request is a staff user, or is a read-only request.
     """
 
     def has_permission(self, request, view):
