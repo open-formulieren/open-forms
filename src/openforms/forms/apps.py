376be271f4d61947755be2ebdbce3716ad0499ff
Bart van der Schoor <bart.vanderschoor@maykinmedia.nl>
2021-04-16 12:21:15 +0200

    :sparkles: Configured django-reversion for Form, FormStep and FormDefinition

--- src/openforms/forms/apps.py
+++ src/openforms/forms/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
+from openforms.forms.versioning import register_versioned_models
+
 
 class CoreConfig(AppConfig):
     name = "openforms.forms"
     verbose_name = "OpenForms Form App"
+
+    def ready(self):
+        register_versioned_models()
