c3b79e500938ee6a64c47dc27cd5aefac944f38d
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2021-04-22 10:43:25 +0200

    :construction: Get first FormIO form rendered on page

--- src/openforms/ui/static/ui/js/components/form-create/form-create.js
+++ src/openforms/ui/static/ui/js/components/form-create/form-create.js
@@ -1,9 +1,48 @@
 import React from "react";
 
-const CreateForm = (props) => {
+import { Form as FormioForm } from 'react-formio';
 
+// Need to send name and slug of form, just those two
+
+const configuration = {
+    components: [
+        {
+            type: 'textfield',
+            key: 'name',
+            label: 'Name',
+            placeholder: 'Enter the form name.',
+            input: true
+        },
+        {
+            type: 'textfield',
+            key: 'slug',
+            label: 'Slug',
+            placeholder: 'Enter the form slug',
+            input: true
+        },
+        {
+            type: 'button',
+            action: 'submit',
+            label: 'Submit',
+            theme: 'primary'
+        }
+    ]
+};
+
+
+const CreateForm = () => {
   return (
-    <h1>Hello React World!</h1>
+    <div className="card">
+      <header className="card__header">
+        <h2 className="title">Create Form with React and FormIO</h2>
+      </header>
+      <div className="card__body" style={{display: 'flex'}}>
+
+        <div style={{width: '75%'}}>
+            <FormioForm form={configuration} onSubmit={console.log} />
+        </div>
+      </div>
+    </div>
   );
 };
 
