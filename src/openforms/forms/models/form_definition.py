f2c4003e62bff3ff2572ef70b023a084c9e666b7
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2021-04-30 15:38:20 +0200

    :sparkles: Add method to create a copy of a form definition

--- src/openforms/forms/models/form_definition.py
+++ src/openforms/forms/models/form_definition.py
@@ -28,6 +28,33 @@ class FormDefinition(models.Model):
     def get_absolute_url(self):
         return reverse("forms:form_definition_detail", kwargs={"slug": self.slug})
 
+    def _get_kopie_name(self):
+        if self.name.endswith('Kopie'):
+            name = f'{self.name} 1'
+        elif self.name[:-2].endswith('Kopie') and self.name[-1].isdigit():
+            num = int(self.name[-1]) + 1
+            name = f'{self.name[:-2]} {num}'
+        else:
+            name = f'{self.name} Kopie'
+        return name
+
+    def _get_slug_name(self):
+        if self.slug.endswith('-kopie'):
+            slug = f'{self.slug}1'
+        elif self.slug[:-1].endswith('-kopie') and self.slug[-1].isdigit():
+            num = int(self.slug[-1]) + 1
+            slug = f'{self.slug[:-1]}{num}'
+        else:
+            slug = f'{self.slug}-kopie'
+        return slug
+
+    def copy(self):
+        self.pk = None
+        self.uuid = uuid.uuid4()
+        self.name = self._get_kopie_name()
+        self.slug = self._get_slug_name()
+        self.save()
+
     def __str__(self):
         return self.name
 
--- src/openforms/forms/tests/test_models.py
+++ src/openforms/forms/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from .factories import FormDefinitionFactory, FormFactory, FormStepFactory
+from ..models import FormDefinition
 
 
 class FormTestCase(TestCase):
@@ -31,3 +32,27 @@ class FormTestCase(TestCase):
         self.assertEqual(form2.name, "A form (kopie)")
         self.assertEqual(form3.slug, "a-form-kopie-2")
         self.assertEqual(form3.name, "A form (kopie)")
+
+
+class FormDefinitionTestCase(TestCase):
+    def setUp(self) -> None:
+        self.form_definition = FormDefinitionFactory.create()
+
+    def test_creating_copy_of_form_definition(self):
+        self.form_definition.copy()
+        self.assertEqual(FormDefinition.objects.count(), 2)
+        form_definition = FormDefinition.objects.order_by('name').last()
+        self.assertEqual(form_definition.name, 'FormDefinition 000 Kopie')
+        self.assertEqual(form_definition.slug, 'fd-0-kopie')
+
+        form_definition.copy()
+        self.assertEqual(FormDefinition.objects.count(), 3)
+        form_definition = FormDefinition.objects.order_by('name').last()
+        self.assertEqual(form_definition.name, 'FormDefinition 000 Kopie 1')
+        self.assertEqual(form_definition.slug, 'fd-0-kopie1')
+
+        form_definition.copy()
+        self.assertEqual(FormDefinition.objects.count(), 4)
+        form_definition = FormDefinition.objects.order_by('name').last()
+        self.assertEqual(form_definition.name, 'FormDefinition 000 Kopie 2')
+        self.assertEqual(form_definition.slug, 'fd-0-kopie2')
