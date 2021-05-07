4b5c79682c9c347625b1f793b01c6376ed32c8c5
Sergei Maertens <sergei@maykinmedia.nl>
2021-05-07 15:21:38 +0200

    :rotating_light: Black

--- src/openforms/forms/migrations/0012_merge_20210507_1158.py
+++ src/openforms/forms/migrations/0012_merge_20210507_1158.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('forms', '0011_auto_20210507_0958'),
-        ('forms', '0009_form__is_deleted'),
+        ("forms", "0011_auto_20210507_0958"),
+        ("forms", "0009_form__is_deleted"),
     ]
 
-    operations = [
-    ]
+    operations = []
