936633e959f13bf1e1f25d3dff17693edfbff5e8
Bart van der Schoor <bart.vanderschoor@maykinmedia.nl>
2021-05-10 14:43:39 +0200

    Added RSIN validator to utils

--- src/openforms/utils/tests/test_validators.py
+++ src/openforms/utils/tests/test_validators.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from ..validators import validate_bsn
+from ..validators import validate_bsn, validate_rsin
 
 
 class BSNValidatorTestCase(TestCase):
@@ -19,3 +19,20 @@ class BSNValidatorTestCase(TestCase):
 
         with self.assertRaises(ValidationError):
             validate_bsn("063-08836")
+
+
+class RSINValidatorTestCase(TestCase):
+    @staticmethod
+    def test_valid_bsns():
+        validate_rsin("063308836")
+        validate_rsin("619183020")
+
+    def test_invalid_bsns(self):
+        with self.assertRaises(ValidationError):
+            validate_rsin("06330883")
+
+        with self.assertRaises(ValidationError):
+            validate_rsin("063a08836")
+
+        with self.assertRaises(ValidationError):
+            validate_rsin("063-08836")
