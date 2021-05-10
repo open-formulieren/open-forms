76c398ef97feba77d235cf347b18da8ef9db1c5b
Bart van der Schoor <bart.vanderschoor@maykinmedia.nl>
2021-05-10 13:01:06 +0200

    Applied PR suggestions to use mixin for zaaktype selection

--- src/openforms/registrations/contrib/zgw_apis/admin.py
+++ src/openforms/registrations/contrib/zgw_apis/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 
 from solo.admin import SingletonModelAdmin
+from zgw_consumers.admin import ListZaaktypenMixin
 
 from .models import ZgwConfig
 
 
 @admin.register(ZgwConfig)
-class ZgwConfigAdmin(SingletonModelAdmin):
-    # TODO display zaaktype and informatieobjecttype fields as choice fields would be nice
-    pass
+class ZgwConfigAdmin(ListZaaktypenMixin, SingletonModelAdmin):
+    zaaktype_fields = [
+        "zaaktype",
+    ]
+    # TODO implement informatieobjecttype suggestions similar to zaaktype
