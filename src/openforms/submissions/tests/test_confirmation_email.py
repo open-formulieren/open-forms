24118eabc0eee8872aaaa95513f9617801555677
Steven Bal <steven@maykinmedia.nl>
2021-05-12 12:43:34 +0200

    :white_check_mark: Add tests for URL stripping in confirmation emails

--- src/openforms/submissions/tests/test_confirmation_email.py
+++ src/openforms/submissions/tests/test_confirmation_email.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ValidationError
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 from ..models import ConfirmationEmailTemplate
 
@@ -10,3 +10,25 @@ class ConfirmationEmailTests(TestCase):
 
         with self.assertRaises(ValidationError):
             email.clean()
+
+    @override_settings(EMAIL_TEMPLATE_URL_WHITELIST=["whitelisted.com"])
+    def test_strip_non_whitelisted_urls(self):
+        email = ConfirmationEmailTemplate(
+            content="test https://google.com https://www.google.com www.google.com google.com https://whitelisted.com test"
+        )
+
+        rendered = email.render({})
+
+        self.assertNotIn("google.com", rendered)
+        self.assertIn("https://whitelisted.com", rendered)
+
+    @override_settings(EMAIL_TEMPLATE_URL_WHITELIST=["whitelisted.com"])
+    def test_strip_non_whitelisted_urls_from_context(self):
+        email = ConfirmationEmailTemplate(content="test {{url1}} {{url2}} test")
+
+        rendered = email.render(
+            {"url1": "https://whitelisted.com", "url2": "https://google.com"}
+        )
+
+        self.assertNotIn("google.com", rendered)
+        self.assertIn("https://whitelisted.com", rendered)
