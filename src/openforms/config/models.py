001446bf2ccef035fbd2ecf2eaca711fd014c7af
Sergei Maertens <sergei@maykinmedia.nl>
2021-05-14 14:55:21 +0200

    :speech_balloon: Refs. #152 -- improve field label and help text for netloc_allowlist

--- src/openforms/config/models.py
+++ src/openforms/config/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.encoding import force_str
 from django.utils.translation import ugettext_lazy as _
 
 from django_better_admin_arrayfield.models.fields import ArrayField
@@ -8,10 +9,11 @@ from solo.models import SingletonModel
 class GlobalConfiguration(SingletonModel):
     email_template_netloc_allowlist = ArrayField(
         models.CharField(max_length=1000),
-        verbose_name=_("Email template netloc allowlist"),
+        verbose_name=_("Toegestane domeinen in e-mails."),
         help_text=_(
-            "A list of network localities (without https://www.) that will not be stripped "
-            "from the content of confirmation emails"
+            "Geef een lijst van toegelaten domeinen op (zonder 'https://www.'). "
+            "Hyperlinks in (bevestigings)e-mails worden verwijderd, tenzij het domein "
+            "hier opgegeven is."
         ),
         blank=True,
         default=list,
@@ -19,3 +21,6 @@ class GlobalConfiguration(SingletonModel):
 
     class Meta:
         verbose_name = _("Global configuration")
+
+    def __str__(self):
+        return force_str(self._meta.verbose_name)
