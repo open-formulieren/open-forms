08fc154c3017c6b620d0e6e31d7054bfb856d528
Steven Bal <steven@maykinmedia.nl>
2021-05-21 16:24:45 +0200

    :bug: Adapt import functionality to reflect correct behaviour
    use existing FormDefinitions if the configurations match, otherwise create a new FormDefinition
    
    fixes: https://github.com/open-formulieren/open-forms/issues/355

--- src/openforms/forms/management/commands/import.py
+++ src/openforms/forms/management/commands/import.py
@@ -7,6 +7,7 @@ from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
 from django.utils.translation import ugettext_lazy as _
 
+from rest_framework.exceptions import ValidationError
 from rest_framework.test import APIRequestFactory
 
 from ...api import serializers as api_serializers
@@ -68,11 +69,42 @@ class Command(BaseCommand):
                             context={"request": request, "form": created_form},
                         )
 
-                        if deserialized.is_valid():
+                        try:
+                            deserialized.is_valid(raise_exception=True)
                             deserialized.save()
                             if resource == "forms":
                                 created_form = deserialized.instance
-                        else:
-                            raise CommandError(
-                                f"Something went wrong while importing: {deserialized.errors}"
-                            )
+                        except ValidationError as e:
+                            if (
+                                resource == "formDefinitions"
+                                and "slug" in e.detail
+                                and e.detail["slug"][0].code == "unique"
+                            ):
+                                existing_fd = FormDefinition.objects.get(
+                                    slug=entry["slug"]
+                                )
+                                existing_fd_hash = existing_fd.get_hash()
+                                imported_fd_hash = FormDefinition(
+                                    configuration=entry["configuration"]
+                                ).get_hash()
+
+                                if existing_fd_hash == imported_fd_hash:
+                                    # The form definition that is being imported
+                                    # is identical to the existing form definition
+                                    # with the same slug, use existing instead
+                                    # of creating new definition
+                                    uuid_mapping[old_uuid] = existing_fd.uuid
+                                else:
+                                    # The imported form definition configuration
+                                    # is different, create a new form definition
+                                    entry.pop("url")
+                                    entry.pop("uuid")
+                                    new_fd = FormDefinition(**entry)
+                                    new_fd.save()
+                                    uuid_mapping[old_uuid] = str(new_fd.uuid)
+                            else:
+                                raise CommandError(
+                                    _(
+                                        "Something went wrong while importing form: {}"
+                                    ).format(e)
+                                ) from e
