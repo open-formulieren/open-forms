6cf568905ad50e382fc9903d1b9bbb1edb811c6a
Sergei Maertens <sergei@maykinmedia.nl>
2021-05-26 11:29:17 +0200

    :sparkles: Fixes #364 -- register the demo plugin as an example

---
+++ src/openforms/prefill/contrib/demo/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class DemoApp(AppConfig):
+    name = "openforms.prefill.contrib.demo"
+    label = "prefill.demo"
+    verbose_name = _("Demo prefill plugin")
+
+    def ready(self):
+        # register the plugin
+        from . import plugin  # noqa
