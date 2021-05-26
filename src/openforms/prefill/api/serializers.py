2903b24669d5b193bceafd680c133a8bfdcc03ab
Sergei Maertens <sergei@maykinmedia.nl>
2021-05-26 16:14:03 +0200

    :sparkles: Fixes #364 -- define endpoint to list available plugin attributes

--- src/openforms/prefill/api/serializers.py
+++ src/openforms/prefill/api/serializers.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from django.utils.translation import gettext_lazy as _
 
 from rest_framework import serializers
@@ -13,4 +15,25 @@ class PluginSerializer(serializers.Serializer):
         source="verbose_name",
         label=_("Label"),
         help_text=_("The human-readable name for a plugin."),
+    )
+
+
+@dataclass
+class ChoiceWrapper:
+    choice: tuple
+
+    def __post_init__(self):
+        self.value = self.choice[0]
+        self.label = self.choice[1]
+
+
+class AttributeSerializer(serializers.Serializer):
+    id = serializers.CharField(
+        source="value",
+        label=_("ID"),
+        help_text=_("The unique attribute identifier"),
+    )
+    label = serializers.CharField(
+        label=_("Label"),
+        help_text=_("The human-readable name for an attribute."),
     )
--- src/openforms/prefill/api/views.py
+++ src/openforms/prefill/api/views.py
@@ -1,19 +1,16 @@
 from django.utils.translation import gettext_lazy as _
 
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import authentication, permissions
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from ..registry import register
-from .serializers import PluginSerializer
+from .serializers import AttributeSerializer, ChoiceWrapper, PluginSerializer
 
 
-class PluginListView(APIView):
-    authentication_classes = (authentication.SessionAuthentication,)
-    permission_classes = (permissions.IsAdminUser,)
-    serializer_class = PluginSerializer
-
+class ListMixin:
     def get_serializer(self, **kwargs):
         return self.serializer_class(
             many=True,
@@ -21,13 +18,46 @@ class PluginListView(APIView):
             **kwargs,
         )
 
-    @extend_schema(
-        summary=_("List available prefill plugins"),
-    )
     def get(self, request, *args, **kwargs):
-        """
-        List all prefill plugins that have been registered.
-        """
-        plugins = list(register)
-        serializer = self.get_serializer(instance=plugins)
+        objects = self.get_objects()
+        serializer = self.get_serializer(instance=objects)
         return Response(serializer.data)
+
+
+@extend_schema_view(
+    get=extend_schema(summary=_("List available prefill plugins")),
+)
+class PluginListView(ListMixin, APIView):
+    """
+    List all prefill plugins that have been registered.
+    """
+
+    authentication_classes = (authentication.SessionAuthentication,)
+    permission_classes = (permissions.IsAdminUser,)
+    serializer_class = PluginSerializer
+
+    def get_objects(self):
+        return list(register)
+
+
+@extend_schema_view(
+    get=extend_schema(summary=_("List available attributes")),
+)
+class PluginAttributesListView(ListMixin, APIView):
+    """
+    List the available prefill attributes for a given plugin.
+    """
+
+    authentication_classes = (authentication.SessionAuthentication,)
+    permission_classes = (permissions.IsAdminUser,)
+    serializer_class = AttributeSerializer
+
+    def get_objects(self):
+        try:
+            plugin = register[self.kwargs["plugin"]]
+        except KeyError:
+            raise NotFound(
+                detail=_("No plugin with ID '{plugin}' found").format(**self.kwargs)
+            )
+        choices = plugin.get_available_attributes()
+        return [ChoiceWrapper(choice) for choice in choices]
