21a65a5d3fec46300e9653b39c3b4ae1160b8286
Sergei Maertens <sergei@maykinmedia.nl>
2021-05-26 17:54:57 +0200

    :white_check_mark: Refs. #364 -- added tests for prefill endpoints

--- src/openforms/prefill/api/tests/test_endpoints.py
+++ src/openforms/prefill/api/tests/test_endpoints.py
@@ -1,7 +1,9 @@
 from unittest.mock import patch
 
+from django.utils.translation import gettext_lazy as _
+
 from rest_framework import status
-from rest_framework.reverse import reverse_lazy
+from rest_framework.reverse import reverse, reverse_lazy
 from rest_framework.test import APITestCase
 
 from openforms.accounts.tests.factories import UserFactory
@@ -55,3 +57,64 @@ class AuthTests(APITestCase):
                 response = self.client.get(endpoint)
 
                 self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class ResponseTests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+
+        cls.user = UserFactory.create(is_staff=True)
+
+    def setUp(self):
+        super().setUp()
+
+        self.client.force_authenticate(user=self.user)
+
+        patcher = patch("openforms.prefill.api.views.register", new=register)
+        patcher.start()
+        self.addCleanup(patcher.stop)
+
+    def test_pefill_list(self):
+        endpoint = reverse("api:plugin-list")
+
+        response = self.client.get(endpoint)
+
+        expected = [
+            {
+                "id": "test",
+                "label": _("Demo"),
+            }
+        ]
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), expected)
+
+    def test_attributes_list(self):
+        endpoint = reverse("api:attribute-list", kwargs={"plugin": "test"})
+
+        response = self.client.get(endpoint)
+
+        expected = [
+            {
+                "id": "random_number",
+                "label": _("Random number"),
+            },
+            {
+                "id": "random_string",
+                "label": _("Random string"),
+            },
+        ]
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), expected)
+
+    def test_attributes_invalid_plugin_ids(self):
+        invalid = ["demo", 1234, "None"]
+        for plugin in invalid:
+            with self.subTest(plugin_id=plugin):
+                endpoint = reverse("api:attribute-list", kwargs={"plugin": plugin})
+
+                response = self.client.get(endpoint)
+
+                self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
