66faeb5f51e1d2162a327b05a61536419c40646b
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2021-05-27 14:29:39 +0200

    :construction: Create structure for stug bg plugin

---
+++ src/openforms/prefill/contrib/stufbg/plugin.py
@@ -0,0 +1,24 @@
+from typing import Any, Dict, Iterable, List, Tuple
+
+from django.utils.translation import gettext_lazy as _
+
+from ...base import BasePlugin
+from ...registry import register
+from .constants import Attributes
+
+from openforms.submissions.models import Submission
+
+
+@register("stufbg")
+class StufBgPrefill(BasePlugin):
+    verbose_name = _("StUF-BG")
+
+    def get_available_attributes(self) -> Iterable[Tuple[str, str]]:
+        return Attributes.choices
+
+    def get_prefill_values(
+        self, submission: Submission, attributes: List[str]
+    ) -> Dict[str, Any]:
+        bsn = submission.bsn
+
+        return {}
