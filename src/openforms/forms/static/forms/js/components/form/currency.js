016c2db356e79e56f9ff5868f8f5bfd8584de30d
Steven Bal <steven@maykinmedia.nl>
2021-05-28 14:55:32 +0200

    :lipstick: Add decimal places option to Currency component

--- src/openforms/forms/static/forms/js/components/form/currency.js
+++ src/openforms/forms/static/forms/js/components/form/currency.js
@@ -2,4 +2,13 @@ import {Formio} from "formiojs";
 import {defineCommonEditFormTabs} from "./abstract";
 import CurrencyEditData from 'formiojs/components/currency/editForm/Currency.edit.data';
 
-defineCommonEditFormTabs(Formio.Components.components.currency, CurrencyEditData);
+let decimalPlaces = {
+    type: 'number',
+    input: true,
+    weight: 80,
+    key: 'decimalLimit',
+    label: 'Decimal Places',
+    tooltip: 'The maximum number of decimal places.'
+};
+
+defineCommonEditFormTabs(Formio.Components.components.currency, CurrencyEditData.concat([decimalPlaces]));
