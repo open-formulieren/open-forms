dbe791921f2a5e27e8ef9463abc0202693be3535
Bart van der Schoor <bart.vanderschoor@maykinmedia.nl>
2021-06-11 12:54:20 +0200

    Added explicit test for allow_redirect_url()

--- src/openforms/authentication/tests/test_registry.py
+++ src/openforms/authentication/tests/test_registry.py
@@ -9,6 +9,7 @@ from rest_framework.reverse import reverse
 from ...forms.tests.factories import FormStepFactory
 from ..base import BasePlugin
 from ..registry import Registry, register as auth_register
+from ..views import allow_redirect_url
 
 
 class Plugin(BasePlugin):
@@ -186,3 +187,38 @@ class RegistryTests(TestCase):
             response = self.client.get(f"{url}?next={bad_url_enc}")
             self.assertEqual(response.content, b"redirect not allowed")
             self.assertEqual(response.status_code, 400)
+
+    @override_settings(
+        CORS_ALLOW_ALL_ORIGINS=False,
+        CORS_ALLOWED_ORIGINS=[],
+        CORS_ALLOWED_ORIGIN_REGEXES=[],
+    )
+    def test_allow_redirect_url(self):
+        self.assertEqual(allow_redirect_url("http://foo.bar"), False)
+        self.assertEqual(allow_redirect_url("http://foo.bar/bazz"), False)
+        self.assertEqual(allow_redirect_url("https://foo.bar"), False)
+        self.assertEqual(allow_redirect_url("http://bazz.buzz"), False)
+
+        with self.settings(
+            CORS_ALLOW_ALL_ORIGINS=True,
+        ):
+            self.assertEqual(allow_redirect_url("http://foo.bar"), True)
+            self.assertEqual(allow_redirect_url("http://foo.bar/bazz"), True)
+            self.assertEqual(allow_redirect_url("https://foo.bar"), True)
+            self.assertEqual(allow_redirect_url("http://bazz.buzz"), True)
+
+        with self.settings(
+            CORS_ALLOWED_ORIGINS=["http://foo.bar"],
+        ):
+            self.assertEqual(allow_redirect_url("http://foo.bar"), True)
+            self.assertEqual(allow_redirect_url("http://foo.bar/bazz"), True)
+            self.assertEqual(allow_redirect_url("https://foo.bar"), False)
+            self.assertEqual(allow_redirect_url("http://bazz.buzz"), False)
+
+        with self.settings(
+            CORS_ALLOWED_ORIGIN_REGEXES=[r".*foo.*"],
+        ):
+            self.assertEqual(allow_redirect_url("http://foo.bar"), True)
+            self.assertEqual(allow_redirect_url("http://foo.bar/bazz"), True)
+            self.assertEqual(allow_redirect_url("https://foo.bar"), True)
+            self.assertEqual(allow_redirect_url("http://bazz.buzz"), False)
