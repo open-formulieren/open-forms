616b7d9ee762d4c3bd52a26fefd824250a0ded09
shea-maykinmedia <shea.meyers@maykinmedia.nl>
2021-06-14 15:40:39 +0200

    :sparkles: Add list filters for active and maintenance_mode fields

--- src/openforms/forms/admin/form.py
+++ src/openforms/forms/admin/form.py
@@ -48,6 +48,7 @@ class FormAdmin(BackendChoiceFieldMixin, OrderedInlineModelAdminMixin, VersionAd
     inlines = (FormStepInline,)
     prepopulated_fields = {"slug": ("name",)}
     actions = ["make_copies", "set_to_maintenance_mode", "remove_from_maintenance_mode"]
+    list_filter = ("active", "maintenance_mode")
 
     change_list_template = (
         "admin/forms/form/change_list.html"  # override reversion template
