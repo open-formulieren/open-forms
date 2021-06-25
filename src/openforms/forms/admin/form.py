1e6779d2f05f1014ac3833ee55ee583afaa1ff2a
Sergei Maertens <sergei@maykinmedia.nl>
2021-06-25 15:21:05 +0200

    :bug: Fixes #449 -- fix toggling React CRUD feature flag
    Bug caused by ModelAdmin instances not being thread safe. Mutating an
    instance (by doing self.change_form_template) affects the next
    requests as well, even if the feature flag says it should be otherwise.
    
    Removed the thread-unsafe code and replaced it by passing the config
    into the template and dealing with the different situations in the
    template.
    
    Side-effect: the SDK snippets are back. Not great yet, but they're back.

--- src/openforms/forms/admin/form.py
+++ src/openforms/forms/admin/form.py
@@ -64,8 +64,7 @@ class FormAdmin(BackendChoiceFieldMixin, OrderedInlineModelAdminMixin, VersionAd
     def render_change_form(
         self, request, context, add=False, change=False, form_url="", obj=None
     ):
-        if self.use_react(request):
-            self.change_form_template = "admin/forms/form/change_form_react.html"
+        context.update({"use_react": self.use_react(request)})
         return super().render_change_form(request, context, add, change, form_url, obj)
 
     def get_inline_instances(self, request, *args, **kwargs):
