01a609dfc3f71e3c95eaa0f1aa635463f9d461a9
sheameyers <shea.meyers@maykinmedia.nl>
2021-06-30 16:40:24 +0200

    :sparkles: Refs #213 - Add postcode component

---
+++ src/openforms/forms/static/forms/js/components/form/postcode.js
@@ -0,0 +1,35 @@
+import {Formio} from "formiojs";
+import {defineCommonEditFormTabs} from "./abstract";
+
+const TextField = Formio.Components.components.textfield;
+
+
+class PostcodeField extends TextField {
+    static schema(...extend) {
+        return TextField.schema({
+            type: 'postcode',
+            label: 'Postcode',
+            key: 'postcode',
+            inputMask: '9999 aa',
+        }, ...extend);
+    }
+
+    static get builderInfo() {
+        return {
+            title: 'Postcode Field',
+            icon: 'home',
+            group: 'basic',
+            weight: 10,
+            schema: PostcodeField.schema(),
+        };
+    }
+
+    get defaultSchema() {
+        return PostcodeField.schema();
+    }
+
+}
+
+defineCommonEditFormTabs(PostcodeField);
+
+export default PostcodeField;
