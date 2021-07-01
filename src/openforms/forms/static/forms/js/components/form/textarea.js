b9b966c01a9857b4fda35f9388d4615b3781c65e
sheameyers <shea.meyers@maykinmedia.nl>
2021-07-01 13:23:49 +0200

    :bug: Refs #210 - Fix modifying object used by other components

--- src/openforms/forms/static/forms/js/components/form/textarea.js
+++ src/openforms/forms/static/forms/js/components/form/textarea.js
@@ -3,18 +3,22 @@ import {Formio} from 'react-formio';
 import {ADVANCED, BASIC, VALIDATION} from './edit/tabs';
 
 
-const numRows = {
-    type: 'number',
-    input: true,
-    weight: 80,
-    key: 'rows',
-    label: 'Number of rows',
-    tooltip: 'The number of rows for this text area.'
+const TextAreaBasicTab = {
+    key: 'basic',
+    label: 'Basic',
+    components: [
+        ...BASIC.components,
+        {
+            type: 'number',
+            input: true,
+            weight: 80,
+            key: 'rows',
+            label: 'Number of rows',
+            tooltip: 'The number of rows for this text area.'
+        }
+    ]
 };
 
-let TextAreaBasicTab = BASIC;
-
-TextAreaBasicTab.components.push(numRows);
 
 const TextAreaTabs = {
     type: 'tabs',
