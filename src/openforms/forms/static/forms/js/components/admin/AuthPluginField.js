26135a3675fca3854ddc504191405d286eead53e
silvia <silvia@maykinmedia.nl>
2021-07-02 16:40:22 +0200

    :lipstick: [#426] Added auth plugin field to admin

---
+++ src/openforms/forms/static/forms/js/components/admin/AuthPluginField.js
@@ -0,0 +1,52 @@
+import Field from "../formsets/Field";
+import {Checkbox} from "../formsets/Inputs";
+import React from "react";
+import Loader from "./Loader";
+import PropTypes from "prop-types";
+
+const AuthPluginField = ({loading, availableAuthPlugins, selectedAuthPlugins, onChange, errors}) => {
+    const authCheckboxes = Object.keys(availableAuthPlugins).map((pluginId, index) => {
+        const plugin = availableAuthPlugins[pluginId];
+        return (
+            <li key={index}>
+                <Checkbox
+                    name={plugin.label}
+                    value={plugin.id}
+                    label={plugin.label}
+                    onChange={onChange}
+                    checked={selectedAuthPlugins.includes(plugin.id)}
+                />
+            </li>
+        );
+    });
+
+    return (
+        <Field
+            name='formAuthPlugin'
+            label='Authentication'
+            helpText='The authentication backends required to be able to fill in the form.'
+            errors={errors}
+            required
+        >
+            {loading ? <Loader/> : <ul>{authCheckboxes}</ul>}
+        </Field>
+    );
+};
+
+AuthPluginField.propTypes = {
+    loading: PropTypes.bool.isRequired,
+    availableAuthPlugins: PropTypes.objectOf(PropTypes.shape({
+        id: PropTypes.string,
+        label: PropTypes.string,
+        providesAuth: PropTypes.arrayOf(PropTypes.string)
+    })),
+    selectedAuthPlugins: PropTypes.arrayOf(PropTypes.string).isRequired,
+    onChange: PropTypes.func,
+    required: PropTypes.bool,
+    errors: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.string),
+        PropTypes.string,
+    ]),
+};
+
+export {AuthPluginField};
--- src/openforms/forms/static/forms/js/components/admin/constants.js
+++ src/openforms/forms/static/forms/js/components/admin/constants.js
@@ -1,3 +1,4 @@
 export const FORM_ENDPOINT = '/api/v1/forms';
 export const FORM_DEFINITIONS_ENDPOINT = '/api/v1/form-definitions';
 export const ADMIN_PAGE = '/admin/forms/form/';
+export const AUTH_PLUGINS_ENDPOINT = '/api/v1/authentication/plugins';
