d8e4db729cb058daa08934a951c9454d4a9fd714
Bart van der Schoor <bart.vanderschoor@maykinmedia.nl>
2021-07-05 14:38:55 +0200

    [#430] Added SubmissionFactory.from_components / .from_data factory helpers

--- src/openforms/submissions/tests/factories.py
+++ src/openforms/submissions/tests/factories.py
@@ -1,6 +1,13 @@
+import copy
+from typing import List
+
 import factory
 
-from openforms.forms.tests.factories import FormFactory
+from openforms.forms.tests.factories import (
+    FormDefinitionFactory,
+    FormFactory,
+    FormStepFactory,
+)
 
 from ..models import Submission, SubmissionReport, SubmissionStep
 
@@ -10,6 +17,69 @@ class SubmissionFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = Submission
+
+    @staticmethod
+    def from_components(
+        components_list: List[dict],
+        submitted_data: dict = None,
+        form_kwargs: dict = None,
+        submission_kwargs: dict = None,
+    ) -> Submission:
+        """
+        generate a complete Form/FormStep/FormDefinition + Submission/SubmissionStep tree from a list of formio components
+
+        optionally: supply a dictionary with data (keys must match the component keys)
+        optionally: supply kwargs for the Form and Submission factories (name, bsn etc)
+        """
+        if form_kwargs is None:
+            form_kwargs = dict()
+        if submission_kwargs is None:
+            submission_kwargs = dict()
+
+        form = FormFactory.create(**form_kwargs)
+        submission = SubmissionFactory.create(form=form, **submission_kwargs)
+
+        components = list()
+
+        for _comp in components_list:
+            component = copy.deepcopy(_comp)
+            key = component["key"]
+            # convenience
+            if not component.get("label"):
+                component["label"] = key.title()
+            if not component.get("type"):
+                component["type"] = "text"
+
+            components.append(component)
+
+        configuration = {"components": components}
+
+        form_definition = FormDefinitionFactory.create(
+            name=f"definition-{key}", configuration=configuration
+        )
+        form_step = FormStepFactory.create(form=form, form_definition=form_definition)
+        SubmissionStepFactory.create(
+            submission=submission, form_step=form_step, data=submitted_data
+        )
+
+        SubmissionReportFactory.create(submission=submission)
+
+        return submission
+
+    @staticmethod
+    def from_data(data_dict, form_kwargs: dict = None, submission_kwargs: dict = None):
+        components = [
+            {
+                "key": key,
+            }
+            for key in data_dict
+        ]
+        return SubmissionFactory.from_components(
+            components,
+            data_dict,
+            form_kwargs=form_kwargs,
+            submission_kwargs=submission_kwargs,
+        )
 
 
 class SubmissionStepFactory(factory.django.DjangoModelFactory):
