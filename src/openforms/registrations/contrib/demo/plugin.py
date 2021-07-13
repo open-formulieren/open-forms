3d4ccb0dc7b4c109a03748a7e87b51218d5cf921
Sergei Maertens <sergei@maykinmedia.nl>
2021-07-13 10:00:00 +0200

    :label: Fix type hint

--- src/openforms/registrations/contrib/demo/plugin.py
+++ src/openforms/registrations/contrib/demo/plugin.py
@@ -15,7 +15,7 @@ class DemoRegistration(BasePlugin):
     verbose_name = _("Demo - print to console")
     configuration_options = DemoOptionsSerializer
 
-    def register_submission(self, submission: Submission, options: dict) -> NoReturn:
+    def register_submission(self, submission: Submission, options: dict) -> None:
         print(submission)
         print(options["extra_line"])
 
