df395c8cc3d1b4e3377bcaf005ba4a9458bc7c08
sheameyers <shea.meyers@maykinmedia.nl>
2021-07-19 13:57:43 +0200

    :white_check_mark: Refs #201 - Add additional unit tests

--- src/openforms/contrib/bag/api/tests/test_api.py
+++ src/openforms/contrib/bag/api/tests/test_api.py
@@ -27,5 +27,47 @@ class GetStreetNameAndCityViewAPITests(BagTestMixin, TestCase):
         )
 
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 2)
+        self.assertEqual(response.json()["streetName"], "Keizersgracht")
+        self.assertEqual(response.json()["city"], "Amsterdam")
+
+    def test_getting_street_name_and_city_without_post_code(self):
+
+        response = self.client.get(
+            f"{reverse('api:get-street-name-and-city-list')}?huisnummer=117"
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'postcode': ['Dit veld is vereist.']})
+
+    def test_getting_street_name_and_city_without_house_number(self):
+
+        response = self.client.get(
+            f"{reverse('api:get-street-name-and-city-list')}?postcode=1015CJ"
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'huisnummer': ['Dit veld is vereist.']})
+
+    @requests_mock.Mocker()
+    def test_getting_street_name_and_city_with_extra_query_params(self, m):
+        mock_service_oas_get(m, "https://bag/", service="bagapiprofileoas3")
+        m.get(
+            "https://bag/api/schema/openapi.yaml?v=3",
+            status_code=200,
+            content=self.load_binary_mock("bagapiprofileoas3.yaml"),
+        )
+        m.get(
+            "https://bag/api/adressen?postcode=1015CJ&huisnummer=117",
+            status_code=200,
+            json=self.load_json_mock("addresses.json"),
+        )
+
+        response = self.client.get(
+            f"{reverse('api:get-street-name-and-city-list')}?postcode=1015CJ&huisnummer=117&random=param"
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 2)
         self.assertEqual(response.json()["streetName"], "Keizersgracht")
         self.assertEqual(response.json()["city"], "Amsterdam")
