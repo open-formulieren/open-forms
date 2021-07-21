0aeaf8b87290a4544320bfe3b85a16e552e793b6
Sergei Maertens <sergei@maykinmedia.nl>
2021-07-21 16:35:34 +0200

    :heavy_plus_sign: Refs #504 -- Added celery once
    Celery Once ensures only a single instance of a task is running. It
    guards against tasks running longer than the scheduled beat interval
    which would cause a cascade of load.
    
    By default, locks expire after an hour so that if something goes wrong,
    it takes maximum one hour for the next job to be triggered again.

--- src/openforms/celery.py
+++ src/openforms/celery.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 from celery import Celery
 
 from .setup import setup_env
@@ -5,6 +7,13 @@ from .setup import setup_env
 setup_env()
 
 app = Celery("open-forms")
-
 app.config_from_object("django.conf:settings", namespace="CELERY")
+app.conf.ONCE = {
+    "backend": "celery_once.backends.Redis",
+    "settings": {
+        "url": settings.CELERY_BROKER_URL,
+        "default_timeout": 60 * 60,  # one hour
+    },
+}
+
 app.autodiscover_tasks()
