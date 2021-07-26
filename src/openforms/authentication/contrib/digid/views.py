ef0550dabbc27254d29977e2d27cf37b7c35373a
silvia <silvia@maykinmedia.nl>
2021-07-26 16:42:15 +0200

    :construction: [#518] Handle digid cancel login

--- src/openforms/authentication/contrib/digid/views.py
+++ src/openforms/authentication/contrib/digid/views.py
@@ -6,13 +6,20 @@ from digid_eherkenning.saml2.digid import DigiDClient
 from digid_eherkenning.views import (
     DigiDAssertionConsumerServiceView as _DigiDAssertionConsumerServiceView,
 )
+from furl import furl
 from onelogin.saml2.errors import OneLogin_Saml2_ValidationError
 
 from openforms.authentication.constants import AuthAttribute
+from openforms.authentication.views import BACKEND_OUTAGE_RESPONSE_PARAMETER
 
 
 class BSNNotPresentError(Exception):
     pass
+
+
+DIGID_MESSAGE_PARAMETER = "_digid-message"
+LOGIN_CANCELLED = "login-cancelled"
+GENERIC_LOGIN_ERROR = "error"
 
 
 class DigiDAssertionConsumerServiceView(
@@ -25,6 +32,12 @@ class DigiDAssertionConsumerServiceView(
     We just need to receive the BSN number.
     """
 
+    def get_failure_url(self, problem_code: str) -> str:
+        url = self.get_success_url()
+        f = furl(url)
+        f.args[DIGID_MESSAGE_PARAMETER] = problem_code
+        return f.url
+
     def get(self, request):
         saml_art = request.GET.get("SAMLart")
         client = DigiDClient()
@@ -32,14 +45,17 @@ class DigiDAssertionConsumerServiceView(
         try:
             response = client.artifact_resolve(request, saml_art)
         except OneLogin_Saml2_ValidationError as exc:
-            self.handle_validation_error(request)
-            raise exc
+            if exc.code == OneLogin_Saml2_ValidationError.STATUS_CODE_AUTHNFAILED:
+                failure_url = self.get_failure_url(LOGIN_CANCELLED)
+            else:
+                failure_url = self.get_failure_url(GENERIC_LOGIN_ERROR)
+            return HttpResponseRedirect(failure_url)
 
         try:
             name_id = response.get_nameid()
         except OneLogin_Saml2_ValidationError as exc:
-            self.handle_validation_error(request)
-            raise exc
+            failure_url = self.get_failure_url(GENERIC_LOGIN_ERROR)
+            return HttpResponseRedirect(failure_url)
 
         sector_code, sectoral_number = name_id.split(":")
 
