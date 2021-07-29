ce2b02af4eafc6bf244f2a309bcac9fc232e4aaf
sheameyers <shea.meyers@maykinmedia.nl>
2021-07-29 15:27:27 +0200

    :recycle: Refs #308 - Make getting session expiry time more robust

--- src/openforms/middleware.py
+++ src/openforms/middleware.py
@@ -41,5 +41,5 @@ class SessionTimeoutMiddleware:
             else config.form_session_timeout
         )
         # https://docs.djangoproject.com/en/2.2/topics/http/sessions/#django.contrib.sessions.backends.base.SessionBase.set_expiry
-        request.session.set_expiry(timedelta(minutes=timeout).seconds)
+        request.session.set_expiry(int(timedelta(minutes=timeout).total_seconds()))
         return self.get_response(request)
