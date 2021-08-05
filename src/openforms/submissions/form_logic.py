c56af5d99e64858ca60bc22f46dd479c99f941a7
Sergei Maertens <sergei@maykinmedia.nl>
2021-08-05 17:01:49 +0200

    :alembic: Implement evaluating the form logic as part of the submission step detail endpoint

--- src/openforms/submissions/form_logic.py
+++ src/openforms/submissions/form_logic.py
@@ -10,10 +10,17 @@ def evaluate_form_logic(step: SubmissionStep, data: Dict[str, Any]) -> Dict[str,
     # grab the configuration that can be **mutated**
     configuration = step.form_step.form_definition.configuration
 
+    # ensure this function is idempotent
+    _evaluated = getattr(step, "_form_logic_evaluated", False)
+    if _evaluated:
+        return configuration
+
     # TODO: actual evaluations
     step._can_submit = False
     for component in configuration["components"]:
         if label := component.get("label"):
             component["label"] = f"{label} (updated by logic check)"
+
+    step._form_logic_evaluated = True
 
     return configuration
