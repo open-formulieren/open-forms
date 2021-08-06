855f9e1a55841b18041af4a5f888a6c097fce711
silvia <silvia@maykinmedia.nl>
2021-08-06 10:40:20 +0200

    :construction: Evaluation of form logic

--- src/openforms/submissions/form_logic.py
+++ src/openforms/submissions/form_logic.py
@@ -1,6 +1,28 @@
 from typing import Any, Dict
 
+from json_logic import jsonLogic
+
+from ..forms.models.form import FormLogic
+from ..prefill import JSONObject
 from .models import SubmissionStep
+
+
+def set_property_value(
+    configuration: JSONObject,
+    component_key: str,
+    property_name: str,
+    property_value: str,
+) -> JSONObject:
+    for index, component in enumerate(configuration["components"]):
+        if "components" in component:
+            configuration = set_property_value(
+                component, component_key, property_name, property_value
+            )
+
+        if component["key"] == component_key:
+            configuration["components"][index][property_name] = property_value
+
+    return configuration
 
 
 def evaluate_form_logic(step: SubmissionStep, data: Dict[str, Any]) -> Dict[str, Any]:
@@ -15,11 +37,22 @@ def evaluate_form_logic(step: SubmissionStep, data: Dict[str, Any]) -> Dict[str,
     if _evaluated:
         return configuration
 
-    # TODO: actual evaluations
-    step._can_submit = False
-    for component in configuration["components"]:
-        if label := component.get("label"):
-            component["label"] = f"{label} (updated by logic check)"
+    rules = FormLogic.objects.filter(form_step=step.form_step)
+    for rule in rules:
+        trigger = jsonLogic(rule.json_logic_trigger, data)
+        for action in rule.actions:
+            if action["type"] == "value":
+                configuration = set_property_value(
+                    configuration, rule.component, "value", trigger
+                )
+            elif trigger and action["type"] == "property":
+                property_name = action["property"]["value"]
+                property_value = action["state"]
+                set_property_value(
+                    configuration, rule.component, property_name, property_value
+                )
+            else:
+                raise NotImplementedError("This action has not been implemented yet.")
 
     step._form_logic_evaluated = True
 
