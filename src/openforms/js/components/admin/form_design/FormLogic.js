e6c998e29d4c899f02e59abe7836f15709cb6902
silvia <silvia@maykinmedia.nl>
2021-08-09 17:07:41 +0200

    :lipstick: Started UI for logic rules

--- src/openforms/js/components/admin/form_design/FormLogic.js
+++ src/openforms/js/components/admin/form_design/FormLogic.js
@@ -2,23 +2,52 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FormioUtils from 'formiojs/utils';
 import {useImmerReducer} from 'use-immer';
+import {TextArea} from "../forms/Inputs";
+import Select from "../forms/Select";
+import {LOGICS_ENDPOINT} from './constants';
+import {get, post, put} from "../../../utils/fetch";
+import useAsync from "react-use/esm/useAsync";
 
 
 const initialState = {
+    components: {
+        loading: true,
+        choices: []
+    },
     rules: [],
 };
 
+const EMPTY_CHOICE = [['', '-------']];
+
 
 const EMPTY_RULE = {
-    logic: {},
+    uuid: '',
+    formStep: '',
+    jsonLogicTrigger: '',
     component: '',
     actions: [],
 };
 
 const reducer = (draft, action) => {
     switch (action.type) {
+        case 'LOADED_RULES': {
+            draft.rules = action.payload;
+            break;
+        }
         case 'ADD_RULE': {
             draft.rules.push(EMPTY_RULE);
+            break;
+        }
+        case 'CHANGED_RULE': {
+            const {index, name, value} = action.payload;
+            draft.rules[index][name] = value;
+            break;
+        }
+        case 'FORMATTED_COMPONENTS_CHOICES': {
+            draft.components = {
+                loading: false,
+                choices: action.payload
+            };
             break;
         }
         default:
@@ -26,22 +55,94 @@ const reducer = (draft, action) => {
     }
 };
 
-
-const FormLogic = ({ formSteps }) => {
+// TODO Context for csrftoken
+const FormLogic = ({ formUuid, formSteps, csrftoken }) => {
     const [state, dispatch] = useImmerReducer(reducer, initialState);
 
     // key-value map of component.key: component, flattened from all available steps
-    const allComponents = formSteps.map(
-        step => FormioUtils.flattenComponents(step.configuration.components || [], false)
-    ).reduce((acc, currentValue) => ({...acc, ...currentValue}), {});
+    const getComponentsChoices = () => {
+        // key-value map of component.key: component, flattened from all available steps
+        const allComponents = formSteps.map(
+            step => FormioUtils.flattenComponents(step.configuration.components || [], false)
+        ).reduce((acc, currentValue) => ({...acc, ...currentValue}), {});
+        dispatch({
+            type: 'FORMATTED_COMPONENTS_CHOICES',
+            payload: Object.entries(allComponents).map( ([key, comp]) => [key, comp.label || comp.key] )
+        });
+    };
 
-    console.log(allComponents);
+    const loadExistingRules = async () => {
+        const rulesResponse = await get(`${LOGICS_ENDPOINT}?form=${formUuid}`);
+        if (rulesResponse.ok) {
+            dispatch({
+                type: 'LOADED_RULES',
+                payload: rulesResponse.data
+            });
+        }
+    };
+
+    const onRuleChange = (index, event) => {
+        const { name, value } = event.target;
+        dispatch({
+            type: 'CHANGED_RULE',
+            payload: {name, value, index},
+        });
+    };
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        for (let index = 0; index < state.rules.length; index++) {
+            const logicRule = state.rules[index];
+            const isNewRule = !logicRule.uuid;
+            const createOrUpdate = isNewRule ? post : put;
+            const endPoint = isNewRule ? LOGICS_ENDPOINT : `${LOGICS_ENDPOINT}/${logicRule.uuid}`;
+
+            var formLogicResponse = await createOrUpdate(
+                endPoint,
+                csrftoken,
+                logicRule
+            );
+
+            if (!formLogicResponse.ok) {
+                throw new Error('An error occurred while saving the form logic.');
+            }
+            var ruleUuid = formLogicResponse.data.uuid;
+            if (isNewRule){
+                dispatch({
+                    type: 'CHANGED_RULE',
+                    payload: {name: 'uuid', value: ruleUuid, index: index}
+                });
+            }
+
+        }
+    }
+
+    const getFormStepChoices = (formSteps) => {
+        return formSteps.map((formStep, index) => [formStep.url, formStep.name]);
+    };
+
+    useAsync(async () => {
+        getComponentsChoices();
+        await loadExistingRules();
+    }, [formSteps]);
 
     return (
         <>
-            {state.rules.map( (rule, i) => (<Rule key={i} {...rule} allComponents={allComponents} />))}
+            {state.rules.map((rule, i) => (
+                <Rule
+                    key={i}
+                    {...rule}
+                    formStepsChoices={getFormStepChoices(formSteps)}
+                    components={state.components}
+                    onChange={onRuleChange.bind(null, i)}
+                />
+            ))}
             <button type="button" onClick={ () => dispatch({type: 'ADD_RULE'}) }>
                 Add rule
+            </button>
+             <button type="button" onClick={onSubmit}>
+                Save logic
             </button>
         </>
     );
@@ -52,25 +153,54 @@ FormLogic.propTypes = {
 };
 
 
-const Rule = ({allComponents, logic, component, actions}) => {
+const Rule = ({components, formStepsChoices, uuid, component, formStep, jsonLogicTrigger, actions, onChange}) => {
+    const onChangeJsonFields = (event) => {
+        const jsonField = JSON.parse(event.target.value);
+        console.log(jsonField);
+        onChange({target: {name: event.target.name, value: jsonField}});
+    }
     return (
         <div>
-            When
-            <select>
-                {Object.entries(allComponents).map( ([key, comp]) => (
-                    <option key={key} value={key}>{comp.label || comp.key}</option>
-                ) )}
-            </select>
+            When the following logic evaluates to true
+            <TextArea
+                name='jsonLogicTrigger'
+                rows={7}
+                cols={20}
+                value={JSON.stringify(jsonLogicTrigger)}
+                onChange={onChangeJsonFields}
+            />
+            update the field
+            <Select
+                name="component"
+                choices={EMPTY_CHOICE.concat(components.choices)}
+                value={component}
+                onChange={onChange}
+            />
+            of the form step
+            <Select
+                name="formStep"
+                choices={EMPTY_CHOICE.concat(formStepsChoices)}
+                value={formStep}
+                onChange={onChange}
+            />
+            with the following actions
+            <TextArea
+                name='actions'
+                rows={7}
+                cols={20}
+                value={JSON.stringify(actions)}
+                onChange={onChangeJsonFields}
+            />
         </div>
     );
 };
 
 
-Rule.propTypes = {
-    allComponents: PropTypes.object,
-    logic: PropTypes.objects,
-    component: PropTypes.string,
-    actions: PropTypes.arrayOf(PropTypes.object),
-};
+// Rule.propTypes = {
+//     allComponents: PropTypes.object,
+//     logic: PropTypes.objects,
+//     component: PropTypes.string,
+//     actions: PropTypes.arrayOf(PropTypes.object),
+// };
 
 export default FormLogic;
--- src/openforms/js/components/admin/form_design/form-creation-form.js
+++ src/openforms/js/components/admin/form_design/form-creation-form.js
@@ -639,6 +639,9 @@ const FormCreationForm = ({csrftoken, formUuid, formHistoryUrl }) => {
                     <Tab>
                         <FormattedMessage defaultMessage="Payment" description="Payment tab title" />
                     </Tab>
+                    <Tab>
+                        <FormattedMessage defaultMessage="Logics" description="Form logic tab title" />
+                    </Tab>
                 </TabList>
 
                 <TabPanel>
@@ -735,7 +738,7 @@ const FormCreationForm = ({csrftoken, formUuid, formHistoryUrl }) => {
 
                 <TabPanel>
                     <Fieldset title="Logica">
-                        <FormLogic formSteps={state.formSteps.data} />
+                        <FormLogic formUuid={state.form.uuid} formSteps={state.formSteps.data} csrftoken={csrftoken}/>
                     </Fieldset>
                 </TabPanel>
             </Tabs>
