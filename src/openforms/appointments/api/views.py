1d81de3ad9394b4a36c6c19a5b5d082d7e90edca
sheameyers <shea.meyers@maykinmedia.nl>
2021-08-31 14:55:50 +0200

    :sparkles: Refs #276 - Fix location of serializers file

--- src/openforms/appointments/api/views.py
+++ src/openforms/appointments/api/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from rest_framework.views import APIView
 
-from openforms.appointments.api.tests.serializers import (
+from openforms.appointments.api.serializers import (
     DateInputSerializer,
     LocationInputSerializer,
     LocationSerializer,
