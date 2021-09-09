1d6d100525585f1b889f8ffd080c17976adb736e
sheameyers <shea.meyers@maykinmedia.nl>
2021-09-09 17:00:13 +0200

    :lipstick: Refs #597 - Style first field

--- src/openforms/js/components/admin/form_design/Appointments.js
+++ src/openforms/js/components/admin/form_design/Appointments.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import ComponentSelection from "./logic/ComponentSelection";
-import {ComponentsContext} from "./logic/Context";
-import PropTypes from "prop-types";
-import {FormLogic} from "./FormLogic";
+import ComponentSelection from './logic/ComponentSelection';
+import {ComponentsContext} from './logic/Context';
+import PropTypes from 'prop-types';
+import {FormLogic} from './FormLogic';
+import Field from '../forms/Field';
+import {FormattedMessage} from 'react-intl';
+import FormRow from '../forms/FormRow';
+import Fieldset from "../forms/Fieldset";
 
 
 const Appointments = ({ availableComponents={} }) => {
     return (
         <ComponentsContext.Provider value={availableComponents}>
-            <ComponentSelection
-                name="component"
-                value=""
-                onChange={() => console.log('I changed')}
-                // Use below to filter on specific
-                // filter={(comp) => (comp.type === componentType)}
-            />
+            <Fieldset>
+                <FormRow>
+                    <Field
+                        name="appointments.Products"
+                        label={<FormattedMessage defaultMessage="Products Component" description="Products Component field label" />}
+                        helpText={<FormattedMessage defaultMessage="Component where products for an appointment will be shown" description="Products Component field help text" />}
+                    >
+                        <ComponentSelection
+                            name="component"
+                            value=""
+                            onChange={() => console.log('I changed')}
+                            // Use below to filter on specific
+                            // filter={(comp) => (comp.type === componentType)}
+                        />
+                    </Field>
+                </FormRow>
+            </Fieldset>
         </ComponentsContext.Provider>
     )
 };
