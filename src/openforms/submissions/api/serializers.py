a480dbc2416837e7a3d2f6cf93c67b4701e49033
Bart van der Schoor <bart.vanderschoor@maykinmedia.nl>
2021-09-09 14:06:38 +0200

    [#264] Add payment fields to submission serializer

--- src/openforms/submissions/api/serializers.py
+++ src/openforms/submissions/api/serializers.py
@@ -96,6 +96,24 @@ class SubmissionSerializer(serializers.HyperlinkedModelSerializer):
         read_only=True,
     )
 
+    payment_required = serializers.BooleanField(
+        label=_("payment required"),
+        help_text=_("Whether the registration requires payment."),
+        read_only=True,
+    )
+    has_paid = serializers.BooleanField(
+        label=_("user had paid"),
+        source="payment_user_has_paid",
+        help_text=_("Whether the user has completed the required payment."),
+        read_only=True,
+    )
+    payment_amount = serializers.BooleanField(
+        label=_("user had paid"),
+        source="product__price",
+        help_text=_("Amount of payment."),
+        read_only=True,
+    )
+
     class Meta:
         model = Submission
         fields = (
@@ -105,6 +123,9 @@ class SubmissionSerializer(serializers.HyperlinkedModelSerializer):
             "steps",
             "next_step",
             "can_submit",
+            "payment_required",
+            "payment_amount",
+            "has_paid",
         )
         extra_kwargs = {
             "id": {
