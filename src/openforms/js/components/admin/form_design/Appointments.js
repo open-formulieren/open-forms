102846c4550bf3247c3dea14d6b3a547e4fbef4b
sheameyers <shea.meyers@maykinmedia.nl>
2021-09-10 15:03:22 +0200

    :sparkles: Refs #597 - Handle choosing new component in select

--- src/openforms/js/components/admin/form_design/Appointments.js
+++ src/openforms/js/components/admin/form_design/Appointments.js
@@ -1,28 +1,80 @@
 import React from 'react';
+import {useImmerReducer} from 'use-immer';
+import PropTypes from 'prop-types';
+import {FormattedMessage} from 'react-intl';
+
 import ComponentSelection from './logic/ComponentSelection';
 import {ComponentsContext} from './logic/Context';
-import PropTypes from 'prop-types';
+
 import {FormLogic} from './FormLogic';
+
 import Field from '../forms/Field';
-import {FormattedMessage} from 'react-intl';
 import FormRow from '../forms/FormRow';
-import Fieldset from "../forms/Fieldset";
+import Fieldset from '../forms/Fieldset';
+
+
+
+const initialState = {
+    products: '',
+};
+
+
+const reducer = (draft, action) => {
+    switch(action.type) {
+        case 'APPOINTMENTS_CONFIGURATION_CHANGED': {
+            const {name, value} = action.payload;
+            draft[name] = value;
+
+            // TODO
+            // clear the dependent fields if needed - e.g. if the component changes, all fields to the right change
+            // const currentFieldIndex = TRIGGER_FIELD_ORDER.indexOf(name);
+            // const nextFieldNames = TRIGGER_FIELD_ORDER.slice(currentFieldIndex + 1);
+            // for (const name of nextFieldNames) {
+            //     draft[name] = initialState[name];
+            // }
+            break;
+        }
+        default: {
+            throw new Error(`Unknown action type: ${action.type}`);
+        }
+    }
+};
 
 
 const Appointments = ({ availableComponents={} }) => {
+
+    const [state, dispatch] = useImmerReducer(reducer, {...initialState});
+
+    const onChange = (event) => {
+        const {name, value} = event.target;
+        dispatch({
+            type: 'APPOINTMENTS_CONFIGURATION_CHANGED',
+            payload: {
+                name,
+                value
+            },
+        });
+    };
+
+    // rendering logic
+    const {
+        products,
+    } = state;
+
+
     return (
         <ComponentsContext.Provider value={availableComponents}>
             <Fieldset>
                 <FormRow>
                     <Field
-                        name="appointments.Products"
+                        name="products"
                         label={<FormattedMessage defaultMessage="Products Component" description="Products Component field label" />}
                         helpText={<FormattedMessage defaultMessage="Component where products for an appointment will be shown" description="Products Component field help text" />}
                     >
                         <ComponentSelection
                             name="component"
-                            value=""
-                            onChange={() => console.log('I changed')}
+                            value={products}
+                            onChange={onChange}
                             filter={(comp) => (comp.type === 'select')}
                         />
                     </Field>
