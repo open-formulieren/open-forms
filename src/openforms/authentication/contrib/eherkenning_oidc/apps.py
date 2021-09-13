d0b2dbae892b8fd4ae0baba3241db0dda1f47856
Steven Bal <steven@maykinmedia.nl>
2021-09-13 16:30:45 +0200

    :ok_hand: PR feedback

--- src/openforms/authentication/contrib/eherkenning_oidc/apps.py
+++ src/openforms/authentication/contrib/eherkenning_oidc/apps.py
@@ -1,10 +1,11 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class eHerkenningOIDCApp(AppConfig):
     name = "openforms.authentication.contrib.eherkenning_oidc"
     label = "eherkenning_oidc"
-    verbose_name = "eHerkenning via OpenID Connect"
+    verbose_name = _("eHerkenning via OpenID Connect")
 
     def ready(self):
         # register the plugin
