e3a805e1511c5094cb6d282335c476f560125b2f
Steven Bal <steven@maykinmedia.nl>
2021-09-13 16:02:14 +0200

    :recycle: Remove duplicated code in OIDC admin forms
    since this code is now part of mozilla-django-oidc-db

--- src/openforms/authentication/contrib/digid_oidc/forms.py
+++ src/openforms/authentication/contrib/digid_oidc/forms.py
@@ -34,49 +34,6 @@ class OpenIDConnectPublicConfigForm(OpenIDConnectConfigForm):
         model = OpenIDConnectPublicConfig
         fields = "__all__"
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # Required endpoints should be optional in the form, if the can be
-        # derived from the discovery endpoint
-        for endpoint in self.required_endpoints:
-            self.fields[endpoint].required = False
-
-    def clean(self):
-        cleaned_data = super().clean()
-
-        discovery_endpoint = cleaned_data.get("oidc_op_discovery_endpoint")
-
-        # Derive the endpoints from the discovery endpoint
-        if discovery_endpoint:
-            try:
-                response = requests.get(
-                    f"{discovery_endpoint}{OPEN_ID_CONFIG_PATH}", timeout=10
-                )
-                configuration = response.json()
-
-                for model_attr, oidc_attr in self.oidc_mapping.items():
-                    cleaned_data[model_attr] = configuration.get(oidc_attr)
-            except (
-                requests.exceptions.RequestException,
-                json.decoder.JSONDecodeError,
-            ):
-                raise forms.ValidationError(
-                    {
-                        "oidc_op_discovery_endpoint": _(
-                            "Something went wrong while retrieving the configuration."
-                        )
-                    }
-                )
-        else:
-            # Verify that the required endpoints were derived from the
-            # discovery endpoint
-            for field in self.required_endpoints:
-                if not cleaned_data.get(field):
-                    self.add_error(field, _("This field is required."))
-
-        return cleaned_data
-
     def clean_enabled(self):
         enabled = self.cleaned_data["enabled"]
 
