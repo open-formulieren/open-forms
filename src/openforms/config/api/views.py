f49c319e69e324a7ab32282be7fc6fd53cec96c5
silvia <silvia@maykinmedia.nl>
2021-09-14 17:41:31 +0200

    :construction: [#642] Improve api schema

--- src/openforms/config/api/views.py
+++ src/openforms/config/api/views.py
@@ -1,3 +1,6 @@
+from django.utils.translation import gettext as _
+
+from drf_spectacular.utils import extend_schema
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -5,8 +8,13 @@ from rest_framework.views import APIView
 from openforms.submissions.api.permissions import AnyActiveSubmissionPermission
 
 from ..models import GlobalConfiguration
+from .serializers import PrivacyPolicyInfo, PrivacyPolicyInfoSerializer
 
 
+@extend_schema(
+    summary=_("Privacy policy info"),
+    responses={200: PrivacyPolicyInfoSerializer},
+)
 class PrivacyPolicyInfoView(APIView):
     permission_classes = (AnyActiveSubmissionPermission,)
 
@@ -15,9 +23,10 @@ class PrivacyPolicyInfoView(APIView):
         if not conf.ask_privacy_consent:
             return Response({"requires_privacy_consent": False})
 
-        return Response(
-            {
-                "requires_privacy_consent": True,
-                "privacy_label": conf.render_privacy_policy_label(),
-            }
+        info = PrivacyPolicyInfo(
+            requires_privacy_consent=True,
+            privacy_label=conf.render_privacy_policy_label(),
         )
+        serializer = PrivacyPolicyInfoSerializer(instance=info)
+
+        return Response(serializer.data)
