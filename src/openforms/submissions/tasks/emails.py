a971d57d6d76e0504cbc81fab4c1679f7734943f
Sergei Maertens <sergei@maykinmedia.nl>
2021-09-15 17:39:33 +0200

    :bug: Refs. #651 -- discarding results of wrong task

--- src/openforms/submissions/tasks/emails.py
+++ src/openforms/submissions/tasks/emails.py
@@ -6,7 +6,7 @@ from ..utils import send_confirmation_email
 __all__ = ["maybe_send_confirmation_email"]
 
 
-@app.task(bind=True)
+@app.task(bind=True, ignore_result=True)
 def maybe_send_confirmation_email(task, submission_id: int) -> None:
     submission = Submission.objects.get(id=submission_id)
     if hasattr(submission.form, "confirmation_email_template"):
