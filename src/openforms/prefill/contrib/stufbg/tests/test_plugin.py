0285b1f3b525c3a96c4b5b389ec3c344d30d0802
silvia <silvia@maykinmedia.nl>
2021-10-06 12:23:09 +0200

    :white_check_mark: [#292] Test response with woonplaatsWaarinGelegen

--- src/openforms/prefill/contrib/stufbg/tests/test_plugin.py
+++ src/openforms/prefill/contrib/stufbg/tests/test_plugin.py
@@ -37,6 +37,28 @@ class StufBgPrefillTests(TestCase):
         self.assertEqual(values["woonplaatsNaam"], "Amsterdam")
 
     @patch("openforms.prefill.contrib.stufbg.plugin.StufBGConfig.get_solo")
+    def test_response_external_woonplaats_returns_correct_attributes(self, client_mock):
+        get_values_for_attributes_mock = (
+            client_mock.return_value.get_client.return_value.get_values_for_attributes
+        )
+        get_values_for_attributes_mock.return_value = loader.render_to_string(
+            "stuf_bg/tests/responses/StufBgResponseWoonplaats.xml"
+        )
+        attributes = FieldChoices.attributes.keys()
+
+        values = self.plugin.get_prefill_values(self.submission, attributes)
+
+        self.assertEqual(values["bsn"], "999992314")
+        self.assertEqual(values["voornamen"], "Media")
+        self.assertEqual(values["geslachtsnaam"], "Maykin")
+        self.assertEqual(values["straatnaam"], "Keizersgracht")
+        self.assertEqual(values["huisnummer"], "117")
+        self.assertEqual(values["huisletter"], "A")
+        self.assertEqual(values["huisnummertoevoeging"], "B")
+        self.assertEqual(values["postcode"], "1015 CJ")
+        self.assertEqual(values["woonplaatsNaam"], "Amsterdam")
+
+    @patch("openforms.prefill.contrib.stufbg.plugin.StufBGConfig.get_solo")
     def test_get_available_attributes_when_some_attributes_are_not_returned(
         self, client_mock
     ):
