fea431b53d098419339eca9355e94b88c8203c15
silvia <silvia@maykinmedia.nl>
2021-10-08 18:21:19 +0200

    :heavy_plus_sign: [#644] i18next-parser

---
+++ i18next-scanner.config.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+module.exports = {
+    input: [
+        'src/**/*.js',
+        // Use ! to filter out files or directories
+        '!**/node_modules/**',
+    ],
+    output: './',
+    options: {
+        debug: true,
+        func: {
+            list: ['i18next.t', 'i18n.t', 'ctx.t', 'this.t'],
+            extensions: ['.js']
+        },
+        trans: {
+            component: 'Trans',
+            i18nKey: 'i18nKey',
+            defaultsKey: 'defaults',
+            extensions: ['.js', '.ejs'],
+            fallbackKey: function(ns, value) {
+                return value;
+            },
+            acorn: {
+                ecmaVersion: 10, // defaults to 10
+                sourceType: 'module', // defaults to 'module'
+                // Check out https://github.com/acornjs/acorn/tree/master/acorn#interface for additional options
+            }
+        },
+        lngs: ['en','nl'],
+        ns: [
+            'locale',
+            'resource'
+        ],
+        defaultLng: 'en',
+        defaultNs: 'resource',
+        defaultValue: '_NOT_TRANSLATED_',
+        resource: {
+            loadPath: 'src/openforms/js/lang/formio/{{lng}}/{{ns}}.json',
+            savePath: 'src/openforms/js/lang/formio/{{lng}}/{{ns}}.json',
+            jsonIndent: 2,
+            lineEnding: '\n'
+        },
+        nsSeparator: false, // namespace separator
+        keySeparator: false, // key separator
+        interpolation: {
+            prefix: '{{',
+            suffix: '}}'
+        }
+    },
+    transform: function customTransform(file, enc, done) {
+        "use strict";
+        const parser = this.parser;
+        const content = fs.readFileSync(file.path, enc);
+        let count = 0;
+
+        parser.parseFuncFromString(content, { list: ['i18next._', 'i18next.__', 'ctx.__', 'this.__'] }, (key, options) => {
+            parser.set(key, Object.assign({}, options, {
+                nsSeparator: false,
+                keySeparator: false
+            }));
+            ++count;
+        });
+
+        if (count > 0) {
+            console.log(`i18next-scanner: count=${chalk.cyan(count)}, file=${chalk.yellow(JSON.stringify(file.relative))}`);
+        }
+
+        done();
+    }
+};
