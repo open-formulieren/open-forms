459f86feee57078a165a3551638b946136b600c8
Steven Bal <steven@maykinmedia.nl>
2021-10-11 14:25:14 +0200

    :sparkles: [#794] Add defaultValue for all fields

--- src/openforms/js/components/form/checkbox.js
+++ src/openforms/js/components/form/checkbox.js
@@ -1,14 +1,4 @@
 import {Formio} from 'formiojs';
 import {defineCommonEditFormTabs} from './abstract';
 
-defineCommonEditFormTabs(Formio.Components.components.checkbox, [
-    {
-        input: true,
-        key: 'defaultValue',
-        label: 'Default Value',
-        placeholder: 'Default Value',
-        tooltip: 'This will be the value for this field before user interaction. Having a default value will override the placeholder text.',
-        type: 'checkbox',
-        weight: 5
-    }
-]);
+defineCommonEditFormTabs(Formio.Components.components.checkbox, []);
