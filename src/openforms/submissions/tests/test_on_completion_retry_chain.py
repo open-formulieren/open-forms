850d331f57c31bd80f925c3aa7f7cdeb96cd16a7
Sergei Maertens <sergei@maykinmedia.nl>
2021-10-26 11:08:28 +0200

    :white_check_mark: Refs. #839 -- added test for payment update retry workflow
    The tests assert that the subtasks are idempotent and the retry chain
    is properly aborted if something keeps going wrong.

--- src/openforms/submissions/tests/test_on_completion_retry_chain.py
+++ src/openforms/submissions/tests/test_on_completion_retry_chain.py
@@ -13,6 +13,8 @@ from openforms.appointments.service import AppointmentUpdateFailed
 from openforms.appointments.tests.factories import AppointmentInfoFactory
 from openforms.emails.tests.factories import ConfirmationEmailTemplateFactory
 from openforms.forms.tests.factories import FormDefinitionFactory
+from openforms.payments.constants import PaymentStatus
+from openforms.payments.tests.factories import SubmissionPaymentFactory
 
 from ..constants import RegistrationStatuses
 from ..models import SubmissionReport, TemporaryFileUpload
@@ -97,6 +99,120 @@ class OnCompletionRetryFailedUpdateAppointmentTests(TestCase):
         self.assertEqual(submission.last_register_date, original_register_date)
         # it failed again, so we must keep re-trying
         self.assertTrue(submission.needs_on_completion_retry)
+
+
+@temp_private_root()
+@override_settings(CELERY_TASK_ALWAYS_EAGER=True)
+class OnCompletionRetryFailedUpdatePaymentStatusTests(TestCase):
+    """
+    Test the various retry branches if the payment status update failed.
+
+    In this scenario, initial backend registration succeeded, but updating the payment
+    status after payment confirmation failed.
+
+    The retry workflow may not register the submission with the backend again, nor may
+    it do anything appointment related.
+    """
+
+    def test_payment_status_update_now_succeeds(self):
+        # set up a complex submission, with an appointment and payment required.
+        submission = SubmissionFactory.create(
+            completed=True,
+            # set by :func:`openforms.payments.tasks.update_submission_payment_status`
+            needs_on_completion_retry=True,
+            registration_success=True,
+            form__registration_backend="zgw-create-zaak",
+            form__payment_backend="ogone-legacy",
+            registration_result={
+                "zaak": {
+                    "url": "https://example.com",
+                    "identificatie": "ZAAK-123",
+                }
+            },
+            public_registration_reference="ZAAK-123",
+        )
+        # status simulates that we received the payment paid event from the payment provider
+        SubmissionPaymentFactory.for_submission(
+            submission, status=PaymentStatus.completed
+        )
+        assert submission.payment_required, "Form must require payment for this test"
+        assert (
+            submission.payment_user_has_paid
+        ), "Paid status should be True - the return view received the payment from Ogone."
+        original_register_date = submission.last_register_date
+        appointment_info = AppointmentInfoFactory.create(
+            submission=submission,
+            registration_ok=True,
+        )
+        original_appointment_id = appointment_info.appointment_id
+
+        # invoke the chain
+        with patch(
+            "openforms.registrations.contrib.zgw_apis.plugin.set_zaak_payment"
+        ) as mock_set_zaak_payment:
+            on_completion_retry(submission.id)()
+
+        # mock_update_appointment.assert_called_once_with(submission)
+        submission.refresh_from_db()
+        appointment_info.refresh_from_db()
+        # assert that we now no longer have to retry
+        self.assertFalse(submission.needs_on_completion_retry)
+        # check that the registration did not run again (idemptotent, a side effect is
+        # that last_register_date is updated if it runs)
+        self.assertEqual(submission.last_register_date, original_register_date)
+        self.assertEqual(appointment_info.appointment_id, original_appointment_id)
+        mock_set_zaak_payment.assert_called_once_with("https://example.com")
+
+    def test_payment_status_update_still_fails(self):
+        # set up a complex submission, with an appointment and payment required.
+        submission = SubmissionFactory.create(
+            completed=True,
+            # set by :func:`openforms.payments.tasks.update_submission_payment_status`
+            needs_on_completion_retry=True,
+            registration_success=True,
+            form__registration_backend="zgw-create-zaak",
+            form__payment_backend="ogone-legacy",
+            registration_result={
+                "zaak": {
+                    "url": "https://example.com",
+                    "identificatie": "ZAAK-123",
+                }
+            },
+            public_registration_reference="ZAAK-123",
+        )
+        # status simulates that we received the payment paid event from the payment provider
+        SubmissionPaymentFactory.for_submission(
+            submission, status=PaymentStatus.completed
+        )
+        assert submission.payment_required, "Form must require payment for this test"
+        assert (
+            submission.payment_user_has_paid
+        ), "Paid status should be True - the return view received the payment from Ogone."
+        original_register_date = submission.last_register_date
+        appointment_info = AppointmentInfoFactory.create(
+            submission=submission,
+            registration_ok=True,
+        )
+        original_appointment_id = appointment_info.appointment_id
+
+        # invoke the chain
+        patcher = patch(
+            "openforms.registrations.contrib.zgw_apis.plugin.set_zaak_payment",
+            side_effect=Exception("Something went wrong"),
+        )
+        with patcher as mock_set_zaak_payment, self.assertRaises(Exception):
+            on_completion_retry(submission.id)()
+
+        # mock_update_appointment.assert_called_once_with(submission)
+        submission.refresh_from_db()
+        appointment_info.refresh_from_db()
+        # assert that we now no longer have to retry
+        self.assertTrue(submission.needs_on_completion_retry)
+        # check that the registration did not run again (idemptotent, a side effect is
+        # that last_register_date is updated if it runs)
+        self.assertEqual(submission.last_register_date, original_register_date)
+        self.assertEqual(appointment_info.appointment_id, original_appointment_id)
+        mock_set_zaak_payment.assert_called_once_with("https://example.com")
 
 
 class OnCompletionRetryFailedRegistrationTests(TestCase):
