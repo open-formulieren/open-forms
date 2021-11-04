27cd09f52d9b7bb0de97dc0ae393c0ad5bb7e2a2
Bart van der Schoor <bart.vanderschoor@maykinmedia.nl>
2021-11-04 15:48:01 +0100

    [#708] Restructure template validator test

--- src/openforms/emails/tests/test_validators.py
+++ src/openforms/emails/tests/test_validators.py
@@ -8,47 +8,51 @@ class DjangoTemplateValidatorTest(TestCase):
     def test_template_syntax(self):
         validator = DjangoTemplateValidator()
 
-        # valid
-        validator("")
-        validator(" \n \n ")
-        validator("aa")
-        validator("{{ aa }}")
-        validator(
-            '{% load i18n %}<p>{% trans "foo" %} {% if aa %}{{ aa.bb.cc }}{% endif %}</p>'
-        )
+        valid = [
+            "",
+            " \n \n ",
+            "aa",
+            "{{ aa }}",
+            '{% load i18n %}<p>{% trans "foo" %} {% if aa %}{{ aa.bb.cc }}{% endif %}</p>',
+            # note: in-complete tags are acceptable to the template language
+            "{{ aa }",
+            "{% foo %",
+        ]
+        for text in valid:
+            with self.subTest(f"valid: {text}"):
+                validator(text)
 
-        # note: in-complete tags are acceptable to the template language
-        validator("{{ aa }")
-        validator("{% foo %")
-
-        # syntax errors
-        with self.assertRaisesRegex(ValidationError, "Could not parse the remainder:"):
-            validator("{{{}}}")
-
-        with self.assertRaisesRegex(
-            ValidationError, "Unexpected end of expression in if tag."
-        ):
-            validator("{% if %}")
-
-        with self.assertRaisesRegex(ValidationError, "Invalid block tag on line 1:"):
-            validator("{% foo %}")
-
-        with self.assertRaisesRegex(ValidationError, "Invalid block tag on line 1:"):
-            validator("{% $$ %}")
+        invalid = [
+            ("{{{}}}", "Could not parse the remainder:"),
+            ("{% if %}", "Unexpected end of expression in if tag."),
+            ("{% foo %}", "Invalid block tag on line 1:"),
+            ("{% $$ %}", "Invalid block tag on line 1:"),
+        ]
+        for text, message in invalid:
+            with self.subTest(f"invalid: {text}"):
+                with self.assertRaisesRegex(ValidationError, message):
+                    validator(text)
 
     def test_required_tags(self):
         validator = DjangoTemplateValidator(required_template_tags=["csrf_token"])
 
-        validator("{% csrf_token %}")
-        validator("{%csrf_token%}")
-        validator("{% load i18n %} aa {% csrf_token %} {{ aa.bb.cc }}")
+        valid = [
+            "{% csrf_token %}",
+            "{%csrf_token%}",
+            "{% load i18n %} aa {% csrf_token %} {{ aa.bb.cc }}",
+        ]
+        for text in valid:
+            with self.subTest(f"valid: {text}"):
+                validator(text)
 
-        with self.assertRaisesRegex(
-            ValidationError, "Missing required template-tag \{% csrf_token %}"
-        ):
-            validator("")
-
-        with self.assertRaisesRegex(
-            ValidationError, "Missing required template-tag \{% csrf_token %}"
-        ):
-            validator("{% load i18n %} aa {{ aa.bb.cc }}")
+        invalid = [
+            ("", "Missing required template-tag \{% csrf_token %}"),
+            (
+                "{% load i18n %} aa {{ aa.bb.cc }}",
+                "Missing required template-tag \{% csrf_token %}",
+            ),
+        ]
+        for text, message in invalid:
+            with self.subTest(f"invalid: {text}"):
+                with self.assertRaisesRegex(ValidationError, message):
+                    validator(text)
