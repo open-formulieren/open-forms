b688a7c3793a741544528df80a4fd7f729259a56
silvia <silvia@maykinmedia.nl>
2021-11-05 12:24:01 +0100

    :construction: [#198] Make select widget configurable

--- src/openforms/js/components/form/select.js
+++ src/openforms/js/components/form/select.js
@@ -1,34 +1,57 @@
-import {Formio} from 'formiojs';
+import {Formio, Utils} from 'formiojs';
 import DEFAULT_TABS, {ADVANCED, BASIC, REGISTRATION, VALIDATION} from './edit/tabs';
 
 const Select = Formio.Components.components.select;
 
-const values = [{
-    type: 'datagrid',
-    input: true,
-    label: 'Values',
-    key: 'data.values',
-    tooltip: 'The radio button values that can be picked for this field. Values are text submitted with the form data. Labels are text that appears next to the radio buttons on the form.',
-    weight: 10,
-    reorder: true,
-    defaultValue: [{label: '', value: ''}],
-    components: [
-        {
-            label: 'Label',
-            key: 'label',
-            input: true,
-            type: 'textfield',
+const values = [
+    {
+        label: 'Widget Type',
+        key: 'widget',
+        type: 'select',
+        data: {
+            values: [
+                {
+                    value: 'choicesjs',
+                    label: 'ChoicesJS'
+                },
+                {
+                    value: 'html5',
+                    label: 'HTML 5'
+                }
+            ]
         },
-        {
-            label: 'Value',
-            key: 'value',
-            input: true,
-            type: 'textfield',
-            allowCalculateOverride: true,
-            calculateValue: {_camelCase: [{var: 'row.label'}]},
-        },
-    ],
-}];
+        dataSrc: 'values',
+        template: '<span>{{ item.label }}</span>',
+        input: true,
+        defaultValue: 'html5'
+    },
+    {
+        type: 'datagrid',
+        input: true,
+        label: 'Values',
+        key: 'data.values',
+        tooltip: 'The radio button values that can be picked for this field. Values are text submitted with the form data. Labels are text that appears next to the radio buttons on the form.',
+        weight: 10,
+        reorder: true,
+        defaultValue: [{label: '', value: ''}],
+        components: [
+            {
+                label: 'Label',
+                key: 'label',
+                input: true,
+                type: 'textfield',
+            },
+            {
+                label: 'Value',
+                key: 'value',
+                input: true,
+                type: 'textfield',
+                allowCalculateOverride: true,
+                calculateValue: {_camelCase: [{var: 'row.label'}]},
+            },
+        ],
+    },
+];
 
 
 class SelectField extends Select {
