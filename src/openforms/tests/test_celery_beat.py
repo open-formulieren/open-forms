d54af52764336780c9cfd8180bbf614be2daff8f
Bart van der Schoor <bart.vanderschoor@maykinmedia.nl>
2021-11-11 16:40:29 +0100

    [#886] Updated BeatConfigTests to check if full import path was used in CELERY_BEAT_SCHEDULE

--- src/openforms/tests/test_celery_beat.py
+++ src/openforms/tests/test_celery_beat.py
@@ -9,11 +9,17 @@ class BeatConfigTests(SimpleTestCase):
         Assert that the task import paths in the Beat config are valid.
         """
         for entry in settings.CELERY_BEAT_SCHEDULE.values():
-            task = entry["task"]
-            with self.subTest(task=task):
+            task_path = entry["task"]
+            with self.subTest(task=task_path):
                 try:
-                    import_string(task)
+                    task = import_string(task_path)
                 except ImportError:
                     self.fail(
-                        f"Could not import task '{task}' in settings.CELERY_BEAT_SCHEDULE"
+                        f"Could not import task '{task_path}' in settings.CELERY_BEAT_SCHEDULE"
                     )
+                else:
+                    # its is important the full path is used or beat won't be able to find the task
+                    if task.name != task_path:
+                        self.fail(
+                            f"Task '{task_path}' in settings.CELERY_BEAT_SCHEDULE should be registered with it's full import path, eg: '{task.name}'"
+                        )
