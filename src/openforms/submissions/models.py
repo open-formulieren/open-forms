50e6120b1b51b7724c4bc2308ad60214e8dfebcd
Sergei Maertens <sergei@maykinmedia.nl>
2021-12-01 16:02:19 +0100

    :card_file_box: Refs. #235 -- add Submission.price field
    Since product prices can change after a submission was completed, it's
    important to freeze/record the price at the moment on the submission
    itself. It's also possible the price is a calculated result from
    logic rules, where the logic rules on the form may change or be removed
    after the submission is completed.

--- src/openforms/submissions/models.py
+++ src/openforms/submissions/models.py
@@ -131,18 +131,21 @@ class Submission(models.Model):
 
     uuid = models.UUIDField(_("UUID"), unique=True, default=uuid.uuid4)
     form = models.ForeignKey("forms.Form", on_delete=models.PROTECT)
+
+    # meta information
+    form_url = models.URLField(
+        _("form URL"),
+        max_length=255,
+        blank=False,
+        default="",
+        help_text=_("URL where the user initialized the submission."),
+        validators=[AllowedRedirectValidator()],
+    )
     created_on = models.DateTimeField(_("created on"), auto_now_add=True)
     completed_on = models.DateTimeField(_("completed on"), blank=True, null=True)
     suspended_on = models.DateTimeField(_("suspended on"), blank=True, null=True)
-    last_register_date = models.DateTimeField(
-        _("last register attempt date"),
-        blank=True,
-        null=True,
-        help_text=_(
-            "The last time the submission registration was attempted with the backend.  "
-            "Note that this date will be updated even if the registration is not successful."
-        ),
-    )
+
+    # authentication state
     bsn = models.CharField(
         _("BSN"),
         max_length=9,
@@ -172,7 +175,31 @@ class Submission(models.Model):
         validators=[AllowedRedirectValidator()],
     )
 
+    # payment state
+    price = models.DecimalField(
+        _("price"),
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        null=True,
+        editable=False,
+        help_text=_(
+            "Cost of this submission. Either derived from the related product, "
+            "or evaluated from price logic rules. The price is calculated and saved "
+            "on submission completion."
+        ),
+    )
+
     # interaction with registration backend
+    last_register_date = models.DateTimeField(
+        _("last register attempt date"),
+        blank=True,
+        null=True,
+        help_text=_(
+            "The last time the submission registration was attempted with the backend.  "
+            "Note that this date will be updated even if the registration is not successful."
+        ),
+    )
     registration_result = JSONField(
         _("registration backend result"),
         blank=True,
@@ -242,6 +269,7 @@ class Submission(models.Model):
         ),
     )
 
+    # relation to earlier submission which is altered after processing
     previous_submission = models.ForeignKey(
         "submissions.Submission", on_delete=models.SET_NULL, null=True, blank=True
     )
