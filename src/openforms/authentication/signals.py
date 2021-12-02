96b693ce66577381070847e428d7cdd9a98249b5
silvia <silvia@maykinmedia.nl>
2021-12-02 17:10:25 +0100

    :fire: [#957] Remove unneeded signals

--- src/openforms/authentication/signals.py
+++ src/openforms/authentication/signals.py
@@ -13,54 +13,6 @@ from .constants import FORM_AUTH_SESSION_KEY, AuthAttribute
 logger = logging.getLogger(__name__)
 
 
-@receiver(submission_start, dispatch_uid="auth.set_submission_kvk")
-def set_kvk_from_session(sender, instance: Submission, request: Request, **kwargs):
-    kvk = request.session.get("kvk")
-    if not kvk:
-        return
-
-    warnings.warn(
-        "The bare 'kvk' session key is deprecated in favour of the generic 'form_auth' "
-        "session key.",
-        DeprecationWarning,
-    )
-
-    instance.kvk = kvk
-    instance.save(update_fields=["kvk"])
-
-
-@receiver(submission_start, dispatch_uid="auth.set_submission_bsn")
-def set_bsn_from_session(sender, instance: Submission, request: Request, **kwargs):
-    bsn = request.session.get("bsn")
-    if not bsn:
-        return
-
-    warnings.warn(
-        "The bare 'bsn' session key is deprecated in favour of the generic 'form_auth' "
-        "session key.",
-        DeprecationWarning,
-    )
-
-    instance.bsn = bsn
-    instance.save(update_fields=["bsn"])
-
-
-@receiver(submission_start, dispatch_uid="auth.set_submission_pseudo")
-def set_pseudo_from_session(sender, instance: Submission, request: Request, **kwargs):
-    pseudo = request.session.get("pseudo")
-    if not pseudo:
-        return
-
-    warnings.warn(
-        "The bare 'pseudo' session key is deprecated in favour of the generic 'form_auth' "
-        "session key.",
-        DeprecationWarning,
-    )
-
-    instance.pseudo = pseudo
-    instance.save(update_fields=["pseudo"])
-
-
 @receiver(submission_start, dispatch_uid="auth.set_submission_form_auth")
 def set_auth_attribute_on_session(
     sender, instance: Submission, request: Request, **kwargs
