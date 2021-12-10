4bd53ab1814b5259a7046152988406fcf7949612
Sergei Maertens <sergei@maykinmedia.nl>
2021-12-10 10:15:20 +0100

    :ok_hand: PR feedback

--- src/openforms/registrations/exceptions.py
+++ src/openforms/registrations/exceptions.py
@@ -1,6 +1,7 @@
 class RegistrationFailed(Exception):
     def __init__(self, *args, **kwargs):
         self.response = kwargs.pop("response", None)
+        super().__init__(*args, **kwargs)
 
 
 class NoSubmissionReference(Exception):
