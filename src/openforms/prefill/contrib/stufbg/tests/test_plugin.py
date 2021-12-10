3bea65dceef1bddad587427c466d026cef2f9de1
silvia <silvia@maykinmedia.nl>
2021-12-10 13:37:48 +0100

    :white_check_mark: [#297] Update tests after refactor

--- src/openforms/prefill/contrib/stufbg/tests/test_plugin.py
+++ src/openforms/prefill/contrib/stufbg/tests/test_plugin.py
@@ -1,13 +1,24 @@
+from unittest.mock import patch
+
+from django.template import loader
 from django.test import TestCase
 
 from openforms.submissions.tests.factories import SubmissionFactory
 from stuf.stuf_bg.constants import FieldChoices
+from stuf.stuf_bg.models import StufBGConfig
+from stuf.tests.factories import StufServiceFactory
 
 from ..plugin import StufBgPrefill
-from .utils import mock_stufbg_client
 
 
 class StufBgPrefillTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        stuf_bg_service = StufServiceFactory.create()
+        config = StufBGConfig.get_solo()
+        config.service = stuf_bg_service
+        config.save()
+
     def setUp(self) -> None:
         super().setUp()
 
@@ -15,11 +26,16 @@ class StufBgPrefillTests(TestCase):
         self.submission = SubmissionFactory.create(bsn="999992314")
 
     def test_get_available_attributes_returns_correct_attributes(self):
-        client_patcher = mock_stufbg_client("StufBgResponse.xml")
-        self.addCleanup(client_patcher.stop)
+        return_value = loader.render_to_string(
+            "stuf_bg/tests/responses/StufBgResponse.xml"
+        )
         attributes = FieldChoices.attributes.keys()
 
-        values = self.plugin.get_prefill_values(self.submission, attributes)
+        with patch(
+            "stuf.stuf_bg.client.StufBGClient.get_values_for_attributes",
+            return_value=return_value,
+        ) as m:
+            values = self.plugin.get_prefill_values(self.submission, attributes)
 
         self.assertEqual(values["bsn"], "999992314")
         self.assertEqual(values["voornamen"], "Media")
@@ -32,11 +48,16 @@ class StufBgPrefillTests(TestCase):
         self.assertEqual(values["woonplaatsNaam"], "Amsterdam")
 
     def test_response_external_municipality_returns_correct_attributes(self):
-        client_patcher = mock_stufbg_client("StufBgResponseGemeenteVanInschrijving.xml")
-        self.addCleanup(client_patcher.stop)
+        return_value = loader.render_to_string(
+            "stuf_bg/tests/responses/StufBgResponseGemeenteVanInschrijving.xml"
+        )
         attributes = FieldChoices.attributes.keys()
 
-        values = self.plugin.get_prefill_values(self.submission, attributes)
+        with patch(
+            "stuf.stuf_bg.client.StufBGClient.get_values_for_attributes",
+            return_value=return_value,
+        ) as m:
+            values = self.plugin.get_prefill_values(self.submission, attributes)
 
         self.assertEqual(values["bsn"], "999992314")
         self.assertEqual(values["voornamen"], "Media")
@@ -49,11 +70,16 @@ class StufBgPrefillTests(TestCase):
         self.assertEqual(values["gemeenteVanInschrijving"], "Amsterdam")
 
     def test_get_available_attributes_when_some_attributes_are_not_returned(self):
-        client_patcher = mock_stufbg_client("StufBgResponseMissingSomeData.xml")
-        self.addCleanup(client_patcher.stop)
+        return_value = loader.render_to_string(
+            "stuf_bg/tests/responses/StufBgResponseMissingSomeData.xml"
+        )
         attributes = FieldChoices.attributes.keys()
 
-        values = self.plugin.get_prefill_values(self.submission, attributes)
+        with patch(
+            "stuf.stuf_bg.client.StufBGClient.get_values_for_attributes",
+            return_value=return_value,
+        ) as m:
+            values = self.plugin.get_prefill_values(self.submission, attributes)
 
         self.assertEqual(values["bsn"], "999992314")
         self.assertEqual(values["voornamen"], "Media")
@@ -66,11 +92,16 @@ class StufBgPrefillTests(TestCase):
         self.assertNotIn("huisletter", values)
 
     def test_voorvoegsel_is_parsed(self):
-        client_patcher = mock_stufbg_client("StufBgResponseWithVoorvoegsel.xml")
-        self.addCleanup(client_patcher.stop)
+        return_value = loader.render_to_string(
+            "stuf_bg/tests/responses/StufBgResponseWithVoorvoegsel.xml"
+        )
         attributes = FieldChoices.attributes.keys()
 
-        values = self.plugin.get_prefill_values(self.submission, attributes)
+        with patch(
+            "stuf.stuf_bg.client.StufBGClient.get_values_for_attributes",
+            return_value=return_value,
+        ) as m:
+            values = self.plugin.get_prefill_values(self.submission, attributes)
 
         self.assertEqual(values["bsn"], "999992314")
         self.assertEqual(values["voornamen"], "Media")
@@ -84,30 +115,42 @@ class StufBgPrefillTests(TestCase):
         self.assertEqual(values["woonplaatsNaam"], "Amsterdam")
 
     def test_get_available_attributes_when_error_occurs(self):
-        client_patcher = mock_stufbg_client("StufBgErrorResponse.xml")
-        self.addCleanup(client_patcher.stop)
+        return_value = loader.render_to_string(
+            "stuf_bg/tests/responses/StufBgErrorResponse.xml"
+        )
         attributes = FieldChoices.attributes.keys()
 
-        with self.assertLogs() as logs:
-            values = self.plugin.get_prefill_values(self.submission, attributes)
+        with patch(
+            "stuf.stuf_bg.client.StufBGClient.get_values_for_attributes",
+            return_value=return_value,
+        ) as m:
+            with self.assertLogs() as logs:
+                values = self.plugin.get_prefill_values(self.submission, attributes)
 
-        self.assertEqual(values, {})
-        self.assertEqual(logs.records[0].fault["faultcode"], "soapenv:Server")
-        self.assertEqual(logs.records[0].fault["faultstring"], "Policy Falsified")
-        self.assertEqual(
-            logs.records[0].fault["detail"][
-                "http://www.layer7tech.com/ws/policy/fault:policyResult"
-            ]["@status"],
-            "Error in Assertion Processing",
-        )
+                self.assertEqual(values, {})
+                self.assertEqual(logs.records[0].fault["faultcode"], "soapenv:Server")
+                self.assertEqual(
+                    logs.records[0].fault["faultstring"], "Policy Falsified"
+                )
+                self.assertEqual(
+                    logs.records[0].fault["detail"][
+                        "http://www.layer7tech.com/ws/policy/fault:policyResult"
+                    ]["@status"],
+                    "Error in Assertion Processing",
+                )
 
     def test_get_available_attributes_when_no_answer_is_returned(self):
-        client_patcher = mock_stufbg_client("StufBgNoAnswerResponse.xml")
-        self.addCleanup(client_patcher.stop)
+        return_value = loader.render_to_string(
+            "stuf_bg/tests/responses/StufBgNoAnswerResponse.xml"
+        )
         attributes = FieldChoices.attributes.keys()
 
-        with self.assertLogs() as logs:
-            values = self.plugin.get_prefill_values(self.submission, attributes)
+        with patch(
+            "stuf.stuf_bg.client.StufBGClient.get_values_for_attributes",
+            return_value=return_value,
+        ) as m:
+            with self.assertLogs() as logs:
+                values = self.plugin.get_prefill_values(self.submission, attributes)
 
-        self.assertEqual(values, {})
-        self.assertEqual(logs.records[0].fault, {})
+                self.assertEqual(values, {})
+                self.assertEqual(logs.records[0].fault, {})
