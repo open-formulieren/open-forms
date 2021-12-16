4bd27395f56d5db65ff4ebaf2f3814a5cdef5f33
Sergei Maertens <sergei@maykinmedia.nl>
2021-12-16 11:59:22 +0100

    :recycle: Clean up eHerkenning tests a bit
    Applies the same patterns as the digid settings

--- src/openforms/authentication/contrib/eherkenning/tests/test_auth.py
+++ src/openforms/authentication/contrib/eherkenning/tests/test_auth.py
@@ -1,6 +1,7 @@
 import os
 from base64 import b64decode, b64encode
 from hashlib import sha1
+from typing import Optional
 from unittest.mock import patch
 
 from django.conf import settings
@@ -10,11 +11,11 @@ from django.urls import reverse
 from django.utils.http import urlencode
 from django.utils.safestring import mark_safe
 
+import requests_mock
 from freezegun import freeze_time
 from furl import furl
 from lxml import etree
 from onelogin.saml2.utils import OneLogin_Saml2_Utils
-from requests_mock import Mocker
 from rest_framework import status
 
 from openforms.forms.tests.factories import (
@@ -25,39 +26,17 @@ from openforms.forms.tests.factories import (
 
 from ....constants import FORM_AUTH_SESSION_KEY, AuthAttribute
 
+TEST_FILES = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+
 EHERKENNING_SERVICE_INDEX = "8888"
 EHERKENNING = {
     "base_url": "https://test-sp.nl",
     "entity_id": "urn:etoegang:DV:00000001111111111000:entities:9000",
     "service_entity_id": "urn:etoegang:DV:00000001111111111000:entities:9000",
-    "metadata_file": os.path.join(
-        settings.DJANGO_PROJECT_DIR,
-        "authentication",
-        "contrib",
-        "eherkenning",
-        "tests",
-        "data",
-        "eherkenning-metadata.xml",
-    ),
+    "metadata_file": os.path.join(TEST_FILES, "eherkenning-metadata.xml"),
     # SSL/TLS key
-    "key_file": os.path.join(
-        settings.DJANGO_PROJECT_DIR,
-        "authentication",
-        "contrib",
-        "digid",
-        "tests",
-        "data",
-        "test.key",
-    ),
-    "cert_file": os.path.join(
-        settings.DJANGO_PROJECT_DIR,
-        "authentication",
-        "contrib",
-        "digid",
-        "tests",
-        "data",
-        "test.certificate",
-    ),
+    "key_file": os.path.join(TEST_FILES, "test.key"),
+    "cert_file": os.path.join(TEST_FILES, "test.certificate"),
     "services": [
         {
             "service_uuid": "75b40657-ec50-4ced-8e7a-e77d55b46040",
@@ -93,6 +72,38 @@ EHERKENNING = {
         "en": "Test Organisation",
     },
 }
+
+
+def _create_test_artifact(service_entity_id: str = "") -> str:
+    if not service_entity_id:
+        service_entity_id = settings.EHERKENNING["service_entity_id"]
+    type_code = b"\x00\x04"
+    endpoint_index = b"\x00\x00"
+    sha_entity_id = sha1(service_entity_id.encode("utf-8")).digest()
+    message_handle = b"01234567890123456789"  # something random
+    b64encoded = b64encode(type_code + endpoint_index + sha_entity_id + message_handle)
+    return b64encoded.decode("ascii")
+
+
+def _get_artifact_response(filename: str, context: Optional[dict] = None) -> bytes:
+    filepath = os.path.join(TEST_FILES, filename)
+    with open(filepath, "r") as template_source_file:
+        template = Template(template_source_file.read())
+
+    rendered = template.render(Context(context or {}))
+    return rendered.encode("utf-8")
+
+
+def _get_encrypted_attribute(kvk: str):
+    with open(settings.EHERKENNING["cert_file"], "r") as cert_file:
+        cert = cert_file.read()
+    return OneLogin_Saml2_Utils.generate_name_id(
+        kvk,
+        sp_nq=None,
+        nq="urn:etoegang:1.9:EntityConcernedID:KvKnr",
+        sp_format="urn:oasis:names:tc:SAML:2.0:nameid-format:persistent",
+        cert=cert,
+    )
 
 
 @override_settings(
@@ -179,15 +190,8 @@ class AuthenticationStep2Tests(TestCase):
 
 
 @override_settings(EHERKENNING=EHERKENNING, CORS_ALLOW_ALL_ORIGINS=True)
-@Mocker()
+@requests_mock.Mocker()
 class AuthenticationStep5Tests(TestCase):
-    def _create_test_artifact(self, service_entity_id) -> bytes:
-        type_code = b"\x00\x04"
-        endpoint_index = b"\x00\x00"
-        sha_entity_id = sha1(service_entity_id.encode("utf-8")).digest()
-        message_handle = b"01234567890123456789"  # something random
-        return b64encode(type_code + endpoint_index + sha_entity_id + message_handle)
-
     @patch(
         "onelogin.saml2.xml_utils.OneLogin_Saml2_XML.validate_xml", return_value=True
     )
@@ -210,74 +214,44 @@ class AuthenticationStep5Tests(TestCase):
         mock_id,
         mock_xml_validation,
     ):
-        with open(
-            os.path.join(
-                settings.DJANGO_PROJECT_DIR,
-                "authentication",
-                "contrib",
-                "eherkenning",
-                "tests",
-                "data",
-                "ArtifactResponse.xml",
-            ),
-            "r",
-        ) as f:
-            artifact_response_success_template = f.read()
-
-        kvk = "123456782"
-        encrypted_attribute = OneLogin_Saml2_Utils.generate_name_id(
-            kvk,
-            sp_nq=None,
-            nq="urn:etoegang:1.9:EntityConcernedID:KvKnr",
-            sp_format="urn:oasis:names:tc:SAML:2.0:nameid-format:persistent",
-            cert=open(settings.EHERKENNING["cert_file"], "r").read(),
-        )
-
-        artifact_response_soap = (
-            Template(artifact_response_success_template)
-            .render(Context({"encrypted_attribute": mark_safe(encrypted_attribute)}))
-            .encode("utf-8")
-        )
-
+        encrypted_attribute = _get_encrypted_attribute("123456782")
         m.post(
             "https://test-iwelcome.nl/broker/ars/1.13",
-            content=artifact_response_soap,
+            content=_get_artifact_response(
+                "ArtifactResponse.xml",
+                {"encrypted_attribute": mark_safe(encrypted_attribute)},
+            ),
         )
-
-        form = FormFactory.create(authentication_backends=["eherkenning"])
-        form_definition = FormDefinitionFactory.create(login_required=True)
-        FormStepFactory.create(form_definition=form_definition, form=form)
-
+        form = FormFactory.create(
+            authentication_backends=["eherkenning"],
+            generate_minimal_setup=True,
+            formstep__form_definition__login_required=True,
+        )
         form_path = reverse("core:form-detail", kwargs={"slug": form.slug})
-        form_url = f"https://testserver{form_path}"
-
         return_url = reverse(
             "authentication:return",
             kwargs={"slug": form.slug, "plugin_id": "eherkenning"},
         )
-        return_url_with_param = f"https://testserver{return_url}?next={form_url}"
+        return_url_with_param = furl(f"https://testserver{return_url}").set(
+            {"next": f"https://testserver{form_path}"}
+        )
 
         url = furl(reverse("eherkenning:acs")).set(
             {
-                "SAMLart": self._create_test_artifact(
-                    EHERKENNING["service_entity_id"]
-                ).decode("ascii"),
-                "RelayState": return_url_with_param,
+                "SAMLart": _create_test_artifact(),
+                "RelayState": str(return_url_with_param),
             }
         )
 
         response = self.client.get(url, follow=True)
 
         self.assertRedirects(
-            response,
-            form_url,
-            status_code=302,
+            response, f"https://testserver{form_path}", status_code=302
         )
         self.assertIn(FORM_AUTH_SESSION_KEY, self.client.session)
-        self.assertEqual(
-            AuthAttribute.kvk, self.client.session[FORM_AUTH_SESSION_KEY]["attribute"]
-        )
-        self.assertEqual(kvk, self.client.session[FORM_AUTH_SESSION_KEY]["value"])
+        session_data = self.client.session[FORM_AUTH_SESSION_KEY]
+        self.assertEqual(session_data["attribute"], AuthAttribute.kvk)
+        self.assertEqual(session_data["value"], "123456782")
 
     @patch(
         "onelogin.saml2.xml_utils.OneLogin_Saml2_XML.validate_xml", return_value=True
@@ -292,35 +266,15 @@ class AuthenticationStep5Tests(TestCase):
         mock_id,
         mock_xml_validation,
     ):
-        with open(
-            os.path.join(
-                settings.DJANGO_PROJECT_DIR,
-                "authentication",
-                "contrib",
-                "eherkenning",
-                "tests",
-                "data",
-                "ArtifactResponseCancelLogin.xml",
-            ),
-            "r",
-        ) as f:
-            artifact_response_cancel_login_template = f.read()
-
-        artifact_response_soap = (
-            Template(artifact_response_cancel_login_template)
-            .render(Context({}))
-            .encode("utf-8")
-        )
-
         m.post(
             "https://test-iwelcome.nl/broker/ars/1.13",
-            content=artifact_response_soap,
+            content=_get_artifact_response("ArtifactResponseCancelLogin.xml"),
         )
-
-        form = FormFactory.create(authentication_backends=["eherkenning"])
-        form_definition = FormDefinitionFactory.create(login_required=True)
-        FormStepFactory.create(form_definition=form_definition, form=form)
-
+        form = FormFactory.create(
+            authentication_backends=["eherkenning"],
+            generate_minimal_setup=True,
+            formstep__form_definition__login_required=True,
+        )
         form_path = reverse("core:form-detail", kwargs={"slug": form.slug})
         form_url = furl(f"http://testserver{form_path}")
         form_url.args["_start"] = "1"
@@ -335,9 +289,7 @@ class AuthenticationStep5Tests(TestCase):
 
         url = furl(reverse("eherkenning:acs")).set(
             {
-                "SAMLart": self._create_test_artifact(
-                    EHERKENNING["service_entity_id"]
-                ).decode("ascii"),
+                "SAMLart": _create_test_artifact(),
                 "RelayState": success_return_url.url,
             }
         )
@@ -345,7 +297,6 @@ class AuthenticationStep5Tests(TestCase):
         response = self.client.get(url, follow=True)
 
         form_url.args["_eherkenning-message"] = "login-cancelled"
-
         self.assertEquals(
             response.redirect_chain[-1],
             (form_url.url, 302),
