5f4229eb4c51bb259981febbd8447580493b1eeb
Sergei Maertens <sergei@maykinmedia.nl>
2021-12-21 15:39:05 +0100

    :sparkles: [#326] -- load Camunda form definitions from API endpoint
    ... and group by key for logical display.

--- src/openforms/js/components/admin/form_design/registrations/camunda.js
+++ src/openforms/js/components/admin/form_design/registrations/camunda.js
@@ -1,17 +1,57 @@
+import groupBy from 'lodash/groupBy';
 import React from 'react';
 import PropTypes from 'prop-types';
 import {FormattedMessage} from 'react-intl';
+import useAsync from 'react-use/esm/useAsync';
 
+import {get} from '../../../../utils/fetch';
 import Field from '../../forms/Field';
 import {CustomFieldTemplate} from '../../RJSFWrapper';
+import Loader from '../../Loader';
+
+
+const PROCESS_DEFINITIONS_ENDPOINT = '/api/v1/registration/plugins/camunda/process-definitions';
+
+
+const useLoadProcessDefinitions = () => {
+    let formDefinitions;
+    const { loading, value, error } = useAsync(
+        async () => {
+            const response = await get(PROCESS_DEFINITIONS_ENDPOINT);
+            if (!response.ok) throw new Error(`Response status: ${response.status}`);
+            return response.data;
+        }, [PROCESS_DEFINITIONS_ENDPOINT]
+    );
+
+    if (!loading && !error) {
+        // transform the process definitions in a grouped structure
+        formDefinitions = groupBy(value, 'key');
+    }
+
+    return {loading, formDefinitions, error};
+};
+
 
 
 const CamundaOptionsForm = ({ name, label, formData, onChange }) => {
     // TODO: handle validation errors properly
 
+    const {loading, formDefinitions, error} = useLoadProcessDefinitions();
+    if (error) {
+        console.error(error);
+        return 'Unexpected error, see console';
+    }
+
+    console.log(formDefinitions);
+
     return (
         <Field name={name} label={label} errors={[]}>
-            <div>yep</div>
+            {loading
+                ? <Loader />
+                : (
+                    <div>yep</div>
+                )
+            }
         </Field>
     );
 };
