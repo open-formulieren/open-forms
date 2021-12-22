861d11c8ebcd88225108df202e832f13e7f5621b
Sergei Maertens <sergei@maykinmedia.nl>
2021-12-22 16:36:08 +0100

    :sparkles: [#326] -- implement starting a process instance
    * Starts an instance based on the provided definition/version
    * Currently some static variables are used (testing against demo Camunda
      instance)
    * Error handling for HTTP 400 and HTTP 500 responses. Camunda throws
      500's happily if it can't execute because of missing variables,
      for example. This is something that needs to be fixed by the
      for designer to align variable injection with the process def.
    * Store the process instance ID + RESTful API URL as registration
      result data.

--- src/openforms/registrations/contrib/camunda/plugin.py
+++ src/openforms/registrations/contrib/camunda/plugin.py
@@ -1,10 +1,15 @@
-from typing import Dict, List, NoReturn, Tuple
+import logging
+from typing import Any, Dict, List, NoReturn, Optional, Tuple
 
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
-from django_camunda.client import get_client
+import requests
+from django_camunda.api import get_process_definitions
 from django_camunda.models import CamundaConfig
+from django_camunda.tasks import start_process
+from django_camunda.types import ProcessVariables
+from django_camunda.utils import serialize_variable
 from rest_framework import serializers
 
 from openforms.submissions.models import Submission
@@ -15,14 +20,21 @@ from ...exceptions import NoSubmissionReference, RegistrationFailed
 from ...registry import register
 from .checks import check_config
 
+logger = logging.getLogger(__name__)
+
+
+# temporary to debug the demo process instances from Camunda
+VARS = {
+    "amount": 10,
+    "invoiceCategory": "Misc",
+}
+
 
 class CamundaOptionsSerializer(JsonSchemaSerializerMixin, serializers.Serializer):
-    # TODO: get choices from django-camunda
     process_definition = serializers.CharField(
         required=True,
         help_text=_("The process definition for which to start a process instance."),
     )
-    # TODO: get choices from django-camunda
     process_definition_version = serializers.IntegerField(
         required=False,
         help_text=_(
@@ -35,6 +47,12 @@ class CamundaOptionsSerializer(JsonSchemaSerializerMixin, serializers.Serializer
     # to be done in react alltogether
 
 
+def serialize_variables(variables: Optional[Dict[str, Any]]) -> ProcessVariables:
+    if variables is None:
+        return {}
+    return {key: serialize_variable(value) for key, value in variables.items()}
+
+
 @register("camunda")
 class CamundaRegistration(BasePlugin):
     verbose_name = _("Camunda")
@@ -43,9 +61,66 @@ class CamundaRegistration(BasePlugin):
     def register_submission(
         self, submission: Submission, options: dict
     ) -> Dict[str, str]:
-        camunda = get_client()
-        # TODO: either use shared task or implement call ourself with the bare client
-        raise NotImplementedError()
+        process_definition = options["process_definition"]
+        version = options["process_definition_version"]
+
+        process_options = {
+            "process_key": process_definition,
+            "variables": serialize_variables(VARS),
+        }
+
+        # if we have a specific version, we need to get the actual process id
+        if version is not None:
+            process_definitions = get_process_definitions()
+            needle = next(
+                (
+                    definition
+                    for definition in process_definitions
+                    if definition.key == process_definition
+                    and definition.version == version
+                ),
+                None,
+            )
+            if needle is None:
+                raise RegistrationFailed(
+                    "Could not start instance for process definition with key "
+                    f"'{process_definition}' and version '{version}'."
+                )
+            process_options["process_id"] = needle.id
+            del process_options["process_key"]
+
+        # celery task, but we call it synchronously since this codepath already runs
+        # outside of a request-response cycle (celery or management command)
+        try:
+            meta_information = start_process(**process_options)
+        except requests.RequestException as exc:
+            if response := exc.response:
+                camunda_response = response.json()
+
+                if response.status_code >= 500:
+                    logger.exception(
+                        "Camunda error on process start: %r", camunda_response
+                    )
+                    raise RegistrationFailed(
+                        f"Failed starting the process instance: {camunda_response}"
+                    ) from exc
+
+                elif response.status_code >= 400:
+                    logger.error(
+                        "Invalid request made to Camunda to start a process instance: %r",
+                        camunda_response,
+                    )
+                    raise RegistrationFailed(
+                        f"Failed starting the process instance: {camunda_response}"
+                    ) from exc
+            raise
+
+        return {
+            "instance": {
+                "id": meta_information["instance_id"],
+                "url": meta_information["instance_url"],
+            }
+        }
 
     def get_reference_from_result(self, result: Dict[str, str]) -> NoReturn:
         """
