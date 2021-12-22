700ee6a3af5d09eb21500616b1fdbdf15eae9a9f
Joeri Bekker <joeri@maykinmedia.nl>
2021-12-22 18:34:52 +0100

    :goal_net: [#1109] -- Catch server errors early.

--- src/openforms/appointments/contrib/qmatic/client.py
+++ src/openforms/appointments/contrib/qmatic/client.py
@@ -3,6 +3,10 @@ from requests import Session
 from .models import QmaticConfig
 
 
+class QmaticException(Exception):
+    pass
+
+
 class QmaticClient(Session):
     """
     Lightweight wrapper around Session to work with the API root and auth
@@ -16,7 +20,20 @@ class QmaticClient(Session):
             self._config = QmaticConfig.get_solo()
 
         api_root = self._config.service.api_root
-        headers = self._config.service.get_auth_header(api_root)
+        headers = {
+            "Content-Type": "application/json",
+        }
+        headers.update(self._config.service.get_auth_header(api_root))
 
         url = f"{api_root}{url}"
-        return super().request(method, url, headers=headers, *args, **kwargs)
+        response = super().request(method, url, headers=headers, *args, **kwargs)
+
+        if response.status_code == 500:
+            error_msg = response.headers.get(
+                "error_message", response.content.decode("utf-8")
+            )
+            raise QmaticException(
+                f"Server error (HTTP {response.status_code}): {error_msg}"
+            )
+
+        return response
