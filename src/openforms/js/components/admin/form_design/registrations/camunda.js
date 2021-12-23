bd6f55bd2aba8a401719b964b92c332a8a282fe9
Sergei Maertens <sergei@maykinmedia.nl>
2021-12-23 11:03:05 +0100

    :sparkles: [#326] -- implement modal to manage process variables
    This is the simple variant.

--- src/openforms/js/components/admin/form_design/registrations/camunda.js
+++ src/openforms/js/components/admin/form_design/registrations/camunda.js
@@ -1,15 +1,17 @@
 import groupBy from 'lodash/groupBy';
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import {useIntl} from 'react-intl';
+import {FormattedMessage, useIntl} from 'react-intl';
 import useAsync from 'react-use/esm/useAsync';
 
 import {get} from '../../../../utils/fetch';
 import {PROCESS_DEFINITIONS_ENDPOINT} from '../constants';
+import ActionButton from '../../forms/ActionButton';
 import Field from '../../forms/Field';
 import Select from '../../forms/Select';
-import {CustomFieldTemplate} from '../../RJSFWrapper';
+import FormModal from '../../FormModal';
 import Loader from '../../Loader';
+import {CustomFieldTemplate} from '../../RJSFWrapper';
 
 
 const useLoadProcessDefinitions = () => {
@@ -44,8 +46,10 @@ const Wrapper = ({children}) => (
 
 
 const FormFields = ({processDefinitions, formData, onChange}) => {
-    const intl = useIntl();
     const { processDefinition='', processDefinitionVersion=null } = formData;
+
+    const intl = useIntl();
+    const [modalOpen, setModalOpen] = useState(true);
 
     const processDefinitionChoices = Object.entries(processDefinitions).map(([processKey, versions]) => {
         // grab the first version name - it is theoretically possible the process name has changed in
@@ -87,52 +91,74 @@ const FormFields = ({processDefinitions, formData, onChange}) => {
     };
 
     return (
-        <Wrapper>
-            <CustomFieldTemplate
-                id="camundaOptions.processDefinition"
-                label={intl.formatMessage({
-                    defaultMessage: 'Process',
-                    description: 'Camunda \'process definition\' label'
-                })}
-                rawErrors={null} errors={null} // TODO
-                rawDescription={intl.formatMessage({
-                    description: 'Camunda \'process definition\' help text',
-                    defaultMessage: 'The process definition for which to start a process instance.'
-                })}
-                required
-                displayLabel
+        <>
+            <Wrapper>
+                <CustomFieldTemplate
+                    id="camundaOptions.processDefinition"
+                    label={intl.formatMessage({
+                        defaultMessage: 'Process',
+                        description: 'Camunda \'process definition\' label'
+                    })}
+                    rawErrors={null} errors={null} // TODO
+                    rawDescription={intl.formatMessage({
+                        description: 'Camunda \'process definition\' help text',
+                        defaultMessage: 'The process definition for which to start a process instance.'
+                    })}
+                    required
+                    displayLabel
+                >
+                    <Select
+                        name="processDefinition"
+                        choices={processDefinitionChoices}
+                        value={processDefinition}
+                        onChange={onFieldChange}
+                        allowBlank
+                    />
+                </CustomFieldTemplate>
+                <CustomFieldTemplate
+                    id="camundaOptions.processDefinitionVersion"
+                    label={intl.formatMessage({
+                        defaultMessage: 'Version',
+                        description: 'Camunda \'process definition version\' label'
+                    })}
+                    rawErrors={null} errors={null} // TODO
+                    rawDescription={intl.formatMessage({
+                        description: 'Camunda \'process definition version\' help text',
+                        defaultMessage: 'Which version of the process definition to start. The latest version is used if not specified.'
+                    })}
+                    required={false}
+                    displayLabel
+                >
+                    <Select
+                        name="processDefinitionVersion"
+                        choices={versionChoices}
+                        value={processDefinitionVersion ? `${processDefinitionVersion}` : ''}
+                        onChange={onFieldChange}
+                        allowBlank
+                    />
+                </CustomFieldTemplate>
+
+                <CustomFieldTemplate id="camundaOptions.manageProcessVars" displayLabel={false} rawErrors={null}>
+                    <ActionButton
+                        text={intl.formatMessage({
+                            description: 'Open manage camnda process vars modal button',
+                            defaultMessage: 'Manage process variables'
+                        })}
+                        type="button"
+                        onClick={() => setModalOpen(!modalOpen)}
+                    />
+                </CustomFieldTemplate>
+
+            </Wrapper>
+
+            <FormModal
+                isOpen={modalOpen}
+                title={<FormattedMessage description="Camunda process var selection modal title" defaultMessage="Manage process variables" />}
+                closeModal={() => setModalOpen(false)}
             >
-                <Select
-                    name="processDefinition"
-                    choices={processDefinitionChoices}
-                    value={processDefinition}
-                    onChange={onFieldChange}
-                    allowBlank
-                />
-            </CustomFieldTemplate>
-            <CustomFieldTemplate
-                id="camundaOptions.processDefinitionVersion"
-                label={intl.formatMessage({
-                    defaultMessage: 'Version',
-                    description: 'Camunda \'process definition version\' label'
-                })}
-                rawErrors={null} errors={null} // TODO
-                rawDescription={intl.formatMessage({
-                    description: 'Camunda \'process definition version\' help text',
-                    defaultMessage: 'Which version of the process definition to start. The latest version is used if not specified.'
-                })}
-                required={false}
-                displayLabel
-            >
-                <Select
-                    name="processDefinitionVersion"
-                    choices={versionChoices}
-                    value={processDefinitionVersion ? `${processDefinitionVersion}` : ''}
-                    onChange={onFieldChange}
-                    allowBlank
-                />
-            </CustomFieldTemplate>
-        </Wrapper>
+                yolo
+            </FormModal>
+        </>
     );
 };
 
