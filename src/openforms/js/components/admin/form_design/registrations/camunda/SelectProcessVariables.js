9343c18e887b778c307df114e75e683730cc5543
Sergei Maertens <sergei@maykinmedia.nl>
2021-12-23 12:13:24 +0100

    :lipstick: [#326] -- Set up tabular layout for process variable management

--- src/openforms/js/components/admin/form_design/registrations/camunda/SelectProcessVariables.js
+++ src/openforms/js/components/admin/form_design/registrations/camunda/SelectProcessVariables.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {FormattedMessage, useIntl} from 'react-intl';
 
 import {ChangelistTableWrapper, HeadColumn} from '../../../tables';
 
@@ -26,9 +27,52 @@ const ALL_COMPONENTS = [
 ];
 
 
-const SelectProcessVariables = ({ onChange }) => {
+const HeadColumns = () => {
+    const intl = useIntl();
+
+    const enabledText = (<FormattedMessage
+        description="Manage process vars enabled checkbox column title"
+        defaultMessage="Enabled"
+    />);
+    const enabledHelp = intl.formatMessage({
+        description: 'Manage process vars enabled checkbox help text',
+        defaultMessage: 'Check to include the field as process variable',
+    });
+
+    const componentText = (<FormattedMessage
+        description="Manage process vars component column title"
+        defaultMessage="Field"
+    />);
+    const componentHelp = intl.formatMessage({
+        description: 'Manage process vars component help text',
+        defaultMessage: 'The value of the selected field will be the process variable value.',
+    });
+
+    const aliasText = (<FormattedMessage
+        description="Manage process vars alias column title"
+        defaultMessage="Alias"
+    />);
+    const aliasHelp = intl.formatMessage({
+        description: 'Manage process vars alias help text',
+        defaultMessage: 'If desired, you can specify a different process variable name.',
+    });
+
     return (
-        null
+        <>
+            <HeadColumn content={enabledText} title={enabledHelp} />
+            <HeadColumn content={componentText} title={componentHelp} />
+            <HeadColumn content={aliasText} title={aliasHelp} />
+        </>
+    );
+};
+
+
+const SelectProcessVariables = ({ onChange }) => {
+
+    return (
+        <ChangelistTableWrapper headColumns={<HeadColumns />}>
+
+        </ChangelistTableWrapper>
     );
 };
 
