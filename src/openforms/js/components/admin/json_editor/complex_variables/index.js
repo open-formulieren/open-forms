a698c82f142e554eadd478e86a22c63210ac11d5
Sergei Maertens <sergei@maykinmedia.nl>
2021-12-30 16:39:27 +0100

    :sparkles: [#326] -- define complex variable polymorphic component
    This component essentially routes between ArrayVariable and ObjectVariable.

---
+++ src/openforms/js/components/admin/json_editor/complex_variables/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Types, {jsonComplex as COMPLEX_TYPES} from '../types';
+import ArrayVariable from './ArrayVariable';
+import ObjectVariable from './ObjectVariable';
+
+
+const TYPE_MAP = {
+    array: ArrayVariable,
+    object: ObjectVariable,
+};
+
+
+const ComplexVariable = ({ type, definition=null, onChange, onEditDefinition }) => {
+    const Component = TYPE_MAP[type];
+    if (!Component) {
+        throw new Error(`Component type '${type}' is not supported`);
+    }
+    return (
+        <Component definition={definition} onChange={onChange} onEditDefinition={onEditDefinition} />
+    );
+};
+
+ComplexVariable.propTypes = {
+    type: PropTypes.oneOf(COMPLEX_TYPES).isRequired,
+    definition: Types.VariableDefinition,
+    onChange: PropTypes.func.isRequired,
+    onEditDefinition: PropTypes.func.isRequired,
+};
+
+export default ComplexVariable;
