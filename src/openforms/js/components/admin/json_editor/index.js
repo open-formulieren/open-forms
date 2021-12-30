607c760fe36d16bff05a72376d9888640588d3b7
Sergei Maertens <sergei@maykinmedia.nl>
2021-12-30 12:48:44 +0100

    :sparkles: [#326] -- set up json-editor component package

---
+++ src/openforms/js/components/admin/json_editor/index.js
@@ -0,0 +1,8 @@
+/**
+ * A user-friendly JSON-editor.
+ *
+ * This editor allows definining an arbitrary JSON schema (object or array) AND set
+ * values. If you only want to describe schema's, look into json-schema instead, and if
+ * you have a pre-defined schema for which you want to render a form, look into
+ * react-json-schema-form.
+ */
