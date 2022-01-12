65c7e6bfc14407c8067f2d04b45dbf35ef16fe67
Bart van der Schoor <bart.vanderschoor@maykinmedia.nl>
2022-01-12 11:02:25 +0100

    [#877] Added data migration to move file/image component properties into 'of' namespace

--- src/openforms/forms/tests/test_data_migrations.py
+++ src/openforms/forms/tests/test_data_migrations.py
@@ -49,3 +49,77 @@ class ReplaceWidgetDataMigrationTests(TestMigrations):
                 "components"
             ][0]["widget"],
         )
+
+
+class MoveOfImageDataMigrationTests(TestMigrations):
+    migrate_from = "0013_auto_20211220_1755"
+    migrate_to = "0014_file_component"
+    app = "forms"
+
+    def setUpBeforeMigration(self, apps):
+        FormDefinition = apps.get_model("forms", "FormDefinition")
+        self.form_definition_uuid = FormDefinition.objects.create(
+            name="Test image replace",
+            slug="test-image-replace",
+            configuration={
+                "components": [
+                    {
+                        "type": "file",
+                        "image": {"resize": {"width": 1000, "height": 1000}},
+                    },
+                    {
+                        "type": "fieldset",
+                        "components": [
+                            {
+                                "type": "file",
+                                "image": {"resize": {"width": 1000, "height": 1000}},
+                            }
+                        ],
+                    },
+                    {
+                        "type": "columns",
+                        "columns": [
+                            {
+                                "components": [
+                                    {
+                                        "type": "file",
+                                        "image": {
+                                            "resize": {"width": 1000, "height": 1000}
+                                        },
+                                    }
+                                ]
+                            }
+                        ],
+                    },
+                ]
+            },
+        ).uuid
+
+    def test_image_recursively_replaced(self):
+        FormDefinition = self.apps.get_model("forms", "FormDefinition")
+        update_form_definition = FormDefinition.objects.get(
+            uuid=self.form_definition_uuid
+        )
+
+        elem = update_form_definition.configuration["components"][0]
+        self.assertEqual(
+            elem["of"],
+            {"image": {"resize": {"width": 1000, "height": 1000}}},
+        )
+        self.assertEqual(elem["image"], False)
+
+        elem = update_form_definition.configuration["components"][1]["components"][0]
+        self.assertEqual(
+            elem["of"],
+            {"image": {"resize": {"width": 1000, "height": 1000}}},
+        )
+        self.assertEqual(elem["image"], False)
+
+        elem = update_form_definition.configuration["components"][2]["columns"][0][
+            "components"
+        ][0]
+        self.assertEqual(
+            elem["of"],
+            {"image": {"resize": {"width": 1000, "height": 1000}}},
+        )
+        self.assertEqual(elem["image"], False)
