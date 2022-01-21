5be10f9ff373b148c07e5120f40b36262ca06008
silvia <silvia@maykinmedia.nl>
2022-01-21 09:54:07 +0100

    :fire: [#1227] Remove multiple option for selectboxes

--- src/openforms/js/components/form/selectBoxes.js
+++ src/openforms/js/components/form/selectBoxes.js
@@ -1,10 +1,21 @@
 import {Formio} from 'formiojs';
-import {DEFAULT_CHOICES_TABS} from "./edit/tabs";
+
+import {DEFAULT_CHOICES_TABS, CHOICES_BASIC} from './edit/tabs';
 
 
 class SelectBoxesField extends Formio.Components.components.selectboxes {
     static editForm() {
-        return {components: [DEFAULT_CHOICES_TABS]};
+        const choicesBasic = {
+            key: 'basic',
+            label: 'Basic',
+            components: [
+                ...CHOICES_BASIC.components.filter(component => component.key !== 'multiple'),
+            ]
+        };
+        let defaultChoicesTabs = {...DEFAULT_CHOICES_TABS};
+        defaultChoicesTabs.components[0] = choicesBasic;
+
+        return {components: [defaultChoicesTabs]};
     }
 }
 
