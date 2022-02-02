e58fddd8d32f47c55cdfddd8a49bf4c97bfdd444
Bart van der Schoor <bart.vanderschoor@maykinmedia.nl>
2022-02-02 12:28:01 +0100

    [#1273] Readonly Yubin logs

--- src/openforms/emails/admin.py
+++ src/openforms/emails/admin.py
@@ -3,6 +3,9 @@ from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import FormView
 
+from django_yubin.admin import Log as YubinLogAdmin
+from django_yubin.models import Log as YubinLog
+
 from ..utils.mixins import UserIsStaffMixin
 from .connection_check import check_email_backend
 from .forms import ConfirmationEmailTemplateForm, EmailTestForm
@@ -33,3 +36,25 @@ class EmailTestAdminView(UserIsStaffMixin, PermissionRequiredMixin, FormView):
         result = check_email_backend([recipient])
         context = self.get_context_data(form=form, result=result)
         return self.render_to_response(context)
+
+
+class LogAdmin(YubinLogAdmin):
+    readonly_fields = [
+        "message",
+        "result",
+        "date",
+        "log_message",
+    ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+admin.site.unregister(YubinLog)
+admin.site.register(YubinLog, LogAdmin)
