515fb9204281f580c122707059ffd4676031e9e9
Bart van der Schoor <bart.vanderschoor@maykinmedia.nl>
2022-02-02 12:53:00 +0100

    [#1273] Readonly cookie-consent logs

--- src/openforms/utils/admin.py
+++ src/openforms/utils/admin.py
@@ -1,5 +1,9 @@
+from django.conf import settings
+from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 
+from cookie_consent.admin import LogItemAdmin as CookieLogAdmin
+from cookie_consent.models import LogItem as CookieLog
 from djchoices import ChoiceItem, DjangoChoices
 
 
@@ -7,3 +11,20 @@ class SubmitActions(DjangoChoices):
     save = ChoiceItem("_save", _("Save"))
     add_another = ChoiceItem("_addanother", _("Save and add another"))
     edit_again = ChoiceItem("_continue", _("Save and continue editing"))
+
+
+# defaults to True
+if getattr(settings, "COOKIE_CONSENT_LOG_ENABLED", True):
+
+    class LogAdmin(CookieLogAdmin):
+        def has_add_permission(self, request):
+            return False
+
+        def has_delete_permission(self, request, obj=None):
+            return False
+
+        def has_change_permission(self, request, obj=None):
+            return False
+
+    admin.site.unregister(CookieLog)
+    admin.site.register(CookieLog, LogAdmin)
