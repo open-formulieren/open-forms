de94c8b2e67b225aeb2169d060647a60d8fed142
Bart van der Schoor <bart.vanderschoor@maykinmedia.nl>
2022-02-04 12:35:02 +0100

    [#1273] Readonly csp-reports

--- src/openforms/utils/admin.py
+++ src/openforms/utils/admin.py
@@ -4,6 +4,8 @@ from django.utils.translation import gettext_lazy as _
 
 from cookie_consent.admin import LogItemAdmin as CookieLogAdmin
 from cookie_consent.models import LogItem as CookieLog
+from cspreports.admin import CSPReportAdmin
+from cspreports.models import CSPReport
 from djchoices import ChoiceItem, DjangoChoices
 
 
@@ -29,3 +31,18 @@ def replace_cookie_log_admin():
 
         admin.site.unregister(CookieLog)
         admin.site.register(CookieLog, LogAdmin)
+
+
+class CSPReportOverrideAdmin(CSPReportAdmin):
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+admin.site.unregister(CSPReport)
+admin.site.register(CSPReport, CSPReportOverrideAdmin)
