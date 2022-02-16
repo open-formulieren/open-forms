941cb5eca8c359b523b7d39b66eaca24c81c72ed
Sergei Maertens <sergei@maykinmedia.nl>
2022-02-16 12:53:32 +0100

    :art: Imports use single quotes

--- src/openforms/js/components/admin/form_design/LogicWarning.js
+++ src/openforms/js/components/admin/form_design/LogicWarning.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
 import PropTypes from 'prop-types';
-import MessageList from "./warnings/MessageList";
+import MessageList from './warnings/MessageList';
 
 
 
