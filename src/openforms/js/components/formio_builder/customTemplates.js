e0fe5e2680f0e59668fd1cba605e9cf5aa0d419d
Silvia Amabilino <silvia@maykinmedia.nl>
2022-02-25 16:20:38 +0100

    :sparkles: [#1378] Override Component template in builder

--- src/openforms/js/components/formio_builder/customTemplates.js
+++ src/openforms/js/components/formio_builder/customTemplates.js
@@ -2,12 +2,14 @@ import builderSidebar from './templates/builderSidebar';
 import coSign from './templates/coSign';
 import fieldTemplate from './templates/field.ejs';
 import columnsTemplate from './templates/columns.ejs';
+import componentTemplate from './templates/component.ejs';
 
 const TEMPLATES = {
     builderSidebar: {form: builderSidebar},
     coSign: {form: coSign},
     field: {form: fieldTemplate},
     columns: {form: columnsTemplate},
+    component: {form: componentTemplate},
 };
 
 export default TEMPLATES;
