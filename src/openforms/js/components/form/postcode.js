406f516cf8b30cd3d61d4499a739bd7e8d8c06f9
SilviaAmAm <silvia@maykinmedia.nl>
2022-03-15 08:48:09 +0100

    :speech_balloon: [#1076] Added comment about postcode

--- src/openforms/js/components/form/postcode.js
+++ src/openforms/js/components/form/postcode.js
@@ -11,8 +11,11 @@ class PostcodeField extends TextField {
             inputMask: '9999 AA',
             validateOn: 'blur',
             validate: {
-              customMessage: 'Invalid Postcode',
-              pattern: '^[1-9][0-9]{3} ?(?!sa|sd|ss|SA|SD|SS)[a-zA-Z]{2}$',
+                customMessage: 'Invalid Postcode',
+                // Dutch postcode has 4 numbers and 2 letters (case insensitive). Letter combinations SS, SD and SA
+                // are not used due to the Nazi-association.
+                // See https://stackoverflow.com/a/17898538/7146757 and https://nl.wikipedia.org/wiki/Postcodes_in_Nederland
+                pattern: '^[1-9][0-9]{3} ?(?!sa|sd|ss|SA|SD|SS)[a-zA-Z]{2}$',
             }
         }, ...extend);
     }
