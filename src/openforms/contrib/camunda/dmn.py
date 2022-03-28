852d7dfb9533ed3777ecd8acdbafcd5b8bcfb830
Sergei Maertens <sergei@maykinmedia.nl>
2022-03-28 16:12:45 +0200

    :alembic: [#1469] -- implement evaluating the DMN table

--- src/openforms/contrib/camunda/dmn.py
+++ src/openforms/contrib/camunda/dmn.py
@@ -1,9 +1,12 @@
+import logging
 from typing import Any, Dict
 
 from django_camunda.client import get_client
 from django_camunda.utils import deserialize_variable
 
 from .utils import serialize_variables
+
+logger = logging.getLogger(__name__)
 
 
 def evaluate_dmn(
@@ -17,10 +20,22 @@ def evaluate_dmn(
 
     The return value is the output deserialized to Python types.
     """
+    logger.debug(
+        "Evaluating DMN table with key %s and dmn_id %s",
+        dmn_key,
+        dmn_id,
+    )
     client = get_client()
     serialized = serialize_variables(input_values)
 
-    result = ...
-    raise NotImplementedError("implement API call")
+    result = client.post(
+        f"decision-definition/key/{dmn_key}/evaluate",
+        json={"variables": serialized},
+    )
 
-    return {var: deserialize_variable(value) for var, value in result.items()}
+    output_variables = {}
+    for output in result:
+        for key, value in output.items():
+            output_variables[key] = deserialize_variable(value)
+
+    return output_variables
