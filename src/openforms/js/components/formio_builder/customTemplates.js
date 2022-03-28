52c279f1d977fd54a734d81905a6d1ca900de0a8
Sergei Maertens <sergei@maykinmedia.nl>
2022-03-28 13:38:11 +0200

    :alembic: [#1469] -- display DMN evaluation component in builder
    Set up a preview of the component in the builder to display the configuration.

--- src/openforms/js/components/formio_builder/customTemplates.js
+++ src/openforms/js/components/formio_builder/customTemplates.js
@@ -4,6 +4,7 @@ import fieldTemplate from './templates/field.ejs';
 import columnsTemplate from './templates/columns.ejs';
 import componentTemplate from './templates/component.ejs';
 import fieldsetTemplate from './templates/fieldset.ejs';
+import dmnTemplate from './templates/dmn.ejs';
 
 const TEMPLATES = {
     builderSidebar: {form: builderSidebar},
@@ -11,7 +12,8 @@ const TEMPLATES = {
     field: {form: fieldTemplate},
     columns: {form: columnsTemplate},
     component: {form: componentTemplate},
-    fieldset: {form: fieldsetTemplate}
+    fieldset: {form: fieldsetTemplate},
+    dmn: {form: dmnTemplate},
 };
 
 export default TEMPLATES;
