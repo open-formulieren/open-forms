3c67eb77bb8e2a2ecc6a569f3d2a0b99d650d9e2
Sergei Maertens <sergei@maykinmedia.nl>
2022-03-28 16:12:45 +0200

    :alembic: [#1469] -- implement evaluating the DMN table

--- src/openforms/custom_field_types/dmn.py
+++ src/openforms/custom_field_types/dmn.py
@@ -1,5 +1,8 @@
+from django.template import Context, Template
+
 from rest_framework.request import Request
 
+from openforms.contrib.camunda.dmn import evaluate_dmn
 from openforms.formio.typing import Component
 from openforms.forms.custom_field_types import register
 from openforms.submissions.models import Submission
@@ -11,5 +14,28 @@ def evaluate_camunda_dmn(
     request: Request,
     submission: Submission,
 ) -> Component:
-    raise NotImplementedError("TODO")
+    # TODO: possibly cache (functools.lrucache?) based on component ID & input variables
+    # to avoid repeated calls to the Camunda API?W
+
+    # TODO: we might want to parse the DMN XML to figure out which input data keys are needed,
+    # or use an explicit mapping of component -> input to avoid sending potentially
+    # sensitive data into the Camunda API.
+    # TODO: right now, the component key must match the inputData key in the DMN model,
+    # it should be possible to apply a mapping for this.
+    input_values = submission.data
+    result = evaluate_dmn(component["decisionTableKey"], input_values=input_values)
+
+    # TODO: how to make sure the result is also stored on the submission?
+
+    context_data = {
+        "result": result,
+        "submission_data": submission.data,
+    }
+    output = Template(component["resultDisplayTemplate"]).render(Context(context_data))
+    component["resultDisplay"] = output
+
+    # set the default value to communicate the evaluation result, the SDK can/should pick
+    # this up.
+    component["defaultValue"] = result
+
     return component
