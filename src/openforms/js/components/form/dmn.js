e0fa2dea150e9eae1754f98738863465b77ea6a2
Sergei Maertens <sergei@maykinmedia.nl>
2022-03-28 14:44:21 +0200

    :alembic: [#1469] -- set up component key for result display in frontend

--- src/openforms/js/components/form/dmn.js
+++ src/openforms/js/components/form/dmn.js
@@ -21,6 +21,7 @@ export default class DMNEvaluation extends Field {
             input: false,
             decisionTableKey: '',
             resultDisplayTemplate: '',
+            resultDisplay: '',
         }, ...extend);
     }
 
