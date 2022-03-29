32b7d18a8591e9b0e4ac939bb878564626be086e
Steven Bal <steven@maykinmedia.nl>
2022-03-29 10:56:52 +0200

    :truck: Move some generic OIDC functionality to mozilla-django-oidc-db

--- src/digid_eherkenning_oidc_generics/backends.py
+++ src/digid_eherkenning_oidc_generics/backends.py
@@ -7,19 +7,15 @@ from mozilla_django_oidc_db.backends import (
     OIDCAuthenticationBackend as _OIDCAuthenticationBackend,
 )
 
-from .utils import obfuscate_claim
-
 logger = logging.getLogger(__name__)
 
 
 class OIDCAuthenticationBackend(_OIDCAuthenticationBackend):
     session_key = ""
-    claim_name_field = "identifier_claim_name"
+    config_identifier_field = "identifier_claim_name"
 
     def extract_claims(self, payload):
-        self.request.session[self.session_key] = payload[
-            self.get_settings(self.claim_name_field)
-        ]
+        self.request.session[self.session_key] = self.retrieve_identifier_claim(payload)
 
     def get_or_create_user(self, access_token, id_token, payload):
         user_info = self.get_userinfo(access_token, id_token, payload)
@@ -33,20 +29,3 @@ class OIDCAuthenticationBackend(_OIDCAuthenticationBackend):
         user = AnonymousUser()
         user.is_active = True
         return user
-
-    def verify_claims(self, claims):
-        """Verify the provided claims to decide if authentication should be allowed."""
-        # TODO configurable sensitive claims?
-        obfuscated_claims = {
-            k: obfuscate_claim(v) if k == self.config.identifier_claim_name else v
-            for k, v in claims.items()
-        }
-        logger.debug("OIDC claims received: %s", obfuscated_claims)
-
-        if (claim_name := self.get_settings(self.claim_name_field)) not in claims:
-            logger.error(
-                "`identifier_claim_name=%s` not in OIDC claims, cannot proceed with authentication",
-                claim_name,
-            )
-            return False
-        return True
