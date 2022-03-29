e7b2221a96d05a66b994a9dc15bbd56f9d751914
Sergei Maertens <sergei@maykinmedia.nl>
2022-03-29 16:41:36 +0200

    :alembic: [#1469] -- disable DMN viewer again

--- src/openforms/js/components/form/dmn/dmn.js
+++ src/openforms/js/components/form/dmn/dmn.js
@@ -1,8 +1,7 @@
 import {Formio} from 'react-formio';
-// import DmnJS from 'dmn-js/lib/Modeler';  // https://www.npmjs.com/package/dmn-js
-import DmnJS from 'dmn-js';  // https://www.npmjs.com/package/dmn-js
+// import DmnJS from 'dmn-js';  // https://www.npmjs.com/package/dmn-js
 
-import {get} from '../../../utils/fetch';
+// import {get} from '../../../utils/fetch';
 import editForm from './editForm';
 
 const Field = Formio.Components.components.field;
@@ -64,29 +63,29 @@ export default class DMNEvaluation extends Field {
             dmnViewer: 'single',
         });
 
-        // get the XML DMN definition (if available) and render it in the viewer
-        get(
-            `/api/v1/dmn/decision-definitions/${this.component.dmn.decisionDefinition.id}/xml`,
-            {
-                engine: this.component.dmn.engine.id,
-                version: this.component.dmn.decisionDefinitionVersion.id,
-            }
-        ).then(response => {
-            if (!response.ok) return;
-            if (!this.refs.dmnViewer) return;
+        // // get the XML DMN definition (if available) and render it in the viewer
+        // get(
+        //     `/api/v1/dmn/decision-definitions/${this.component.dmn.decisionDefinition.id}/xml`,
+        //     {
+        //         engine: this.component.dmn.engine.id,
+        //         version: this.component.dmn.decisionDefinitionVersion.id,
+        //     }
+        // ).then(response => {
+        //     if (!response.ok) return;
+        //     if (!this.refs.dmnViewer) return;
 
-            const xml = response.data.xml;
-            if (!xml) return;
+        //     const xml = response.data.xml;
+        //     if (!xml) return;
 
-            if (!this.viewer) this.viewer = new DmnJS();
-            return this.viewer.importXML(xml);
-        }).then(() => {
-            if (!this.viewer) return;
+        //     if (!this.viewer) this.viewer = new DmnJS();
+        //     return this.viewer.importXML(xml);
+        // }).then(() => {
+        //     if (!this.viewer) return;
 
-            this.viewer.attachTo(this.refs.dmnViewer);
-            const view = this.viewer.getViews().find(view => view.type === 'decisionTable');
-            this.viewer.open(view);
-        });
+        //     this.viewer.attachTo(this.refs.dmnViewer);
+        //     const view = this.viewer.getViews().find(view => view.type === 'decisionTable');
+        //     this.viewer.open(view);
+        // });
 
         return super.attach(element);
     }
