8b17f3b50f92a67c0b5c5769785ce4064f4f6067
Sergei Maertens <sergei@maykinmedia.nl>
2022-03-31 12:39:50 +0200

    :bookmark: Bump version to 1.0.5 and update changelog

--- src/openforms/__init__.py
+++ src/openforms/__init__.py
@@ -1,6 +1,6 @@
 from .celery import app as celery_app
 
 __all__ = ("celery_app",)
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 __author__ = "Maykin Media"
 __homepage__ = "https://github.com/open-formulieren/open-forms"
