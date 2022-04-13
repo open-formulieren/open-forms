ea817c4cbc8b8e29b776c9622414a45f5b0259b7
SilviaAmAm <silvia@maykinmedia.nl>
2022-04-13 16:58:57 +0200

    :white_check_mark: [#1472] Test state property

--- src/openforms/forms/tests/test_api_form_logic.py
+++ src/openforms/forms/tests/test_api_form_logic.py
@@ -921,6 +921,52 @@ class FormLogicAPITests(APITestCase):
         )
         self.assertEqual("blank", response.json()["invalidParams"][0]["code"])
 
+    def test_cant_have_empty_state_in_property_action(self):
+        user = SuperUserFactory.create()
+        self.client.force_authenticate(user=user)
+        form = FormFactory.create(
+            generate_minimal_setup=True,
+            formstep__form_definition__configuration={
+                "components": [
+                    {
+                        "type": "textfield",
+                        "key": "text1",
+                    },
+                    {
+                        "type": "textfield",
+                        "key": "text2",
+                    },
+                ]
+            },
+        )
+
+        form_url = reverse("api:form-detail", kwargs={"uuid_or_slug": form.uuid})
+        form_logic_data = {
+            "form": f"http://testserver{form_url}",
+            "json_logic_trigger": {"==": [{"var": "text1"}, {"var": "text2"}]},
+            "actions": [
+                {
+                    "component": "text1",
+                    "action": {
+                        "name": "Hide element",
+                        "type": "property",
+                        "property": {"value": "hidden", "type": "bool"},
+                        "state": "",  # Empty state!
+                    },
+                }
+            ],
+            "is_advanced": False,
+        }
+        url = reverse("api:form-logics-list")
+
+        response = self.client.post(url, data=form_logic_data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            "actions.0.action.state", response.json()["invalidParams"][0]["name"]
+        )
+        self.assertEqual("blank", response.json()["invalidParams"][0]["code"])
+
     def test_cant_have_empty_component_in_mark_step_as_not_applicable(self):
         user = SuperUserFactory.create()
         self.client.force_authenticate(user=user)
