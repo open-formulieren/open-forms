0fd93fb492f4192a743530411f672d5f9c7c22ee
SilviaAmAm <silvia@maykinmedia.nl>
2022-04-20 15:32:05 +0200

    :white_check_mark: [#1510] Update tests

--- src/openforms/forms/tests/admin/test_tasks.py
+++ src/openforms/forms/tests/admin/test_tasks.py
@@ -1,5 +1,8 @@
+import os
 import zipfile
+from pathlib import Path
 
+from django.conf import settings
 from django.core import mail
 from django.test import TestCase, override_settings
 from django.urls import reverse
@@ -11,8 +14,8 @@ from privates.test import temp_private_root
 from openforms.accounts.tests.factories import SuperUserFactory
 from openforms.utils.urls import build_absolute_uri
 
-from ...admin.tasks import process_forms_export
-from ...models.form import FormsExport
+from ...admin.tasks import process_forms_export, process_forms_import
+from ...models.form import Form, FormsExport
 from ..factories import FormFactory
 
 
@@ -60,3 +63,43 @@ class ExportFormsTaskTests(TestCase):
         self.assertIn(download_url, sent_mail.body)
         self.assertEqual("Forms export ready", sent_mail.subject)
         self.assertIn("test@email.nl", sent_mail.to)
+
+
+@temp_private_root()
+class ImportFormsTaskTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        form1, form2 = FormFactory.create_batch(2)
+        user = SuperUserFactory.create(email="test@email.nl")
+
+        process_forms_export(
+            forms_uuids=[form1.uuid, form2.uuid],
+            email="test@email.nl",
+            user_id=user.id,
+        )
+
+        cls.form_export = FormsExport.objects.get()
+
+    def _copy_file_to_imports_tempdir(self):
+        import_dir = Path(settings.PRIVATE_MEDIA_ROOT, "imports")
+        if not import_dir.exists():
+            os.mkdir(import_dir)
+
+        exported_zip_file = self.form_export.export_content
+        exported_zip_file.seek(0)
+        imported_zip_file = Path(import_dir, "tmp_import_file.zip")
+
+        with open(imported_zip_file, "wb") as f_private:
+            for chunk in exported_zip_file.chunks():
+                f_private.write(chunk)
+
+        return imported_zip_file
+
+    def test_import_forms(self):
+        imported_file_path = self._copy_file_to_imports_tempdir()
+        process_forms_import(str(imported_file_path))
+
+        self.assertEqual(4, Form.objects.count())
+
+        # Check that no files are left over
+        self.assertEqual(0, len(list(imported_file_path.parent.iterdir())))
--- src/openforms/forms/tests/admin/test_views.py
+++ src/openforms/forms/tests/admin/test_views.py
@@ -1,5 +1,7 @@
 import io
+import json
 from unittest.mock import patch
+from zipfile import ZipFile
 
 from django.core.files import File
 from django.test import TestCase, override_settings
@@ -13,6 +15,7 @@ from openforms.accounts.tests.factories import (
     SuperUserFactory,
     UserFactory,
 )
+from openforms.forms.admin.tasks import process_forms_export
 from openforms.forms.models.form import FormsExport
 from openforms.forms.tests.factories import FormFactory
 from openforms.utils.urls import build_absolute_uri
@@ -179,3 +182,123 @@ class TestDownloadExportFormView(TestCase):
         response = self.client.get(download_url)
 
         self.assertEqual(404, response.status_code)
+
+
+@temp_private_root()
+@override_settings(LANGUAGE_CODE="en")
+class TestImportView(WebTest):
+    def test_not_superuser_cant_access(self):
+        user = UserFactory(is_staff=False, is_superuser=False)
+        self.client.force_login(user)
+
+        response = self.client.get(reverse("admin:forms_import"))
+
+        self.assertEqual(302, response.status_code)
+
+    def test_no_form_add_permissions_cant_access(self):
+        user = StaffUserFactory()
+        self.client.force_login(user)
+
+        self.assertEqual(0, user.user_permissions.count())
+
+        response = self.client.get(reverse("admin:forms_import"))
+
+        self.assertEqual(403, response.status_code)
+
+    def test_form_add_permissions_can_access(self):
+        user = StaffUserFactory(user_permissions=["forms.add_form"])
+        self.client.force_login(user)
+
+        response = self.client.get(reverse("admin:forms_import"))
+
+        self.assertEqual(200, response.status_code)
+
+    @patch("openforms.forms.admin.views.process_forms_import.delay")
+    def test_bulk_import(self, m_bulk_import):
+        form1, form2 = FormFactory.create_batch(2)
+        user = SuperUserFactory.create(email="test@email.nl")
+
+        process_forms_export(
+            forms_uuids=[form1.uuid, form2.uuid],
+            email="test@email.nl",
+            user_id=user.id,
+        )
+
+        form_export = FormsExport.objects.get()
+
+        response = self.app.get(reverse("admin:forms_import"), user=user)
+
+        self.assertEqual(200, response.status_code)
+
+        html_form = response.form
+        html_form["file"] = (
+            "file.zip",
+            form_export.export_content.read(),
+        )
+        submission_response = html_form.submit("_import")
+
+        self.assertEqual(302, submission_response.status_code)
+        m_bulk_import.assert_called()
+
+        submission_response = submission_response.follow()
+        messages = list(submission_response.context.get("messages"))
+
+        self.assertEqual(1, len(messages))
+        self.assertEqual(
+            "The bulk import is being processed! The imported forms will soon be available.",
+            messages[0].message,
+        )
+
+    @patch("openforms.forms.admin.views.ImportFormsView._import_single_form")
+    def test_single_import(self, m_single_import):
+        file = io.BytesIO()
+        with ZipFile(file, mode="w") as zf:
+            with zf.open("forms.json", "w") as f:
+                f.write(
+                    json.dumps(
+                        [
+                            {
+                                "uuid": "b8315e1d-3134-476f-8786-7661d8237c51",
+                                "name": "Form 000",
+                                "internal_name": "Form internal",
+                                "slug": "bed",
+                                "product": None,
+                                "authentication_backends": ["digid"],
+                            }
+                        ]
+                    ).encode("utf-8")
+                )
+
+            with zf.open("formSteps.json", "w") as f:
+                f.write(b"[]")
+
+            with zf.open("formDefinitions.json", "w") as f:
+                f.write(b"[]")
+
+            with zf.open("formLogic.json", "w") as f:
+                f.write(b"[]")
+
+        user = SuperUserFactory.create()
+        response = self.app.get(reverse("admin:forms_import"), user=user)
+
+        self.assertEqual(200, response.status_code)
+
+        html_form = response.form
+        file.seek(0)
+        html_form["file"] = (
+            "file.zip",
+            file.read(),
+        )
+        submission_response = html_form.submit("_import")
+
+        self.assertEqual(302, submission_response.status_code)
+        m_single_import.assert_called()
+
+        submission_response = submission_response.follow()
+        messages = list(submission_response.context.get("messages"))
+
+        self.assertEqual(1, len(messages))
+        self.assertEqual(
+            "Form successfully imported!",
+            messages[0].message,
+        )
