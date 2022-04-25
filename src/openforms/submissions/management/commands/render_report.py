2b8a92301e73771b5d3093fb6d17b13da022ce30
Sergei Maertens <sergei@maykinmedia.nl>
2022-04-25 16:02:00 +0200

    :alembic: Set up management command scaffolding for POC

---
+++ src/openforms/submissions/management/commands/render_report.py
@@ -0,0 +1,34 @@
+from django.conf import settings
+from django.core.management import BaseCommand, CommandError
+
+from ...models import Submission
+
+
+class Command(BaseCommand):
+    help = (
+        "Display the submission data in the terminal. This is a proof of concept of a "
+        "generic rendering API capable of outputting to different modes."
+    )
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "submission_id",
+            type=int,
+            help="Submission ID to display data from.",
+        )
+        parser.add_argument(
+            "-l",
+            "--limit-value-key",
+            nargs="+",
+            help=(
+                "If provided, limits the output data keys to the specified set. Can be "
+                "specified multiple times."
+            ),
+        )
+
+    def handle(self, **options):
+        if not settings.DEBUG:
+            raise CommandError("This command is only allowed in dev environments")
+
+        submission = Submission.objects.get(pk=options["submission_id"])
+        limit_value_keys = options["limit_value_key"]
