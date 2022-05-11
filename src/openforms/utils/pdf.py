c7ec6bd4d7292dccdda94924cae22bd5a176aa11
Sergei Maertens <sergei@maykinmedia.nl>
2022-05-11 16:03:51 +0200

    :sparkles: Fixes #1444 -- resolve media files locally too with WeasyPrint

--- src/openforms/utils/pdf.py
+++ src/openforms/utils/pdf.py
@@ -2,13 +2,13 @@ import logging
 import mimetypes
 from io import BytesIO
 from pathlib import PurePosixPath
-from typing import Tuple
-from urllib.parse import urljoin, urlparse
+from typing import Optional, Tuple
+from urllib.parse import ParseResult, urljoin, urlparse
 
 from django.conf import settings
 from django.contrib.staticfiles import finders
 from django.contrib.staticfiles.storage import staticfiles_storage
-from django.core.files.storage import FileSystemStorage
+from django.core.files.storage import FileSystemStorage, default_storage
 from django.template.loader import render_to_string
 
 import weasyprint
@@ -23,32 +23,41 @@ class UrlFetcher:
     """
 
     def __init__(self):
-        static_url = settings.STATIC_URL
-        if not urlparse(static_url).netloc:
-            static_url = urljoin(settings.BASE_URL, settings.STATIC_URL)
-        self.static_url = urlparse(static_url)
-        self.local_storage = issubclass(
+        self.static_url = self._get_fully_qualified_url(settings.STATIC_URL)
+        is_static_local_storage = issubclass(
             staticfiles_storage.__class__, FileSystemStorage
         )
 
+        self.media_url = self._get_fully_qualified_url(settings.MEDIA_URL)
+        is_media_local_storage = issubclass(
+            default_storage.__class__, FileSystemStorage
+        )
+
+        self.candidates = (
+            (self.static_url, staticfiles_storage, is_static_local_storage),
+            (self.media_url, default_storage, is_media_local_storage),
+        )
+
+    @staticmethod
+    def _get_fully_qualified_url(setting: str):
+        fully_qualified_url = setting
+        if not urlparse(setting).netloc:
+            fully_qualified_url = urljoin(settings.BASE_URL, setting)
+        return urlparse(fully_qualified_url)
+
     def __call__(self, url: str) -> dict:
         orig_url = url
-        url = urlparse(url)
-        same_base = (self.static_url.scheme, self.static_url.netloc) == (
-            url.scheme,
-            url.netloc,
-        )
-        if (
-            self.local_storage
-            and same_base
-            and url.path.startswith(self.static_url.path)
-        ):
-            path = PurePosixPath(url.path).relative_to(self.static_url.path)
+        parsed_url = urlparse(url)
+
+        candidate = self.get_match_candidate(parsed_url)
+        if candidate is not None:
+            base_url, storage = candidate
+            path = PurePosixPath(parsed_url.path).relative_to(base_url.path)
 
             absolute_path = None
-            if staticfiles_storage.exists(path):
-                absolute_path = staticfiles_storage.path(path)
-            elif settings.DEBUG:
+            if storage.exists(path):
+                absolute_path = storage.path(path)
+            elif settings.DEBUG and storage is staticfiles_storage:
                 # use finders so that it works in dev too, we already check that it's
                 # using filesystem storage earlier
                 absolute_path = finders.find(str(path))
@@ -68,6 +77,23 @@ class UrlFetcher:
                 result["file_obj"] = BytesIO(f.read())
             return result
         return weasyprint.default_url_fetcher(orig_url)
+
+    def get_match_candidate(
+        self, url: ParseResult
+    ) -> Optional[Tuple[ParseResult, FileSystemStorage]]:
+        for parsed_base_url, storage, is_local_storage in self.candidates:
+            if not is_local_storage:
+                continue
+            same_base = (parsed_base_url.scheme, parsed_base_url.netloc) == (
+                url.scheme,
+                url.netloc,
+            )
+            if not same_base:
+                continue
+            if not url.path.startswith(parsed_base_url.path):
+                continue
+            return (parsed_base_url, storage)
+        return None
 
 
 def render_to_pdf(template_name: str, context: dict) -> Tuple[str, bytes]:
