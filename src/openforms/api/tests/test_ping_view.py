81c4595c6faa2f1f57d20fd6b148b17113cc0f63
Sergei Maertens <sergei@maykinmedia.nl>
2022-05-12 16:04:42 +0200

    :sparkles: [#1308] -- set up ping view
    Endpoint access is granted to anonymous clients filling out submissions
    or staff users (who are logged in).

---
+++ src/openforms/api/tests/test_ping_view.py
@@ -0,0 +1,41 @@
+from rest_framework import status
+from rest_framework.reverse import reverse_lazy
+from rest_framework.test import APITestCase
+
+from openforms.accounts.tests.factories import StaffUserFactory, UserFactory
+from openforms.submissions.models import submission
+from openforms.submissions.tests.factories import SubmissionFactory
+from openforms.submissions.tests.mixins import SubmissionsMixin
+
+
+class PingViewTests(SubmissionsMixin, APITestCase):
+    endpoint = reverse_lazy("api:ping")
+
+    def test_not_authenticated(self):
+        response = self.client.get(self.endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_not_staff_user(self):
+        user = UserFactory.create()
+        self.client.force_authenticate(user=user)
+
+        response = self.client.get(self.endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_staff_user(self):
+        user = StaffUserFactory.create()
+        self.client.force_authenticate(user=user)
+
+        response = self.client.get(self.endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_anon_user_with_submission(self):
+        submission = SubmissionFactory.create()
+        self._add_submission_to_session(submission)
+
+        response = self.client.get(self.endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
