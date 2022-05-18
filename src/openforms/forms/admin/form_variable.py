904f97a67e57da1fbd03d8f6f1a4e24fa7fdb935
SilviaAmAm <silvia@maykinmedia.nl>
2022-05-18 12:28:28 +0200

    :construction: [#1325] Updated admin change view

--- src/openforms/forms/admin/form_variable.py
+++ src/openforms/forms/admin/form_variable.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from openforms.forms.models.form_variable import FormVariable
+from ..constants import FormVariableSources
+from ..models.form_variable import FormVariable
 
 
 @admin.register(FormVariable)
@@ -8,3 +9,38 @@ class FormVariableAdmin(admin.ModelAdmin):
     list_display = ("name", "key", "form", "source")
     list_filter = ("source", "data_type", "is_sensitive_data", "form")
     search_fields = ("name", "form__slug", "form__uuid")
+    raw_id_fields = ("form_definition",)
+
+    readonly_fields = (
+        "form",
+        "name",
+        "key",
+        "source",
+        "prefill_plugin",
+        "prefill_attribute",
+        "data_type",
+        "is_sensitive_data",
+        "initial_value",
+    )
+    fields = (
+        "form",
+        "form_definition",
+        "name",
+        "key",
+        "source",
+        "prefill_plugin",
+        "prefill_attribute",
+        "data_type",
+        "data_format",
+        "is_sensitive_data",
+        "initial_value",
+    )
+
+    def has_delete_permission(self, request, obj=None):
+        can_delete = super().has_delete_permission(request, obj)
+
+        if can_delete and obj:
+            if obj.source != FormVariableSources.user_defined:
+                return False
+
+        return can_delete
