44596896f678c1a577d037d356cc925c148d0c5b
Sergei Maertens <sergei@maykinmedia.nl>
2022-06-08 11:56:50 +0200

    :bug: CVE-2022-31040 -- Fixes open redirect in cookie-consent 'close' button
    The referer value is now only emitted if it passes the allowed-hosts
    configuration or the CORS policy.

--- src/openforms/utils/redirect.py
+++ src/openforms/utils/redirect.py
@@ -1,5 +1,8 @@
 from urllib.parse import urlparse, urlunparse
 
+from django.conf import settings
+from django.utils.http import url_has_allowed_host_and_scheme
+
 from corsheaders.conf import conf as cors_conf
 from corsheaders.middleware import CorsMiddleware
 
@@ -18,6 +21,20 @@ def allow_redirect_url(url: str) -> bool:
     are allowed to access Open Forms. We leverage this configuration to block or allow
     redirects to external hosts.
     """
+    # first, check if the URL is in ALLOWED_HOSTS. We deliberately exclude the wildcard
+    # setting to require explicit configuration either via ALLOWED_HOSTS or CORS_* settings.
+    allowed_hosts_check = url_has_allowed_host_and_scheme(
+        url=url,
+        allowed_hosts=[host for host in settings.ALLOWED_HOSTS if host != "*"],
+        # settings.ALLOWED_HOSTS means we are serving the domain, so we can enforce our
+        # own custom HTTPS setting.
+        require_https=settings.IS_HTTPS,
+    )
+    # if we pass via ALLOWED_HOSTS, short-circuit, otherwise we check the CORS policy
+    # for allowed external domains.
+    if allowed_hosts_check:
+        return True
+
     cors = CorsMiddleware()
     origin = origin_from_url(url)
     parts = urlparse(url)
