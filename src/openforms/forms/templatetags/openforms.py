b54ec38b1844649d31e6ca312e607f2ff21157b1
Sergei Maertens <sergei@maykinmedia.nl>
2022-06-08 11:56:50 +0200

    :bug: CVE-2022-31040 -- Fixes open redirect in cookie-consent 'close' button
    The referer value is now only emitted if it passes the allowed-hosts
    configuration or the CORS policy.

--- src/openforms/forms/templatetags/openforms.py
+++ src/openforms/forms/templatetags/openforms.py
@@ -4,6 +4,7 @@ from django.template.defaultfilters import stringfilter
 from rest_framework.reverse import reverse
 
 from openforms.config.models import GlobalConfiguration
+from openforms.utils.redirect import allow_redirect_url
 
 from ..context_processors import sdk_urls
 
@@ -30,3 +31,20 @@ def sdk_info_banner():
         "enabled": config.display_sdk_information,
         **sdk_urls(request=None),
     }
+
+
+@register.simple_tag
+def get_allowed_redirect_url(*candidates: str) -> str:
+    """
+    Output the first variable passed that is not empty and is an allowed redirect URL.
+
+    Output nothing if none of the values satisfy the requirements.
+
+    Heavily insired on the builtin {% firstof %} tag.
+    """
+    for candidate in candidates:
+        if not candidate:
+            continue
+        if allow_redirect_url(candidate):
+            return candidate
+    return ""
