f2acee093cd7cf7f9c15e7024c26319b2b1dd33b
SilviaAmAm <silvia@maykinmedia.nl>
2022-06-09 16:19:31 +0200

    :sparkles: [#1325] Management command to check number of forms with duplicate component keys

---
+++ src/openforms/forms/management/commands/check_duplicate_component_keys.py
@@ -0,0 +1,56 @@
+from typing import List
+
+from django.core.management import BaseCommand
+
+from tabulate import tabulate
+
+from openforms.formio.utils import iter_components
+
+from ...models.form import Form
+
+
+class Command(BaseCommand):
+    help = "Check if there are forms with components with duplicate keys."
+
+    def handle(self, *args, **options):
+        forms = Form.objects.all()
+
+        duplicates = []
+        for form in forms:
+            duplicate_keys = self.get_duplicate_keys(form)
+            if duplicate_keys:
+                duplicates += duplicate_keys
+
+        if not duplicates:
+            self.stdout.write(
+                self.style.SUCCESS(
+                    "No forms with duplicate keys in the form definitions found."
+                )
+            )
+            return
+
+        self.stdout.write(self.style.ERROR("\nFound the following duplicates:\n"))
+
+        table = tabulate(duplicates, headers=["Form", "Form Step", "Component key"])
+        for line in table.splitlines():
+            self.stdout.write(line)
+
+    def get_duplicate_keys(self, form: "Form") -> List[List[str]]:
+        form_steps = form.formstep_set.select_related("form_definition")
+
+        duplicates = []
+
+        existing_keys = []
+        for form_step in form_steps:
+            form_definition_configuration = form_step.form_definition.configuration
+            for component in iter_components(
+                configuration=form_definition_configuration, recursive=True
+            ):
+                if component["key"] in existing_keys:
+                    duplicates.append(
+                        [form.name, form_step.form_definition.name, component["key"]]
+                    )
+                else:
+                    existing_keys.append(component["key"])
+
+        return duplicates
