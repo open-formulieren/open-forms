17e28b7e4f03eb0ed28ea1600c49225ff82b6dd1
SilviaAmAm <silvia@maykinmedia.nl>
2022-06-21 11:30:13 +0200

    :fire: Remove wrong renderer

--- src/openforms/variables/views.py
+++ src/openforms/variables/views.py
@@ -2,7 +2,6 @@ from django.utils.translation import gettext_lazy as _
 
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import authentication, permissions
-from rest_framework.renderers import JSONRenderer
 from rest_framework.views import APIView
 
 from openforms.api.serializers import ExceptionSerializer
@@ -25,7 +24,6 @@ class StaticFormVariablesView(ListMixin, APIView):
     authentication_classes = (authentication.SessionAuthentication,)
     permission_classes = (permissions.IsAdminUser,)
     serializer_class = FormVariableSerializer
-    renderer_classes = [JSONRenderer]
 
     def get_objects(self):
         return FormVariable.get_default_static_variables()
