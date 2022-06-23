6615a28a4594e4e27ebb3259d8f5cd9979b46729
Sergei Maertens <sergei@maykinmedia.nl>
2022-06-23 12:52:38 +0200

    :truck: [#1546] -- Move FormLogic model into it's own module

--- src/openforms/forms/models/form.py
+++ src/openforms/forms/models/form.py
@@ -427,30 +427,6 @@ class Form(models.Model):
         return self.internal_name or self.name
 
 
-class FormLogic(models.Model):
-    uuid = models.UUIDField(_("UUID"), unique=True, default=_uuid.uuid4)
-    form = models.ForeignKey(
-        to="forms.Form",
-        on_delete=models.CASCADE,
-        help_text=_("Form to which the JSON logic applies."),
-    )
-    json_logic_trigger = models.JSONField(
-        verbose_name=_("JSON logic"),
-        help_text=_("JSON logic associated with a step in a form."),
-    )
-    actions = models.JSONField(
-        verbose_name=_("actions"),
-        help_text=_("Which action(s) to perform if the JSON logic evaluates to true."),
-    )
-    is_advanced = models.BooleanField(
-        verbose_name=_("is advanced"),
-        help_text=_(
-            "Is this an advanced rule (the admin user manually wrote the trigger as JSON)?"
-        ),
-        default=False,
-    )
-
-
 class FormsExportQuerySet(DeleteFilesQuerySetMixin, models.QuerySet):
     pass
 
---
+++ src/openforms/forms/models/logic.py
@@ -0,0 +1,28 @@
+import uuid as _uuid
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class FormLogic(models.Model):
+    uuid = models.UUIDField(_("UUID"), unique=True, default=_uuid.uuid4)
+    form = models.ForeignKey(
+        to="forms.Form",
+        on_delete=models.CASCADE,
+        help_text=_("Form to which the JSON logic applies."),
+    )
+    json_logic_trigger = models.JSONField(
+        verbose_name=_("JSON logic"),
+        help_text=_("JSON logic associated with a step in a form."),
+    )
+    actions = models.JSONField(
+        verbose_name=_("actions"),
+        help_text=_("Which action(s) to perform if the JSON logic evaluates to true."),
+    )
+    is_advanced = models.BooleanField(
+        verbose_name=_("is advanced"),
+        help_text=_(
+            "Is this an advanced rule (the admin user manually wrote the trigger as JSON)?"
+        ),
+        default=False,
+    )
