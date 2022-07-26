cabdfd0ea77459f71400e4b33aafefbf29445c22
Ewen <ewen@maykinmedia.nl>
2022-07-26 15:15:40 +0200

    :recycle: [#1752] -- refactor filtering logic process

--- src/openforms/js/components/admin/submissions/filter.js
+++ src/openforms/js/components/admin/submissions/filter.js
@@ -1,44 +1,48 @@
-const init = async () => {
+function init() {
   // Retrieve the filter link that displays only triggered rules
   const triggeredLink = document.querySelector('.logic-rules-filter > .selected > a');
 
-  // Only triggered rules are displayed
-  document
-    .querySelectorAll('.logic-logs__col-body--trigger-' + triggeredLink.dataset.trigger)
-    .forEach(trigger => {
-      trigger.parentNode.classList.remove('logic-logs__row-body--hidden');
-    });
-};
-
-document.querySelectorAll('.logic-rules-filter__link').forEach(link => {
-  link.addEventListener('click', event => {
-    event.preventDefault();
-
-    // Hiding all the rows first
-    document.querySelectorAll('.logic-logs__row-body').forEach(row => {
-      row.classList.add('logic-logs__row-body--hidden');
-    });
-
-    const filterValue = event.target.dataset.trigger;
-
-    // Change the focus of the selected filter
-    document.querySelector('.selected').classList.remove('selected');
-    event.target.parentNode.classList.add('selected');
-
-    // Showing only rules corresponding to the selected trigger link
-    if (filterValue == 'all') {
+  document.querySelectorAll('.logic-rules-filter__link').forEach(link => {
+    link.addEventListener('click', event => {
+      // Hiding all the rows first
       document.querySelectorAll('.logic-logs__row-body').forEach(row => {
-        row.classList.remove('logic-logs__row-body--hidden');
+        row.classList.add('logic-logs__row-body--hidden');
       });
-    } else {
-      document
-        .querySelectorAll('.logic-logs__col-body--trigger-' + filterValue)
-        .forEach(trigger => {
-          trigger.parentNode.classList.remove('logic-logs__row-body--hidden');
-        });
-    }
+
+      const filterValue = event.target.dataset.trigger;
+
+      // Change the focus of the selected filter
+      document.querySelector('.logic-rules-filter .selected').classList.remove('selected');
+      event.target.parentNode.classList.add('selected');
+
+      let rows;
+      switch (filterValue) {
+        case 'all': {
+          rows = document.querySelectorAll('.logic-logs__row-body');
+          break;
+        }
+        case 'yes': {
+          rows = [...document.querySelectorAll('.logic-logs__col-body--trigger-yes')].map(
+            row => row.parentNode
+          );
+          break;
+        }
+        case 'no': {
+          rows = [...document.querySelectorAll('.logic-logs__col-body--trigger-no')].map(
+            row => row.parentNode
+          );
+          break;
+        }
+      }
+      for (const row of rows) {
+        row.classList.remove('logic-logs__row-body--hidden');
+      }
+    });
   });
-});
+
+  // Simulates the click on the default selected link
+  triggeredLink.click();
+}
 
 if (document.readyState !== 'loading') {
   init();
