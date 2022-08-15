6739d969ee3d4430d1a9883f8bf78919dbf2bf63
SilviaAmAm <silvia@maykinmedia.nl>
2022-08-15 11:59:18 +0200

    :white_check_mark: [#1661] Test model validation admin

---
+++ src/openforms/authentication/tests/test_admin.py
@@ -0,0 +1,53 @@
+from django.test import override_settings
+from django.urls import reverse
+
+from django_webtest import WebTest
+
+from openforms.accounts.tests.factories import UserFactory
+
+from ..constants import AuthAttribute
+from .factories import AuthInfoFactory
+
+
+class AuthInfoAdminValidationTest(WebTest):
+    def setUp(self):
+        super().setUp()
+        self.user = UserFactory.create(is_superuser=True, is_staff=True, app=self.app)
+
+    @override_settings(LANGUAGE_CODE="en")
+    def test_validate_invalid_bsn(self):
+        auth_info_bsn = AuthInfoFactory.create()
+
+        response = self.app.get(
+            reverse(
+                "admin:of_authentication_authinfo_change", args=(auth_info_bsn.pk,)
+            ),
+            user=self.user,
+        )
+        form = response.forms["authinfo_form"]
+        form["value"] = "invalid-bsn"
+
+        form_response = form.submit()
+
+        self.assertEqual(200, form_response.status_code)
+        self.assertContains(form_response, "Expected a numerical value.", html=True)
+
+    @override_settings(LANGUAGE_CODE="nl")
+    def test_validate_invalid_kvk(self):
+        auth_info_kvk = AuthInfoFactory.create(attribute=AuthAttribute.kvk)
+
+        response = self.app.get(
+            reverse(
+                "admin:of_authentication_authinfo_change", args=(auth_info_kvk.pk,)
+            ),
+            user=self.user,
+        )
+        form = response.forms["authinfo_form"]
+        form["value"] = "123456789"
+
+        form_response = form.submit()
+
+        self.assertEqual(200, form_response.status_code)
+        self.assertContains(
+            form_response, "KvK-nummer moet 8 tekens lang zijn.", html=True
+        )
