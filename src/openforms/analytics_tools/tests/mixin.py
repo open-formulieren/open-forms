7846c527056a0fd7adeae79b8f5da80b6887dd42
Ewen <ewen@maykinmedia.nl>
2022-08-15 15:06:29 +0200

    :white_check_mark: [#1294] -- split tests of analytics tools in different files + hardcoded json files

---
+++ src/openforms/analytics_tools/tests/mixin.py
@@ -0,0 +1,18 @@
+from django.test import TestCase, override_settings
+
+from cookie_consent.models import CookieGroup
+
+from openforms.analytics_tools.models import AnalyticsToolsConfiguration
+
+
+@override_settings(
+    SOLO_CACHE=None,
+)
+class AnalyticsToolsMixinTestCase(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.config = AnalyticsToolsConfiguration.get_solo()
+        self.config.analytics_cookie_consent_group = CookieGroup.objects.get(
+            varname="analytical"
+        )
+        self.addCleanup(lambda: AnalyticsToolsConfiguration.get_solo().delete())
---
+++ src/openforms/analytics_tools/tests/test_piwik_pro.py
@@ -0,0 +1,86 @@
+import uuid
+
+from django.core.exceptions import ValidationError
+
+from cookie_consent.models import Cookie
+
+from openforms.config.models import CSPSetting
+
+from .mixin import AnalyticsToolsMixinTestCase
+
+
+class PiwikProTests(AnalyticsToolsMixinTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.piwik_pro_url = "https://example.com"
+        cls.piwik_pro_site_id = uuid.uuid4()
+
+        cls.json_cookies = [
+            {"name": f"_pk_id.{cls.piwik_pro_site_id}.e5c3", "path": "/"},
+            {"name": f"_pk_ses.{cls.piwik_pro_site_id}.e5c3", "path": "/"},
+        ]
+
+        cls.json_csp = [
+            {"directive": "script-src", "value": cls.piwik_pro_url},
+            {"directive": "connect-src", "value": cls.piwik_pro_url},
+            {"directive": "img-src", "value": cls.piwik_pro_url},
+            {"directive": "font-src", "value": cls.piwik_pro_url},
+            {"directive": "style-src", "value": cls.piwik_pro_url},
+        ]
+
+    def test_piwik_pro_properly_enabled(self):
+        self.config.piwik_pro_url = self.piwik_pro_url
+        self.config.piwik_pro_site_id = self.piwik_pro_site_id
+
+        self.config.enable_piwik_pro_site_analytics = True
+        self.config.clean()
+        self.config.save()
+
+        for cookie in self.json_cookies:
+            with self.subTest("Test creation of cookies"):
+                try:
+                    Cookie.objects.filter(name=cookie["name"])
+                except Cookie.DoesNotExist as e:
+                    self.fail(f"Unexpected exception : {e}")
+
+        for csp in self.json_csp:
+            with self.subTest("Test creation of CSP"):
+                try:
+                    CSPSetting.objects.get(
+                        value=csp["value"], directive=csp["directive"]
+                    )
+                except CSPSetting.DoesNotExist as e:
+                    self.fail(f"Unexpected exception : {e}")
+
+    def test_piwik_pro_properly_disabled(self):
+        self.config.piwik_pro_url = self.piwik_pro_url
+        self.config.piwik_pro_site_id = self.piwik_pro_site_id
+
+        # creation of cookies
+        self.config.enable_piwik_pro_site_analytics = True
+        self.config.clean()
+        self.config.save()
+
+        # Deletion of cookies
+        self.config.enable_piwik_pro_site_analytics = False
+        self.config.clean()
+        self.config.save()
+
+        for cookie in self.json_cookies:
+            with self.subTest("Test deletion of cookies"):
+                self.assertFalse(Cookie.objects.filter(name=cookie["name"]).exists())
+
+        for csp in self.json_csp:
+            with self.subTest("Test deletion of CSP"):
+                self.assertFalse(
+                    CSPSetting.objects.filter(
+                        value=csp["value"], directive=csp["directive"]
+                    ).exists()
+                )
+
+    def test_piwik_pro_enabled_but_related_fields_are_not(self):
+        self.config.enable_piwik_pro_site_analytics = True
+
+        with self.assertRaises(ValidationError):
+            self.config.clean()
