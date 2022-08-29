a229eadb4dd3b39dc7a93951e1a7764ce76314db
Sergei Maertens <sergei@maykinmedia.nl>
2022-08-29 17:27:57 +0200

    :recycle: Define public API in service file for variables

---
+++ src/openforms/variables/service.py
@@ -0,0 +1,31 @@
+"""
+Public Python API to access (static) form variables.
+"""
+from typing import List, Optional
+
+from openforms.forms.models import FormVariable
+from openforms.submissions.models import Submission
+
+from .registry import register_static_variable as register
+
+__all__ = ["get_static_variables"]
+
+
+def get_static_variables(
+    *, submission: Optional[Submission] = None
+) -> List[FormVariable]:
+    """
+    Return the full collection of static variables registered by all apps.
+
+    :param submission: Optional, but recommended - the submission instance to get the
+      variables for. Many of the static variables require the submission for sufficient
+      context to be able to produce a value. Variables are static within that submission
+      context, i.e. they don't change because of filling out the form.
+
+    You should not rely on the order of returned variables, as they are registered in
+    the order the Django apps are loaded - and this may change without notice.
+    """
+    return [
+        registered_variable.get_static_variable(submission=submission)
+        for registered_variable in register
+    ]
