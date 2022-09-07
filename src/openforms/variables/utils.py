96937ff6c03c5a37a6f92f342d01a36552393e29
SilviaAmAm <silvia@maykinmedia.nl>
2022-09-07 13:19:58 +0200

    :sparkles: [#1991] Check initial value matches type for user_defined variables

---
+++ src/openforms/variables/utils.py
@@ -0,0 +1,27 @@
+from datetime import date, datetime, time
+from typing import Any
+
+
+def check_date(value: str) -> str:
+    try:
+        date.fromisoformat(value)
+    except ValueError:
+        datetime.fromisoformat(value)
+    return value
+
+
+def check_time(value: str) -> str:
+    try:
+        time.fromisoformat(value)
+    except ValueError:
+        datetime.fromisoformat(value)
+    return value
+
+
+def check_initial_value(initial_value: Any, data_type: str) -> Any:
+    from .constants import CHECK_VARIABLE_TYPE, DEFAULT_INITIAL_VALUE
+
+    try:
+        return CHECK_VARIABLE_TYPE[data_type](initial_value)
+    except (ValueError, TypeError):
+        return DEFAULT_INITIAL_VALUE[data_type]
