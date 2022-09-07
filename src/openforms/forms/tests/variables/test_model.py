96a58b5e59de0d173a87856c6d8773ea78465e50
SilviaAmAm <silvia@maykinmedia.nl>
2022-09-07 13:19:22 +0200

    :white_check_mark: [#1991] Test user defined variable initial value

--- src/openforms/forms/tests/variables/test_model.py
+++ src/openforms/forms/tests/variables/test_model.py
@@ -95,6 +95,73 @@ class FormVariableModelTests(TestCase):
         self.assertEqual(FormVariableDataTypes.array, variable3.data_type)
 
     def test_variable_with_array_default_value(self):
-        variable = FormVariableFactory.create(initial_value=[])
+        variable = FormVariableFactory.create(
+            data_type=FormVariableDataTypes.array, initial_value=[]
+        )
 
         self.assertEqual([], variable.get_initial_value())
+
+    def test_user_defined_variables_cast_initial_value(self):
+        values_to_try = {
+            FormVariableDataTypes.string: ["", "Some value"],
+            FormVariableDataTypes.boolean: ["", False, True, None],
+            FormVariableDataTypes.object: ["", {}, {"some": "object"}],
+            FormVariableDataTypes.array: ["", "Test", [], ["another", "test"]],
+            FormVariableDataTypes.int: [None, "", 1.1, 1.0, 1],
+            FormVariableDataTypes.float: [None, "", 1.4],
+            FormVariableDataTypes.datetime: [
+                "",
+                "Invalid date",
+                "2022-09-05",
+                "2022-09-08T00:00:00+02:00",
+            ],
+            FormVariableDataTypes.time: [
+                "",
+                "invalid time",
+                "11:30",
+                "11:30:00",
+                "2022-09-08T11:30:00+02:00",
+            ],
+        }
+
+        expected_values = {
+            FormVariableDataTypes.string: ["", "Some value"],
+            FormVariableDataTypes.boolean: [False, False, True, False],
+            FormVariableDataTypes.object: [{}, {}, {"some": "object"}],
+            FormVariableDataTypes.array: [
+                [],
+                ["T", "e", "s", "t"],
+                [],
+                ["another", "test"],
+            ],
+            FormVariableDataTypes.int: [None, None, 1, 1, 1],
+            FormVariableDataTypes.float: [None, None, 1.4],
+            FormVariableDataTypes.datetime: [
+                "",
+                "",
+                "2022-09-05",
+                "2022-09-08T00:00:00+02:00",
+            ],
+            FormVariableDataTypes.time: [
+                "",
+                "",
+                "11:30",
+                "11:30:00",
+                "2022-09-08T11:30:00+02:00",
+            ],
+        }
+
+        for data_type, data_type_label in FormVariableDataTypes.choices:
+            for index, initial_value in enumerate(values_to_try[data_type]):
+                with self.subTest(
+                    f"Data type: {data_type_label}, initial value: {initial_value}"
+                ):
+                    variable = FormVariableFactory.create(
+                        user_defined=True,
+                        initial_value=initial_value,
+                        data_type=data_type,
+                    )
+
+                    self.assertEqual(
+                        expected_values[data_type][index], variable.initial_value
+                    )
