e3f190a1913ac011fab5f40c42dfe08c757d8a9e
SilviaAmAm <silvia@maykinmedia.nl>
2022-09-07 15:04:08 +0200

    :white_check_mark: [#1948] Test get_updated_step_data with unlinked submission var

--- src/openforms/submissions/tests/test_variables/test_model.py
+++ src/openforms/submissions/tests/test_variables/test_model.py
@@ -1,8 +1,15 @@
 from django.db import IntegrityError
 from django.test import TestCase
 
+from openforms.forms.tests.factories import FormFactory
+
+from ...logic.datastructures import DataContainer
 from ...models import SubmissionValueVariable
-from ..factories import SubmissionValueVariableFactory
+from ..factories import (
+    SubmissionFactory,
+    SubmissionStepFactory,
+    SubmissionValueVariableFactory,
+)
 
 
 class SubmissionValueVariableModelTests(TestCase):
@@ -25,3 +32,32 @@ class SubmissionValueVariableModelTests(TestCase):
     def test_can_create_instances(self):
         SubmissionValueVariableFactory.create()
         SubmissionValueVariableFactory.create()
+
+    def test_get_submission_step_data(self):
+        form = FormFactory.create(
+            generate_minimal_setup=True,
+            formstep__form_definition__configuration={
+                "components": [
+                    {"type": "textfield", "key": "test1"},
+                    {"type": "textfield", "key": "test2"},
+                ]
+            },
+        )
+        form_step = form.formstep_set.first()
+        submission = SubmissionFactory.create(form=form)
+
+        submission_step = SubmissionStepFactory.create(
+            submission=submission,
+            form_step=form_step,
+            data={"test1": "some data 1", "test2": "some data 1"},
+        )
+
+        form.formvariable_set.all().delete()
+
+        submission_variables_state = submission.load_submission_value_variables_state()
+
+        data_container = DataContainer(state=submission_variables_state)
+
+        data = data_container.get_updated_step_data(submission_step)
+
+        self.assertEqual(data, {"test1": "some data 1", "test2": "some data 1"})
