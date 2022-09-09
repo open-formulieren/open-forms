cf1a18a80818a5458aeb435ffd9172c42525f9d9
SilviaAmAm <silvia@maykinmedia.nl>
2022-09-09 14:02:32 +0200

    :lipstick: [#1995] Use right widget for variable type

--- src/openforms/js/utils/JsonPropTypeValidator.js
+++ src/openforms/js/utils/JsonPropTypeValidator.js
@@ -2,8 +2,11 @@ const jsonPropTypeValidator = (props, propName, componentName) => {
   const value = props[propName];
   if (value === '') return;
 
+  let jsonToValidate = value;
+  if (typeof value !== 'string') jsonToValidate = JSON.stringify(value);
+
   try {
-    JSON.parse(value);
+    JSON.parse(jsonToValidate);
   } catch (e) {
     return new Error(
       `Invalid JSON prop ${propName} supplied to ${componentName}. Validation failed.`
