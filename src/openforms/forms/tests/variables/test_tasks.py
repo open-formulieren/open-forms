5f1fe18e4d2ae1da7afb64643318598ea035ef85
Sergei Maertens <sergei@maykinmedia.nl>
2022-09-13 12:45:01 +0200

    :art: Improve race condition test determinism
    The test did not always trigger the race condition, much like the real
    race condition that we guard against.

--- src/openforms/forms/tests/variables/test_tasks.py
+++ src/openforms/forms/tests/variables/test_tasks.py
@@ -1,5 +1,8 @@
 import threading
+import time
+from unittest.mock import patch
 
+from django.db import close_old_connections
 from django.test import TestCase, TransactionTestCase, tag
 
 from openforms.forms.tasks import recouple_submission_variables_to_form_variables
@@ -59,19 +62,35 @@ class FormVariableTasksTest(TestCase):
         )
 
 
+class ThreadWithExceptionHandling(threading.Thread):
+    def run(self):
+        self.exc = None
+        try:
+            super().run()
+        except Exception as e:
+            self.exc = e
+
+    def join(self, *args, **kwargs):
+        super().join(*args, **kwargs)
+        if self.exc:
+            raise self.exc
+
+
 class FormVariableRaceConditionTasksTest(TransactionTestCase):
     @tag("gh-1970")
     def test_recouple_submission_variables_race_condition(self):
         """
         Race condition:
 
-        The task recouple_submission_variables_to_form_variables starts, it retrieves the form variables on a form and
-        tries to relate them to existing submission variables.
+        The task recouple_submission_variables_to_form_variables starts, it retrieves
+        the form variables on a form and tries to relate them to existing submission
+        variables.
 
-        While it's processing the submission vars, the form is saved again, triggering an update of form variables.
+        While it's processing the submission vars, the form is saved again, triggering
+        an update of form variables.
 
-        The task gives an integrity error when saving the submission variables because they are being related to
-        non-existing form variables.
+        The task gives an integrity error when saving the submission variables because
+        they are being related to non-existing form variables.
         """
         form = FormFactory.create(
             generate_minimal_setup=True,
@@ -96,32 +115,42 @@ class FormVariableRaceConditionTasksTest(TransactionTestCase):
         FormVariableFactory.create(key="test1", form=form)
         FormVariableFactory.create(key="test2", form=form)
 
-        class ThreadWithExceptionHandling(threading.Thread):
-            def run(self):
-                self.exc = None
-                try:
-                    super().run()
-                except Exception as e:
-                    self.exc = e
-
-            def join(self, *args, **kwargs):
-                super().join(*args, **kwargs)
-                if self.exc:
-                    raise self.exc
-
         def recouple_variables_task():
-            recouple_submission_variables_to_form_variables(form.id)
+            real_bulk_update = SubmissionValueVariable.objects.bulk_update
+
+            def wrapped_bulk_update(*args, **kwargs):
+                time.sleep(0.6)
+                # give some time for the delete to complete before we
+                # make a DB query
+                return real_bulk_update(*args, **kwargs)
+
+            with patch(
+                "openforms.submissions.models.submission_value_variable"
+                ".SubmissionValueVariableManager.bulk_update",
+                wraps=wrapped_bulk_update,
+            ) as mock_bulk_update:
+                try:
+                    recouple_submission_variables_to_form_variables(form.id)
+                finally:
+                    close_old_connections()
+            mock_bulk_update.assert_called_once()
 
         def race_condition():
-            form.formvariable_set.all().delete()
+            # ensure the delete runs at some point _after_ the
+            # recouple_variables_thread has started
+            time.sleep(0.5)
+            try:
+                form.formvariable_set.all().delete()
+            finally:
+                close_old_connections()
 
         recouple_variables_thread = ThreadWithExceptionHandling(
             target=recouple_variables_task
         )
         race_condition_thread = threading.Thread(target=race_condition)
 
-        recouple_variables_thread.start()
         race_condition_thread.start()
+        recouple_variables_thread.start()
 
         race_condition_thread.join()
         recouple_variables_thread.join()
