bf9b5f8bb7d4659a840e194512a24f9b13d87534
Sergei Maertens <sergei@maykinmedia.nl>
2022-09-19 14:20:26 +0200

    :boom: Removed logout endpoint
    Instead, use the submission-specific logout.

--- src/openforms/authentication/api/urls.py
+++ src/openforms/authentication/api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import AuthenticationLogoutView, PluginListView, SubmissionLogoutView
+from .views import PluginListView, SubmissionLogoutView
 
 urlpatterns = [
     path("plugins", PluginListView.as_view(), name="authentication-plugin-list"),
-    path("session", AuthenticationLogoutView.as_view(), name="logout"),
     path(
         "<uuid:uuid>/session", SubmissionLogoutView.as_view(), name="submission-logout"
     ),
