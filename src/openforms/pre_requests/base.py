138e148d58039ecd1882d08c1dfe1a4e24b159f0
SilviaAmAm <silvia@maykinmedia.nl>
2022-10-31 12:45:58 +0100

    :construction: [#2197] Add context to custom client

--- src/openforms/pre_requests/base.py
+++ src/openforms/pre_requests/base.py
@@ -1,5 +1,9 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from openforms.submissions.models import Submission
 
 
 @dataclass
@@ -7,5 +11,7 @@ class PreRequestHookBase(ABC):
     identifier: str
 
     @abstractmethod
-    def __call__(self, method: str, url: str, kwargs: dict) -> None:
+    def __call__(
+        self, submission: "Submission", method: str, url: str, kwargs: dict
+    ) -> None:
         raise NotImplementedError()  # pragma: nocover
