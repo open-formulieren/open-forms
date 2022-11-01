effb84f2750a67a1be9d1c1d88cdc4f173597fb8
Sergei Maertens <sergei@maykinmedia.nl>
2022-11-01 12:01:43 +0100

    :hammer: Extend TestMigrations test case
    * Support migrating to/from zero migration
    * Support overriding settings before running the migration

--- src/openforms/utils/tests/test_migrations.py
+++ src/openforms/utils/tests/test_migrations.py
@@ -1,6 +1,8 @@
 from django.db import connection
 from django.db.migrations.executor import MigrationExecutor
-from django.test import TestCase
+from django.test import TestCase, override_settings
+
+unset = object()
 
 
 class TestMigrations(TestCase):
@@ -11,28 +13,36 @@ class TestMigrations(TestCase):
     """
 
     app = None
-    migrate_from = None
-    migrate_to = None
+    migrate_from = unset
+    migrate_to = unset
+    setting_overrides = None
 
     def setUp(self):
-        assert self.migrate_from and self.migrate_to and self.app, (
+        _checks = (
+            self.migrate_from is not unset,
+            self.migrate_to is not unset,
+            self.app,
+        )
+        assert all(_checks), (
             "TestCase '%s' must define migrate_from, migrate_to and app properties"
             % type(self).__name__
         )
         self.migrate_from = [(self.app, self.migrate_from)]
         self.migrate_to = [(self.app, self.migrate_to)]
         executor = MigrationExecutor(connection)
-        old_apps = executor.loader.project_state(self.migrate_from).apps
 
         # Reverse to the original migration
-        executor.migrate(self.migrate_from)
+        old_migrate_state = executor.migrate(self.migrate_from)
+        old_apps = old_migrate_state.apps
 
         self.setUpBeforeMigration(old_apps)
 
         # Run the migration to test
-        executor = MigrationExecutor(connection)
-        executor.loader.build_graph()  # reload.
-        executor.migrate(self.migrate_to)
+        overrides = self.setting_overrides or {}
+        with override_settings(**overrides):
+            executor = MigrationExecutor(connection)
+            executor.loader.build_graph()  # reload.
+            executor.migrate(self.migrate_to)
 
         self.apps = executor.loader.project_state(self.migrate_to).apps
 
