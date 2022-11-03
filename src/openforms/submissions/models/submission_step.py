7d8860a17e32d5cca43ded2cbb17547af2215d91
Sergei Maertens <sergei@maykinmedia.nl>
2022-11-03 16:13:30 +0100

    :bug: [#2135] -- on formstep delete, keep submissionstep records
    And record the formstep data on the submissionstep so that we can
    fix the display from local data rather than related FK data.

--- src/openforms/submissions/models/submission_step.py
+++ src/openforms/submissions/models/submission_step.py
@@ -1,8 +1,82 @@
 import uuid
 from typing import Optional
 
+from django.core import serializers
+from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+from frozendict import frozendict
+
+from openforms.forms.models import FormStep
+
+
+def _make_frozen(obj):
+    if not isinstance(obj, (dict, list)):
+        return obj
+
+    elif isinstance(obj, list):
+        return tuple([_make_frozen(item) for item in obj])
+
+    retval = frozendict()
+    for key, value in obj.items():
+        if isinstance(value, dict):
+            frozen_value = _make_frozen(value)
+        elif isinstance(value, list):
+            frozen_value = tuple([_make_frozen(item) for item in value])
+        else:
+            frozen_value = value
+        retval = retval.set(key, frozen_value)
+    return retval
+
+
+def _make_unfrozen(obj):
+    if isinstance(obj, frozendict):
+        return {key: _make_unfrozen(value) for key, value in obj.items()}
+
+    elif isinstance(obj, tuple):
+        return [_make_unfrozen(item) for item in obj]
+
+    return obj
+
+
+# Note that this exists because of data loss bug #2135 - when a FormStep is being
+# deleted in the form designer (or during import/export), we do not want to destroy
+# the submitted data for that step.
+#
+# This implements a bandaid fix where we record just enough information to reconstruct
+# the FormStep in memory.
+def RECORD_HISTORICAL_FORM_STEP(collector, field, sub_objs, using):
+    form_steps = list(collector.data[FormStep])
+    assert (
+        len(form_steps) == 1
+    ), "Only this specific case for SubmissionStep cascade delete is supported"
+    form_step = form_steps[0]
+    serialized_form_definition = serializers.serialize(
+        "python", [form_step.form_definition]
+    )[0]
+    serialized_form_step = serializers.serialize("python", [form_step])[0]
+
+    # update the history field
+    history_field = SubmissionStep._meta.get_field("form_step_history")
+    frozen_history = _make_frozen(
+        {
+            "form_step": serialized_form_step,
+            "form_definition": serialized_form_definition,
+        }
+    )
+    collector.add_field_update(history_field, frozen_history, sub_objs)
+
+    # our own reference can now become NULL
+    collector.add_field_update(field, None, sub_objs)
+
+
+class FrozenDjangoJSONEncoder(DjangoJSONEncoder):
+    def default(self, o):
+        if isinstance(o, frozendict):
+            unfrozen = _make_unfrozen(o)
+            return unfrozen
+        return super().default(o)
 
 
 class SubmissionStep(models.Model):
@@ -16,12 +90,27 @@ class SubmissionStep(models.Model):
 
     uuid = models.UUIDField(_("UUID"), unique=True, default=uuid.uuid4)
     submission = models.ForeignKey("submissions.Submission", on_delete=models.CASCADE)
-    form_step = models.ForeignKey("forms.FormStep", on_delete=models.CASCADE)
+    form_step = models.ForeignKey(
+        "forms.FormStep",
+        on_delete=RECORD_HISTORICAL_FORM_STEP,
+        null=True,
+        blank=True,
+    )
     _data = models.JSONField(_("data"), blank=True, null=True)
     # _data is deprecated and replaced with variables. This is still kept around to be
     # able to automatically migrate in case there were earlier migration bugs.
     created_on = models.DateTimeField(_("created on"), auto_now_add=True)
     modified = models.DateTimeField(_("modified on"), auto_now=True)
+
+    # bugfix for #2135
+    form_step_history = models.JSONField(
+        _("form step (historical)"),
+        encoder=FrozenDjangoJSONEncoder,
+        editable=False,
+        default=None,
+        blank=True,
+        null=True,
+    )
 
     # can be modified by logic evaluations/checks
     _can_submit = True
