c97c7de684241d2319826a5dffc928408ee10b99
Sergei Maertens <sergei@maykinmedia.nl>
2022-11-04 15:50:32 +0100

    :test_tube: [#2135] -- add regression test for formstep delete via form version restore

--- src/openforms/submissions/tests/test_regressions.py
+++ src/openforms/submissions/tests/test_regressions.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, override_settings, tag
 
+from openforms.forms.models import FormVersion
 from openforms.forms.tests.factories import FormFactory, FormStepFactory
 from openforms.submissions.models import Submission, SubmissionStep
 
@@ -130,6 +131,69 @@ class SubmissionStepDeletedRegressionTests(TestCase):
         # delete a form step
         step1.delete()
         step1.form_definition.delete()
+
+        # reload submission as fresh data object so we don't have any cached states
+        submission = Submission.objects.get(pk=submission.pk)
+        self.assertEqual(
+            submission.data,
+            {
+                "step1": "stippenlift",
+                "step2": "ik ben een alien",
+                "someDate": "2022-11-03",
+            },
+        )
+        self.assertTrue(SubmissionStep.objects.filter(pk=submission_step1.pk).exists())
+        self.assertTrue(SubmissionStep.objects.filter(pk=submission_step2.pk).exists())
+        renderer = Renderer(submission=submission, mode=RenderModes.pdf, as_html=False)
+        rendered = [node.render() for node in renderer]
+        self.assertIn("step1: stippenlift", rendered)
+        self.assertIn("step2: ik ben een alien", rendered)
+        # check that type info is used for display
+        self.assertIn("someDate: 3 november 2022", rendered)
+
+    def test_restore_old_form_version(self):
+        form = FormFactory.create()
+        step1 = FormStepFactory.create(
+            form=form,
+            form_definition__configuration={
+                "components": [
+                    {
+                        "type": "textfield",
+                        "key": "step1",
+                    },
+                    {
+                        "type": "date",
+                        "key": "someDate",
+                    },
+                ]
+            },
+        )
+        step2 = FormStepFactory.create(
+            form=form,
+            form_definition__configuration={
+                "components": [
+                    {
+                        "type": "textfield",
+                        "key": "step2",
+                    },
+                ]
+            },
+        )
+        version = FormVersion.objects.create_for(form=form)
+        submission = SubmissionFactory.create(form=form, completed=True)
+        submission_step1 = SubmissionStepFactory.create(
+            submission=submission,
+            form_step=step1,
+            data={"step1": "stippenlift", "someDate": "2022-11-03"},
+        )
+        submission_step2 = SubmissionStepFactory.create(
+            submission=submission,
+            form_step=step2,
+            data={"step2": "ik ben een alien"},
+        )
+
+        # trigger form step deletion via form version restoration
+        form.restore_old_version(version.uuid)
 
         # reload submission as fresh data object so we don't have any cached states
         submission = Submission.objects.get(pk=submission.pk)
