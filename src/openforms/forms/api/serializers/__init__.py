9501f7394c167792e299c5f56087f25626086ecb
Steven Bal <steven@maykinmedia.nl>
2022-11-07 16:48:26 +0100

    :memo: [#2313] Display admin fields warning for other viewsets
    * FormStepViewSet
    * FormDefinitionViewSet

--- src/openforms/forms/api/serializers/__init__.py
+++ src/openforms/forms/api/serializers/__init__.py
@@ -1,7 +1,7 @@
 from .form import FormExportSerializer, FormImportSerializer, FormSerializer
 from .form_admin_message import FormAdminMessageSerializer
 from .form_definition import FormDefinitionDetailSerializer, FormDefinitionSerializer
-from .form_step import FormStepSerializer
+from .form_step import FormStepLiteralsSerializer, FormStepSerializer
 from .form_variable import FormVariableListSerializer, FormVariableSerializer
 from .form_version import FormVersionSerializer
 from .logic.form_logic import FormLogicSerializer
@@ -15,6 +15,7 @@ __all__ = [
     "FormImportSerializer",
     "FormDefinitionSerializer",
     "FormDefinitionDetailSerializer",
+    "FormStepLiteralsSerializer",
     "FormStepSerializer",
     "FormVersionSerializer",
     "FormAdminMessageSerializer",
