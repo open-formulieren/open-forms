4da6c1e28ff9246d16112dd3890954444a099200
Sergei Maertens <sergei@maykinmedia.nl>
2022-11-08 12:14:32 +0100

    :fire: Replace own strip_tags_plus and sanitize_content with mail-cleaner API

--- src/openforms/emails/utils.py
+++ src/openforms/emails/utils.py
@@ -1,22 +1,18 @@
 import logging
 import re
-from functools import partial
-from html import unescape
 from typing import Any, List, Optional, Sequence, Tuple
-from urllib.parse import urlparse, urlsplit
+from urllib.parse import urlsplit
 
 from django.conf import settings
-from django.core.exceptions import SuspiciousOperation
 from django.template import Context, Template
 from django.template.loader import get_template
-from django.utils.html import strip_tags as django_strip_tags
 
-from lxml.html import fromstring, tostring
 from mail_cleaner.mail import send_mail_plus
+from mail_cleaner.sanitizer import sanitize_content as _sanitize_content
+from mail_cleaner.text import strip_tags_plus
 
 from openforms.config.models import GlobalConfiguration
 
-from .constants import URL_REGEX
 from .context import get_wrapper_context
 
 logger = logging.getLogger(__name__)
@@ -24,15 +20,6 @@ logger = logging.getLogger(__name__)
 MESSAGE_SIZE_LIMIT = 2 * 1024 * 1024
 
 RE_NON_WHITESPACE = re.compile(r"\S")
-
-
-def sanitize_urls(allowlist: List[str], match) -> str:
-    parsed = urlparse(match.group())
-    if parsed.netloc in allowlist:
-        return match.group()
-
-    logger.debug("Sanitized URL from email: %s", match.group())
-    return ""
 
 
 def get_system_netloc_allowlist():
@@ -52,13 +39,9 @@ def sanitize_content(content: str) -> str:
     * strip URLs that are not present in the explicit allow list
     """
     config = GlobalConfiguration.get_solo()
-
     # strip out any hyperlinks that are not in the configured allowlist
     allowlist = get_system_netloc_allowlist() + config.email_template_netloc_allowlist
-    replace_urls = partial(sanitize_urls, allowlist)
-    stripped = re.sub(URL_REGEX, replace_urls, content)
-
-    return stripped
+    return _sanitize_content(content, allowlist)
 
 
 AttachmentsType = Optional[Sequence[Tuple[str, str, Any]]]
@@ -94,111 +77,6 @@ def send_mail_html(
         fail_silently=fail_silently,
         attachments=attachment_tuples,
     )
-
-
-NEWLINE_CHARS = (
-    "\n",
-    "\r",
-    "\r\n",
-    "\v",
-    "\x0b",
-    "\f",
-    "\x0c",
-    "\x1c",
-    "\x1d",
-    "\x1e",
-    "\x85",
-    "\u2028",
-    "\u2029",
-)
-
-
-def strip_tags_plus(text: str, keep_leading_whitespace: bool = False) -> str:
-    """
-    NOTE this renders unescaped user-data and should never used for display as HTML content
-
-    copied and modified from https://bitbucket.org/maykinmedia/werkbezoek/src/develop/src/werkbezoek/utils/email.py
-    """
-    text = unwrap_anchors(text)
-    # <br> is eaten completely by strip_tags, so replace them by newlines
-    text = text.replace("<br>", "\n")
-    text = django_strip_tags(text)
-    lines = text.splitlines()
-    transformed_lines = transform_lines(
-        lines, keep_leading_whitespace=keep_leading_whitespace
-    )
-    deduplicated_newlines = deduplicate_newlines(transformed_lines)
-
-    return "".join(deduplicated_newlines)
-
-
-def transform_lines(
-    lines: List[str], keep_leading_whitespace: bool = False
-) -> List[str]:
-    transformed_lines = []
-
-    for line in lines:
-        unescaped_line = unescape(line)
-
-        if (
-            keep_leading_whitespace
-            and unescaped_line.startswith(" ")
-            and (match := RE_NON_WHITESPACE.search(unescaped_line)) is not None
-        ):
-            start = match.start()
-            leading_whitespace = unescaped_line[:start]
-            transformed = f"{' '.join(unescaped_line[start:].split())}".rstrip()
-            split_line = f"{leading_whitespace}{transformed}"
-        else:
-            split_line = f"{' '.join(unescaped_line.split())}".rstrip()
-
-        transformed_lines.append(f"{split_line}\n")
-
-    return transformed_lines
-
-
-def deduplicate_newlines(lines: List[str]) -> List[str]:
-    deduplicated_newlines = []
-
-    for line in lines:
-        if not deduplicated_newlines:
-            deduplicated_newlines.append(line)
-            continue
-
-        is_newline = line in NEWLINE_CHARS
-
-        if is_newline and deduplicated_newlines[-1] in NEWLINE_CHARS:
-            continue
-
-        deduplicated_newlines.append(line)
-
-    return deduplicated_newlines
-
-
-def unwrap_anchors(html_str: str) -> str:
-    """
-    ugly util to append the href inside the anchor text so we can use strip-tags
-
-    note this runs on un-trusted HTML content
-    """
-    if len(html_str) > MESSAGE_SIZE_LIMIT:
-        raise SuspiciousOperation("email content-length exceeded safety limit")
-
-    root = fromstring(html_str)
-
-    for link in root.iterfind(".//a"):
-        url = link.attrib.get("href", None)
-        if not url:
-            continue
-
-        span_child = link.find(".//span")
-        if not link.text and span_child is not None:
-            # Issue #2154 Case in which the link has a special colour, so it's wrapped in a span.
-            span_child.text += f" ({url})"
-        else:
-            link.text += f" ({url})"
-
-    return tostring(root, encoding="utf8").decode("utf8")
 
 
 def render_email_template(template: str, context: dict, **extra_context: Any) -> str:
--- src/openforms/emails/validators.py
+++ src/openforms/emails/validators.py
@@ -1,10 +1,10 @@
-from urllib.parse import urlparse
+from urllib.parse import urlsplit
 
 from django.core.exceptions import ValidationError
 from django.utils.deconstruct import deconstructible
 from django.utils.translation import gettext_lazy as _
 
-from openforms.emails.constants import URL_REGEX
+from mail_cleaner.constants import URL_REGEX
 
 
 @deconstructible
@@ -13,7 +13,10 @@ class URLSanitationValidator:
 
     def __call__(self, value):
         """
-        this operation matches the logic of .utils.sanitize_content()
+        Validate that only allowed domains are present as URLs.
+
+        This operation matches the logic of
+        :func:`openforms.emails.utils.sanitize_content`.
         """
 
         # local imports because we use this on GlobalConfiguration itself
@@ -27,7 +30,7 @@ class URLSanitationValidator:
         )
 
         for m in URL_REGEX.finditer(value):
-            parsed = urlparse(m.group())
+            parsed = urlsplit(m.group())
             if parsed.netloc not in allowlist:
                 raise ValidationError(
                     self.message.format(netloc=parsed.netloc), code="invalid"
