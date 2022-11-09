eb42d8d745777b5751216e1cbbb629e988daa8ff
Bart van der Schoor <bart@maykinmedia.nl>
2022-11-09 14:43:48 +0100

    [#2329] Added Bleach to CSP post-processor

--- src/csp_post_processor/__init__.py
+++ src/csp_post_processor/__init__.py
@@ -1,7 +1,7 @@
-from .processor import post_process_html
+from .processor import bleach_wysiwyg_content, post_process_html
 
 __version__ = "0.1.0"
 
-__all__ = ["post_process_html"]
+__all__ = ["bleach_wysiwyg_content", "post_process_html"]
 
 default_app_config = "csp_post_processor.apps.CSPPostProcessConfig"
