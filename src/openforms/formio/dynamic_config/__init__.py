5c6532cc6d2c8a0b6b3c168be81e0850d3c2c6f7
Sergei Maertens <sergei@maykinmedia.nl>
2022-11-24 17:14:48 +0100

    :pencil: [#1068] -- fix outdated docstring

--- src/openforms/formio/dynamic_config/__init__.py
+++ src/openforms/formio/dynamic_config/__init__.py
@@ -27,11 +27,11 @@ def rewrite_formio_components(
     :arg configuration_wrapper: Container object holding the Formio form configuration
       to be updated (if applicable). The rewriting loops over all components one-by-one
       and applies the changes.
+    :arg submission: The submission instance for which we are rewriting the
+      configurations. This allows you to inspect state and use convenience methods
+      that may not be available in the variables data.
     :arg data: key-value mapping of variable name to variable value. If a submission
       context is available, the variables of the submission are included here.
-    :arg request: The HTTP request received by the API view when rewriting is done as
-      part of an HTTP request-response cycle. This is ``None`` otherwise (e.g. in
-      background tasks).
     """
     data = data or {}  # normalize
     for component in configuration_wrapper:
