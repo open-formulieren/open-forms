a56d63f15453170b0fc9f2d08c053b1929689695
Anna Shamray <anna.shamray@maykinmedia.nl>
2023-01-09 18:05:52 +0100

    :recycle: [#2471] use registry choices for appointment config

--- src/openforms/appointments/constants.py
+++ src/openforms/appointments/constants.py
@@ -1,13 +1,6 @@
 from djchoices import ChoiceItem, DjangoChoices
 
 
-class AppointmentsConfigPaths(DjangoChoices):
-    jcc = ChoiceItem("openforms.appointments.contrib.jcc.models.JccConfig", "Jcc")
-    qmatic = ChoiceItem(
-        "openforms.appointments.contrib.qmatic.models.QmaticConfig", "Qmatic"
-    )
-
-
 class AppointmentDetailsStatus(DjangoChoices):
     success = ChoiceItem("success", "Success")
     missing_info = ChoiceItem(
