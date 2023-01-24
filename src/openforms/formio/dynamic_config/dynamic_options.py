782322f8777997fb6ee1c035769deefbe29ea615
SilviaAmAm <silvia@maykinmedia.nl>
2023-01-24 12:49:18 +0100

    :sparkles: [#1902] Dynamic update of options in backend

---
+++ src/openforms/formio/dynamic_config/dynamic_options.py
@@ -0,0 +1,45 @@
+from glom import assign, glom
+from json_logic import jsonLogic
+
+from openforms.typing import DataMapping
+
+from ..typing import Component
+
+
+def add_options_to_config(
+    component: Component,
+    data: DataMapping,
+    options_path: str = "values",
+    data_src_path: str = "dataSrc",
+) -> None:
+    if glom(component, data_src_path, default=None) != "variable":
+        return
+
+    items_path = glom(component, "data.itemsPath")
+
+    # The array of items from which we need to get the values
+    items_array = jsonLogic(items_path, data)
+
+    if not items_array or len(items_array) == 0:
+        return
+
+    # Is each item a dict, like for repeating groups, or are they primitives
+    # ready to use?
+    is_obj = isinstance(items_array[0], dict)
+
+    if not is_obj:
+        assign(
+            component,
+            options_path,
+            [{"label": item, "value": item} for item in items_array],
+            missing=dict,
+        )
+        return
+
+    values = []
+    value_path = glom(component, "data.valuePath")
+    for item in items_array:
+        value = jsonLogic(value_path, item)
+        values.append({"label": value, "value": value})
+
+    assign(component, options_path, values, missing=dict)
