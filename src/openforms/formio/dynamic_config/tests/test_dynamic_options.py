89983875c5f06c72d3146ce381d40ca55f6de2f5
SilviaAmAm <silvia@maykinmedia.nl>
2023-01-25 10:16:43 +0100

    :white_check_mark: [#1902] Test missing valuePath

--- src/openforms/formio/dynamic_config/tests/test_dynamic_options.py
+++ src/openforms/formio/dynamic_config/tests/test_dynamic_options.py
@@ -371,3 +371,74 @@ class TestDynamicConfigAddingOptions(TestCase):
             configuration["components"][3]["values"],
             [{"label": "", "value": ""}],
         )
+
+    def test_variable_options_repeating_group_missing_value_path(self):
+        configuration = {
+            "components": [
+                {
+                    "key": "repeatingGroup",
+                    "type": "editgrid",
+                    "components": [{"type": "textfield", "key": "name"}],
+                },
+                {
+                    "label": "Select Boxes",
+                    "key": "selectBoxes",
+                    "type": "selectboxes",
+                    "values": [
+                        {"label": "", "value": ""},
+                    ],
+                    "dataSrc": "variable",
+                    "data": {
+                        "itemsPath": {"var": "repeatingGroup"},
+                        # Missing valuePath
+                    },
+                },
+                {
+                    "label": "Select",
+                    "key": "select",
+                    "data": {
+                        "values": [
+                            {"label": "", "value": ""},
+                        ],
+                        "dataSrc": "variable",
+                        "itemsPath": {"var": "repeatingGroup"},
+                        # Missing valuePath
+                    },
+                    "type": "select",
+                },
+                {
+                    "label": "Radio",
+                    "key": "radio",
+                    "type": "radio",
+                    "values": [
+                        {"label": "", "value": ""},
+                    ],
+                    "dataSrc": "variable",
+                    "data": {
+                        "itemsPath": {"var": "repeatingGroup"},
+                        # Missing valuePath
+                    },
+                },
+            ]
+        }
+
+        submission = SubmissionFactory.create()
+
+        rewrite_formio_components(
+            FormioConfigurationWrapper(configuration),
+            submission,
+            {"repeatingGroup": [{"name": "Test1"}, {"name": "Test2"}]},
+        )
+
+        self.assertEqual(
+            configuration["components"][1]["values"],
+            [{"label": "", "value": ""}],
+        )
+        self.assertEqual(
+            configuration["components"][2]["data"]["values"],
+            [{"label": "", "value": ""}],
+        )
+        self.assertEqual(
+            configuration["components"][3]["values"],
+            [{"label": "", "value": ""}],
+        )
