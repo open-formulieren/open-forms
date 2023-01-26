5f34bdc47b7b1de5b654a0e529f57ecbb12957a0
Jiro Ghianni <jiro@maykinmedia.nl>
2023-01-26 10:57:31 +0100

    :alembic: Try to style cookie banner between stuck/static positions

--- src/openforms/js/public.js
+++ src/openforms/js/public.js
@@ -17,3 +17,35 @@ const init = () => {
 };
 
 onLoaded(init);
+
+// observe whether cookiebanner is in sticky mode or not
+// note: rootMargin must be specified in pixels or percent, not REMs/EMs.
+const observer = new IntersectionObserver(
+  ([e]) => e.target.classList.toggle('stuck', e.intersectionRatio < 1),
+  {root: document.querySelector('body'), rootMargin: '0px 0px 31px 0px', threshold: [1]}
+);
+
+observer.observe(document.querySelector('.cookie-notice'));
+
+// in case page height is less than the full device height
+// (this is a state where the IntersectionObserver will not be triggered, but shadow still needs to be removed)
+const hasScrollbar = document.body.scrollHeight > window.innerHeight;
+
+if (hasScrollbar === false) {
+  document.querySelector('.cookie-notice').classList.add('no-shadow');
+}
+
+// ugly solution that may sent too many requests:
+
+// window.onscroll = function () {
+//   var totalPageHeight = document.body.scrollHeight;
+//   var scrollPoint = window.scrollY + window.innerHeight;
+
+//   // check if we hit the bottom of the page
+//   if (scrollPoint >= totalPageHeight) {
+//     console.log('scrolled to the bottom');
+//     document.querySelector('.cookie-notice').classList.add('stuck');
+//   } else {
+//     document.querySelector('.cookie-notice').classList.remove('stuck');
+//   }
+// };
