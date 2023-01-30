54e248f85ec68690320543b8f0236bd4466a2848
SilviaAmAm <silvia@maykinmedia.nl>
2023-01-30 12:56:28 +0100

    :ok_hand: [#1902] Refactor select editForm

--- src/openforms/js/components/form/select.js
+++ src/openforms/js/components/form/select.js
@@ -1,115 +1,33 @@
 import {Formio} from 'formiojs';
+import cloneDeep from 'lodash/cloneDeep';
 
-import DEFAULT_TABS, {ADVANCED, BASIC, REGISTRATION, TRANSLATIONS, VALIDATION} from './edit/tabs';
+import {
+  CLEAR_ON_HIDE,
+  DESCRIPTION,
+  HIDDEN,
+  IS_SENSITIVE_DATA,
+  KEY,
+  LABEL_REQUIRED,
+  MULTIPLE,
+  OPTIONS_CHOICES,
+  PRESENTATION,
+} from './edit/options';
+import DEFAULT_TABS, {ADVANCED, REGISTRATION, TRANSLATIONS, VALIDATION} from './edit/tabs';
 import {localiseSchema} from './i18n';
 
 const Select = Formio.Components.components.select;
 
-const optionsChoices = [
-  {
-    type: 'select',
-    key: 'openForms.dataSrc',
-    label: 'Data source',
-    description: 'What data to use for the options of this field.',
-    defaultValue: 'manual',
-    data: {
-      values: [
-        {
-          label: 'Manually fill in',
-          value: 'manual',
-        },
-        {
-          label: 'Variable',
-          value: 'variable',
-        },
-      ],
-    },
-    validate: {
-      required: true,
-    },
-  },
-  {
-    type: 'datagrid',
-    input: true,
-    label: 'Values',
-    key: 'data.values',
-    tooltip:
-      'The values that can be picked for this field. Values are text submitted with the form data. ' +
-      'Labels are text that appears next to the radio buttons on the form.',
-    weight: 10,
-    reorder: true,
-    defaultValue: [{label: '', value: ''}],
-    components: [
-      {
-        label: 'Label',
-        key: 'label',
-        input: true,
-        type: 'textfield',
-      },
-      {
-        label: 'Value',
-        key: 'value',
-        input: true,
-        type: 'textfield',
-        allowCalculateOverride: true,
-        calculateValue: {_camelCase: [{var: 'row.label'}]},
-        validate: {
-          required: true,
-        },
-      },
-    ],
-    conditional: {
-      show: true,
-      when: 'openForms.dataSrc',
-      eq: 'manual',
-    },
-  },
-  {
-    label: 'Default Value',
-    key: 'defaultValue',
-    tooltip: 'This will be the initial value for this field, before user interaction.',
-    input: true,
-    conditional: {
-      show: true,
-      when: 'openForms.dataSrc',
-      eq: 'manual',
-    },
-  },
-  {
-    label: 'Items',
-    key: 'openForms.itemsExpression',
-    type: 'textarea',
-    tooltip:
-      'A JSON logic expression returning a variable (of array type) whose items should be used as the options for this component.',
-    editor: 'ace',
-    as: 'json',
-    // Documentation for editor settings: https://ajaxorg.github.io/ace-api-docs/interfaces/Ace.EditorOptions.html
-    // but most of the settings don't seem to work.
-    conditional: {
-      show: true,
-      when: 'openForms.dataSrc',
-      eq: 'variable',
-    },
-    validate: {
-      required: true,
-    },
-  },
-  {
-    label: 'Items value',
-    key: 'openForms.valueExpression',
-    type: 'textarea',
-    tooltip:
-      'In case the items are objects, provide a JSON logic expression to construct the value to use from each item of the array. ' +
-      'For example, this is needed when referring to repeating groups.',
-    editor: 'ace',
-    as: 'json',
-    conditional: {
-      show: true,
-      when: 'openForms.dataSrc',
-      eq: 'variable',
-    },
-  },
-];
+const getOptionsChoices = () => {
+  let selectOptionsChoices = cloneDeep(OPTIONS_CHOICES);
+  if (selectOptionsChoices[1].key !== 'values') {
+    throw new Error(
+      `Expected the second component to be "values", has ${selectOptionsChoices[1].key}`
+    );
+  }
+  // For radio and selectboxes components, the values have key 'values'. For select, it's 'data.values'
+  selectOptionsChoices[1].key = 'data.values';
+  return selectOptionsChoices;
+};
 
 class SelectField extends Select {
   static schema(...extend) {
@@ -125,8 +43,19 @@ class SelectField extends Select {
 
   static editForm() {
     const BASIC_TAB = {
-      ...BASIC,
-      components: [...BASIC.components, ...optionsChoices],
+      key: 'basic',
+      label: 'Basic',
+      components: [
+        LABEL_REQUIRED,
+        KEY,
+        DESCRIPTION,
+        PRESENTATION,
+        MULTIPLE,
+        HIDDEN,
+        CLEAR_ON_HIDE,
+        IS_SENSITIVE_DATA,
+        ...getOptionsChoices(),
+      ],
     };
     const TABS = {
       ...DEFAULT_TABS,
