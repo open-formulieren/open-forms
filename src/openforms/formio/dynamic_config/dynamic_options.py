e45c1386a4fcafff102cb942c887bffffd19b196
SilviaAmAm <silvia@maykinmedia.nl>
2023-02-02 14:47:31 +0100

    :sparkles: [#2623] make different key/value options possible

--- src/openforms/formio/dynamic_config/dynamic_options.py
+++ src/openforms/formio/dynamic_config/dynamic_options.py
@@ -1,7 +1,6 @@
 import json
 
 from django.template.defaultfilters import escape_filter as escape
-from django.utils.datastructures import OrderedSet
 from django.utils.translation import gettext as _
 
 from glom import assign, glom
@@ -9,9 +8,36 @@ from json_logic import jsonLogic
 
 from openforms.logging import logevent
 from openforms.submissions.models import Submission
-from openforms.typing import DataMapping
+from openforms.typing import DataMapping, JSONValue
 
 from ..typing import Component
+
+
+def normalise_option(option: JSONValue) -> list[JSONValue]:
+    if not isinstance(option, list):
+        return [option, option]
+
+    return option[:2]
+
+
+def is_or_contains_none(option):
+    if isinstance(option, list):
+        return None in option
+    return option is None
+
+
+def escape_option(option: list[JSONValue]) -> list[str]:
+    return [escape(item) for item in option]
+
+
+def deduplicate_options(
+    options: list[JSONValue],
+) -> list[JSONValue]:
+    new_options = []
+    for option in options:
+        if option not in new_options:
+            new_options.append(option)
+    return new_options
 
 
 def add_options_to_config(
@@ -39,7 +65,25 @@ def add_options_to_config(
         )
         return
 
-    if any([isinstance(item, (list, dict)) for item in items_array]):
+    # Remove any None values
+    if any([is_or_contains_none(item) for item in items_array]):
+        logevent.form_configuration_error(
+            submission.form,
+            component,
+            _(
+                "Expression %(items_expression)s did not return a valid option for each item."
+            )
+            % {"items_expression": json.dumps(items_expression)},
+        )
+        items_array = [item for item in items_array if not is_or_contains_none(item)]
+
+    normalised_options = [normalise_option(option) for option in items_array]
+    if any(
+        [
+            isinstance(item_key, (dict, list)) or isinstance(item_label, (dict, list))
+            for item_key, item_label in normalised_options
+        ]
+    ):
         logevent.form_configuration_error(
             submission.form,
             component,
@@ -50,26 +94,14 @@ def add_options_to_config(
         )
         return
 
-    # Remove any None values
-    if None in items_array:
-        logevent.form_configuration_error(
-            submission.form,
-            component,
-            _(
-                "Expression %(items_expression)s did not return a valid option for each item."
-            )
-            % {"items_expression": json.dumps(items_expression)},
-        )
-        items_array = [item for item in items_array if item is not None]
-
-    # items_array contains user input! We also don't want duplicate values
-    escaped_values = OrderedSet(map(escape, items_array))
+    escaped_options = [escape_option(option) for option in normalised_options]
+    deduplicated_options = deduplicate_options(escaped_options)
     assign(
         component,
         options_path,
         [
-            {"label": escaped_value, "value": escaped_value}
-            for escaped_value in escaped_values
+            {"label": escaped_label, "value": escaped_key}
+            for escaped_key, escaped_label in deduplicated_options
         ],
         missing=dict,
     )
