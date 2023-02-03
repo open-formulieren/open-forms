58ca3cfe8243c636c638104ef6eb49061be3a4ec
SilviaAmAm <silvia@maykinmedia.nl>
2023-02-03 14:57:27 +0100

    :bug: [#2641] Deal with urls with subdomains

--- src/openforms/emails/validators.py
+++ src/openforms/emails/validators.py
@@ -7,6 +7,16 @@ from django.utils.translation import gettext_lazy as _
 from mail_cleaner.constants import URL_REGEX
 
 
+def is_in_allowlist(split_allow_list, domain):
+    split_domain = domain.split(".")
+    for index in range(len(split_domain)):
+        domain_bit = tuple(split_domain[index:])
+        if domain_bit in split_allow_list:
+            return True
+
+    return False
+
+
 @deconstructible
 class URLSanitationValidator:
     message = _("This domain is not in the global netloc allowlist: {netloc}")
@@ -28,10 +38,13 @@ class URLSanitationValidator:
         allowlist = (
             get_system_netloc_allowlist() + config.email_template_netloc_allowlist
         )
+        split_allow_list = {
+            tuple(allowed_domain.split(".")) for allowed_domain in allowlist
+        }
 
         for m in URL_REGEX.finditer(value):
             parsed = urlsplit(m.group())
-            if parsed.netloc not in allowlist:
+            if not is_in_allowlist(split_allow_list, parsed.netloc):
                 raise ValidationError(
                     self.message.format(netloc=parsed.netloc), code="invalid"
                 )
