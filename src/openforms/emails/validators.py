c96d913493fcedbb0920dc9ff3f9bcd3e6f2a9de
SilviaAmAm <silvia@maykinmedia.nl>
2023-02-06 17:11:47 +0100

    :ok_hand: [#2646] Make checks simpler

--- src/openforms/emails/validators.py
+++ src/openforms/emails/validators.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 from urllib.parse import urlsplit
 
 from django.core.exceptions import ValidationError
@@ -7,14 +8,18 @@ from django.utils.translation import gettext_lazy as _
 from mail_cleaner.constants import URL_REGEX
 
 
-def is_in_allowlist(split_allow_list, domain):
-    split_domain = domain.split(".")
-    for index in range(len(split_domain)):
-        domain_bit = tuple(split_domain[index:])
-        if domain_bit in split_allow_list:
-            return True
+def subdomains(domain: str) -> Iterable[str]:
+    """
+    Return an iterable with all the subdomains for a particular domain.
 
-    return False
+    For example, for open-forms.test.maykin.nl, this would return
+    {'open-forms.test.maykin.nl', 'nl', 'maykin.nl', 'test.maykin.nl'}
+    """
+    domain_bits_reversed = domain.split(".")[::-1]
+    subdomains_split = [domain_bits_reversed[0]]
+    for domain_bit in domain_bits_reversed[1:]:
+        subdomains_split.append(".".join([domain_bit, subdomains_split[-1]]))
+    return set(subdomains_split)
 
 
 @deconstructible
@@ -38,13 +43,11 @@ class URLSanitationValidator:
         allowlist = (
             get_system_netloc_allowlist() + config.email_template_netloc_allowlist
         )
-        split_allow_list = {
-            tuple(allowed_domain.split(".")) for allowed_domain in allowlist
-        }
 
         for m in URL_REGEX.finditer(value):
             parsed = urlsplit(m.group())
-            if not is_in_allowlist(split_allow_list, parsed.netloc):
+
+            if not any(stem for stem in subdomains(parsed.netloc) if stem in allowlist):
                 raise ValidationError(
                     self.message.format(netloc=parsed.netloc), code="invalid"
                 )
