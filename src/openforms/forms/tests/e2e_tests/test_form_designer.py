ad96e011bf52f795e0ab144ae2437501ef483f74
SilviaAmAm <silvia@maykinmedia.nl>
2023-03-01 11:13:32 +0100

    :white_check_mark: [#2798] Test automatic option key update

--- src/openforms/forms/tests/e2e_tests/test_form_designer.py
+++ src/openforms/forms/tests/e2e_tests/test_form_designer.py
@@ -351,6 +351,51 @@ class FormDesignerComponentTranslationTests(E2ETestCase):
             # the modal should close
             await expect(page.locator("css=.formio-dialog-content")).to_be_hidden()
 
+    async def test_key_automatically_generated_for_select_options(self):
+        @sync_to_async
+        def setUpTestData():
+            # set up a form
+            form = FormFactory.create(
+                name="Playwright test",
+                name_nl="Playwright test",
+                generate_minimal_setup=True,
+                formstep__form_definition__name_nl="Playwright test",
+                formstep__form_definition__configuration={
+                    "components": [],
+                },
+            )
+            return form
+
+        await create_superuser()
+        form = await setUpTestData()
+        admin_url = str(
+            furl(self.live_server_url)
+            / reverse("admin:forms_form_change", args=(form.pk,))
+        )
+
+        async with browser_page() as page:
+            await self._admin_login(page)
+            await page.goto(str(admin_url))
+            await page.get_by_role("tab", name="Steps and fields").click()
+
+            # Drag and drop a component
+            await page.get_by_text("Keuzelijst").hover()
+            await page.mouse.down()
+            await page.locator('css=[ref="-container"]').hover()
+            await page.mouse.up()
+
+            # Check that the modal is open
+            await expect(page.locator("css=.formio-dialog-content")).to_have_count(1)
+
+            # Update the label
+            value_label_input = page.locator('css=[name="data[data.values][0][label]"]')
+            await value_label_input.click()
+            await value_label_input.fill("Test")
+
+            # Check that the key has been updated
+            value_key_input = page.locator('css=[name="data[data.values][0][value]"]')
+            await expect(value_key_input).to_have_value("test")
+
 
 class FormDesignerRegressionTests(E2ETestCase):
     async def test_user_defined_variable_boolean_initial_value_false(self):
