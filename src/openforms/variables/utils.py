709a40c20a3a2c4faca2df2873dd41bf615fc0cd
SilviaAmAm <silvia@maykinmedia.nl>
2023-03-03 16:07:29 +0100

    :sparkles: [#2804] Static variables in confirmation template
    Backport-Of: open-formulieren/open-forms#2818

--- src/openforms/variables/utils.py
+++ src/openforms/variables/utils.py
@@ -1,7 +1,11 @@
 from datetime import datetime, time
+from typing import TYPE_CHECKING
 
 from openforms.typing import JSONObject
 from openforms.utils.date import parse_date
+
+if TYPE_CHECKING:
+    from openforms.submissions.models import Submission
 
 
 def check_date(value: str) -> str:
@@ -25,3 +29,29 @@ def check_initial_value(initial_value: JSONObject, data_type: str) -> JSONObject
         return CHECK_VARIABLE_TYPE[data_type](initial_value)
     except (ValueError, TypeError):
         return DEFAULT_INITIAL_VALUE[data_type]
+
+
+def get_variables_for_context(submission: "Submission") -> dict[str, JSONObject]:
+    """Return the key/value pairs of static variables and submission variables.
+
+    This returns the saved component variables and user defined variables for a submission, plus the static variables
+    (which are never saved). Note that depending on when it is called, the 'auth' static variables
+    (auth_bsn, auth_kvk...) can be already hashed.
+
+    .. note::
+       This has inherent problems with form variables having dots in the keys. These
+       should expand to nested dicts/objects, but currently they are injected in the
+       template as "foo.bar" context keys which cannot actually be used (probably).
+
+       See https://github.com/open-formulieren/open-forms/issues/2784 for a possible
+       solution in the future.
+    """
+    from .service import get_static_variables
+
+    return {
+        **{
+            variable.key: variable.initial_value
+            for variable in get_static_variables(submission=submission)
+        },
+        **submission.data,
+    }
