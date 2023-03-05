2930c0c0945202e59cebb79179798773749781bf
Sergei Maertens <sergei@maykinmedia.nl>
2023-03-05 23:15:03 +0100

    :hammer: Fix HMR for scss files with storybook

--- postcss.config.js
+++ postcss.config.js
@@ -1,37 +1,37 @@
 let selectorLintConfig = {
-    global: {
-        // Simple
-        type: true,
-        class: true,
-        id: false,
-        universal: false,
-        attribute: false,
+  global: {
+    // Simple
+    type: true,
+    class: true,
+    id: false,
+    universal: false,
+    attribute: false,
 
-        // Pseudo
-        pseudo: true,
-    },
+    // Pseudo
+    pseudo: true,
+  },
 
-    local: {
-        // Simple
-        type: true,
-        class: true,
-        id: false,
-        universal: true,
-        attribute: true,
+  local: {
+    // Simple
+    type: true,
+    class: true,
+    id: false,
+    universal: true,
+    attribute: true,
 
-        // Pseudo
-        pseudo: true,
-    },
+    // Pseudo
+    pseudo: true,
+  },
 
-    options: {
-        excludedFiles: ['admin_overrides.scss'],
-    }
+  options: {
+    excludedFiles: ['admin_overrides.scss'],
+  },
 };
 
 module.exports = {
-    plugins: [
-        require('autoprefixer'),
-        require('cssnano'),
-        require('postcss-selector-lint')(selectorLintConfig)
-    ]
+  plugins: [
+    require('autoprefixer'),
+    require('cssnano'),
+    // require('postcss-selector-lint')(selectorLintConfig)
+  ],
 };
