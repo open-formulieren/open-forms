a1907b5805e98622019bf590cf45a6d145a04a1c
Paul Schilling <mail@paulschilling.de>
2023-03-06 14:39:19 +0100

    [#2822] fix form variable datatype
    *ensure that if form component has datatype `date`, the corresponding form variable
    has the same datatype
    
    Backport-Of: open-formulieren/open-forms#2829

--- src/openforms/forms/tests/test_data_migrations.py
+++ src/openforms/forms/tests/test_data_migrations.py
@@ -1,5 +1,6 @@
 from openforms.formio.utils import iter_components
 from openforms.utils.tests.test_migrations import TestMigrations
+from openforms.variables.constants import FormVariableDataTypes
 
 
 class TestChangeOpenWhenEmptyConfiguration(TestMigrations):
@@ -73,3 +74,60 @@ class TestChangeOpenWhenEmptyConfiguration(TestMigrations):
             component = components["editgrid4"]
 
             self.assertFalse(component["openWhenEmpty"])
+
+
+class TestFormVariableDataType(TestMigrations):
+    """Assert that if type(form_component) == date, then type(form_varialbe) == date"""
+
+    migrate_from = "0071_merge_20230213_1106"
+    migrate_to = "0072_check_form_variable_datatype"
+    app = "forms"
+
+    def setUpBeforeMigration(self, apps):
+        Form = apps.get_model("forms", "Form")
+        FormDefinition = apps.get_model("forms", "FormDefinition")
+        FormVariable = apps.get_model("forms", "FormVariable")
+
+        form = Form.objects.create(name="form")
+        configuration = {
+            "components": [
+                {"key": "field1", "type": "datetime"},
+                {"key": "field2", "type": "date"},
+            ]
+        }
+        form_def = FormDefinition.objects.create(
+            name="form_def",
+            configuration=configuration,
+        )
+        FormVariable.objects.create(
+            key="field1",
+            form=form,
+            form_definition=form_def,
+            data_type=FormVariableDataTypes.datetime,
+        )
+        FormVariable.objects.create(
+            key="field2",
+            form=form,
+            form_definition=form_def,
+            data_type=FormVariableDataTypes.datetime,
+        )
+        FormVariable.objects.create(
+            key="unrelated",
+            form=form,
+            data_type=FormVariableDataTypes.datetime,
+        )
+
+    def test_form_variable_datatype(self):
+        FormVariable = self.apps.get_model("forms", "FormVariable")
+        form_var1 = FormVariable.objects.filter(key="field1").first()
+        form_var2 = FormVariable.objects.filter(key="field2").first()
+        form_var3 = FormVariable.objects.filter(key="unrelated").first()
+
+        # form variable 1 should be unchanged (datetime > datetime)
+        self.assertEqual(form_var1.data_type, FormVariableDataTypes.datetime)
+
+        # form variable 2 should be changed (datetime > date)
+        self.assertEqual(form_var2.data_type, FormVariableDataTypes.date)
+
+        # Sanity check: unrelated form variable should be unchanged (datetime > datetime)
+        self.assertEqual(form_var3.data_type, FormVariableDataTypes.datetime)
