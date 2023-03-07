open-formulieren/open-forms

commit 2fa33ca311664931348453393091aebb8ed757e6

👌[#2454] Process review
 - Adds tags and tickets for todo, removes skips where possible
 - rephrased comments
 - rename a test and add a toy case as runnable, more approachable documentation
 - cleans up quoting code and use separate escaping for the slightly
   different environments: uri paths & uri query strings

src/openforms/variables/models.py
@@ -1,4 +1,4 @@
-from urllib.parse import quote
+from urllib.parse import quote, quote_plus
 
 from django.core.exceptions import ValidationError
 from django.db import models
@@ -108,24 +108,41 @@ class ServiceFetchConfiguration(models.Model):
         """Return a dictionary with keyword arguments for a
         zgw_consumers.Service client request call.
         """
+        # The zgw-consumers client uses the requests library to perform the request,
+        # but requests sends out malformed headers if you ask it to. We are
+        # interpolating user input into headers, so we might ask it to if we aren't careful.
+        #
+        # The Internet Standard currently applicable is RFC9110
+        # What you and I call http headers, it calls `fields`
+        # https://www.rfc-editor.org/rfc/rfc9110#section-5
+        #
+        # Validating the contents of headers is beyond the scope of this
+        # function, we only make sure the requests we send are within the
+        # Internet Standard spec. If some service needs empoji in the header,
+        # we force it into a sequence of bytes that is allowed.
+        #
+        # extra knowledge not in the RFC: latin1 is a different name for ISO-8859-1
+
         headers = {
-            # no leading spaces and the value into the legal field-value codespace
-            header: value.format(**context).strip().encode("utf-8").decode("latin1")
+            # map all unicode into what the RFC allows with utf-8; remove leading space
+            header: value.format(**context).encode("utf-8").decode("latin1").lstrip()
             for header, value in (self.headers or {}).items()
         }
         # before we go further
         # assert headers are still valid after we put submmitter data in there
+        # this catches faults requests doesn't: https://github.com/psf/requests/issues/6359
         HeaderValidator()(headers)
 
-        escaped_values = {k: quote(str(v), safe="") for k, v in context.items()}
+        escaped_for_query = {k: quote(str(v)) for k, v in context.items()}
+        escaped_for_path = {k: quote_plus(str(v)) for k, v in context.items()}
 
-        query_params = self.query_params.format(**escaped_values)
+        query_params = self.query_params.format(**escaped_for_query)
         query_params = (
             query_params[1:] if query_params.startswith("?") else query_params
         )
 
         request_args = dict(
-            path=self.path.format(**escaped_values),
+            path=self.path.format(**escaped_for_path),
             params=query_params,
             method=self.method,
             headers=headers,

src/openforms/submissions/tests/form_logic/test_fetching_form_varaiable_values_from_services.py
@@ -2,9 +2,9 @@ from functools import lru_cache
 from pathlib import Path
 from typing import Any
 from unittest import skip
-from urllib.parse import unquote
+from urllib.parse import unquote_plus
 
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, tag
 
 import requests_mock
 from factory.django import FileField
@@ -69,11 +69,57 @@ class ServiceFetchConfigVariableBindingTests(SimpleTestCase):
 
         self.assertEqual(value["url"], "https://httpbin.org/get")
 
+    def test_it_can_construct_service_endpoint_path_parameters_from_a_well_behaved_user(
+        self,
+    ):
+        var = FormVariableFactory.build(
+            service_fetch_configuration=ServiceFetchConfigurationFactory.build(
+                service=self.service,
+                path="delay/{seconds}",
+            )
+        )
+        context = {"seconds": 6}
+
+        with requests_mock.Mocker() as m:
+            m.get(requests_mock.ANY)
+            perform_service_fetch(var, context)
+            request = m.last_request
+
+        self.assertEqual(request.url, "https://httpbin.org/delay/6")
+
+    def test_substitutions_all_happen_at_once(self):
+        # It should not be possible for substitution values to be interpreted as template variables
+        var = FormVariableFactory.build(
+            service_fetch_configuration=ServiceFetchConfigurationFactory.build(
+                service=self.service,
+                path="delay/{seconds}.{second_fragments}",
+            )
+        )
+        # Intended use
+        context = {"seconds": 6, "second_fragments": "5"}
+
+        with requests_mock.Mocker() as m:
+            m.get(requests_mock.ANY)
+            perform_service_fetch(var, context)
+            request = m.last_request
+
+        self.assertEqual(request.url, "https://httpbin.org/delay/6.5")
+
+        # Edge case
+        context = {"seconds": 6, "second_fragments": "{seconds}"}
+
+        with requests_mock.Mocker() as m:
+            m.get(requests_mock.ANY)
+            perform_service_fetch(var, context)
+            request = m.last_request
+
+        self.assertEqual(request.url, "https://httpbin.org/delay/6.%7Bseconds%7D")
+
     @given(data_mapping_values())
     @example("../otherendpoint")
     @example("./../.")
-    @example("foo/.")
-    def test_it_can_construct_simple_path_parameters(self, field_value):
+    @example("foo/ +.")
+    def test_it_can_construct_simple_path_parameters_from_any_input(self, field_value):
         assume(field_value != ".")  # request_mock eats the single dot :pacman:
         # https://swagger.io/docs/specification/describing-parameters/#path-parameters
         context = {"seconds": field_value}
@@ -90,15 +136,20 @@ class ServiceFetchConfigVariableBindingTests(SimpleTestCase):
             _ = perform_service_fetch(var, context)
             request = m.last_request
 
-        head, rest = request.url[:26], request.url[26:]
-        self.assertEqual(head, "https://httpbin.org/delay/")
-        # the service should get the value
-        self.assertEqual(unquote(rest), str(field_value))
+        expected_stem_length = len("https://httpbin.org/delay/")
+        stem = request.url[:expected_stem_length]
+        rest = request.url[expected_stem_length:]
 
-        # I guess if the rest is unmangled, the rest of the assertions are not
-        # really needed; I think
-        # ∄ a way for Bobby Tables to drop a table ∧ become an unmangled entry in it
-        # but asserting is cheaper than a CVE
+        self.assertEqual(stem, "https://httpbin.org/delay/")
+        # values should not be able to traverse the api tree
+        self.assertNotIn("/", rest)
+        self.assertNotIn(" ", rest)
+        # the service should get the value
+        self.assertEqual(unquote_plus(rest), str(field_value))
+
+        # I guess if `rest` is unmangled, the following assertions are
+        # tautologies; Better safe than sorry. They MUST hold, even if I can't
+        # think of a way they can't be true.
 
         # it shouldn't change other parts of the request
         self.assertIs(request.body, None)
@@ -107,7 +158,8 @@ class ServiceFetchConfigVariableBindingTests(SimpleTestCase):
         # and a weak assertion
         self.assertNotIn(str(field_value), request.headers.values())
 
-    @skip("TODO")
+    # TODO
+    @tag("gh-2745")
     @given(
         st.one_of(
             st.lists(elements=data_mapping_values()),
@@ -187,7 +239,8 @@ class ServiceFetchConfigVariableBindingTests(SimpleTestCase):
         self.assertNotIn(str(some_text), request.headers.values())
         self.assertNotIn(str(some_value), request.headers.values())
 
-    @skip("TODO")
+    # TODO
+    @tag("gh-2745")
     @given(
         st.one_of(
             st.lists(elements=st.one_of(st.text(), st.integers(), st.floats())),
@@ -245,7 +298,7 @@ class ServiceFetchConfigVariableBindingTests(SimpleTestCase):
         )
 
         # force unicode into a str with just characters in [\x00 .. \xff]
-        expected_value = str(field_value).strip().encode("utf-8").decode("latin1")
+        expected_value = str(field_value).encode("utf-8").decode("iso-8859-1").lstrip()
 
         with requests_mock.Mocker(case_sensitive=True) as m:
             m.get("https://httpbin.org/cache")
@@ -349,6 +402,7 @@ class ServiceFetchConfigVariableBindingTests(SimpleTestCase):
         self.assertEqual(value, "https://httpbin.org/get")
 
     @skip("This will go into an infinite loop")
+    @tag("dangerous", "gh-2744")
     @requests_mock.Mocker()
     def test_it_does_not_hang_on_infinite_jq_recursion(self, m):
         m.get("https://httpbin.org/get", json={"url": "https://httpbin.org/get"})

commit 861924b223ffa6989fcf0fff28f00bd38b70ffd2

🩹[#2819] Fix a bug introduced in 2fa33ca311
2fa33ca311 made the code do what the comment above said. The code was
correct.

src/openforms/variables/models.py
@@ -118,14 +118,14 @@ class ServiceFetchConfiguration(models.Model):
         #
         # Validating the contents of headers is beyond the scope of this
         # function, we only make sure the requests we send are within the
-        # Internet Standard spec. If some service needs empoji in the header,
+        # Internet Standard spec. If some service needs emoji in the header,
         # we force it into a sequence of bytes that is allowed.
         #
         # extra knowledge not in the RFC: latin1 is a different name for ISO-8859-1
 
         headers = {
-            # map all unicode into what the RFC allows with utf-8; remove leading space
-            header: value.format(**context).encode("utf-8").decode("latin1").lstrip()
+            # map all unicode into what the RFC allows with utf-8; remove padding space
+            header: value.format(**context).encode("utf-8").decode("latin1").strip()
             for header, value in (self.headers or {}).items()
         }
         # before we go further

src/openforms/submissions/tests/form_logic/test_fetching_form_varaiable_values_from_services.py
@@ -284,6 +284,7 @@ class ServiceFetchConfigVariableBindingTests(SimpleTestCase):
             st.datetimes(),
         )
     )
+    @example(field_value="0 ")
     def test_it_can_construct_simple_header_parameters(self, field_value):
         "Assert the happy path"
         # https://swagger.io/docs/specification/describing-parameters/#header-parameters
@@ -298,7 +299,7 @@ class ServiceFetchConfigVariableBindingTests(SimpleTestCase):
         )
 
         # force unicode into a str with just characters in [\x00 .. \xff]
-        expected_value = str(field_value).encode("utf-8").decode("iso-8859-1").lstrip()
+        expected_value = str(field_value).encode("utf-8").decode("iso-8859-1").strip()
 
         with requests_mock.Mocker(case_sensitive=True) as m:
             m.get("https://httpbin.org/cache")
@@ -312,7 +313,10 @@ class ServiceFetchConfigVariableBindingTests(SimpleTestCase):
         )
         # assert headers we sent were valid RFC 9110 (requests doesn't
         # guarantee this, it just checks for CR but it will happily send \x00)
-        HeaderValidator()(request.headers)
+        try:
+            HeaderValidator()(request.headers)
+        except ValidationError as e:
+            raise self.failureException("Constructed invalid header") from e
 
         # it shouldn't change other parts of the request
         self.assertIs(request.body, None)