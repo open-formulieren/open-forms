9b7ac906460ff47bf88fe0beb02229896289409f
SilviaAmAm <silvia@maykinmedia.nl>
2023-03-14 13:24:02 +0100

    :white_check_mark: [#2872] Test new task

--- src/openforms/submissions/tests/test_tasks_registration.py
+++ src/openforms/submissions/tests/test_tasks_registration.py
@@ -5,87 +5,8 @@ from unittest.mock import patch
 from django.db import close_old_connections
 from django.test import TestCase, TransactionTestCase
 
-from ..tasks.registration import (
-    generate_unique_submission_reference,
-    obtain_submission_reference,
-)
+from ..tasks.registration import generate_submission_reference
 from .factories import SubmissionFactory
-
-
-class ObtainSubmissionReferenceTests(TestCase):
-    @patch(
-        "openforms.submissions.tasks.registration.generate_unique_submission_reference"
-    )
-    def test_source_reference_from_registration_result(self, mock_generate):
-        """
-        Check that a reference is sourced from the registration result if it's available.
-        """
-        submission = SubmissionFactory.create(
-            form__registration_backend="zgw-create-zaak",
-            completed=True,
-            registration_success=True,
-            registration_result={"zaak": {"identificatie": "AEY64"}},
-        )
-
-        obtain_submission_reference(submission.id)
-
-        submission.refresh_from_db()
-        self.assertEqual(submission.public_registration_reference, "AEY64")
-        mock_generate.assert_not_called()
-
-    @patch(
-        "openforms.submissions.tasks.registration.generate_unique_submission_reference",
-        wraps=generate_unique_submission_reference,
-    )
-    def test_fallback_to_local_reference_generation(self, mock_generate):
-        """
-        Assert that there is always a reference generated.
-
-        Check that if sourcing the reference from the registration result fails, a
-        local reference is generated.
-        """
-        submission = SubmissionFactory.create(
-            form__registration_backend="zgw-create-zaak",
-            completed=True,
-            registration_success=True,
-            registration_result={"bad": {"result": "shape"}},
-        )
-
-        with patch(
-            "openforms.submissions.tasks.registration.get_random_string",
-            return_value="UNIQUE",
-        ):
-            obtain_submission_reference(submission.id)
-
-        submission.refresh_from_db()
-        self.assertEqual(submission.public_registration_reference, "OF-UNIQUE")
-        mock_generate.assert_called_once_with()
-
-    def test_reference_generator_checks_for_used_references(self):
-        RANDOM_STRINGS = ["UNIQUE", "OTHER"]
-        SubmissionFactory.create(
-            completed=True,
-            registration_success=True,
-            public_registration_reference=f"OF-{RANDOM_STRINGS[0]}",
-        )
-        submission = SubmissionFactory.create(
-            form__registration_backend="zgw-create-zaak",
-            completed=True,
-            registration_success=True,
-            registration_result={"bad": {"result": "shape"}},
-        )
-
-        def get_random_string(*args, **kwargs):
-            return RANDOM_STRINGS.pop(0)
-
-        with patch(
-            "openforms.submissions.tasks.registration.get_random_string",
-            new=get_random_string,
-        ):
-            obtain_submission_reference(submission.id)
-
-        submission.refresh_from_db()
-        self.assertEqual(submission.public_registration_reference, "OF-OTHER")
 
 
 class RaceConditionTests(TransactionTestCase):
@@ -124,7 +45,7 @@ class RaceConditionTests(TransactionTestCase):
                 "openforms.submissions.tasks.registration.generate_unique_submission_reference",
                 new=generate_unique_submission_reference,
             ):
-                obtain_submission_reference(submission1.id)
+                generate_submission_reference(submission1.id, post_registration=True)
                 close_old_connections()
 
         race_condition_thread = threading.Thread(target=race_condition)
@@ -140,3 +61,64 @@ class RaceConditionTests(TransactionTestCase):
         submission2.refresh_from_db()
         self.assertEqual(submission2.public_registration_reference, "OF-UNIQUE")
         self.assertEqual(submission1.public_registration_reference, "OF-OTHER")
+
+
+class GenerateSubmissionReferenceTests(TestCase):
+    @patch(
+        "openforms.submissions.tasks.registration.generate_unique_submission_reference",
+        return_value="OF-1234",
+    )
+    def test_pre_registration_no_registration_backend(self, m):
+        """If no registration backend is specified, the reference is generated before the registration"""
+
+        submission = SubmissionFactory.create(
+            completed=True,
+        )
+
+        self.assertEqual(submission.public_registration_reference, "")
+
+        generate_submission_reference(submission.id, post_registration=False)
+        submission.refresh_from_db()
+
+        self.assertEqual(submission.public_registration_reference, "OF-1234")
+
+    @patch(
+        "openforms.submissions.tasks.registration.generate_unique_submission_reference",
+        return_value="OF-1234",
+    )
+    def test_pre_registration_registration_backend_does_not_generate_reference(self, m):
+        """If the registration backend does not generate a reference, then reference is generated before the registration"""
+
+        submission = SubmissionFactory.create(
+            form__registration_backend="email",
+            completed=True,
+        )
+
+        self.assertEqual(submission.public_registration_reference, "")
+
+        generate_submission_reference(submission.id, post_registration=False)
+        submission.refresh_from_db()
+
+        self.assertEqual(submission.public_registration_reference, "OF-1234")
+
+    def test_registration_backend_generates_reference(self):
+        """If the registration backend sets the reference, then it should not be only set after registration"""
+
+        submission = SubmissionFactory.create(
+            form__registration_backend="zgw-create-zaak",
+            completed=True,
+            registration_success=True,
+            registration_result={"zaak": {"identificatie": "AEY64"}},
+        )
+
+        self.assertEqual(submission.public_registration_reference, "")
+
+        generate_submission_reference(submission.id, post_registration=False)
+        submission.refresh_from_db()
+
+        self.assertEqual(submission.public_registration_reference, "")
+
+        generate_submission_reference(submission.id, post_registration=True)
+        submission.refresh_from_db()
+
+        self.assertEqual(submission.public_registration_reference, "AEY64")
