1c4a5aa29cdca6e087ab1d8b278d803cd5900aa5
Sergei Maertens <sergei@maykinmedia.nl>
2023-03-16 16:18:41 +0100

    :pencil: Add instruction for Postgres 15 init
    To be turned into proper entrypoint initdb scripts.

--- docker-init-db.sql
+++ docker-init-db.sql
@@ -1,4 +1,5 @@
 CREATE USER openforms;
 CREATE DATABASE openforms;
 GRANT ALL PRIVILEGES ON DATABASE openforms TO openforms;
-
+-- On Postgres 15+, connect to the database and grant schema permissions.
+-- GRANT USAGE, CREATE ON SCHEMA public TO openforms;
