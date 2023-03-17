94babc5008d70e13ecb6a8a3e77a82fb128ecb8f
Sergei Maertens <sergei@maykinmedia.nl>
2023-03-17 16:18:48 +0100

    :white_check_mark: Added tests for ModelValidator

--- src/openforms/api/tests/test_validators.py
+++ src/openforms/api/tests/test_validators.py
@@ -1,6 +1,9 @@
+from django.core.exceptions import ValidationError
 from django.test import SimpleTestCase
 
 from rest_framework import serializers
+
+from openforms.accounts.models import User
 
 from ..validators import AllOrNoneRequiredFieldsValidator, ModelValidator
 
@@ -53,3 +56,61 @@ class AllOrNoneRequiredFieldsValidatorTests(SimpleTestCase):
                 is_valid = serializer.is_valid()
 
                 self.assertFalse(is_valid)
+
+
+def validate_not_staff(user: User):
+    err = ValidationError("May not be staff", code="privileges")
+    if user.is_staff:
+        raise ValidationError({"is_staff": err})
+
+
+def validate_non_field_attrs_not_set(user: User):
+    if hasattr(user, "extra_field"):
+        raise ValidationError("Non-model field attr set", code="bad_attr")
+
+
+class UserSerializer(serializers.ModelSerializer):
+    extra_field = serializers.CharField(required=False)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "is_staff", "extra_field")
+        extra_kwargs = {
+            "username": {
+                "validators": []
+            },  # disable unique validator which does queries
+        }
+        validators = [
+            ModelValidator[User](validate_not_staff),
+        ]
+
+
+class ModelValidatorTests(SimpleTestCase):
+    def test_runs_validator_function_on_serializer_data(self):
+        serializer = UserSerializer(
+            data={
+                "username": "bob",
+                "email": "bob@example.com",
+                "is_staff": True,
+            }
+        )
+
+        is_valid = serializer.is_valid()
+
+        self.assertFalse(is_valid)
+        self.assertIn("is_staff", serializer.errors)
+        self.assertEqual(serializer.errors["is_staff"][0].code, "privileges")
+
+    def test_non_model_field_attrs_not_set(self):
+        serializer = UserSerializer(
+            data={
+                "username": "bob",
+                "email": "bob@example.com",
+                "is_staff": False,
+                "extra_field": "may not be present",
+            }
+        )
+
+        is_valid = serializer.is_valid()
+
+        self.assertTrue(is_valid)
