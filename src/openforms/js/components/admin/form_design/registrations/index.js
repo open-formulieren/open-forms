caf6fd2710f6c802afe7e316cf5b843658032609
SilviaAmAm <silvia@maykinmedia.nl>
2023-04-12 16:22:02 +0200

    :lipstick: [#2872] Use custom ui widgets for the RJSF fields

--- src/openforms/js/components/admin/form_design/registrations/index.js
+++ src/openforms/js/components/admin/form_design/registrations/index.js
@@ -1,3 +1,5 @@
+import {WysiwygWidget} from 'components/admin/RJSFWrapper';
+
 import CamundaOptionsForm from './camunda';
 import {onStepEdit} from './handlers';
 
@@ -9,5 +11,16 @@ export const BACKEND_OPTIONS_FORMS = {
   camunda: {
     form: CamundaOptionsForm,
     onStepEdit: onStepEdit,
+  },
+  email: {
+    uiSchema: {
+      emailContentTemplateText: {
+        'ui:widget': 'textarea',
+        'ui:options': {
+          rows: 5,
+        },
+      },
+      emailContentTemplateHtml: {'ui:widget': WysiwygWidget},
+    },
   },
 };
