7be861cdb505a845871a5126269a8e5e6a6d3c40
Paul Schilling <mail@paulschilling.de>
2023-04-14 12:27:34 +0200

    [#2977] enforce stuf postcode uppercase
    Backport-of: openformulieren/open-forms#2991

--- src/openforms/registrations/contrib/stuf_zds/tests/test_backend.py
+++ src/openforms/registrations/contrib/stuf_zds/tests/test_backend.py
@@ -434,7 +434,7 @@ class StufZDSPluginTests(StUFZDSTestBase):
                 "voornaam": "Foo",
                 "achternaam": "Bar",
                 "tussenvoegsel": "de",
-                "postcode": "1000 AA",
+                "postcode": "1000 aa",
                 "geboortedatum": "2000-12-31",
                 "coordinaat": [52.36673378967122, 4.893164274470299],
                 "voorletters": "J.W.",
@@ -539,6 +539,7 @@ class StufZDSPluginTests(StUFZDSTestBase):
             },
         )
 
+        # postcode should also be converted (through template filters) to uppercase (#2977)
         with self.subTest("#2422: postcode must be normalized"):
             self.assertXPathEquals(
                 xml_doc,
