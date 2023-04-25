177c3fd6b3b984705acf112bbe6f30955eb83bfb
SilviaAmAm <silvia@maykinmedia.nl>
2023-04-25 12:13:04 +0200

    :bug: [#1959] Reset attribute_hashed when updating AuthInfo data from session

--- src/openforms/authentication/utils.py
+++ src/openforms/authentication/utils.py
@@ -21,14 +21,19 @@ class FormAuth(BaseAuth):
     machtigen: Optional[dict]
 
 
-def store_auth_details(submission: Submission, form_auth: FormAuth) -> None:
+def store_auth_details(
+    submission: Submission, form_auth: FormAuth, attribute_hashed: bool = False
+) -> None:
     attribute = form_auth["attribute"]
 
     assert (
         attribute in AuthAttribute.values
     ), f"Unexpected auth attribute {attribute} specified"
 
-    AuthInfo.objects.update_or_create(submission=submission, defaults=form_auth)
+    AuthInfo.objects.update_or_create(
+        submission=submission,
+        defaults={**form_auth, **{"attribute_hashed": attribute_hashed}},
+    )
 
 
 def store_registrator_details(
