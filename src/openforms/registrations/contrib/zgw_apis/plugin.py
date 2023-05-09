028ca06541f45352f42a331ec1d97053c6e737fc
SilviaAmAm <silvia@maykinmedia.nl>
2023-05-09 17:01:09 +0200

    :sparkles: [#1424] Implement pre-registration for ZGW

--- src/openforms/registrations/contrib/zgw_apis/plugin.py
+++ src/openforms/registrations/contrib/zgw_apis/plugin.py
@@ -1,3 +1,4 @@
+import logging
 from functools import partial, wraps
 from typing import Dict, List, Optional, Tuple
 
@@ -33,6 +34,8 @@ from ...utils import execute_unless_result_exists
 from .checks import check_config
 from .models import ZgwConfig
 from .validators import RoltypeOmschrijvingValidator
+
+logger = logging.getLogger(__name__)
 
 
 class ZaakOptionsSerializer(JsonSchemaSerializerMixin, serializers.Serializer):
@@ -150,32 +153,14 @@ class ZGWRegistration(BasePlugin):
         self, submission: Submission, options: dict
     ) -> Optional[dict]:
         """
-        Create a zaak and document with the submitted data as PDF.
-
-        NOTE: this requires that the report was generated before the submission is
-        being registered. See
-        :meth:`openforms.submissions.api.viewsets.SubmissionViewSet._complete` where
-        celery tasks are chained to guarantee this.
+        Add the PDF document with the submission data (confirmation report) to the zaak created during pre-registration.
         """
         zgw = ZgwConfig.get_solo()
         zgw.apply_defaults_to(options)
 
-        zaak_data = apply_data_mapping(
-            submission, self.zaak_mapping, REGISTRATION_ATTRIBUTE
-        )
+        result = submission.registration_result
+        zaak = result["zaak"]
 
-        _create_zaak = partial(
-            create_zaak,
-            options,
-            payment_required=submission.payment_required,
-            existing_reference=submission.public_registration_reference,
-            **zaak_data,
-        )
-        zaak = execute_unless_result_exists(
-            _create_zaak,
-            submission,
-            "intermediate.zaak",
-        )
         submission_report = SubmissionReport.objects.get(submission=submission)
         document = execute_unless_result_exists(
             partial(
@@ -188,7 +173,7 @@ class ZGWRegistration(BasePlugin):
             "intermediate.documents.report.document",
         )
         execute_unless_result_exists(
-            partial(relate_document, zaak["url"], document["url"]),
+            partial(relate_document, result["zaak"]["url"], document["url"]),
             submission,
             "intermediate.documents.report.relation",
         )
@@ -280,15 +265,19 @@ class ZGWRegistration(BasePlugin):
                 f"intermediate.documents.{attachment.id}.relation",
             )
 
-        result = {
-            "zaak": zaak,
-            "document": document,
-            "status": status,
-            "rol": rol,
-        }
+        result.update(
+            {
+                "document": document,
+                "status": status,
+                "rol": rol,
+            }
+        )
+
         if submission.has_registrator:
             result["medewerker_rol"] = medewerker_rol
 
+        submission.registration_result = result
+        submission.save()
         return result
 
     def get_reference_from_result(self, result: Dict[str, str]) -> str:
@@ -327,3 +316,38 @@ class ZGWRegistration(BasePlugin):
                 ),
             ),
         ]
+
+    @wrap_api_errors
+    def pre_register_submission(self, submission: "Submission", options: dict) -> None:
+        """
+        Create a Zaak, so that we can have a registration ID.
+
+        Note: The Rol, Status, the documents for the files uploaded by the user in the form (attachments) and the
+        confirmation report PDF will be added in the registration task (after the report has been generated).
+        """
+        zgw = ZgwConfig.get_solo()
+        zgw.apply_defaults_to(options)
+
+        zaak_data = apply_data_mapping(
+            submission, self.zaak_mapping, REGISTRATION_ATTRIBUTE
+        )
+
+        _create_zaak = partial(
+            create_zaak,
+            options,
+            payment_required=submission.payment_required,
+            existing_reference=submission.public_registration_reference,
+            **zaak_data,
+        )
+        zaak = execute_unless_result_exists(
+            _create_zaak,
+            submission,
+            "intermediate.zaak",
+        )
+
+        result = {"zaak": zaak}
+        submission.registration_result.update(result)
+        submission.public_registration_reference = self.get_reference_from_result(
+            result
+        )
+        submission.save()
