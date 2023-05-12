99a82b5bf82244e988b016a8eb1cec60ded9d787
bart-maykin <bart.burgman@maykinmedia.nl>
2023-05-12 17:39:23 +0200

    :green_heart: [#2999] override solo cache settings

--- src/openforms/registrations/contrib/microsoft_graph/tests/test_backend.py
+++ src/openforms/registrations/contrib/microsoft_graph/tests/test_backend.py
@@ -2,7 +2,7 @@ import json
 from decimal import Decimal
 from unittest.mock import patch
 
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.utils.translation import ugettext as _
 
 from freezegun import freeze_time
@@ -38,13 +38,14 @@ class MockFolder:
 
 
 @temp_private_root()
-@patch(
-    "openforms.registrations.contrib.microsoft_graph.models.MSGraphRegistrationConfig.get_solo",
-    return_value=MSGraphRegistrationConfig(service=MSGraphServiceFactory.create()),
-)
+@override_settings(SOLO_CACHE=None)
 class MSGraphRegistrationBackendTests(TestCase):
     @classmethod
     def setUpTestData(cls):
+        config = MSGraphRegistrationConfig.get_solo()
+        config.service = MSGraphServiceFactory.create()
+        config.save()
+
         cls.options = dict(folder_path="/open-forms/")
 
     @classmethod
@@ -53,7 +54,7 @@ class MSGraphRegistrationBackendTests(TestCase):
         clear_caches()
 
     @patch.object(MockFolder, "upload_file", return_value=None)
-    def test_submission(self, upload_mock, mock_get_solo):
+    def test_submission(self, upload_mock):
         data = {"foo": "bar", "some_list": ["value1", "value2"]}
 
         components = [
@@ -140,7 +141,7 @@ class MSGraphRegistrationBackendTests(TestCase):
             self.assertEqual(content, f"{_('payment required')}: € 11.35")
 
     @patch.object(MockFolder, "upload_file", return_value=None)
-    def test_update_payment_status(self, upload_mock, mock_get_solo):
+    def test_update_payment_status(self, upload_mock):
         data = {"foo": "bar", "some_list": ["value1", "value2"]}
 
         submission = SubmissionFactory.create(
@@ -185,18 +186,21 @@ class MSGraphRegistrationBackendTests(TestCase):
 
 
 @temp_private_root()
-@patch(
-    "openforms.registrations.contrib.microsoft_graph.models.MSGraphRegistrationConfig.get_solo",
-    return_value=MSGraphRegistrationConfig(service=MSGraphServiceFactory.create()),
-)
+@override_settings(SOLO_CACHE=None)
 @patch.object(MockFolder, "upload_file", return_value=None)
 class MSGraphRegistrationOptionsTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        config = MSGraphRegistrationConfig.get_solo()
+        config.service = MSGraphServiceFactory.create()
+        config.save()
+
     @classmethod
     def addClassCleanup(cls):
         # clear the config from cache
         clear_caches()
 
-    def test_folder_path(self, upload_mock, mock_get_solo):
+    def test_folder_path(self, upload_mock):
         submission = SubmissionFactory.from_components(
             components_list=[
                 {
@@ -238,7 +242,7 @@ class MSGraphRegistrationOptionsTests(TestCase):
             path = f"{folder}/data.json"
             self.assertEqual(call.args[1], path)
 
-    def test_folder_path_with_date(self, upload_mock, mock_get_solo):
+    def test_folder_path_with_date(self, upload_mock):
         submission = SubmissionFactory.from_components(
             components_list=[
                 {
