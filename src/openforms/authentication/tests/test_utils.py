686c3703eadf8fd9859a3855919637219e76ce90
SilviaAmAm <silvia@maykinmedia.nl>
2023-05-19 14:25:02 +0200

    :white_check_mark: [#1530] Test cosign attribute on form

--- src/openforms/authentication/tests/test_utils.py
+++ src/openforms/authentication/tests/test_utils.py
@@ -1,8 +1,12 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from django.urls import reverse
 
+from furl import furl
+
+from openforms.forms.tests.factories import FormStepFactory
 from openforms.submissions.tests.factories import SubmissionFactory
 
-from ..utils import store_auth_details, store_registrator_details
+from ..utils import get_cosign_login_info, store_auth_details, store_registrator_details
 
 
 class UtilsTests(TestCase):
@@ -23,3 +27,64 @@ class UtilsTests(TestCase):
                 submission,
                 {"plugin": "digid", "attribute": "WRONG", "value": "some-value"},
             )
+
+    def test_get_cosign_info_when_no_cosign_on_form(self):
+        form_step = FormStepFactory.create(
+            form_definition__configuration={
+                "components": [
+                    {"key": "textField", "label": "Not cosign", "type": "textfield"}
+                ]
+            }
+        )
+
+        factory = RequestFactory()
+        request = factory.get("/foo")
+
+        login_info = get_cosign_login_info(request=request, form=form_step.form)
+
+        self.assertIsNone(login_info)
+
+    def test_get_cosign_info(self):
+        form_step = FormStepFactory.create(
+            form__slug="form-with-cosign",
+            form_definition__configuration={
+                "components": [
+                    {
+                        "key": "cosignField",
+                        "label": "Cosign",
+                        "type": "cosign",
+                        "authPlugin": "digid",
+                    }
+                ]
+            },
+        )
+
+        factory = RequestFactory()
+        request = factory.get("/foo")
+
+        login_info = get_cosign_login_info(request=request, form=form_step.form)
+
+        self.assertIsNotNone(login_info)
+
+        login_url = furl(login_info["url"])
+        submission_find_url = furl(
+            login_url.args["next"]
+        )  # Page where the user will look for the submission to cosign
+
+        self.assertEqual(
+            login_url.path,
+            reverse(
+                "authentication:start",
+                kwargs={
+                    "slug": "form-with-cosign",
+                    "plugin_id": "digid",
+                },
+            ),
+        )
+        self.assertEqual(
+            submission_find_url.path,
+            reverse(
+                "submissions:find-submission-for-cosign",
+                kwargs={"form_slug": "form-with-cosign"},
+            ),
+        )
