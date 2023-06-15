9ac491a9cdb89dc412a9422a5e3dfeeec8104bc4
SilviaAmAm <silvia@maykinmedia.nl>
2023-06-15 12:46:55 +0200

    :sparkles: [#1530] Add migrations to update templates

---
+++ src/openforms/utils/regex.py
@@ -0,0 +1,9 @@
+import re
+
+
+def add_cosign_info_templatetag(template: str) -> str:
+    pattern = re.compile(
+        r"(\{% summary %\}|\{% appointment_information %\}|\{% payment_information %\})"
+    )
+    replacement = r"{% cosign_info %}\n\1"
+    return pattern.sub(replacement, template, count=1)
