531c27a6c55abc15428b5c195b7a3e20a33cc471
SilviaAmAm <silvia@maykinmedia.nl>
2023-06-29 11:32:29 +0200

    :bug: [#3182] Add cosign tag if missing

--- src/openforms/emails/api/serializers.py
+++ src/openforms/emails/api/serializers.py
@@ -1,6 +1,9 @@
+import re
+
 from rest_framework import serializers
 
 from openforms.translations.api.serializers import ModelTranslationsSerializer
+from openforms.utils.migrations_utils.regex import add_cosign_info_templatetag
 
 from ..models import ConfirmationEmailTemplate
 
@@ -15,3 +18,23 @@ class ConfirmationEmailTemplateSerializer(serializers.ModelSerializer):
             "content",
             "translations",
         )
+
+    def to_internal_value(self, data):
+        if content := data.get("content"):
+            data["content"] = self._add_cosign_templatetag(content)
+
+        return super().to_internal_value(data)
+
+    def _add_cosign_templatetag(self, content):
+        is_import = self.context.get("is_import", False)
+
+        if not is_import:
+            return content
+
+        pattern = re.compile(r"\{%\s?cosign_information\s?%\}")
+        match = pattern.search(content)
+
+        if not match:
+            content = add_cosign_info_templatetag(content)
+
+        return content
