0bd23171b3dd081037c336e8fbc672f239bda412
vasileios <zigras00@gmail.com>
2023-07-07 10:48:06 +0200

    [#3130] Added method for str truncate and failing test for formdefinition copy

--- src/openforms/forms/tests/test_formdefinition_admin.py
+++ src/openforms/forms/tests/test_formdefinition_admin.py
@@ -1,9 +1,13 @@
+from django.test import override_settings
 from django.urls import reverse
 
 from django_webtest import WebTest
 
 from openforms.accounts.tests.factories import StaffUserFactory, SuperUserFactory
 from openforms.tests.utils import disable_2fa
+
+from ..models.form_definition import FormDefinition
+from .factories import FormDefinitionFactory
 
 
 @disable_2fa
@@ -26,3 +30,33 @@ class FormDefinitionAdminTests(WebTest):
         response = self.app.get(url, user=user)
 
         self.assertEqual(response.status_code, 200)
+
+    @override_settings(LANGUAGE_CODE="en")
+    def test_copy_with_long_names_succeeds(self):
+        user = SuperUserFactory.create()
+        FormDefinitionFactory.create(
+            name="This is a really long long name for formdefinition",
+            internal_name="This is a really long long name for formdefinition",
+        )
+
+        self.assertEqual(FormDefinition.objects.count(), 1)
+
+        url = reverse("admin:forms_formdefinition_changelist")
+        response = self.app.get(url, user=user)
+        form = response.forms["changelist-form"]
+        form["action"] = "make_copies"
+        form["_selected_action"].checked = True
+        response = form.submit("submit", value="0")
+
+        form_definition_copy = FormDefinition.objects.last()
+
+        self.assertEqual(FormDefinition.objects.count(), 2)
+        self.assertRedirects(response, reverse("admin:forms_formdefinition_changelist"))
+        self.assertEqual(
+            form_definition_copy.name,
+            "This is a really long long name for formde\u2026 (copy)",
+        )
+        self.assertEqual(
+            form_definition_copy.internal_name,
+            "This is a really long long name for formde\u2026 (copy)",
+        )
---
+++ src/openforms/utils/helpers.py
@@ -0,0 +1,12 @@
+def truncate_str_if_needed(original_str: str, new_str: str, max_length: int) -> str:
+    """
+    Given the original string and the new string, return either the original one if
+    it doesn't exceed the given max length, or the new one truncated and by adding
+    the ellipsis to the original one.
+    """
+    if len(new_str) <= max_length:
+        return new_str
+
+    new_str_length = len(new_str) - len(original_str)
+    str_max_length = max_length - new_str_length
+    return original_str[: str_max_length - 1] + "\u2026" + new_str[-new_str_length:]
