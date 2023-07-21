a0b8309ef6c6173a57b3578fe2b6054b7791067e
Sergei Maertens <sergei@maykinmedia.nl>
2023-07-21 13:08:30 +0200

    :bug: Ensure the client context is proxied to the underlying client
    The context is set on the top-level client instance, but for HC tihs
    wraps the zgw consumers client instance, which actually requires the
    context.
    
    Now the context is properly passed along to the wrapped client.

--- src/openforms/prefill/contrib/haalcentraal/client.py
+++ src/openforms/prefill/contrib/haalcentraal/client.py
@@ -4,8 +4,8 @@ from typing import Protocol
 
 from requests import RequestException
 from zds_client import ClientError
-from zgw_consumers.client import ZGWClient
 
+from openforms.pre_requests.clients import PreRequestClientContext, PreRequestZGWClient
 from openforms.typing import JSONObject
 
 from .constants import AttributesV2
@@ -14,7 +14,9 @@ logger = logging.getLogger(__name__)
 
 
 class HaalCentraalClient(Protocol):
-    def __init__(self, service_client: ZGWClient):  # pragma: no cover
+    context: PreRequestClientContext | None
+
+    def __init__(self, service_client: PreRequestZGWClient):  # pragma: no cover
         ...
 
     def find_person(self, bsn: str, **kwargs) -> JSONObject | None:  # pragma: no cover
@@ -24,13 +26,23 @@ class HaalCentraalClient(Protocol):
         ...
 
 
-class HaalCentraalV1Client:
+class BaseClient:
+    def __init__(self, service_client: PreRequestZGWClient):
+        self.service_client = service_client
+
+    @property
+    def context(self) -> PreRequestClientContext | None:
+        return self.service_client.context
+
+    @context.setter
+    def context(self, context: PreRequestClientContext) -> None:
+        self.service_client.context = context
+
+
+class HaalCentraalV1Client(BaseClient):
     """
     Haal Centraal 1.3 compatible client.
     """
-
-    def __init__(self, service_client: ZGWClient):
-        self.service_client = service_client
 
     def find_person(self, bsn: str, **kwargs) -> JSONObject | None:
         try:
@@ -57,13 +69,10 @@ class HaalCentraalV1Client:
             raise
 
 
-class HaalCentraalV2Client:
+class HaalCentraalV2Client(BaseClient):
     """
     Haal Centraal 2.0 compatible client.
     """
-
-    def __init__(self, service_client: ZGWClient):
-        self.service_client = service_client
 
     def find_person(self, bsn: str, **kwargs) -> JSONObject | None:
         attributes: Sequence[str] = kwargs.pop("attributes")
