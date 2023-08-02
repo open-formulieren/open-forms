18f8c1390dc10c62efc7fcd6e32723138a85305a
Sergei Maertens <sergei@maykinmedia.nl>
2023-08-02 16:50:15 +0200

    :technologist: [#2017] Expose form steps in the admin for development

--- src/openforms/forms/admin/__init__.py
+++ src/openforms/forms/admin/__init__.py
@@ -2,5 +2,6 @@ from . import category  # noqa
 from . import form  # noqa
 from . import form_definition  # noqa
 from . import form_logic  # noqa
+from . import form_step  # noqa
 from . import form_variable  # noqa
 from . import form_version  # noqa
