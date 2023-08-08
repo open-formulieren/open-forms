180ba38f2d6473a23f761c52ad3f6c533c74824b
Chris Wesseling <chris@maykinmedia.nl>
2023-08-08 13:30:57 +0200

    💚 Force reset to latest migration after migration test

--- src/openforms/utils/tests/test_migrations.py
+++ src/openforms/utils/tests/test_migrations.py
@@ -1,3 +1,4 @@
+from django.core.management import call_command
 from django.db import connection
 from django.db.migrations.executor import MigrationExecutor
 from django.test import TransactionTestCase, override_settings
@@ -48,3 +49,9 @@ class TestMigrations(TransactionTestCase):
 
     def setUpBeforeMigration(self, apps):
         pass
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        # reset to latest migration
+        call_command("migrate", verbosity=0, database=connection._alias)
