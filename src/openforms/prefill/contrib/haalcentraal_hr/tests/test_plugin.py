add83d55a5f9a35904588c59d9ccf7b721504258
SilviaAmAm <silvia@maykinmedia.nl>
2023-08-17 14:02:38 +0200

    :white_check_mark: [#3185] Test prefill Haal centraal HR

--- src/openforms/prefill/contrib/haalcentraal_hr/tests/test_plugin.py
+++ src/openforms/prefill/contrib/haalcentraal_hr/tests/test_plugin.py
@@ -1,21 +1,21 @@
 import json
-import os
 from pathlib import Path
 from unittest.mock import patch
-from zgw_consumers.test import mock_service_oas_get
 
 from django.test import TestCase
 
 from requests_mock import Mocker
+from zgw_consumers.constants import APITypes
+from zgw_consumers.test import mock_service_oas_get
 
 from openforms.authentication.constants import AuthAttribute
 from openforms.registrations.contrib.zgw_apis.tests.factories import ServiceFactory
 from openforms.submissions.tests.factories import SubmissionFactory
-from soap.tests.factories import SoapServiceFactory
 
-from ...haalcentraal.tests.utils import load_binary_mock
 from ..models import HaalCentraalHRConfig
 from ..plugin import HaalCentraalHRPrefill
+
+FILES_DIR = Path(__file__).parent / "files"
 
 
 class HaalCentraalHRPluginTests(TestCase):
@@ -36,28 +36,41 @@ class HaalCentraalHRPluginTests(TestCase):
         self.assertEqual(data, {})
 
     @Mocker()
-    def test_error_while_getting_saml_token(self, m):
-        plugin = HaalCentraalHRPrefill("haalcentraal_hr")
-        submission = SubmissionFactory.create(
-            auth_info__attribute=AuthAttribute.kvk, auth_info__value="111111111"
+    def test_happy_flow(self, m):
+        mock_service_oas_get(
+            m,
+            url="http://haalcentraal-hr.nl/api/",
+            service="haalcentraal-hr-oas",
+            oas_url="https://haalcentraal-hr.nl/api/schema/openapi.yaml",
         )
 
-        mock_service_oas_get(m, "http://haalcentraal-hr.nl/haalcentraal/api/hr", oas_url="http://haalcentraal-hr.nl/api/oas", service="haalcentraal-hr-oas")
-        path_response = Path(__file__).parent / "files" / "maatschapplelijkeactiviteiten-response.json"
-        m.get("http://haalcentraal-hr.nl/haalcentraal/api/hr/maatschappelijkeactiviteiten/111111111", json=json.loads(path_response.read_bytes()))
+        with open(FILES_DIR / "maatschapplelijkeactiviteiten-response.json", "rb") as f:
+            m.get(
+                "http://haalcentraal-hr.nl/api/maatschappelijkeactiviteiten/111222333",
+                json=json.load(f),
+            )
+
+        plugin = HaalCentraalHRPrefill("haalcentraal_hr")
+        submission = SubmissionFactory.create(
+            auth_info__attribute=AuthAttribute.kvk, auth_info__value="111222333"
+        )
+        service = ServiceFactory.create(
+            api_type=APITypes.orc,
+            api_root="http://haalcentraal-hr.nl/api/",
+            oas="https://haalcentraal-hr.nl/api/schema/openapi.yaml",
+        )
 
         with patch(
             "openforms.prefill.contrib.haalcentraal_hr.plugin.HaalCentraalHRConfig.get_solo",
-            return_value=HaalCentraalHRConfig(
-                token_service=SoapServiceFactory.create(
-                    url="http://token-service.nl/tokenservice",
-                ),
-                service=ServiceFactory.create(api_root="http://haalcentraal-hr.nl/haalcentraal/api/hr", oas="http://haalcentraal-hr.nl/api/oas")
-            ),
-        ), patch(
-            "openforms.prefill.contrib.haalcentraal_hr.plugin.HaalCentraalHRPrefill.get_saml_token",
-            return_value="the-magic-token",
-        ):  # TODO fix when you get WSDL
-            data = plugin.get_prefill_values(submission, ["heeftAlsEigenaar.natuurlijkPersoon.burgerservicenummer"])
+            return_value=HaalCentraalHRConfig(service=service),
+        ):
+            data = plugin.get_prefill_values(
+                submission,
+                ["kvkNummer", "heeftAlsEigenaar.natuurlijkPersoon.burgerservicenummer"],
+            )
 
-        self.assertEqual(data, {"heeftAlsEigenaar.natuurlijkPersoon.burgerservicenummer": "555555021"})
+        self.assertEqual(data["kvkNummer"], "111222333")
+        self.assertEqual(
+            data["heeftAlsEigenaar.natuurlijkPersoon.burgerservicenummer"],
+            "555555021",
+        )
