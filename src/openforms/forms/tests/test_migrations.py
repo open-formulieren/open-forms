46f42754434fb67cbdd0ad0d5623c95d4ac7bf3b
SilviaAmAm <silvia@maykinmedia.nl>
2023-09-04 15:58:31 +0200

    :white_check_mark: [#1884] Test migration to have the new errors in the time component

--- src/openforms/forms/tests/test_migrations.py
+++ src/openforms/forms/tests/test_migrations.py
@@ -1047,3 +1047,58 @@ class RevertEnableMultipleRegistrationBackendsTest(TestMigrations):
                 "to_emails": ["tome@example.com", "toyou@example.com"],
             },
         )
+
+
+class TestAddMoreCustomErrorMessagesTimeComponent(TestMigrations):
+    app = "forms"
+    migrate_from = "0091_auto_20230831_1152"
+    migrate_to = "0092_more_time_custom_errors"
+
+    def setUpBeforeMigration(self, apps):
+        FormDefinition = apps.get_model("forms", "FormDefinition")
+        FormStep = apps.get_model("forms", "FormStep")
+        Form = apps.get_model("forms", "Form")
+
+        self.form_def = FormDefinition.objects.create(
+            name="Time",
+            slug="time",
+            configuration={
+                "components": [
+                    {
+                        "key": "timeWithCustomError",
+                        "type": "time",
+                        "label": "Tijd with custom error",
+                        "maxTime": "13:00:00",
+                        "minTime": "12:00:00",
+                        "translatedErrors": {
+                            "en": {"required": "", "invalid_time": "Tralala"},
+                            "nl": {"required": "", "invalid_time": "Tralala"},
+                        },
+                    }
+                ]
+            },
+        )
+        form = Form.objects.create(name="Form time")
+
+        FormStep.objects.create(form=form, form_definition=self.form_def, order=0)
+
+    def test_the_custom_error_is_added(self):
+        self.form_def.refresh_from_db()
+
+        self.assertEqual(
+            self.form_def.configuration["components"][0]["translatedErrors"],
+            {
+                "en": {
+                    "required": "",
+                    "invalid_time": "Tralala",
+                    "minTime": "",
+                    "maxTime": "",
+                },
+                "nl": {
+                    "required": "",
+                    "invalid_time": "Tralala",
+                    "minTime": "",
+                    "maxTime": "",
+                },
+            },
+        )
