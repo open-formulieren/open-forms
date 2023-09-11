bbc9aecf65f78858a49134089bf922dfa73f4b1f
Sergei Maertens <sergei@maykinmedia.nl>
2023-09-11 15:57:48 +0200

    :art: Rewrite stuf/soap service migration history
    Turn the separate-database-and-state operations into real ones,
    sorting out the correct order of migrations in the
    squashed migrations.
    
    Existing installations are already in the correct state, and
    the squashed migrations are executed for fresh installs which
    sets up things the right way from the get-go rather than
    using advanced migration techniques.

--- src/stuf/migrations/0001_initial_pre_openforms_v230.py
+++ src/stuf/migrations/0001_initial_pre_openforms_v230.py
@@ -22,109 +22,10 @@ class Migration(migrations.Migration):
     ]
 
     dependencies = [
-        ("simple_certmanager", "0001_initial"),
+        ("soap", "0001_initial"),
     ]
 
     operations = [
-        migrations.CreateModel(
-            name="SoapService",
-            fields=[
-                (
-                    "id",
-                    models.AutoField(
-                        auto_created=True,
-                        primary_key=True,
-                        serialize=False,
-                        verbose_name="ID",
-                    ),
-                ),
-                (
-                    "url",
-                    models.URLField(
-                        blank=True,
-                        help_text="URL of the service to connect to.",
-                        verbose_name="URL",
-                    ),
-                ),
-                (
-                    "label",
-                    models.CharField(
-                        default="LABEL MISSING",
-                        help_text="Human readable label to identify services",
-                        max_length=100,
-                        verbose_name="label",
-                    ),
-                ),
-                (
-                    "soap_version",
-                    models.CharField(
-                        choices=[("1.1", "SOAP 1.1"), ("1.2", "SOAP 1.2")],
-                        default="1.2",
-                        help_text="The SOAP version to use for the message envelope.",
-                        max_length=5,
-                        verbose_name="SOAP version",
-                    ),
-                ),
-                (
-                    "client_certificate",
-                    models.ForeignKey(
-                        blank=True,
-                        help_text="The SSL certificate file used for client identification. If left empty, mutual TLS is disabled.",
-                        null=True,
-                        on_delete=django.db.models.deletion.PROTECT,
-                        related_name="soap_services_client",
-                        to="simple_certmanager.certificate",
-                    ),
-                ),
-                (
-                    "endpoint_security",
-                    models.CharField(
-                        blank=True,
-                        choices=[
-                            ("basicauth", "Basic authentication"),
-                            ("wss", "SOAP extension: WS-Security"),
-                            ("wss_basicauth", "Both"),
-                        ],
-                        help_text="The security to use for messages sent to the endpoints.",
-                        max_length=20,
-                        verbose_name="Security",
-                    ),
-                ),
-                (
-                    "password",
-                    models.CharField(
-                        blank=True,
-                        help_text="Password to use in the XML security context.",
-                        max_length=200,
-                        verbose_name="password",
-                    ),
-                ),
-                (
-                    "server_certificate",
-                    models.ForeignKey(
-                        blank=True,
-                        help_text="The SSL/TLS certificate of the server",
-                        null=True,
-                        on_delete=django.db.models.deletion.PROTECT,
-                        related_name="soap_services_server",
-                        to="simple_certmanager.certificate",
-                    ),
-                ),
-                (
-                    "user",
-                    models.CharField(
-                        blank=True,
-                        help_text="Username to use in the XML security context.",
-                        max_length=200,
-                        verbose_name="user",
-                    ),
-                ),
-            ],
-            options={
-                "verbose_name": "SOAP service",
-                "verbose_name_plural": "SOAP services",
-            },
-        ),
         migrations.CreateModel(
             name="StufService",
             fields=[
@@ -237,7 +138,7 @@ class Migration(migrations.Migration):
                         help_text="The soap service this stuf service uses",
                         on_delete=django.db.models.deletion.CASCADE,
                         related_name="stuf_service",
-                        to="stuf.soapservice",
+                        to="soap.soapservice",
                     ),
                 ),
             ],
