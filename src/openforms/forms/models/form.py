457ab2fece22845e81b3ee2b1e6f2f392c6841dc
vasileios <zigras00@gmail.com>
2023-09-18 12:11:37 +0200

    [#3378] Reduce sql queries concerning logic,removed unnecessary test configs

--- src/openforms/forms/models/form.py
+++ src/openforms/forms/models/form.py
@@ -471,7 +471,7 @@ class Form(models.Model):
 
             form_step.save()
 
-        for logic in self.formlogic_set.all():
+        for logic in self.formlogic_set.all().select_related("trigger_from_step"):
             logic.pk = None
             logic.uuid = _uuid.uuid4()
             logic.form = copy
