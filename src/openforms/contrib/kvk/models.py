a114d9ab1b24f15491ec744e7b41f4f076f10796
Sergei Maertens <sergei@maykinmedia.nl>
2023-09-21 17:45:15 +0200

    :card_file_box: [#3489] Remove now-obsolete configuration fields
    The client situation is refactored so that the OAS files are no longer
    needed, hence the difference services aren't either and a common,
    single service is sufficient to configure KVK interaction.

--- src/openforms/contrib/kvk/models.py
+++ src/openforms/contrib/kvk/models.py
@@ -9,8 +9,9 @@ class KVKConfigManager(models.Manager):
     def get_queryset(self):
         qs = super().get_queryset()
         return qs.select_related(
-            "_service",
-            "_profiles",
+            "service",
+            "service__client_certificate",
+            "service__server_certificate",
         )
 
 
@@ -29,47 +30,7 @@ class KVKConfig(SingletonModel):
         null=True,
     )
 
-    _service = models.OneToOneField(
-        "zgw_consumers.Service",
-        verbose_name=_("KvK API Zoeken"),
-        help_text=_("API used for validation of KvK, RSIN and vestigingsnummer's"),
-        on_delete=models.PROTECT,
-        limit_choices_to={"api_type": APITypes.orc},
-        related_name="+",
-        null=True,
-    )
-
-    _profiles = models.OneToOneField(
-        "zgw_consumers.Service",
-        verbose_name=_("KvK API Basisprofiel"),
-        help_text=_("API used to retrieve basis profielen"),
-        on_delete=models.PROTECT,
-        limit_choices_to={"api_type": APITypes.orc},
-        related_name="+",
-        null=True,
-    )
-
     objects = KVKConfigManager()
 
     class Meta:
         verbose_name = _("KvK configuration")
-
-    # FIXME: Patch ZGW Consumer behaviour to allow for 2 KvK API's.
-    #
-    # The KvK uses a single API root path to serve 2 API's. They differ in path
-    # but the path is included in the schema and thus is added automatically by
-    # ZGW Consumer. You cannot store 2 API's with the same API root, and adding
-    # their API base path causes the API base path to be duplicated in
-    # requests.
-
-    @property
-    def legacy_service(self):
-        s = self._service
-        s.api_root = s.api_root.replace("/v1/zoeken", "")
-        return s
-
-    @property
-    def profiles(self):
-        p = self._profiles
-        p.api_root = p.api_root.replace("/v1/basisprofielen", "")
-        return p
