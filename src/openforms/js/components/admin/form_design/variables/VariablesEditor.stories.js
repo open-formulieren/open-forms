897b823a559f20af4d7d4a562f14511f5142ef87
Sergei Maertens <sergei@maykinmedia.nl>
2023-10-23 16:08:53 +0200

    :pencil: [#2629] Add story for form variables editor

---
+++ src/openforms/js/components/admin/form_design/variables/VariablesEditor.stories.js
@@ -0,0 +1,44 @@
+import {FormDecorator} from '../story-decorators';
+import VariablesEditor from './VariablesEditor';
+
+export default {
+  title: 'Form design / Variables editor',
+  component: VariablesEditor,
+  decorators: [FormDecorator],
+  args: {
+    variables: [
+      {
+        form: 'http://localhost:8000/api/v2/forms/36612390',
+        formDefinition: 'http://localhost:8000/api/v2/form-definitions/6de1ea5a',
+        name: 'Form.io component',
+        key: 'formioComponent',
+        source: 'component',
+        prefillPlugin: '',
+        prefillAttribute: '',
+        prefillIdentifierRole: 'main',
+        dataType: 'string',
+        dataFormat: undefined,
+        isSensitiveData: false,
+        serviceFetchConfiguration: undefined,
+        initialValue: '',
+      },
+      {
+        form: 'http://localhost:8000/api/v2/forms/36612390',
+        formDefinition: undefined,
+        name: 'User defined',
+        key: 'userDefined',
+        source: 'user_defined',
+        prefillPlugin: '',
+        prefillAttribute: '',
+        prefillIdentifierRole: 'main',
+        dataType: 'array',
+        dataFormat: undefined,
+        isSensitiveData: false,
+        serviceFetchConfiguration: undefined,
+        initialValue: [],
+      },
+    ],
+  },
+};
+
+export const Default = {};
