5fb8ae861eef23dd3c46a45b89adf17be00ef0d5
Sergei Maertens <sergei@maykinmedia.nl>
2023-11-09 09:16:41 +0100

    :sparkles: Set up management command for CSP form-action directive generation
    This is so the configuration is run automatically on existing instances,
    otherwise the lack of form-action directive values combined with our
    new setting in 2.4 would prevent DigiD/eHerkenning login or payment,
    unless the admins manually re-save the configuration the admin.
    
    This is not manageable if you have many instances.

---
+++ src/openforms/config/management/commands/create_csp_form_action_directives_from_config.py
@@ -0,0 +1,31 @@
+from django.core.management import BaseCommand
+
+from digid_eherkenning.models import DigidConfiguration, EherkenningConfiguration
+
+from openforms.payments.contrib.ogone.models import OgoneMerchant
+
+
+class Command(BaseCommand):
+    help = (
+        "Introspect the runtime configuration and generate the relevant CSP "
+        "form-action directives"
+    )
+
+    def handle(self, **options):
+        ogone_merchants = OgoneMerchant.objects.exclude(
+            endpoint_preset="",
+            endpoint_custom="",
+        )
+        for ogone_merchant in ogone_merchants:
+            ogone_merchant.save()  # triggers CSPSetting record creation
+
+        for model_cls in (DigidConfiguration, EherkenningConfiguration):
+            configured_instance = model_cls.objects.exclude(
+                idp_metadata_file=""
+            ).first()
+            if configured_instance is None:
+                continue
+
+            configured_instance.save()  # triggers CSPSetting record creation
+
+        self.stdout.write("Nothing to do.")
