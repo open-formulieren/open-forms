55d7546b9ebab01ec73a722e0e0abd6f5d97d674
Sergei Maertens <sergei@maykinmedia.nl>
2023-11-21 10:10:26 +0100

    :wrench: [#3620] Launch storybook with version popup disabled
    Found the config options via the TypeScript definitions :)

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   core: {
     disableTelemetry: true,
+    disableWhatsNewNotifications: true,
   },
   stories: ['../src/openforms/js/**/*.mdx', '../src/openforms/js/**/*.stories.@(js|jsx|ts|tsx)'],
 
