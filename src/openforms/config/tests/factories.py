a84e807edd9f6a21f4b8038141f52e29e4eb4268
Sergei Maertens <sergei@maykinmedia.nl>
2023-12-07 18:37:31 +0100

    :white_check_mark: [#3403] Add tests to address drops in coverage

--- src/openforms/config/tests/factories.py
+++ src/openforms/config/tests/factories.py
@@ -1,6 +1,14 @@
 import factory
 
 
+class RichTextColorFactory(factory.django.DjangoModelFactory):
+    color = factory.Faker("hex_color")
+    label = factory.Faker("color_name")
+
+    class Meta:
+        model = "config.RichTextColor"
+
+
 class ThemeFactory(factory.django.DjangoModelFactory):
     name = factory.Faker("bs")
 
