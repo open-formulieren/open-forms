5ee9426cff14c4c0626803b7c8d7dab7d4fabdc6
vasileios <zigras00@gmail.com>
2023-12-15 14:45:57 +0100

    [#3692] Fixed next url when authenticated user cancels login
    Backport-of: #3696
    Co-authored-by: @SilviaAmAm

--- src/openforms/authentication/contrib/digid_eherkenning_oidc/tests/digid/test_auth_procedure.py
+++ src/openforms/authentication/contrib/digid_eherkenning_oidc/tests/digid/test_auth_procedure.py
@@ -8,6 +8,7 @@ from furl import furl
 from rest_framework import status
 
 from digid_eherkenning_oidc_generics.models import OpenIDConnectPublicConfig
+from openforms.accounts.tests.factories import StaffUserFactory
 from openforms.authentication.views import BACKEND_OUTAGE_RESPONSE_PARAMETER
 from openforms.forms.tests.factories import FormFactory
 
@@ -149,7 +150,7 @@ class DigiDOIDCTests(TestCase):
         ).set({"next": redirect_form_url})
 
         session = self.client.session
-        session["oidc_login_next"] = redirect_url.url
+        session["of_redirect_next"] = redirect_url.url
         session.save()
 
         with patch(
@@ -323,11 +324,11 @@ class DigiDOIDCTests(TestCase):
         )
         self.assertEqual(query_params["kc_idp_hint"], "oidc-digid")
 
-    @tag("gh-3656")
-    # This is an example of a specific provider. It may differs when a different provider is used.
+    @tag("gh-3656", "gh-3692")
+    # This is an example of a specific provider. It may differ when a different provider is used.
     # According to https://openid.net/specs/openid-connect-core-1_0.html#AuthError and
     # https://www.rfc-editor.org/rfc/rfc6749.html#section-4.1.2.1 , this is the error we expect from OIDC
-    def test_redirect_to_form_login_cancelled(self):
+    def test_redirect_to_form_when_login_cancelled_by_anonymous_user(self):
         form_path = reverse("core:form-detail", kwargs={"slug": self.form.slug})
         form_url = f"http://testserver{form_path}"
         redirect_form_url = furl(form_url).set({"_start": "1"})
@@ -339,7 +340,39 @@ class DigiDOIDCTests(TestCase):
         ).set({"next": redirect_form_url})
 
         session = self.client.session
-        session["oidc_login_next"] = redirect_url.url
+        session["of_redirect_next"] = redirect_url.url
+        session.save()
+
+        response = self.client.get(
+            reverse("digid_oidc:callback"),
+            {"error": "access_denied", "error_description": "The user cancelled"},
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+        parsed = furl(response.url)
+        query_params = parsed.query.params
+
+        self.assertEqual(query_params["_digid-message"], "login-cancelled")
+        self.assertIsNone(query_params.get("of-auth-problem"))
+
+    @tag("gh-3656", "gh-3692")
+    def test_redirect_to_form_when_login_cancelled_by_authenticated_user(self):
+        user = StaffUserFactory.create()
+        self.client.force_login(user=user)
+
+        form_path = reverse("core:form-detail", kwargs={"slug": self.form.slug})
+        form_url = f"http://testserver{form_path}"
+        redirect_form_url = furl(form_url).set({"_start": "1"})
+        redirect_url = furl(
+            reverse(
+                "authentication:return",
+                kwargs={"slug": self.form.slug, "plugin_id": "digid_oidc"},
+            )
+        ).set({"next": redirect_form_url})
+
+        session = self.client.session
+        session["of_redirect_next"] = redirect_url.url
         session.save()
 
         response = self.client.get(
