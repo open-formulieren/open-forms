6c1d1cf936bfe7fbbf9a83be64ae5810f71dc7f4
vasileios <zigras00@gmail.com>
2024-01-23 11:35:44 +0100

    [#3600] Added story for form fields

--- src/openforms/js/components/admin/form_design/registrations/zgw/ZGWOptionsFormFields.stories.js
+++ src/openforms/js/components/admin/form_design/registrations/zgw/ZGWOptionsFormFields.stories.js
@@ -1,10 +1,80 @@
-import {FormDecorator} from 'components/admin/form_design/story-decorators';
+import {useArgs} from '@storybook/client-api';
 
-import ZGWOptionsForm from './ZGWOptionsForm';
+import {FormDecorator} from 'components/admin/form_design/story-decorators';
+import Field from 'components/admin/forms/Field';
+import Fieldset from 'components/admin/forms/Fieldset';
+import FormRow from 'components/admin/forms/FormRow';
+
+import ZGWFormFields from './ZGWOptionsFormFields';
+import VariablePropertyModal from './ZGWOptionsVariablesProperties';
+
+const render = ({index, label, name, schema}) => {
+  const [{formData}, updateArgs] = useArgs();
+  const onChange = newValues => {
+    updateArgs({formData: newValues});
+  };
+
+  return (
+    <Fieldset>
+      <FormRow>
+        <Field name={name} label={label}>
+          <ZGWFormFields
+            index={index}
+            name={name}
+            schema={schema}
+            formData={formData}
+            onChange={onChange}
+          />
+        </Field>
+      </FormRow>
+    </Fieldset>
+  );
+};
 
 export default {
   title: 'Form design/Registration/ZGW',
-  component: ZGWOptionsForm,
+  component: VariablePropertyModal,
+  decorators: [FormDecorator],
+  render,
+  args: {
+    schema: {
+      properties: {
+        zgwApiGroup: {
+          enum: [1],
+          enumNames: ['ZGW API'],
+        },
+        contentJson: '',
+        informatieobjecttype: '',
+        medewerkerRoltype: '',
+        objecttype: '',
+        objecttypeVersion: '',
+        organisatieRsin: '',
+        variablesProperties: [
+          {
+            componentKey: '',
+            eigenshap: '',
+          },
+        ],
+        zaakVertrouwelijkheidaanduiding: {
+          enum: ['openbaar'],
+          enumNames: ['Openbaar'],
+        },
+        zaaktype: 'http://example.com',
+      },
+    },
+    formData: {
+      variablesProperties: [{eigenshap: '', componentKey: ''}],
+      zaaktype: '',
+    },
+    availableComponents: {
+      textField1: {
+        label: 'textfield1',
+      },
+      textField2: {
+        label: 'textfield2',
+      },
+    },
+  },
 };
 
 export const Default = {};
