cdc9a9238d5f99b3ff6593afbc0d7ca481e95f56
vasileios <zigras00@gmail.com>
2024-02-05 08:59:12 +0100

    [#2617] Define if form needs payment in submission as well
    With this we evaluate the pricing rules (if they exist) in the
    submission method in order to see if a payment is still needed to be
    done, since a rule could result to zero(0) amount and no
    payment.Additional check that the product has valid price.

--- src/openforms/submissions/models/submission.py
+++ src/openforms/submissions/models/submission.py
@@ -16,6 +16,7 @@ import elasticapm
 from django_jsonform.models.fields import ArrayField
 from furl import furl
 from glom import glom
+from json_logic import jsonLogic
 
 from openforms.config.models import GlobalConfiguration
 from openforms.formio.datastructures import FormioConfigurationWrapper
@@ -708,7 +709,39 @@ class Submission(models.Model):
 
     @property
     def payment_required(self) -> bool:
-        return self.form.payment_required
+        if not self.form.payment_required:
+            return False
+
+        # test the rules one by one, if relevant
+        price_rules = self.form.formpricelogic_set.all()
+        for rule in price_rules:
+            # logic does not match, no point in bothering
+            if not jsonLogic(rule.json_logic_trigger, self.data):
+                continue
+            logger.debug(
+                "Price for submission %s calculated using logic trigger %d: %r",
+                self.uuid,
+                rule.id,
+                rule.json_logic_trigger,
+            )
+            # first logic match wins
+            # TODO: validate on API/backend/frontend that logic triggers must be unique for
+            # a form
+            self.price = rule.price
+            if self.price:
+                return True
+            return False
+
+        # no price rules or no match found -> use linked product
+        logger.debug(
+            "Falling back to product price for submission %s after trying %d price rules",
+            self.uuid,
+            len(price_rules),
+        )
+        self.price = self.form.product.price
+        if self.price:
+            return True
+        return False
 
     @property
     def payment_user_has_paid(self) -> bool:
