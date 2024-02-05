d5b78f2ab9487cfdd280538dfbd4742d458a3e41
Sergei Maertens <sergei@maykinmedia.nl>
2024-02-05 14:22:47 +0100

    :hammer: [#3049] Disable MFA by default in dev

--- src/openforms/conf/dev.py
+++ src/openforms/conf/dev.py
@@ -154,7 +154,7 @@ if DISABLE_CSP_RATELIMITING:
 CSP_EXCLUDE_URL_PREFIXES += ("/dev/",)
 
 # None of the authentication backends require two-factor authentication.
-if config("DISABLE_2FA", default=False):
+if config("DISABLE_2FA", default=True):
     MAYKIN_2FA_ALLOW_MFA_BYPASS_BACKENDS = AUTHENTICATION_BACKENDS
 
 # THOU SHALT NOT USE NAIVE DATETIMES
