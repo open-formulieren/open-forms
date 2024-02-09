fc1499a9b629ba78ed0b047b73becd05c860f5cc
SilviaAmAm <silvia@maykinmedia.nl>
2024-02-09 12:17:42 +0100

    :heavy_minus_sign: [#3049] Remove ddt-api-calls
    We don't actively use it (we don't have many server-side rendered pages
    making API calls) and it breaks on Django 4.2. Because of its limited
    use, it makes more sense to not invest any more effort.

--- src/openforms/conf/dev.py
+++ src/openforms/conf/dev.py
@@ -99,26 +99,9 @@ CACHES.update(
 ELASTIC_APM["DEBUG"] = config("DISABLE_APM_IN_DEV", default=True)
 
 # Django debug toolbar
-INSTALLED_APPS += ["debug_toolbar", "ddt_api_calls"]
+INSTALLED_APPS += ["debug_toolbar"]
 MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
 INTERNAL_IPS = ("127.0.0.1",)
-DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}
-DEBUG_TOOLBAR_PANELS = [
-    "debug_toolbar.panels.versions.VersionsPanel",
-    "debug_toolbar.panels.timer.TimerPanel",
-    "debug_toolbar.panels.settings.SettingsPanel",
-    "debug_toolbar.panels.headers.HeadersPanel",
-    "debug_toolbar.panels.request.RequestPanel",
-    "debug_toolbar.panels.sql.SQLPanel",
-    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
-    "debug_toolbar.panels.templates.TemplatesPanel",
-    "debug_toolbar.panels.cache.CachePanel",
-    "debug_toolbar.panels.signals.SignalsPanel",
-    "debug_toolbar.panels.logging.LoggingPanel",
-    "debug_toolbar.panels.redirects.RedirectsPanel",
-    "debug_toolbar.panels.profiling.ProfilingPanel",
-    "ddt_api_calls.panels.APICallsPanel",
-]
 
 # Django extensions
 INSTALLED_APPS += ["django_extensions"]
