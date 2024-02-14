0b453cff769690e5bc942849bd5318d3dcd89a0d
SilviaAmAm <silvia@maykinmedia.nl>
2024-02-14 16:38:22 +0100

    :sparkles: [#3855] Added endpoints to retrieve input/outputs

--- src/openforms/dmn/api/serializers.py
+++ src/openforms/dmn/api/serializers.py
@@ -4,6 +4,8 @@ from rest_framework import serializers
 
 from openforms.plugins.api.serializers import PluginBaseSerializer
 
+from .constants import DMNTypes
+
 
 class DecisionPluginSerializer(PluginBaseSerializer):
     pass
@@ -41,3 +43,45 @@ class DecisionDefinitionXMLSerializer(serializers.Serializer):
         label=_("DMN XML"),
         help_text=_("If no XML can be obtained, the value will be an empty string."),
     )
+
+
+class DecisionDefinitionInputSerializer(serializers.Serializer):
+    id = serializers.CharField(
+        label=_("ID"), help_text=_("Unique identifier of the decision table input.")
+    )
+    label = serializers.CharField(
+        label=_("label"), help_text=_("Short description of the input.")
+    )
+    expression = serializers.CharField(
+        label=_("expression"),
+        help_text=_(
+            "Specifies how the value of the input clause is generated. "
+            "It usually simple references a variable which is available during the evaluation."
+        ),
+    )
+    type_ref = serializers.ChoiceField(
+        choices=DMNTypes.choices,
+        help_text=_("The type of the input expression after being evaluated."),
+    )
+
+
+class DecisionDefinitionOutputSerializer(serializers.Serializer):
+    id = serializers.CharField(
+        label=_("ID"), help_text=_("Unique identifier of the decision table output.")
+    )
+    label = serializers.CharField(
+        label=_("label"), help_text=_("Short description of the output.")
+    )
+    name = serializers.CharField(
+        label=_("name"),
+        help_text=_("Used to reference the value of the output."),
+    )
+    type_ref = serializers.ChoiceField(
+        choices=DMNTypes.choices,
+        help_text=_("The type of the output clause."),
+    )
+
+
+class DecisionDefinitionIntrospectionResultSerializer(serializers.Serializer):
+    inputs = DecisionDefinitionInputSerializer(many=True)
+    outputs = DecisionDefinitionOutputSerializer(many=True)
