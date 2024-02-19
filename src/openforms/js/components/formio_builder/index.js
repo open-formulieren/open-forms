0425da507bfbdc7dbb05e2e782b3323e47e8a31c
Sergei Maertens <sergei@maykinmedia.nl>
2024-02-19 09:25:49 +0100

    :fire: Delete legacy translation handling

--- src/openforms/js/components/formio_builder/index.js
+++ src/openforms/js/components/formio_builder/index.js
@@ -4,11 +4,6 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 
 import AppWrapper, {getWrapperProps} from 'components/admin/AppWrapper';
-import {
-  clearObsoleteLiterals,
-  isNewBuilderComponent,
-  persistComponentTranslations,
-} from 'components/formio_builder/translation';
 import {onLoaded} from 'utils/dom';
 
 import FormIOBuilder from './builder';
@@ -16,8 +11,6 @@ import FormIOBuilder from './builder';
 onLoaded(async () => {
   const nodes = document.querySelectorAll('.form-builder');
   const wrapperProps = await getWrapperProps();
-
-  const {react_formio_builder_enabled = false} = wrapperProps.featureFlags;
 
   for (const node of nodes) {
     const configurationInput = node.querySelector('.form-builder__configuration-input');
@@ -33,38 +26,12 @@ onLoaded(async () => {
       configurationInput.value = JSON.stringify(newConfiguration);
     };
 
-    /**
-     * Update the translations store when a component is altered.
-     */
-    const onComponentMutated = (mutationType, schema, ...rest) => {
-      let configuration;
-
-      switch (mutationType) {
-        case 'changed': {
-          configuration = rest[1];
-          break;
-        }
-        case 'removed': {
-          configuration = rest[0];
-          break;
-        }
-        default:
-          throw new Error(`Unknown mutation type '${mutationType}'`);
-      }
-
-      if (!react_formio_builder_enabled || !isNewBuilderComponent(schema)) {
-        persistComponentTranslations(componentTranslations, schema);
-        componentTranslations = clearObsoleteLiterals(componentTranslations, configuration);
-        componentTranslationsInput.value = JSON.stringify(componentTranslations);
-      }
-    };
     const root = createRoot(node.querySelector('.form-builder__container'));
     root.render(
       <AppWrapper {...wrapperProps}>
         <FormIOBuilder
           configuration={configuration}
           onChange={onChange}
-          onComponentMutated={onComponentMutated}
           componentTranslations={componentTranslations}
         />
       </AppWrapper>
