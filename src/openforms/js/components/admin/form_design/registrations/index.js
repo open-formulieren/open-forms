b2f1450801054991f47df765c090d18ef9bb578d
Viicos <65306057+Viicos@users.noreply.github.com>
2024-02-21 11:33:37 +0100

    [#3688] Switch from the JSON Schema form to the explicit one

--- src/openforms/js/components/admin/form_design/registrations/index.js
+++ src/openforms/js/components/admin/form_design/registrations/index.js
@@ -2,6 +2,7 @@ import {WysiwygWidget} from 'components/admin/RJSFWrapper';
 
 import CamundaOptionsForm from './camunda';
 import {onStepEdit} from './handlers';
+import ObjectsApiOptionsForm from './objectsapi/ObjectsApiOptionsForm';
 import ObjectsApiSummaryHandler from './objectsapi/ObjectsApiSummaryHandler';
 import ZGWOptionsForm from './zgw';
 
@@ -22,20 +23,8 @@ export const BACKEND_OPTIONS_FORMS = {
     onStepEdit: onStepEdit,
   },
   objects_api: {
-    uiSchema: {
-      contentJson: {
-        'ui:widget': 'textarea',
-        'ui:options': {
-          rows: 5,
-        },
-      },
-      paymentStatusUpdateJson: {
-        'ui:widget': 'textarea',
-        'ui:options': {
-          rows: 5,
-        },
-      },
-    },
+    form: ObjectsApiOptionsForm,
+    onStepEdit: null,
     configurableFromVariables: options => options.version === 2,
     formVariableConfigured: (variableKey, options) =>
       options.variablesMapping.some(mapping => mapping.variableKey === variableKey),
