7a152bcaab864f939a95f60124084a7c7ad0b3b6
Viicos <65306057+Viicos@users.noreply.github.com>
2024-02-21 11:30:55 +0100

    [#3688] Add the V2 form
    Handling the effects when selecting an objecttype/version is hard,
    so lots of special casing

---
+++ src/openforms/js/components/admin/form_design/registrations/objectsapi/hooks.js
@@ -0,0 +1,28 @@
+import useAsync from 'react-use/esm/useAsync';
+
+import {REGISTRATION_OBJECTTYPES_ENDPOINT} from 'components/admin/form_design/constants';
+import {get} from 'utils/fetch';
+
+export const useGetAvailableObjectTypes = () => {
+  const {
+    loading,
+    value: availableObjecttypes = [],
+    error,
+  } = useAsync(
+    async () => {
+      const response = await get(REGISTRATION_OBJECTTYPES_ENDPOINT);
+      if (!response.ok) {
+        throw new Error('Loading available object types failed');
+      }
+      return response.data;
+    },
+    // available object types only need to be loaded once when the component mounts
+    []
+  );
+
+  return {
+    loading,
+    availableObjecttypes,
+    error,
+  };
+};
