efdbdd7effefa71d3f7815af3656e558b5699084
SilviaAmAm <silvia@maykinmedia.nl>
2024-03-07 15:17:30 +0100

    :white_check_mark: [#3975,#3052] Update existing tests

--- src/openforms/submissions/tests/test_variables/test_fetch_with_logic.py
+++ src/openforms/submissions/tests/test_variables/test_fetch_with_logic.py
@@ -1,22 +1,22 @@
 from pathlib import Path
 
+from django.test import TestCase
+
 import requests_mock
 from factory.django import FileField
 from freezegun import freeze_time
-from rest_framework.reverse import reverse
-from rest_framework.test import APITestCase
 from zgw_consumers.constants import APITypes, AuthTypes
 from zgw_consumers.test.factories import ServiceFactory
 
 from openforms.forms.constants import LogicActionTypes
-from openforms.forms.tests.factories import FormLogicFactory
+from openforms.forms.tests.factories import FormLogicFactory, FormVariableFactory
 from openforms.variables.tests.factories import ServiceFetchConfigurationFactory
 
+from ...form_logic import evaluate_form_logic
 from ..factories import SubmissionFactory
-from ..mixins import SubmissionsMixin
 
 
-class ServiceFetchWithActionsTest(SubmissionsMixin, APITestCase):
+class ServiceFetchWithActionsTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
@@ -31,11 +31,14 @@ class ServiceFetchWithActionsTest(SubmissionsMixin, APITestCase):
 
     @requests_mock.Mocker(case_sensitive=True)
     def test_it_calls_service_for_the_answer(self, m):
-        submission = SubmissionFactory.from_components(
-            [{"type": "number", "key": "someField"}]
-        )
+        submission = SubmissionFactory.from_components([])
         fetch_config = ServiceFetchConfigurationFactory.create(
             service=self.service, path="get"
+        )
+        FormVariableFactory.create(
+            key="someVariable",
+            form=submission.form,
+            service_fetch_configuration=fetch_config,
         )
 
         FormLogicFactory.create(
@@ -43,38 +46,37 @@ class ServiceFetchWithActionsTest(SubmissionsMixin, APITestCase):
             json_logic_trigger=True,
             actions=[
                 {
-                    "variable": "someField",
+                    "variable": "someVariable",
                     "action": {
                         "name": "Fetch some field from some server",
                         "type": LogicActionTypes.fetch_from_service,
-                        "value": fetch_config.id,
                     },
                 }
             ],
         )
-        self._add_submission_to_session(submission)
         m.get("https://httpbin.org/get", json=42)
 
-        endpoint = reverse(
-            "api:submission-steps-logic-check",
-            kwargs={
-                "submission_uuid": submission.uuid,
-                "step_uuid": submission.form.formstep_set.first().uuid,
-            },
+        evaluate_form_logic(
+            submission, submission.submissionstep_set.first(), submission.data
         )
 
-        response = self.client.post(endpoint, data={"data": {"someField": None}})
+        state = submission.load_submission_value_variables_state()
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["step"]["data"], {"someField": 42})
+        variable = state.get_variable("someVariable")
+
+        self.assertEqual(variable.value, 42)
 
     @requests_mock.Mocker(case_sensitive=True)
     def test_it_handles_bad_service_responses(self, m):
-        submission = SubmissionFactory.from_components(
-            [{"type": "number", "key": "someField"}]
-        )
+        submission = SubmissionFactory.from_components([])
         fetch_config = ServiceFetchConfigurationFactory.create(
             service=self.service, path="get"
+        )
+        FormVariableFactory.create(
+            key="someVariable",
+            form=submission.form,
+            service_fetch_configuration=fetch_config,
+            initial_value="some initial value",
         )
 
         FormLogicFactory.create(
@@ -82,41 +84,33 @@ class ServiceFetchWithActionsTest(SubmissionsMixin, APITestCase):
             json_logic_trigger=True,
             actions=[
                 {
-                    "variable": "someField",
+                    "variable": "someVariable",
                     "action": {
                         "name": "Fetch some field from some server",
                         "type": LogicActionTypes.fetch_from_service,
-                        "value": fetch_config.id,
                     },
                 }
             ],
         )
-        self._add_submission_to_session(submission)
 
         m.get("https://httpbin.org/get", status_code=500)
 
-        endpoint = reverse(
-            "api:submission-steps-logic-check",
-            kwargs={
-                "submission_uuid": submission.uuid,
-                "step_uuid": submission.form.formstep_set.first().uuid,
-            },
+        evaluate_form_logic(
+            submission, submission.submissionstep_set.first(), submission.data
         )
 
-        response = self.client.post(endpoint, data={"data": {"someField": None}})
+        state = submission.load_submission_value_variables_state(refresh=True)
 
-        self.assertEqual(response.status_code, 200)
+        variable = state.get_variable("someVariable")
         # assert no new value
-        # if a prefill service is down or misconfigured, a submitter's values
+        # if a fetch service is down or misconfigured, a submitter's values
         # shouldn't be overwritten
-        self.assertEqual(response.data["step"]["data"], {})
+        self.assertEqual(variable.value, "some initial value")
 
     @requests_mock.Mocker(case_sensitive=True)
     def test_requests_not_made_multiple_times(self, m):
         submission = SubmissionFactory.from_components(
             [
-                {"type": "textfield", "key": "fieldA"},
-                {"type": "textfield", "key": "fieldB"},
                 {"type": "textfield", "key": "fieldC"},
             ]
         )
@@ -126,6 +120,16 @@ class ServiceFetchWithActionsTest(SubmissionsMixin, APITestCase):
         fetch_config2 = ServiceFetchConfigurationFactory.create(
             service=self.service, path="get", query_params={"fieldC": ["{{ fieldC }}"]}
         )
+        FormVariableFactory.create(
+            key="someVariable1",
+            form=submission.form,
+            service_fetch_configuration=fetch_config1,
+        )
+        FormVariableFactory.create(
+            key="someVariable2",
+            form=submission.form,
+            service_fetch_configuration=fetch_config2,
+        )
 
         FormLogicFactory.create(
             form=submission.form,
@@ -133,11 +137,10 @@ class ServiceFetchWithActionsTest(SubmissionsMixin, APITestCase):
             json_logic_trigger=True,
             actions=[
                 {
-                    "variable": "fieldA",
+                    "variable": "someVariable1",
                     "action": {
                         "name": "Fetch some field from some server",
                         "type": LogicActionTypes.fetch_from_service,
-                        "value": fetch_config1.id,
                     },
                 }
             ],
@@ -148,50 +151,42 @@ class ServiceFetchWithActionsTest(SubmissionsMixin, APITestCase):
             json_logic_trigger=True,
             actions=[
                 {
-                    "variable": "fieldB",
+                    "variable": "someVariable2",
                     "action": {
                         "name": "Fetch some field from some server",
                         "type": LogicActionTypes.fetch_from_service,
-                        "value": fetch_config2.id,
                     },
                 }
             ],
         )
 
-        self._add_submission_to_session(submission)
-
         m.get("https://httpbin.org/get", json=42)
 
-        endpoint = reverse(
-            "api:submission-steps-logic-check",
-            kwargs={
-                "submission_uuid": submission.uuid,
-                "step_uuid": submission.form.formstep_set.first().uuid,
-            },
+        evaluate_form_logic(
+            submission, submission.submissionstep_set.first(), {"fieldC": 42}
         )
 
-        response = self.client.post(endpoint, data={"data": {"fieldC": 42}})
-
-        self.assertEqual(response.status_code, 200)
         self.assertEqual(len(m.request_history), 2)
         self.assertEqual(m.request_history[-1].url, "https://httpbin.org/get?fieldC=42")
         self.assertEqual(m.request_history[-2].url, "https://httpbin.org/get")
 
-        response = self.client.post(endpoint, data={"data": {"fieldC": 43}})
+        evaluate_form_logic(
+            submission, submission.submissionstep_set.first(), {"fieldC": 43}
+        )
 
-        self.assertEqual(response.status_code, 200)
         self.assertEqual(len(m.request_history), 3)
         self.assertEqual(m.request_history[-1].url, "https://httpbin.org/get?fieldC=43")
 
     @requests_mock.Mocker(case_sensitive=True)
     def test_requests_cache_timeout(self, m):
-        submission = SubmissionFactory.from_components(
-            [
-                {"type": "textfield", "key": "fieldA"},
-            ]
-        )
+        submission = SubmissionFactory.from_components([])
         fetch_config = ServiceFetchConfigurationFactory.create(
             service=self.service, path="get", cache_timeout=30
+        )
+        FormVariableFactory.create(
+            key="someVariable",
+            form=submission.form,
+            service_fetch_configuration=fetch_config,
         )
 
         FormLogicFactory.create(
@@ -200,38 +195,29 @@ class ServiceFetchWithActionsTest(SubmissionsMixin, APITestCase):
             json_logic_trigger=True,
             actions=[
                 {
-                    "variable": "fieldA",
+                    "variable": "someVariable",
                     "action": {
                         "name": "Fetch some field from some server",
                         "type": LogicActionTypes.fetch_from_service,
-                        "value": fetch_config.id,
                     },
                 }
             ],
         )
 
-        self._add_submission_to_session(submission)
-
         m.get("https://httpbin.org/get", json=42)
 
-        endpoint = reverse(
-            "api:submission-steps-logic-check",
-            kwargs={
-                "submission_uuid": submission.uuid,
-                "step_uuid": submission.form.formstep_set.first().uuid,
-            },
-        )
-
         with freeze_time("2023-02-21T18:00:00Z"):
-            response = self.client.post(endpoint, data={"data": {}})
+            evaluate_form_logic(
+                submission, submission.submissionstep_set.first(), submission.data
+            )
 
-        self.assertEqual(response.status_code, 200)
         self.assertEqual(len(m.request_history), 1)
         self.assertEqual(m.request_history[-1].url, "https://httpbin.org/get")
 
         with freeze_time("2023-02-21T18:01:00Z"):
-            response = self.client.post(endpoint, data={"data": {}})
+            evaluate_form_logic(
+                submission, submission.submissionstep_set.first(), submission.data
+            )
 
-        self.assertEqual(response.status_code, 200)
         self.assertEqual(len(m.request_history), 2)
         self.assertEqual(m.request_history[-1].url, "https://httpbin.org/get")
