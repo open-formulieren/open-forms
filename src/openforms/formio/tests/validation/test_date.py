3b0382ecd1765bb357f12a8359d8b9f4ddd349e7
Sergei Maertens <sergei@maykinmedia.nl>
2024-03-11 15:19:24 +0100

    :white_check_mark: [#3977] Test validation for date component

---
+++ src/openforms/formio/tests/validation/test_date.py
@@ -0,0 +1,56 @@
+from django.test import SimpleTestCase
+
+from ...typing import DateComponent
+from .helpers import extract_error, validate_formio_data
+
+
+class DateFieldValidationTests(SimpleTestCase):
+
+    def test_datefield_required_validation(self):
+        component: DateComponent = {
+            "type": "date",
+            "key": "foo",
+            "label": "Foo",
+            "validate": {"required": True},
+            "datePicker": None,
+        }
+
+        invalid_values = [
+            ({}, "required"),
+            ({"foo": None}, "null"),
+        ]
+
+        for data, error_code in invalid_values:
+            with self.subTest(data=data):
+                is_valid, errors = validate_formio_data(component, data)
+
+                self.assertFalse(is_valid)
+                self.assertIn(component["key"], errors)
+                error = extract_error(errors, component["key"])
+                self.assertEqual(error.code, error_code)
+
+    def test_min_max_date(self):
+        component: DateComponent = {
+            "type": "date",
+            "key": "foo",
+            "label": "Foo",
+            "validate": {"required": False},
+            "datePicker": {
+                "minDate": "2024-03-10",
+                "maxDate": "2025-03-10",
+            },
+        }
+
+        invalid_values = [
+            ({"foo": "2023-01-01"}, "min_value"),
+            ({"foo": "2025-12-30"}, "max_value"),
+        ]
+
+        for data, error_code in invalid_values:
+            with self.subTest(data=data):
+                is_valid, errors = validate_formio_data(component, data)
+
+                self.assertFalse(is_valid)
+                self.assertIn(component["key"], errors)
+                error = extract_error(errors, component["key"])
+                self.assertEqual(error.code, error_code)
