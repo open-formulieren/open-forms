ad3eabd3110f0343f7766f1f55c1b46e12c0facc
vasileios <zigras00@gmail.com>
2024-03-13 15:32:34 +0100

    [#3881] Added task for recoupling reusable form definition's variables
    When there is a reusable definition and we update the form variables, we want
    to kick off a celery task which will update all the forms which use the
    same form definition.
    
    Backport-of: #3991

--- src/openforms/forms/tasks.py
+++ src/openforms/forms/tasks.py
@@ -1,14 +1,36 @@
 import copy
 import logging
 
+from django.db import transaction
 from django.db.utils import IntegrityError
 
+from celery import chain
 from djangorestframework_camel_case.util import camelize
+
+from openforms.variables.constants import FormVariableSources
 
 from ..celery import app
 from .models import Form
 
 logger = logging.getLogger(__name__)
+
+
+def on_variables_bulk_update_event(form_id: int) -> None:
+    """
+    Celery chain of tasks to execute on a bulk update of variables event.
+    """
+    repopulate_reusable_definition_variables_task = (
+        repopulate_reusable_definition_variables_to_form_variables.si(form_id)
+    )
+    recouple_submission_variables_task = (
+        recouple_submission_variables_to_form_variables.si(form_id)
+    )
+
+    actions_chain = chain(
+        repopulate_reusable_definition_variables_task,
+        recouple_submission_variables_task,
+    )
+    actions_chain.delay()
 
 
 @app.task(ignore_result=True)
@@ -82,3 +104,30 @@ def recouple_submission_variables_to_form_variables(form_id: int) -> None:
         # Issue #1970: If the form is saved again from the form editor while this task was running, the form variables
         # retrieved don't exist anymore. Another task will be scheduled from the endpoint, so nothing more to do here.
         logger.info("Form variables were updated while this task was runnning.")
+
+
+@app.task(ignore_result=True)
+@transaction.atomic()
+def repopulate_reusable_definition_variables_to_form_variables(form_id: int) -> None:
+    """Fix inconsistencies created by updating a re-usable form definition which is used in other forms.
+
+    When the FormVariable bulk create/update endpoint is called, all existing FormVariable related to the form are
+    deleted and new are created. If there are any form definitions which are reusable, we want to update all the forms
+    which are also using these FormDefinitions (concerning the FormVariables). This task updates the FormVariables
+    of each related form in the database, by replacing the old state with the newly derived set of form variables.
+    """
+    from .models import FormDefinition, FormVariable  # due to circular import
+
+    fds = FormDefinition.objects.filter(formstep__form=form_id, is_reusable=True)
+
+    other_forms = Form.objects.filter(formstep__form_definition__in=fds).exclude(
+        id=form_id
+    )
+
+    # delete the existing form variables, we will re-create them
+    FormVariable.objects.filter(
+        form__in=other_forms, source=FormVariableSources.component
+    ).delete()
+
+    for form in other_forms:
+        FormVariable.objects.create_for_form(form)
