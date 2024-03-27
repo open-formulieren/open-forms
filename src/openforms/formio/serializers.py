85290120942d2a494ef81c4d008a5e9ca8d888ca
Sergei Maertens <sergei@maykinmedia.nl>
2024-03-27 12:10:59 +0100

    :bug: [#4065] Skip validation in serializer fields when components are not visible
    This mimicks the validation logic from Formio itself, which forces
    a component to be valid when it's not visible or conditionally
    hidden.

--- src/openforms/formio/serializers.py
+++ src/openforms/formio/serializers.py
@@ -10,9 +10,12 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, TypeAlias
 
-from glom import assign
+from glom import assign, glom
 from rest_framework import serializers
 
+from openforms.typing import DataMapping, JSONObject
+
+from .datastructures import FormioConfigurationWrapper
 from .typing import Component
 from .utils import iter_components
 
@@ -23,9 +26,59 @@ if TYPE_CHECKING:
 FieldOrNestedFields: TypeAlias = serializers.Field | dict[str, "FieldOrNestedFields"]
 
 
+class StepDataSerializer(serializers.Serializer):
+
+    def apply_hidden_state(
+        self, configuration: JSONObject, fields: dict[str, FieldOrNestedFields]
+    ) -> None:
+        """
+        Apply the hidden/visible state of the formio components to the serializer.
+
+        Components may be hidden in a static fashing (through the 'hidden' property),
+        or be hidden dynamically based on the input data via the 'conditional' property.
+
+        Hidden fields in Formio don't run *any* validation, see the
+        ``Component.shouldSkipValidation`` method for reference.
+        """
+        config_wrapper = FormioConfigurationWrapper(configuration)
+
+        # initial_data is only set if Serializer(data=...) was used, and we can't take
+        # (dynamic) hidden state into account without initial data.
+        if not hasattr(self, "initial_data"):
+            return
+
+        # can't use FormioData yet because of is_visible_in_frontend
+        values: DataMapping = self.initial_data
+
+        # loop over all components and delegate application to the registry
+        for component in iter_components(configuration, recurse_into_editgrid=False):
+            # XXX: is_visible_in_frontend does not understand editgrid at all yet, which
+            # is a broader issue, but also manifests here.
+            is_visible = config_wrapper.is_visible_in_frontend(component["key"], values)
+
+            # we don't have to do anything when the component is visible, regular
+            # validation rules apply
+            if is_visible:
+                continue
+
+            # when it's not visible, grab the field from the serializer and remove all
+            # the validators to match Formio's behaviour.
+            serializer_field = glom(fields, component["key"])
+            self._remove_validations_from_field(serializer_field)
+
+    def _remove_validations_from_field(self, field: serializers.Field) -> None:
+        field.required = False
+        # note that prefilled fields are validated separately when they're read-only
+        # to protect against tampering
+        field.allow_null = True
+        # reset the validators to the default validators, discarding any additional ones
+        # added based on component['validate']
+        field.validators = field.get_validators()
+
+
 def dict_to_serializer(
     fields: dict[str, FieldOrNestedFields], **kwargs
-) -> serializers.Serializer:
+) -> StepDataSerializer:
     """
     Recursively convert a mapping of field names to a serializer instance.
 
@@ -33,7 +86,7 @@ def dict_to_serializer(
     serializer field instances or nested mappings. Nested mappings result in nested
     serializers.
     """
-    serializer = serializers.Serializer(**kwargs)
+    serializer = StepDataSerializer(**kwargs)
 
     for bit, field in fields.items():
         match field:
@@ -51,7 +104,7 @@ def dict_to_serializer(
 
 def build_serializer(
     components: list[Component], register: ComponentRegistry, **kwargs
-) -> serializers.Serializer:
+) -> StepDataSerializer:
     """
     Translate a sequence of Formio.js component definitions into a serializer.
 
@@ -60,12 +113,11 @@ def build_serializer(
     """
     fields: dict[str, FieldOrNestedFields] = {}
 
-    # TODO: check that editgrid nested components are not yielded here!
-    for component in iter_components(
-        {"components": components}, recurse_into_editgrid=False
-    ):
+    config: JSONObject = {"components": components}
+    for component in iter_components(config, recurse_into_editgrid=False):
         field = register.build_serializer_field(component)
         assign(obj=fields, path=component["key"], val=field, missing=dict)
 
     serializer = dict_to_serializer(fields, **kwargs)
+    serializer.apply_hidden_state(config, fields)
     return serializer
