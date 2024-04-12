d65e99b924c5e4c45c78935b8a3136f241f86627
Viicos <65306057+Viicos@users.noreply.github.com>
2024-04-12 16:00:24 +0200

    [#4156] Adapt validator tests

--- src/openforms/payments/tests/test_validators.py
+++ src/openforms/payments/tests/test_validators.py
@@ -1,35 +1,46 @@
 from django.core.exceptions import ValidationError
-from django.test import TestCase
+from django.test import SimpleTestCase
 
-from openforms.payments.validators import validate_payment_order_id_prefix
+from openforms.payments.validators import validate_payment_order_id_template
 
 
-class PaymentOrderIDValidatorTests(TestCase):
-    def test_valid_prefixes(self):
+class PaymentOrderIDValidatorTests(SimpleTestCase):
+    def test_uid_present(self):
+        valid = "{uid}"
+        with self.subTest(value=valid):
+            validate_payment_order_id_template(valid)
+
+        invalid = "other_things"
+        with self.subTest(value=invalid):
+            with self.assertRaises(ValidationError):
+                validate_payment_order_id_template(invalid)
+
+    def test_valid_templates(self):
         valid = [
-            "",
-            "ab",
-            "12",
-            "ab12",
-            # placeholder
-            "{year}" "aa{year}" "a1{year}a1",
+            "{uid}",
+            "{uid}ab",
+            "{uid}12",
+            "{uid}ab12",
+            "{uid}a-.///---_",
+            # placeholders:
+            "{uid}{year}",
+            "{year}{reference}{uid}",
         ]
 
         for value in valid:
             with self.subTest(value=value):
-                validate_payment_order_id_prefix(value)
+                validate_payment_order_id_template(value)
 
     def test_raises_for_invalid_prefixes(self):
         invalid = [
-            " ",
-            "aa ",
-            " aa",
-            "a-2",
-            "{yearrr",
-            "{bad}",
+            " {uid}",
+            "aa {uid}",
+            " aa{uid}",
+            "{yearrr{uid}",
+            "{bad}{uid}",
         ]
 
         for value in invalid:
             with self.subTest(value=value):
                 with self.assertRaises(ValidationError):
-                    validate_payment_order_id_prefix(value)
+                    validate_payment_order_id_template(value)
