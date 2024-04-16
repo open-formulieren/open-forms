803ca742042d78730ab45f88d10407f8500a748d
Sergei Maertens <sergei@maykinmedia.nl>
2024-04-16 12:46:01 +0200

    :test_tube: [#4172] Add regression test for date validation
    We can actually get datetimes (as that is also what the frontend
    expects) rather than just plain date strings.
    
    The regression test simulates a realistic feature use where a
    min/max date is configured in the form builder to be in
    the future/past.

--- src/openforms/formio/tests/validation/test_date.py
+++ src/openforms/formio/tests/validation/test_date.py
@@ -1,5 +1,10 @@
 from django.test import SimpleTestCase, tag
+from django.utils import timezone
 
+from openforms.submissions.models import Submission
+
+from ...datastructures import FormioConfigurationWrapper
+from ...dynamic_config import rewrite_formio_components
 from ...typing import DateComponent
 from .helpers import extract_error, validate_formio_data
 
@@ -54,6 +59,48 @@ class DateFieldValidationTests(SimpleTestCase):
                 self.assertIn(component["key"], errors)
                 error = extract_error(errors, component["key"])
                 self.assertEqual(error.code, error_code)
+
+    @tag("gh-4172")
+    def test_min_max_can_be_datetimes(self):
+        # Our dynamic logic that calculates dates/datetimes is shared between date
+        # and datetime components, so it produces datetime strings (!)
+        component: DateComponent = {
+            "type": "date",
+            "key": "foo",
+            "label": "Foo",
+            "validate": {"required": False},
+            "openForms": {
+                "minDate": {
+                    "mode": "future",
+                    "includeToday": True,
+                },
+            },
+        }
+        submission = Submission()  # this test is not supposed to hit the DB
+        config_wraper = FormioConfigurationWrapper(
+            configuration={"components": [component]}
+        )
+        config_wraper = rewrite_formio_components(
+            config_wraper,
+            submission=submission,
+            data={"now": timezone.now()},
+        )
+
+        updated_component = config_wraper["foo"]
+        # check that rewrite_formio_components behaved as expected
+        assert "datePicker" in updated_component
+        assert "minDate" in updated_component["datePicker"]
+
+        with self.subTest("valid value"):
+            today = timezone.now().date().isoformat()
+            is_valid, _ = validate_formio_data(component, {"foo": today})
+
+            self.assertTrue(is_valid)
+
+        with self.subTest("invalid value"):
+            is_valid, _ = validate_formio_data(component, {"foo": "2020-01-01"})
+
+            self.assertFalse(is_valid)
 
     @tag("gh-4068")
     def test_empty_default_value(self):
