7ffc1909bd26f6dc6bafd57b471882cbe2af34e9
vasileios <zigras00@gmail.com>
2024-04-26 11:52:47 +0200

    Improve formio date component types

--- src/openforms/formio/typing/custom.py
+++ src/openforms/formio/typing/custom.py
@@ -1,8 +1,9 @@
 from typing_extensions import NotRequired
 
 from .base import Component
-from .dates import DatePickerConfig
+from .dates import DatePickerConfig, DatePickerCustomOptions
 
 
 class DateComponent(Component):
     datePicker: NotRequired[DatePickerConfig]
+    customOptions: NotRequired[DatePickerCustomOptions]
