62e0a9d8300d18284c0dfc4e68abaa6b62ed93ad
Viicos <65306057+Viicos@users.noreply.github.com>
2024-04-29 15:46:08 +0200

    [#4205] Allow any `https:` scheme for CSP `form-action` directive
    Backport-of: #4223

--- src/openforms/contrib/digid_eherkenning/constants.py
+++
@@ -1,6 +0,0 @@
-from digid_eherkenning.models import DigidConfiguration, EherkenningConfiguration
-
-ADDITIONAL_CSP_VALUES = {
-    DigidConfiguration: "https://digid.nl https://*.digid.nl",
-    EherkenningConfiguration: "https://*.eherkenning.nl",
-}
