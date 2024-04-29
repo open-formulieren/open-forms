4687b587995f914c121cc88b65fde610f50f1bbd
Steven Bal <steven@maykinmedia.nl>
2024-04-29 15:46:01 +0200

    :white_check_mark: [#4229] Test KVK results caching for validation
    and add cassette for valid RSIN

--- src/openforms/contrib/kvk/tests/test_validators.py
+++ src/openforms/contrib/kvk/tests/test_validators.py
@@ -1,12 +1,16 @@
 from functools import partial
 
 from django.core.exceptions import ValidationError
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, override_settings
 from django.utils.translation import gettext as _
 
+import requests_mock
+from freezegun import freeze_time
 from privates.test import temp_private_root
 
 from openforms.submissions.models import Submission
+from openforms.tests.utils import NOOP_CACHES
+from openforms.utils.tests.cache import clear_caches
 from openforms.utils.tests.vcr import OFVCRMixin
 
 from ..validators import (
@@ -49,6 +53,7 @@ class KvKValidatorTestCase(SimpleTestCase):
             validate_branchNumber("11223-445566")
 
 
+@override_settings(CACHES=NOOP_CACHES)
 @temp_private_root()
 class KvKRemoteValidatorTestCase(OFVCRMixin, KVKTestMixin, SimpleTestCase):
     VCR_TEST_FILES = TEST_FILES
@@ -76,11 +81,10 @@ class KvKRemoteValidatorTestCase(OFVCRMixin, KVKTestMixin, SimpleTestCase):
             validator("bork")
 
     def test_rsin_validator(self):
-        # kvk test environment does not provide an example of valid rsin numbers so we
-        # only test against the invalid one
-        # https://developers.kvk.nl/documentation/testing
-
         validator = partial(KVKRSINRemoteValidator("id"), submission=Submission())
+
+        # valid rsin
+        validator("992760562")
 
         # invalid rsin(404)
         with self.assertRaisesMessage(
@@ -119,3 +123,107 @@ class KvKRemoteValidatorTestCase(OFVCRMixin, KVKTestMixin, SimpleTestCase):
             ValidationError, _("Expected a numerical value.")
         ):
             validator("bork")
+
+
+class KvKRemoteValidatorCachingTestCase(KVKTestMixin, SimpleTestCase):
+    # Rather than using VCR, we use requests mock because we don't check the contents
+    # of the responses (they are not relevant for the behaviour being tested).
+    # So, instead we use requests-mock and check the amount of calls made.
+
+    def setUp(self):
+        super().setUp()
+
+        self.addCleanup(clear_caches)
+        clear_caches()
+
+        self.mocker = requests_mock.Mocker()
+        self.addCleanup(self.mocker.stop)
+        self.mocker.start()
+        self.mocker.get(requests_mock.ANY, json={"resultaten": ["MOCK"]})
+
+    def assertNumRequests(self, expected: int):
+        num_requests = len(self.mocker.request_history)
+        self.assertEqual(
+            num_requests,
+            expected,
+            f"Expected {expected} requests, got {num_requests} instead.",
+        )
+
+    def test_kvkNumber_validator_caching(self):
+        # valid-existing kvkNummer
+        validator = partial(KVKNumberRemoteValidator("id"), submission=Submission())
+
+        with freeze_time("2024-01-01T12:00:00"):
+            self.assertNumRequests(0)
+
+            validator("69599084")
+
+            self.assertNumRequests(1)
+
+            validator("69599084")
+
+            # No additional request has been made because the result was cached
+            self.assertNumRequests(1)
+
+            validator("68727720")
+
+            # validate with different KVK number should trigger new request
+            self.assertNumRequests(2)
+
+        with freeze_time("2024-01-01T12:05:01"):
+            validator("69599084")
+
+        # Request is made, because cached result is timed out
+        self.assertNumRequests(3)
+
+    def test_rsin_validator_caching(self):
+        validator = partial(KVKRSINRemoteValidator("id"), submission=Submission())
+
+        with freeze_time("2024-01-01T12:00:00"):
+            self.assertNumRequests(0)
+
+            validator("992760562")
+
+            self.assertNumRequests(1)
+
+            validator("992760562")
+
+            # No additional request has been made because the result was cached
+            self.assertNumRequests(1)
+
+            validator("858311537")
+
+            # validate with different RSIN should trigger new request
+            self.assertNumRequests(2)
+
+        with freeze_time("2024-01-01T12:05:01"):
+            validator("992760562")
+
+        # Request is made, because cached result is timed out
+        self.assertNumRequests(3)
+
+    def test_branchNumber_validator_caching(self):
+        validator = partial(KVKBranchNumberRemoteValidator(""), submission=Submission())
+
+        with freeze_time("2024-01-01T12:00:00"):
+            self.assertNumRequests(0)
+
+            validator("990000541921")
+
+            self.assertNumRequests(1)
+
+            validator("990000541921")
+
+            # No additional request has been made because the result was cached
+            self.assertNumRequests(1)
+
+            validator("990000216645")
+
+            # validate with different branchnumber should trigger new request
+            self.assertNumRequests(2)
+
+        with freeze_time("2024-01-01T12:05:01"):
+            validator("990000541921")
+
+        # Request is made, because cached result is timed out
+        self.assertNumRequests(3)
