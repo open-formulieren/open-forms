d4dc506d5f2a45c2e12670c80b11cd8d3679bc34
Viicos <65306057+Viicos@users.noreply.github.com>
2024-04-29 12:16:12 +0200

    [#4236] Adapt tests

--- src/openforms/forms/tests/test_copy.py
+++ src/openforms/forms/tests/test_copy.py
@@ -1,15 +1,8 @@
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Permission
-from django.db.models import Q
-from django.http import HttpRequest
 from django.test import override_settings
-from django.urls import reverse
 from django.utils.translation import gettext as _
 
-from rest_framework import status
 from rest_framework.test import APITestCase
 
-from openforms.accounts.tests.factories import SuperUserFactory, TokenFactory
 from openforms.tests.utils import NOOP_CACHES
 from openforms.variables.constants import FormVariableSources
 from openforms.variables.tests.factories import ServiceFetchConfigurationFactory
@@ -25,55 +18,20 @@ from .factories import (
 )
 
 
-class CopyFormAPITests(APITestCase):
-    def setUp(self):
-        User = get_user_model()
-        self.user = User.objects.create_user(
-            username="john", password="secret", email="john@example.com"
-        )
-        self.token = TokenFactory(user=self.user)
+class CopyFormTests(APITestCase):
 
     @override_settings(CACHES=NOOP_CACHES)
     def test_form_copy_with_reusable_definition(self):
-        self.user.user_permissions.add(Permission.objects.get(codename="change_form"))
-        self.user.is_staff = True
-        self.user.save()
-
         form = FormFactory.create()
         form_definition = FormDefinitionFactory.create(is_reusable=True)
         form_step = FormStepFactory.create(form=form, form_definition=form_definition)
 
-        url = reverse("api:form-copy", args=(form.uuid,))
-        response = self.client.post(
-            url, format="json", HTTP_AUTHORIZATION=f"Token {self.token.key}"
-        )
-
-        copied_form = Form.objects.last()
+        copied_form = form.copy()
         copied_form_step = copied_form.formstep_set.first()
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertTrue(response.has_header("Location"))
-        self.assertEqual(response.json()["uuid"], str(copied_form.uuid))
-        self.assertEqual(response.json()["name"], copied_form.name)
-        self.assertEqual(response.json()["loginRequired"], copied_form.login_required)
-        self.assertEqual(response.json()["product"], copied_form.product)
-        self.assertEqual(response.json()["slug"], copied_form.slug)
-        self.assertEqual(
-            response.json()["steps"][0]["uuid"], str(copied_form_step.uuid)
-        )
-        self.assertEqual(
-            response.json()["steps"][0]["formDefinition"],
-            copied_form_step.form_definition.name,
-        )
 
         self.assertEqual(Form.objects.count(), 2)
         self.assertEqual(FormDefinition.objects.count(), 1)
         self.assertEqual(FormStep.objects.count(), 2)
-
-        self.assertIn(
-            reverse("api:form-detail", kwargs={"uuid_or_slug": copied_form.uuid}),
-            response["Location"],
-        )
 
         self.assertNotEqual(copied_form.pk, form.pk)
         self.assertNotEqual(copied_form.uuid, str(form.uuid))
@@ -90,115 +48,41 @@ class CopyFormAPITests(APITestCase):
 
     @override_settings(CACHES=NOOP_CACHES)
     def test_form_copy_with_non_reusable_definition(self):
-        self.user.user_permissions.add(Permission.objects.get(codename="change_form"))
-        self.user.is_staff = True
-        self.user.save()
 
         form = FormFactory.create()
         form_definition = FormDefinitionFactory.create(is_reusable=False)
         form_step = FormStepFactory.create(form=form, form_definition=form_definition)
 
-        url = reverse("api:form-copy", args=(form.uuid,))
-        response = self.client.post(
-            url, format="json", HTTP_AUTHORIZATION=f"Token {self.token.key}"
-        )
-
-        copied_form = Form.objects.exclude(uuid=form.uuid).get()
+        copied_form = form.copy()
         copied_form_step = copied_form.formstep_set.first()
 
         self.assertEqual(Form.objects.count(), 2)
         self.assertEqual(FormDefinition.objects.count(), 2)
         self.assertEqual(FormStep.objects.count(), 2)
 
-        self.assertIn(
-            reverse("api:form-detail", kwargs={"uuid_or_slug": copied_form.uuid}),
-            response["Location"],
-        )
-
         self.assertNotEqual(copied_form_step.form_definition, form_step.form_definition)
 
     def test_form_copy_already_exists(self):
-        self.user.user_permissions.add(Permission.objects.get(codename="change_form"))
-        self.user.is_staff = True
-        self.user.save()
 
         form = FormFactory.create()
+        copied_form_1 = form.copy()
 
-        url = reverse("api:form-copy", args=(form.uuid,))
-        response = self.client.post(
-            url, format="json", HTTP_AUTHORIZATION=f"Token {self.token.key}"
-        )
+        self.assertEqual(copied_form_1.name, _("{name} (copy)").format(name=form.name))
+        self.assertEqual(copied_form_1.slug, _("{slug}-copy").format(slug=form.slug))
 
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertTrue(response.has_header("Location"))
-        self.assertEqual(
-            response.json()["name"], _("{name} (copy)").format(name=form.name)
-        )
-        self.assertEqual(
-            response.json()["slug"], _("{slug}-copy").format(slug=form.slug)
-        )
+        copied_form_2 = form.copy()
 
-        copy_form_slug = response.json()["slug"]
+        self.assertEqual(copied_form_2.name, _("{name} (copy)").format(name=form.name))
+        self.assertEqual(copied_form_2.slug, "{}-2".format(copied_form_1.slug))
 
-        response = self.client.post(
-            url, format="json", HTTP_AUTHORIZATION=f"Token {self.token.key}"
-        )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertTrue(response.has_header("Location"))
-        self.assertEqual(
-            response.json()["name"], _("{name} (copy)").format(name=form.name)
-        )
-        self.assertEqual(response.json()["slug"], "{}-2".format(copy_form_slug))
+        copied_form_3 = form.copy()
 
-        response = self.client.post(
-            url, format="json", HTTP_AUTHORIZATION=f"Token {self.token.key}"
-        )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertTrue(response.has_header("Location"))
-        self.assertEqual(
-            response.json()["name"], _("{name} (copy)").format(name=form.name)
-        )
-        self.assertEqual(response.json()["slug"], "{}-3".format(copy_form_slug))
-
-    def test_form_copy_token_auth_required(self):
-        form = FormFactory.create()
-        url = reverse("api:form-copy", kwargs={"uuid_or_slug": form.uuid})
-        response = self.client.post(url)
-
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    def test_form_copy_session_auth_not_allowed(self):
-        self.user.is_staff = True
-        self.user.save()
-
-        self.client.login(
-            request=HttpRequest(), username=self.user.username, password="secret"
-        )
-
-        form = FormFactory.create()
-        url = reverse("api:form-copy", kwargs={"uuid_or_slug": form.uuid})
-        response = self.client.post(url)
-
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    def test_form_copy_staff_required(self):
-        self.user.is_staff = False
-        self.user.save()
-
-        form = FormFactory.create()
-        url = reverse("api:form-copy", kwargs={"uuid_or_slug": form.uuid})
-        response = self.client.post(
-            url,
-            HTTP_AUTHORIZATION=f"Token {self.token.key}",
-        )
-
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(copied_form_3.name, _("{name} (copy)").format(name=form.name))
+        self.assertEqual(copied_form_3.slug, "{}-3".format(copied_form_1.slug))
 
     def test_copy_form_with_variables(self):
-        super_user = SuperUserFactory.create()
-        token = TokenFactory(user=super_user)
 
-        form: Form = FormFactory.create(slug="test-copying-with-vars")
+        form = FormFactory.create(slug="test-copying-with-vars")
         FormStepFactory.create(
             form=form,
             form_definition__configuration={
@@ -236,18 +120,11 @@ class CopyFormAPITests(APITestCase):
         self.assertEqual(3, FormVariable.objects.filter(form=form).count())
         self.assertEqual(form.formlogic_set.count(), 1)
 
-        url = reverse("api:form-copy", args=(form.uuid,))
-        response = self.client.post(
-            url, format="json", HTTP_AUTHORIZATION=f"Token {token.key}"
-        )
-
-        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
-
+        form_copy = form.copy()
         forms = Form.objects.all()
 
         self.assertEqual(2, forms.count())
 
-        form_copy = forms.get(~Q(slug__in=["test-copying-with-vars"]))
         variables_copy = form_copy.formvariable_set.all()
 
         self.assertEqual(3, variables_copy.count())
@@ -260,27 +137,17 @@ class CopyFormAPITests(APITestCase):
         self.assertEqual(form_copy.formlogic_set.count(), 1)
 
     def test_copy_form_with_registration_backends(self):
-        super_user = SuperUserFactory.create()
-        token = TokenFactory(user=super_user)
-
         form = FormFactory.create(slug="test-copying-with-backends")
         FormRegistrationBackendFactory.create_batch(2, form=form)
 
         self.assertEqual(Form.objects.count(), 1)
         self.assertEqual(form.registration_backends.count(), 2)
 
-        url = reverse("api:form-copy", args=(form.uuid,))
-        response = self.client.post(
-            url, format="json", HTTP_AUTHORIZATION=f"Token {token.key}"
-        )
-
-        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
-
+        form_copy = form.copy()
         forms = Form.objects.all()
 
         self.assertEqual(2, forms.count())
 
-        form_copy = forms.get(~Q(slug__in=["test-copying-with-backends"]))
         registration_backends_copy = form_copy.registration_backends.all()
 
         self.assertEqual(registration_backends_copy.count(), 2)
