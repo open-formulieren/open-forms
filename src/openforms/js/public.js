ffab818341308d7fda10012dc1f7e2944d764eb3
Sergei Maertens <sergei@maykinmedia.nl>
2024-05-10 18:07:21 +0200

    :art: Move cookiebar JS to webpack bundle
    No more inline script and CSP shenanigans.

--- src/openforms/js/public.js
+++ src/openforms/js/public.js
@@ -1,3 +1,5 @@
+import {showCookieBar} from 'django-cookie-consent';
+
 import 'components/auth-mode';
 import {onLoaded} from 'utils/dom';
 
@@ -12,8 +14,33 @@ const registerPrintHandler = () => {
   });
 };
 
+const initCookieBar = () => {
+  const templateNode = document.getElementById('cookie-consent__cookie-bar');
+  if (!templateNode) return;
+
+  const {varName, statusUrl} = templateNode.dataset;
+  showCookieBar({
+    statusUrl,
+    templateSelector: '#cookie-consent__cookie-bar',
+    cookieGroupsSelector: '#cookie-consent__cookie-groups',
+    acceptSelector: '.cookie-notice__accept',
+    declineSelector: '.cookie-notice__decline',
+    insertBefore: '#cookie-consent__cookie-bar',
+    onAccept: groups => {
+      const isAnalyticsEnabled = groups.find(group => group.varname === varName);
+      if (!isAnalyticsEnabled) return;
+      const analyticsTemplateNodes = document.querySelectorAll('.analytics-scripts');
+      analyticsTemplateNodes.forEach(templateNode => {
+        const clone = templateNode.content.cloneNode(true);
+        templateNode.parentNode.insertBefore(clone, templateNode);
+      });
+    },
+  });
+};
+
 const init = () => {
   registerPrintHandler();
+  initCookieBar();
 };
 
 onLoaded(init);
