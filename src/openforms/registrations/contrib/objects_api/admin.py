e653b61c7674ef00c80c1d0a59d36a64d41978bf
Viicos <65306057+Viicos@users.noreply.github.com>
2024-05-27 17:14:45 +0200

    [#4267] Register `ObjectsAPIGroupConfig` in admin

--- src/openforms/registrations/contrib/objects_api/admin.py
+++ src/openforms/registrations/contrib/objects_api/admin.py
@@ -2,9 +2,34 @@ from django.contrib import admin
 
 from solo.admin import SingletonModelAdmin
 
-from .models import ObjectsAPIConfig
+from .models import ObjectsAPIConfig, ObjectsAPIGroupConfig
 
 
 @admin.register(ObjectsAPIConfig)
 class ObjectsAPIConfigAdmin(SingletonModelAdmin):
     pass
+
+
+@admin.register(ObjectsAPIGroupConfig)
+class ObjectsAPIGroupConfigAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "objects_service",
+        "objecttypes_service",
+        "drc_service",
+        "catalogi_service",
+    )
+    list_select_related = (
+        "objects_service",
+        "objecttypes_service",
+        "drc_service",
+        "catalogi_service",
+    )
+    search_fields = ("name",)
+    raw_id_fields = (
+        "objects_service",
+        "objecttypes_service",
+        "drc_service",
+        "catalogi_service",
+    )
+    ordering = ("name",)
