a36a7e37e56022a7fe429d36cae3f543fb535048
Sergei Maertens <sergei@maykinmedia.nl>
2024-06-03 09:45:25 +0200

    :alien: [#4341] Use automatic JSX runtime in babel config
    This was probably an implicit default in older SB versions - without
    it, the existing stories don't render.

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/react'],
+  presets: [
+    '@babel/preset-env',
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+      },
+    ],
+  ],
   plugins: [
     [
       'formatjs',
