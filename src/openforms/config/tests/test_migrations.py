facc701d10c8b9521a40b6c13689a4dae03de6e3
Sergei Maertens <sergei@maykinmedia.nl>
2024-06-04 09:11:05 +0200

    :white_check_mark: [#4346] Add tests for migrations

--- src/openforms/config/tests/test_migrations.py
+++ src/openforms/config/tests/test_migrations.py
@@ -95,3 +95,59 @@ class MigrateToCoSignRequestTemplateWithNoLinksOption(TestMigrations):
             self.assertTrue(
                 config.cosign_request_template_nl.startswith("<p>Dit is een verzoek")
             )
+
+
+class MigrateFeatureFlagsTests(TestMigrations):
+    app = "config"
+    migrate_from = "0060_merge_20240517_1517"
+    migrate_to = "0061_move_feature_flags"
+
+    def setUpBeforeMigration(self, apps: StateApps):
+        GlobalConfiguration = apps.get_model("config", "GlobalConfiguration")
+        GlobalConfiguration.objects.create(
+            enable_demo_plugins=True,
+            display_sdk_information=False,
+        )
+        FlagState = apps.get_model("flags", "FlagState")
+        FlagState.objects.all().delete()
+
+    def test_feature_flags_created(self):
+        FlagState = self.apps.get_model("flags", "FlagState")
+        flags = FlagState.objects.all()
+
+        self.assertEqual(len(flags), 2)
+        by_name = {flag.name: flag for flag in flags}
+
+        flag1 = by_name["ENABLE_DEMO_PLUGINS"]
+        self.assertEqual(flag1.condition, "boolean")
+        self.assertEqual(flag1.value, "True")
+
+        flag2 = by_name["DISPLAY_SDK_INFORMATION"]
+        self.assertEqual(flag2.condition, "boolean")
+        self.assertEqual(flag2.value, "False")
+
+
+class ReverseMigrateFeatureFlagsTests(TestMigrations):
+    app = "config"
+    migrate_from = "0061_move_feature_flags"
+    migrate_to = "0060_merge_20240517_1517"
+
+    def setUpBeforeMigration(self, apps: StateApps):
+        FlagState = apps.get_model("flags", "FlagState")
+        FlagState.objects.all().delete()
+        FlagState.objects.create(
+            name="ENABLE_DEMO_PLUGINS", condition="boolean", value="yes"
+        )
+        FlagState.objects.create(
+            name="UNRELATED", condition="invalid", value="irrelevant"
+        )
+
+        GlobalConfiguration = apps.get_model("config", "GlobalConfiguration")
+        GlobalConfiguration.objects.create()
+
+    def test_feature_flags_created(self):
+        GlobalConfiguration = self.apps.get_model("config", "GlobalConfiguration")
+        config = GlobalConfiguration.objects.get()
+
+        self.assertTrue(config.enable_demo_plugins)
+        self.assertFalse(config.display_sdk_information)
