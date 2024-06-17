2fd9bd2f62c19fe5d08082b842aa9d9deda2e4dc
Sergei Maertens <sergei@maykinmedia.nl>
2024-06-17 10:24:51 +0200

    :white_check_mark: [#4246] Move disabled-plugin API endpoint tests
    These tests were specified for each plugin, but the general mechanism
    is the same and belongs at the authenticaton-module level.

--- src/openforms/authentication/api/tests/test_endpoints.py
+++ src/openforms/authentication/api/tests/test_endpoints.py
@@ -1,12 +1,14 @@
 from unittest.mock import patch
 
 from django.db.models import TextChoices
+from django.test import override_settings
 
 from rest_framework import status
 from rest_framework.reverse import reverse, reverse_lazy
 from rest_framework.test import APITestCase
 
 from openforms.accounts.tests.factories import UserFactory
+from openforms.config.models import GlobalConfiguration
 from openforms.utils.tests.feature_flags import enable_feature_flag
 
 from ...base import BasePlugin
@@ -69,6 +71,7 @@ class AuthTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
+@override_settings(SOLO_CACHE=None)
 class ResponseTests(APITestCase):
     @classmethod
     def setUpTestData(cls):
@@ -130,3 +133,16 @@ class ResponseTests(APITestCase):
         ]
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json(), expected)
+
+    def test_disabled_plugins_are_not_returned(self):
+        config = GlobalConfiguration.get_solo()
+        config.plugin_configuration = {
+            "authentication": {"plugin1": {"enabled": False}},
+        }
+        config.save()
+        endpoint = reverse("api:authentication-plugin-list")
+
+        response = self.client.get(endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), [])
