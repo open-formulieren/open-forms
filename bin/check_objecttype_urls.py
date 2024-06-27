042276634c188a6303aa7497353a44ef27be5123
Viicos <65306057+Viicos@users.noreply.github.com>
2024-06-27 14:39:28 +0200

    [#4267] Use `tabulate` in check script

--- bin/check_objecttype_urls.py
+++ bin/check_objecttype_urls.py
@@ -8,6 +8,7 @@ from uuid import UUID
 import django
 
 import click
+from tabulate import tabulate
 
 SRC_DIR = Path(__file__).parent.parent / "src"
 sys.path.insert(0, str(SRC_DIR.resolve()))
@@ -16,29 +17,52 @@ sys.path.insert(0, str(SRC_DIR.resolve()))
 def check_objecttype_urls() -> bool:
     from openforms.forms.models import FormRegistrationBackend
 
+    problems: list[list[str]] = []
+
     for backend in FormRegistrationBackend.objects.filter(backend="objects_api"):
         objecttype_url = backend.options.get("objecttype")
         if not objecttype_url:
-            click.echo(
-                click.style(
-                    f"No Objecttype URL present for backend {backend.name!r} (key: {backend.key!r})",
-                    fg="red",
-                )
+            problems.append(
+                [
+                    backend.form.admin_name,
+                    backend.name,
+                    backend.key,
+                    "No objecttype URL present",
+                ]
             )
-            return False
+            continue
 
         try:
             UUID(objecttype_url.rsplit("/", 1)[1])
         except (IndexError, ValueError):
-            click.echo(
-                click.style(
-                    f"{objecttype_url} is invalid for {backend.name!r} (key: {backend.key!r})",
-                    fg="red",
-                )
+            problems.append(
+                [
+                    backend.form.admin_name,
+                    backend.name,
+                    backend.key,
+                    f"Invalid objecttype URL: {objecttype_url}",
+                ]
             )
-            return False
 
-    return True
+    if not problems:
+        click.echo(click.style("No problems found.", fg="green"))
+        return True
+
+    click.echo(click.style("Found problems in form registration backends.", fg="red"))
+    click.echo("")
+    click.echo(
+        tabulate(
+            problems,
+            headers=(
+                "Form",
+                "Registration backend name",
+                "Regisration backend key",
+                "Problem",
+            ),
+        )
+    )
+
+    return False
 
 
 def main(skip_setup: bool = False) -> bool:
