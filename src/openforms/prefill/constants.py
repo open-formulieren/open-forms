1d6a2b12b61ddf661593ded16be104e77e05e78d
Sergei Maertens <sergei@maykinmedia.nl>
2024-07-02 12:06:25 +0200

    :bulb: [#4246] Update code comments
    The TODO was handled in previous commits and can be removed.

--- src/openforms/prefill/constants.py
+++ src/openforms/prefill/constants.py
@@ -4,9 +4,4 @@ from django.utils.translation import gettext_lazy as _
 
 class IdentifierRoles(models.TextChoices):
     main = "main", _("Main")
-    # TODO: this value is wrongly named:
-    # 1. authorised person only applies for DigiD machtigen
-    # 2. with eHerkenning (bewindvoering), the authorizee is de *company* (legal
-    #    subject). For the acting subject, we only get an opaque, encrypted identifier
-    #    that cannot be used to prefill information (this is by design).
     authorizee = "authorizee", _("Authorizee")
