c5bff5b1b4568665552fe8f706a6e0f53ff40cd4
Sergei Maertens <sergei@maykinmedia.nl>
2024-07-04 13:02:40 +0200

    :card_file_box: [#4486] Squash config app migrations
    Squashed all the migrations from Open Forms 2.5 until 2.7.
    
    * Cleared the RunPython operations that are guaranteed to have been
      executed, since upgrading to 2.7 requires you to be on at least
      2.6.7 (which includes some migrations)
    * Removed the RunPython operations from the squashed migration,
      as these are not relevant for new installs.

--- src/openforms/config/migrations/0054_enable_new_builder.py
+++ src/openforms/config/migrations/0054_enable_new_builder.py
@@ -3,23 +3,10 @@
 from django.db import migrations
 
 
-def enable_new_builder(apps, _):
-    GlobalConfiguration = apps.get_model("config", "GlobalConfiguration")
-    # there can only ever be one record
-    config = GlobalConfiguration.objects.first()
-    if config is None or config.enable_react_formio_builder:
-        return
-
-    config.enable_react_formio_builder = True
-    config.save()
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
         ("config", "0001_initial_to_v250"),
     ]
 
-    operations = [
-        migrations.RunPython(enable_new_builder, migrations.RunPython.noop),
-    ]
+    operations = []
