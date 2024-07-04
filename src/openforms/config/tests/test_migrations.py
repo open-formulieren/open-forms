748278d2aa8119feb6146ce5cd9465180661acde
Sergei Maertens <sergei@maykinmedia.nl>
2024-07-04 13:55:12 +0200

    :fire: [#4486] Delete obsolete tests
    The migrations themselves are made empty/removed, so the tests
    no longer make sense.

--- src/openforms/config/tests/test_migrations.py
+++ src/openforms/config/tests/test_migrations.py
@@ -3,104 +3,10 @@ from django.db.migrations.state import StateApps
 from openforms.utils.tests.test_migrations import TestMigrations
 
 
-class EnableNewBuilderMigrationTests(TestMigrations):
-    app = "config"
-    migrate_from = "0001_initial_to_v250"
-    migrate_to = "0054_enable_new_builder"
-
-    def setUpBeforeMigration(self, apps):
-        GlobalConfiguration = apps.get_model("config", "GlobalConfiguration")
-        GlobalConfiguration.objects.create(enable_react_formio_builder=False)
-
-    def test_builder_enabled(self):
-        GlobalConfiguration = self.apps.get_model("config", "GlobalConfiguration")
-        config = GlobalConfiguration.objects.get()
-
-        self.assertTrue(config.enable_react_formio_builder)
-
-
-class MigrateToOrderIdTemplateExistingMigrationTests(TestMigrations):
-    app = "config"
-    migrate_from = "0056_globalconfiguration_enable_backend_formio_validation"
-    migrate_to = "0057_migrate_to_order_id_template"
-    setting_overrides = {"RELEASE": "a_new_release"}
-
-    def setUpBeforeMigration(self, apps: StateApps) -> None:
-        GlobalConfiguration = apps.get_model("config", "GlobalConfiguration")
-        VersionInfo = apps.get_model("upgrades", "VersionInfo")
-
-        GlobalConfiguration.objects.create(payment_order_id_prefix="{year}CUSTOM")
-        version_info = VersionInfo.objects.first()
-        assert version_info is not None
-        version_info.current = "some_existing_version"
-        version_info.save()
-
-    def test_template_from_prefix(self) -> None:
-        GlobalConfiguration = self.apps.get_model("config", "GlobalConfiguration")
-        config = GlobalConfiguration.objects.get()
-
-        self.assertEqual(config.payment_order_id_template, "{year}CUSTOM{uid}")
-
-
-class MigrateToOrderIdTemplateNewMigrationTests(TestMigrations):
-    app = "config"
-    migrate_from = "0056_globalconfiguration_enable_backend_formio_validation"
-    migrate_to = "0057_migrate_to_order_id_template"
-    setting_overrides = {"RELEASE": "dev"}
-
-    def setUpBeforeMigration(self, apps: StateApps) -> None:
-        GlobalConfiguration = apps.get_model("config", "GlobalConfiguration")
-        VersionInfo = apps.get_model("upgrades", "VersionInfo")
-
-        GlobalConfiguration.objects.create()
-
-        # For some reason, `current` isn't set to `"dev"` on CI:
-        version_info = VersionInfo.objects.first()
-        assert version_info is not None
-        version_info.current = "dev"
-        version_info.save()
-
-    def test_template_default_value(self) -> None:
-        GlobalConfiguration = self.apps.get_model("config", "GlobalConfiguration")
-        config = GlobalConfiguration.objects.get()
-
-        self.assertEqual(
-            config.payment_order_id_template, "{year}/{public_reference}/{uid}"
-        )
-
-
-class MigrateToCoSignRequestTemplateWithNoLinksOption(TestMigrations):
-    app = "config"
-    migrate_from = "0057_migrate_to_order_id_template"
-    migrate_to = "0058_globalconfiguration_cosign_request_template_and_more"
-
-    def setUpBeforeMigration(self, apps: StateApps):
-        GlobalConfiguration = apps.get_model("config", "GlobalConfiguration")
-        GlobalConfiguration.objects.create(show_form_link_in_cosign_email=False)
-
-    def test_template_does_not_contain_form_url_var(self):
-        GlobalConfiguration = self.apps.get_model("config", "GlobalConfiguration")
-        config = GlobalConfiguration.objects.get()
-
-        with self.subTest("Link setting respected"):
-            self.assertNotIn(r"{{ form_url }}", config.cosign_request_template_en)
-            self.assertNotIn(r"{{ form_url }}", config.cosign_request_template_nl)
-
-        with self.subTest("Translated in migrations"):
-            self.assertTrue(
-                config.cosign_request_template_en.startswith(
-                    "<p>This is a request to co-sign"
-                )
-            )
-            self.assertTrue(
-                config.cosign_request_template_nl.startswith("<p>Dit is een verzoek")
-            )
-
-
 class MigrateFeatureFlagsTests(TestMigrations):
     app = "config"
-    migrate_from = "0060_merge_20240517_1517"
-    migrate_to = "0061_move_feature_flags"
+    migrate_from = "0001_initial_to_v250"
+    migrate_to = "0054_v250_to_v270"
 
     def setUpBeforeMigration(self, apps: StateApps):
         GlobalConfiguration = apps.get_model("config", "GlobalConfiguration")
@@ -129,8 +35,8 @@ class MigrateFeatureFlagsTests(TestMigrations):
 
 class ReverseMigrateFeatureFlagsTests(TestMigrations):
     app = "config"
-    migrate_from = "0061_move_feature_flags"
-    migrate_to = "0060_merge_20240517_1517"
+    migrate_from = "0054_v250_to_v270"
+    migrate_to = "0001_initial_to_v250"
 
     def setUpBeforeMigration(self, apps: StateApps):
         FlagState = apps.get_model("flags", "FlagState")
