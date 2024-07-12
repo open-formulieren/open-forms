acbcb4dfabe93a0656b68eb34ea3f1ccb220e9c1
Sergei Maertens <sergei@maykinmedia.nl>
2024-07-12 12:25:21 +0200

    :label: [#4051] Fix type annotation for json logic validator
    It takes any json, as primitives or arrays are also valid JSON logic.

--- src/openforms/utils/json_logic/api/validators.py
+++ src/openforms/utils/json_logic/api/validators.py
@@ -3,11 +3,13 @@ from django.utils.translation import gettext as _
 from json_logic import jsonLogic
 from rest_framework import serializers
 
+from openforms.typing import JSONValue
+
 
 class JsonLogicValidator:
     """Validate that a json object is a valid jsonLogic expression"""
 
-    def __call__(self, value: dict):
+    def __call__(self, value: JSONValue):
         try:
             jsonLogic(value)
         except Exception as exc:
