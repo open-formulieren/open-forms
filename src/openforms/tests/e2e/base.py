6fcd8383b1d7bf044ab9d8e4adebad4e15072e08
Sergei Maertens <sergei@maykinmedia.nl>
2024-07-12 15:54:42 +0200

    :alien: [#4051] Browsers don't have the same permissions machinery
    See https://github.com/microsoft/playwright/issues/13037 for more
    context.

--- src/openforms/tests/e2e/base.py
+++ src/openforms/tests/e2e/base.py
@@ -1,6 +1,6 @@
 import os
 from contextlib import asynccontextmanager
-from typing import Literal
+from typing import Any, Literal, TypeAlias
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import override_settings, tag
@@ -13,10 +13,10 @@ from playwright.async_api import BrowserType, Page, async_playwright
 
 from openforms.accounts.tests.factories import SuperUserFactory
 
+SupportedBrowser: TypeAlias = Literal["chromium", "firefox", "webkit"]
+
 HEADLESS = "NO_E2E_HEADLESS" not in os.environ
-BROWSER: Literal["chromium", "firefox", "webkit"] = os.getenv(
-    "E2E_DRIVER", default="chromium"
-)  # type:ignore
+BROWSER: SupportedBrowser = os.getenv("E2E_DRIVER", default="chromium")  # type:ignore
 SLOW_MO = int(os.environ.get("SLOW_MO", "100"))
 PLAYWRIGHT_BROWSERS_PATH = os.getenv("PLAYWRIGHT_BROWSERS_PATH", default=None)
 
@@ -24,6 +24,10 @@ LAUNCH_KWARGS = {
     "headless": HEADLESS,
     "slow_mo": SLOW_MO,
     "executable_path": PLAYWRIGHT_BROWSERS_PATH,
+}
+
+BROWSER_PERMISSIONS: dict[SupportedBrowser, list[str]] = {
+    "chromium": ["clipboard-read", "clipboard-write"],
 }
 
 
@@ -36,15 +40,18 @@ def create_superuser(**kwargs):
 
 @asynccontextmanager
 async def browser_page():
+    context_kwargs: dict[str, Any] = {
+        "locale": "en-UK",
+        "timezone_id": "Europe/Amsterdam",
+    }
+    if permissions := BROWSER_PERMISSIONS.get(BROWSER):
+        context_kwargs["permissions"] = permissions
+
     async with async_playwright() as p:
         try:
             _browser: BrowserType = getattr(p, BROWSER)
             browser = await _browser.launch(**LAUNCH_KWARGS)
-            context = await browser.new_context(
-                locale="en-UK",
-                timezone_id="Europe/Amsterdam",
-                permissions=["clipboard-read", "clipboard-write"],
-            )
+            context = await browser.new_context(**context_kwargs)
             page = await context.new_page()
             yield page
         finally:
