64ff07c04d951487dc94614cfe409ab124e45882
Sergei Maertens <sergei@maykinmedia.nl>
2024-07-29 12:59:20 +0200

    :sparkles: [#4567] Mimick Django's collapsible fieldset
    Django admin supports the collapse/expand functionality on admin
    fieldsets, which helps focusing the important fields while still
    exposing the finer details without being too overwhelming. This
    functionality is now also part of our React component.
    
    This ties in with the validation errors - the fieldset should be
    expanded if there are validation errors inside to make them visible.
    The user of the fieldset needs to provide the list of field names to
    watch for, and the parent component needs to provide the validation
    error context so that presence of errors can be detected.
    
    If validation errors relevant to the fieldset are detected, the initial
    fieldset collapse state is set to 'open'. The user can still toggle
    this in the UI as usual.

--- src/openforms/js/components/admin/forms/Fieldset.js
+++ src/openforms/js/components/admin/forms/Fieldset.js
@@ -1,14 +1,76 @@
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import React from 'react';
+import {useContext, useState} from 'react';
+import {FormattedMessage} from 'react-intl';
 
-const Fieldset = ({title = '', children, extraClassName, ...extra}) => {
-  const titleNode = title ? <h2>{title}</h2> : null;
-  const className = `module aligned ${extraClassName || ''}`;
+import {ValidationErrorContext} from './ValidationErrors';
+
+const checkHasErrors = (errors = [], fieldNames) => {
+  // no field names known -> we don't know what to check, so assume no errors inside
+  if (!fieldNames.length) return false;
+  // check the error keys against the fieldName list
+  for (const error of errors) {
+    const errorKey = error[0];
+    // check for exact membership
+    if (fieldNames.includes(errorKey)) return true;
+    // check for key prefix match for potential nested errors
+    const partialMatch = fieldNames.some(name => errorKey.startsWith(`${name}.`));
+    if (partialMatch) return true;
+  }
+  return false;
+};
+
+const Fieldset = ({
+  title = '',
+  children,
+  extraClassName,
+  collapsible,
+  initialCollapsed = true,
+  fieldNames = [],
+  ...extra
+}) => {
+  const validationErrors = useContext(ValidationErrorContext);
+  const hasErrorsInside = checkHasErrors(validationErrors, fieldNames);
+
+  if (initialCollapsed && hasErrorsInside) {
+    initialCollapsed = false;
+  }
+
+  const [collapsed, setCollapsed] = useState(collapsible && initialCollapsed);
+
+  const titleNode = title ? (
+    <h2>
+      {title}
+      {collapsible && (
+        <>
+          {' '}
+          <a
+            href="#"
+            onClick={e => {
+              e.preventDefault();
+              setCollapsed(!collapsed);
+            }}
+            className="collapse-toggle"
+          >
+            <FormattedMessage
+              description="Fieldset collapse link text"
+              defaultMessage="{collapsed, select, true {(Show)} other {(Hide)}}"
+              values={{collapsed}}
+            />
+          </a>
+        </>
+      )}
+    </h2>
+  ) : null;
+  const className = classNames('module', 'aligned', extraClassName, {
+    'collapse in': collapsible,
+    collapsed: collapsed,
+  });
 
   return (
     <fieldset className={className} {...extra}>
       {titleNode}
-      {children}
+      {!collapsed && children}
     </fieldset>
   );
 };
@@ -16,6 +78,15 @@ const Fieldset = ({title = '', children, extraClassName, ...extra}) => {
 Fieldset.propTypes = {
   title: PropTypes.node,
   children: PropTypes.node,
+  collapsible: PropTypes.bool,
+  initialCollapsed: PropTypes.bool,
+  /**
+   * List of field names to monitor for validation errors.
+   *
+   * If validation errors are present for any of these fields, and the fieldset is in
+   * collapsed state -> ensure that the fieldset is initially opened.
+   */
+  fieldNames: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default Fieldset;
