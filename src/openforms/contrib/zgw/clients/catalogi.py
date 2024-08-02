f01fde6c2aa6a6cc9e1fefc7a9a7b7df5723341f
Sergei Maertens <sergei@maykinmedia.nl>
2024-08-02 14:55:10 +0200

    :sparkles: [#4577] Filter document type versions based on version date
    This required adding a mechanism to determine which particular version
    of the standard an API implements, since using query params that were
    added in newer versions on an older version results in HTTP 400
    responses because unknown query parameters are used.
    
    There's quite a bit of checking to do and server providers make
    mistakes, so we can now explicitly throw errors that have to do with
    the API standard (when we think as a client that the server is
    implementing it wrong).
    
    For APIs that don't have this parameter, we do the filtering in Python.

--- src/openforms/contrib/zgw/clients/catalogi.py
+++ src/openforms/contrib/zgw/clients/catalogi.py
@@ -1,8 +1,15 @@
-from typing import Callable, Iterator, NotRequired, TypedDict
+from datetime import date
+from operator import itemgetter
+from typing import Callable, Iterator, NotRequired, TypeAlias, TypedDict
 
+from requests import Response
 from zgw_consumers.nlx import NLXClient
 
 from openforms.utils.api_clients import PaginatedResponseData, pagination_helper
+
+
+class StandardViolation(RuntimeError):
+    pass
 
 
 def noop_matcher(roltypen: list) -> list:
@@ -41,7 +48,44 @@ class InformatieObjectType(TypedDict):
     concept: NotRequired[bool]
 
 
+CatalogiAPIVersion: TypeAlias = tuple[
+    int,  # major
+    int,  # minor
+    int,  # patch
+]
+
+
 class CatalogiClient(NLXClient):
+    _api_version: CatalogiAPIVersion | None = None
+
+    @property
+    def api_version(self) -> CatalogiAPIVersion:
+        if self._api_version is None:
+            # hit a known endpoint and parse the response headers. VRSN is just a made
+            # up domain to limit the result set, hopefully it's even entirely empty.
+            response = self.get("catalogussen", params={"domein": "VRSN"})
+            self._api_version = self._determine_api_version(response)
+        return self._api_version
+
+    def _determine_api_version(self, response: Response) -> CatalogiAPIVersion:
+        try:
+            version = response.headers["API-version"]
+        except KeyError as exc:
+            raise StandardViolation(
+                "API-version is a required response header."
+            ) from exc
+        try:
+            major, minor, patch = [int(bit) for bit in version.split(".")]
+        except ValueError as exc:
+            raise StandardViolation("API-version must follow semver format.") from exc
+        return (major, minor, patch)
+
+    def request(self, *args, **kwargs):
+        response = super().request(*args, **kwargs)
+        if not self._api_version:
+            self._determine_api_version(response)
+        return response
+
     def get_all_catalogi(self) -> Iterator[dict]:
         """
         List all available catalogi, consuming pagination if relevant.
@@ -62,7 +106,7 @@ class CatalogiClient(NLXClient):
         data: PaginatedResponseData[Catalogus] = response.json()
 
         if (num_results := len(data["results"])) > 1:
-            raise RuntimeError(
+            raise StandardViolation(
                 "Combination of domain + rsin must be unique according to the standard."
             )
         if num_results == 0:
@@ -87,6 +131,7 @@ class CatalogiClient(NLXClient):
         *,
         catalogus: str,
         description: str,
+        valid_on: date | None = None,
     ) -> list[InformatieObjectType] | None:
         """
         Look up an informatieobjecttype within the specified catalogue.
@@ -94,19 +139,40 @@ class CatalogiClient(NLXClient):
         The description is unique within a catalogue, but multiple versions may exist.
         If no results are found, ``None`` is returned.
         """
-        response = self.get(
-            "informatieobjecttypen",
-            params={
-                "catalogus": catalogus,
-                "omschrijving": description,
-            },
-        )
+        _supports_filtering_valid_on = self.api_version >= (1, 2, 0)
+
+        params = {
+            "catalogus": catalogus,
+            "omschrijving": description,
+        }
+
+        # datumGeldigheid was added in v1.2.0 of the APIs & the APIs reject query string
+        # parameters that are unkonwn, so we can not just optmistically send it :(
+        if valid_on and _supports_filtering_valid_on:
+            params["datumGeldigheid"] = valid_on.isoformat()
+
+        response = self.get("informatieobjecttypen", params=params)
         response.raise_for_status()
         data: PaginatedResponseData[InformatieObjectType] = response.json()
         if data["count"] == 0:
             return None
 
-        all_versions = list(pagination_helper(self, data))
+        all_versions = sorted(
+            list(pagination_helper(self, data)),
+            key=itemgetter("beginGeldigheid"),
+        )
+
+        # otherwise do the filtering manually
+        # TODO: set up testing for this! probably can only do this with mocks
+        if valid_on and not _supports_filtering_valid_on:
+            date_str = valid_on.isoformat()
+            all_versions = [
+                version
+                for version in all_versions
+                if version["beginGeldigheid"] <= date_str
+                if (end := version.get("eindeGeldigheid")) is None or end > date_str
+            ]
+
         return all_versions
 
     def list_statustypen(self, zaaktype: str) -> list[dict]:
