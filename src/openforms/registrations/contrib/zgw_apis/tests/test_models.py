3f1c6d33f214b283d75d4c580f9521baac443f42
Sergei Maertens <sergei@maykinmedia.nl>
2024-08-19 18:45:13 +0200

    :recycle: [#4606] Remove shared state from tests

--- src/openforms/registrations/contrib/zgw_apis/tests/test_models.py
+++ src/openforms/registrations/contrib/zgw_apis/tests/test_models.py
@@ -5,24 +5,16 @@ from .factories import ZGWApiGroupConfigFactory
 
 
 class ZGWApiGroupModelTests(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        config = ZGWApiGroupConfigFactory.create(
-            zrc_service__api_root="https://zaken.nl/api/v1/",
-            drc_service__api_root="https://documenten.nl/api/v1/",
-            ztc_service__api_root="https://catalogus.nl/api/v1/",
-        )
-        config.full_clean()
-        cls.config = config
-
     def test_model_validate(self):
-        self.config.rsin = "619183020"
-        self.config.full_clean()
+        config = ZGWApiGroupConfigFactory.create(organisatie_rsin="619183020")
+        config.full_clean()
 
     def test_model_invalid_rsin(self):
+        config = ZGWApiGroupConfigFactory.create()
+        config.organisatie_rsin = "063-08836"
+
         with self.assertRaises(ValidationError):
-            self.config.organisatie_rsin = "063-08836"
-            self.config.full_clean()
+            config.full_clean()
 
     def test_model_string(self):
         api_group = ZGWApiGroupConfigFactory.create(name="ZGW API test")
