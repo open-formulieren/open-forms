b80e3c83eaadba6e5d43a6f47904e6d06665af67
Sergei Maertens <sergei@maykinmedia.nl>
2024-08-19 18:47:04 +0200

    :white_check_mark: [#4606] Add tests for catalogue fields in ZGW API group
    * Added tests for the database check constraints
    * Added tests for the catalogue validation

--- src/openforms/registrations/contrib/zgw_apis/tests/test_models.py
+++ src/openforms/registrations/contrib/zgw_apis/tests/test_models.py
@@ -1,7 +1,14 @@
+from pathlib import Path
+
 from django.core.exceptions import ValidationError
-from django.test import TestCase
+from django.db import IntegrityError, transaction
+from django.test import TestCase, override_settings
+
+from openforms.utils.tests.vcr import OFVCRMixin
 
 from .factories import ZGWApiGroupConfigFactory
+
+VCR_TEST_FILES = Path(__file__).parent / "files"
 
 
 class ZGWApiGroupModelTests(TestCase):
@@ -20,3 +27,79 @@ class ZGWApiGroupModelTests(TestCase):
         api_group = ZGWApiGroupConfigFactory.create(name="ZGW API test")
 
         self.assertEqual(str(api_group), "ZGW API test")
+
+    def test_can_save_without_catalogue_information(self):
+        # checks that the check constraints are defined correctly
+        instance = ZGWApiGroupConfigFactory.create(
+            catalogue_domain="",
+            catalogue_rsin="",
+        )
+
+        self.assertIsNotNone(instance.pk)
+
+    def test_can_save_with_catalogue_information(self):
+        # checks that the check constraints are defined correctly
+        instance = ZGWApiGroupConfigFactory.create(
+            catalogue_domain="Test",
+            catalogue_rsin="123456782",
+        )
+
+        self.assertIsNotNone(instance.pk)
+
+    def test_cannot_save_with_partial_information(self):
+        # checks that the check constraints are defined correctly
+        with self.assertRaises(IntegrityError), transaction.atomic():
+            ZGWApiGroupConfigFactory.create(catalogue_domain="Test", catalogue_rsin="")
+
+        with self.assertRaises(IntegrityError), transaction.atomic():
+            ZGWApiGroupConfigFactory.create(
+                catalogue_domain="", catalogue_rsin="123456782"
+            )
+
+
+@override_settings(LANGUAGE_CODE="en")
+class ZGWAPIGroupValidationTests(OFVCRMixin, TestCase):
+    VCR_TEST_FILES = VCR_TEST_FILES
+
+    def test_validate_no_catalogue_specified(self):
+        config = ZGWApiGroupConfigFactory.create(catalogue_domain="", catalogue_rsin="")
+
+        try:
+            config.full_clean()
+        except ValidationError as exc:
+            raise self.failureException("Not specifying a catalogue is valid.") from exc
+
+    def test_catalogue_specified_but_ztc_service_is_missing(self):
+        config = ZGWApiGroupConfigFactory.create(
+            ztc_service=None, catalogue_domain="TEST", catalogue_rsin="000000000"
+        )
+
+        with self.assertRaises(ValidationError) as exc_context:
+            config.full_clean()
+
+        self.assertIn("ztc_service", exc_context.exception.error_dict)
+
+    def test_validate_catalogue_exists(self):
+        # validates against the fixtures in docker/open-zaak
+        config = ZGWApiGroupConfigFactory.create(
+            for_test_docker_compose=True,
+            catalogue_domain="NOPE",  # does not exist in fixtures
+            catalogue_rsin="000000000",
+        )
+
+        with self.subTest("invalid catalogue"):
+            with self.assertRaisesMessage(
+                ValidationError,
+                "The specified catalogue does not exist. Maybe you made a typo in the "
+                "domain or RSIN?",
+            ):
+                config.full_clean()
+
+        with self.subTest("valid catalogue"):
+            config.catalogue_domain = "TEST"  # exists in the fixture
+            try:
+                config.full_clean()
+            except ValidationError as exc:
+                raise self.failureException(
+                    "Catalogue exists and should validate"
+                ) from exc
