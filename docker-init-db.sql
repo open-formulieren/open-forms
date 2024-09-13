97da4653827eb7e6d4e19484db2d24ca58e16b73
Sergei Maertens <sergei@maykinmedia.nl>
2024-09-13 17:33:03 +0200

    :whale: [#4648] Avoid public schema permission issues
    The application user is allowed to be owner of the entire app database,
    this way we don't need to fiddle with the public schema permissions
    in docker compose.

--- docker-init-db.sql
+++ docker-init-db.sql
@@ -1,5 +1,2 @@
 CREATE USER openforms;
-CREATE DATABASE openforms;
-GRANT ALL PRIVILEGES ON DATABASE openforms TO openforms;
--- On Postgres 15+, connect to the database and grant schema permissions.
--- GRANT USAGE, CREATE ON SCHEMA public TO openforms;
+CREATE DATABASE openforms WITH OWNER openforms;
