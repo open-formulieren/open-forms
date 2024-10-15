8b3c1e387f0ba9afba0fa6d0bbe55d483c293c09
Steven Bal <steven@maykinmedia.nl>
2024-10-15 16:09:11 +0200

    :rewind: [#4375] Remove disable hidden fields envvar for objects API

--- src/openforms/formio/rendering/default.py
+++ src/openforms/formio/rendering/default.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from typing import Callable, Iterator
 
-from django.conf import settings
 from django.urls import reverse
 from django.utils.html import format_html_join
 from django.utils.safestring import SafeString, mark_safe
@@ -36,10 +35,7 @@ class ContainerMixin:
         # class.
         # In registration mode, we need to treat layout/container nodes as visible so
         # that their children are emitted too.
-        visible_modes = {RenderModes.export, RenderModes.registration}
-        if settings.DISABLE_SENDING_HIDDEN_FIELDS:
-            visible_modes.remove(RenderModes.registration)
-        if self.mode in visible_modes:
+        if self.mode in {RenderModes.export, RenderModes.registration}:
             return True
 
         # We only pass the step data, since frontend logic only has access to the current step data.
