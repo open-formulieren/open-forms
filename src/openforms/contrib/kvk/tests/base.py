9eedc223ce2222b160dc91fed47f4150ae34e8c1
vasileios <zigras00@gmail.com>
2024-10-22 15:48:02 +0200

    [#4777] Re-record KVK API VCR cassettes

--- src/openforms/contrib/kvk/tests/base.py
+++ src/openforms/contrib/kvk/tests/base.py
@@ -12,7 +12,6 @@ from openforms.contrib.kvk.models import KVKConfig
 
 TEST_FILES = Path(__file__).parent.resolve() / "files"
 
-G1_ROOT = TEST_FILES / "Staat_der_Nederlanden_Private_Root_CA_-_G1.crt"
 
 TestFileNames = Literal[
     "basisprofiel_response.json",
@@ -26,13 +25,6 @@ def load_json_mock(name: TestFileNames):
         return json.load(f)
 
 
-# See https://developers.kvk.nl/documentation/testing
-SERVER_CERT = CertificateFactory.build(
-    label="Staat der Nederlanden Private Root CA - G1",
-    type=CertificateTypes.cert_only,
-    public_certificate__filepath=str(G1_ROOT),
-)
-
 KVK_SERVICE = ServiceFactory.build(
     api_root="https://api.kvk.nl/test/api/",
     oas="https://api.kvk.nl/test/api/",  # ignored/unused
@@ -40,7 +32,6 @@ KVK_SERVICE = ServiceFactory.build(
     auth_type=AuthTypes.api_key,
     header_key="apikey",
     header_value="l7xx1f2691f2520d487b902f4e0b57a0b197",
-    server_certificate=SERVER_CERT,
 )
 
 
@@ -60,8 +51,3 @@ class KVKTestMixin:
         )
         self.config_mock = patcher.start()
         self.addCleanup(patcher.stop)
-
-        # ensure that the certificate exists on disk, even with SimpleTestCase
-        g1_cert = Path(SERVER_CERT.public_certificate.path)
-        if not g1_cert.exists():
-            g1_cert.write_bytes(G1_ROOT.read_bytes())
