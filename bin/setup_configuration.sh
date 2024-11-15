353b5f88c3668f9735c3bd6e41c91eba99a019ef
Steven Bal <steven@maykinmedia.nl>
2024-11-15 14:50:27 +0100

    :whale: [#4788] Disable setup_config command in upgrade tests
    to avoid having to bring up the Open Zaak and Objects/Objecttypes docker compose stacks for the CI tests to simulate upgrading Open Forms

--- bin/setup_configuration.sh
+++ bin/setup_configuration.sh
@@ -5,10 +5,18 @@
 
 set -e
 
-# wait for required services
-${SCRIPTPATH}/wait_for_db.sh
+if [[ "${RUN_SETUP_CONFIG,,}" =~ ^(true|1|yes)$ ]]; then
+    # wait for required services
+    ${SCRIPTPATH}/wait_for_db.sh
 
-src/manage.py migrate
-src/manage.py setup_configuration \
-                --yaml-file data/services.yaml \
-                --yaml-file data/objects_api.yaml
+    if [[ "${SKIP_SELFTEST,,}" =~ ^(true|1|yes)$ ]]; then
+        NO_SELFTEST_FLAG="--no-selftest"
+    else
+        NO_SELFTEST_FLAG=""
+    fi
+
+    src/manage.py migrate
+    src/manage.py setup_configuration $NO_SELFTEST_FLAG \
+                    --yaml-file data/services.yaml \
+                    --yaml-file data/objects_api.yaml
+fi
