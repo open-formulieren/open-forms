ee719f05017e3fcc2b8d7bb731ea7657295720d7
Steven Bal <steven@maykinmedia.nl>
2024-11-19 13:51:55 +0100

    :whale: [#4788] Update setup_configuration docker setup
    identifiers were added instead of slugs, and the selftest functionality has been removed

--- bin/setup_configuration.sh
+++ bin/setup_configuration.sh
@@ -9,14 +9,8 @@ if [[ "${RUN_SETUP_CONFIG,,}" =~ ^(true|1|yes)$ ]]; then
     # wait for required services
     ${SCRIPTPATH}/wait_for_db.sh
 
-    if [[ "${SKIP_SELFTEST,,}" =~ ^(true|1|yes)$ ]]; then
-        NO_SELFTEST_FLAG="--no-selftest"
-    else
-        NO_SELFTEST_FLAG=""
-    fi
-
     src/manage.py migrate
-    src/manage.py setup_configuration $NO_SELFTEST_FLAG \
+    src/manage.py setup_configuration \
                     --yaml-file data/services.yaml \
                     --yaml-file data/objects_api.yaml
 fi
