61c1876ded395f18b1e4eaf62f3d72205359a5af
robinvandermolen <robin@maykinmedia.nl>
2024-11-21 11:16:26 +0100

    :sparkles: [#2173] Add map tile layer admin configuration

--- src/openforms/config/models/__init__.py
+++ src/openforms/config/models/__init__.py
@@ -1,11 +1,13 @@
 from .color import RichTextColor
 from .config import GlobalConfiguration
 from .csp import CSPSetting
+from .map import MapTileLayer
 from .theme import Theme
 
 __all__ = [
     "CSPSetting",
     "GlobalConfiguration",
     "RichTextColor",
+    "MapTileLayer",
     "Theme",
 ]
