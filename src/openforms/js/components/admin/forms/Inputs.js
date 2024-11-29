02de36429e58fa34a5910a606806b53a13b0c8a2
Sergei Maertens <sergei@maykinmedia.nl>
2024-11-29 16:01:29 +0100

    :alien: Fix markup of checkboxes with help text
    Left-over from the Django 4.2 upgrade where the HTML was changed.
    
    This ensures the help text is below the checkbox, rather than next to
    it with some awkward horizontal space between.

--- src/openforms/js/components/admin/forms/Inputs.js
+++ src/openforms/js/components/admin/forms/Inputs.js
@@ -102,19 +102,23 @@ const Checkbox = ({name, label, helpText, noVCheckbox = false, ...extraProps}) =
   name = prefix ? `${prefix}-${name}` : name;
   const idFor = disabled ? undefined : `id_${name}`;
   return (
-    <div
-      className={classNames('flex-container', 'checkbox-row', {'checkbox-row--disabled': disabled})}
-    >
-      <input type="checkbox" name={name} id={idFor} {...extraProps} />{' '}
-      <label className={classNames('inline', {vCheckboxLabel: !noVCheckbox})} htmlFor={idFor}>
-        {label}
-      </label>
-      {helpText ? (
+    <>
+      <div
+        className={classNames('flex-container', 'checkbox-row', {
+          'checkbox-row--disabled': disabled,
+        })}
+      >
+        <input type="checkbox" name={name} id={idFor} {...extraProps} />{' '}
+        <label className={classNames('inline', {vCheckboxLabel: !noVCheckbox})} htmlFor={idFor}>
+          {label}
+        </label>
+      </div>
+      {helpText && (
         <div className="help">
           <div>{helpText}</div>
         </div>
-      ) : null}
-    </div>
+      )}
+    </>
   );
 };
 
