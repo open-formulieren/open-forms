aa7abce8b2d5e9be3f3b5a4a7bcc1b5706d05252
Sergei Maertens <sergei@maykinmedia.nl>
2024-12-12 22:34:14 +0100

    :fire: [#3283] Remove OR-filter behaviour on form definitions endpoint
    Providing both the used_in and is_reusable filter parameters no longer
    returns records that match either condition, instead it applies the
    default AND behaviour.

--- src/openforms/forms/api/filters.py
+++ src/openforms/forms/api/filters.py
@@ -1,10 +1,6 @@
-import warnings
-
-from django.db.models import Q
 from django.utils.translation import gettext_lazy as _
 
 from django_filters import rest_framework as filters
-from django_filters.constants import EMPTY_VALUES
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema_field
 
@@ -31,36 +27,3 @@ class FormDefinitionFilter(filters.FilterSet):
     class Meta:
         model = FormDefinition
         fields = ("is_reusable",)
-        or_fields = ("is_reusable", "used_in")
-
-    def filter_queryset(self, queryset):
-        """
-        Override base implementation to add OR-behaviour instead of the standard AND.
-
-        See https://github.com/carltongibson/django-filter/discussions/1426
-        """
-        _or = Q()
-
-        for name in self.Meta.or_fields:
-            # this essentially re-implements django_filters.filters.Filter.filter,
-            # as otherwise we would have to introspect private django query/queryset API
-            # *shudders*
-            or_filter_value = self.form.cleaned_data.pop(name)
-            if or_filter_value in EMPTY_VALUES:
-                continue
-            or_filter = self.filters[name]
-            if or_filter.distinct:
-                queryset = queryset.distinct()
-            lookup = "%s__%s" % (or_filter.field_name, or_filter.lookup_expr)
-            q_expr = Q(**{lookup: or_filter_value})
-            if or_filter.exclude:
-                q_expr = ~q_expr
-            _or |= q_expr
-
-        if _or:
-            warnings.warn(
-                "Using OR filters is deprecated and will be removed starting with Open Forms 3.0",
-                DeprecationWarning,
-            )
-            queryset = queryset.filter(_or)
-        return super().filter_queryset(queryset)
