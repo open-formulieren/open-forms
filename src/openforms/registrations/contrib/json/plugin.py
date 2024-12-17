df91e88d5f01d8bd8c494397807d49aa0eb23bdf
Viktor van Wijk <viktorvanwijk@hotmail.com>
2024-12-17 12:15:34 +0100

    :truck: [#4908] Rename plugin
    Plugin name did not make sense before

--- src/openforms/registrations/contrib/json/plugin.py
+++ src/openforms/registrations/contrib/json/plugin.py
@@ -3,13 +3,13 @@ from django.utils.translation import gettext_lazy as _
 from openforms.submissions.models import Submission
 from ...base import BasePlugin, OptionsT  # openforms.registrations.base
 from ...registry import register  # openforms.registrations.registry
-from .config import NewPluginOptionsSerializer
+from .config import JSONOptionsSerializer
 
 
-@register("new_plugin")
-class NewPlugin(BasePlugin):
+@register("json")
+class JSONPlugin(BasePlugin):
     verbose_name = _("New fancy plugin")
-    configuration_options = NewPluginOptionsSerializer
+    configuration_options = JSONOptionsSerializer
 
     def register_submission(self, submission: Submission, options: OptionsT) -> None:
         print(options)
