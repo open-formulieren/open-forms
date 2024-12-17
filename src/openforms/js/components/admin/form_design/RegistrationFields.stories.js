5360b24acb38018f492164504615a32b07766752
Viktor van Wijk <viktorvanwijk@hotmail.com>
2024-12-17 12:09:15 +0100

    :construction: [#4908] WIP

--- src/openforms/js/components/admin/form_design/RegistrationFields.stories.js
+++ src/openforms/js/components/admin/form_design/RegistrationFields.stories.js
@@ -390,6 +390,21 @@ export default {
           type: 'object',
         },
       },
+      {
+        id: 'json',
+        label: 'JSON',
+        schema: {
+          properties: {
+            // TODO-4098: add actual relevant properties
+            extraLine: {
+              minLength: 1,
+              title: 'Extra print statement',
+              type: 'string',
+            },
+          },
+          type: 'object',
+        },
+      },
     ],
     configuredBackends: [],
     onChange: fn(),
@@ -730,6 +745,12 @@ export const ConfiguredBackends = {
           ],
         },
       },
+      {
+        key: 'backend11',
+        name: 'JSON',
+        backend: 'json',
+        options: {extraLine: 'We are checking.'},
+      },
     ],
     validationErrors: [
       ['form.registrationBackends.1.options.zgwApiGroup', 'You sure about this?'],
@@ -966,6 +987,28 @@ export const STUFZDS = {
           zdsZaaktypeOmschrijving: '',
           zdsZaaktypeStatusCode: '',
           zdsZaaktypeStatusOmschrijving: '',
+        },
+      },
+    ],
+  },
+
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+
+    await userEvent.click(canvas.getByRole('button', {name: 'Opties instellen'}));
+  },
+};
+
+
+export const JSON = {
+  args: {
+    configuredBackends: [
+      {
+        key: 'backend11',
+        name: 'JSON',
+        backend: 'json',
+        options: {
+          extraLine: 'We are checking.',
         },
       },
     ],
