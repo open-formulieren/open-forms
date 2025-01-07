7931ff088b3acb6a447014ff5e651a778a70e2c6
Viktor van Wijk <viktorvanwijk@hotmail.com>
2025-01-07 10:32:34 +0100

    :sparkles: [#4980] Add json schema definition to static variables
    The schema for the registration variables are left out for now, as they can't be selected in the JSON dump plugin options

--- src/openforms/variables/base.py
+++ src/openforms/variables/base.py
@@ -4,7 +4,7 @@ from typing import ClassVar
 from openforms.forms.models import FormVariable
 from openforms.plugins.plugin import AbstractBasePlugin
 from openforms.submissions.models import Submission
-from openforms.typing import StrOrPromise
+from openforms.typing import JSONObject, StrOrPromise
 
 from .constants import FormVariableDataTypes
 
@@ -15,6 +15,13 @@ class BaseStaticVariable(ABC, AbstractBasePlugin):
 
     @abstractmethod
     def get_initial_value(self, submission: Submission | None = None):
+        raise NotImplementedError()
+
+    @staticmethod
+    @abstractmethod
+    # TODO-4980: maybe `get_json_schema()` or property `json_schema`?
+    def as_json_schema() -> JSONObject:
+        """Return JSON schema for this static variable."""
         raise NotImplementedError()
 
     def get_static_variable(self, submission: Submission | None = None):
