3f7ac79cd05932b7f46d7860adcb7e45744718d2
robinvandermolen <robin@maykinmedia.nl>
2025-01-14 15:39:21 +0100

    :label: [#2177] Added interactions to MapComponent type

--- src/openforms/formio/typing/map.py
+++ src/openforms/formio/typing/map.py
@@ -4,3 +4,9 @@ from typing import NotRequired, TypedDict
 class MapInitialCenter(TypedDict):
     lat: NotRequired[float]
     lng: NotRequired[float]
+
+
+class MapInteractions(TypedDict):
+    marker: bool
+    polygon: bool
+    polyline: bool
