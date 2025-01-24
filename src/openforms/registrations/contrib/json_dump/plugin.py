19c8ffc87c55d824151e724c7f635b1b3b8ffe37
Viktor van Wijk <viktorvanwijk@hotmail.com>
2025-01-24 11:17:44 +0100

    :art: [#4980] Clean up post-processing
    * Rewrite fomio components only once before the processing, as it can be called 10-100 times if done for each component separately.
    * Perform component matching based on the dict, as it makes the structure flatter and easier to follow
    * Make `post_process` a plain function, as there is not much reason for it to be part of the plugin class. Also, if the base plugin ever gains a `post_process` hook, this would be problematic

--- src/openforms/registrations/contrib/json_dump/plugin.py
+++ src/openforms/registrations/contrib/json_dump/plugin.py
@@ -1,5 +1,6 @@
 import base64
 import json
+from typing import cast
 
 from django.core.exceptions import SuspiciousOperation
 from django.core.serializers.json import DjangoJSONEncoder
@@ -7,17 +8,18 @@ from django.utils.translation import gettext_lazy as _
 
 from zgw_consumers.client import build_client
 
-from openforms.formio.components.utils import to_multiple
-from openforms.formio.dynamic_config import rewrite_formio_components
-from openforms.formio.typing import Component
-from openforms.forms.utils import form_variables_to_json_schema
-from openforms.submissions.logic.datastructures import DataContainer
-from openforms.submissions.models import (
-    Submission,
-    SubmissionFileAttachment,
-    SubmissionValueVariable,
+from openforms.formio.service import rewrite_formio_components
+from openforms.formio.typing import (
+    FileComponent,
+    RadioComponent,
+    SelectBoxesComponent,
+    SelectComponent,
 )
-from openforms.typing import JSONObject, JSONValue
+from openforms.forms.utils import form_variables_to_json_schema
+from openforms.submissions.models import Submission, SubmissionFileAttachment
+from openforms.submissions.service import DataContainer
+from openforms.typing import JSONObject
+from openforms.utils.json_schema import to_multiple
 from openforms.variables.constants import FormVariableSources
 
 from ...base import BasePlugin  # openforms.registrations.base
@@ -51,7 +53,7 @@ class JSONDumpRegistration(BasePlugin):
         schema = form_variables_to_json_schema(submission.form, options["variables"])
 
         # Post-processing
-        self.post_processing(submission, values, schema)
+        post_process(values, schema, submission)
 
         # Send to the service
         data = json.dumps({"values": values, "schema": schema}, cls=DjangoJSONEncoder)
@@ -69,134 +71,145 @@ class JSONDumpRegistration(BasePlugin):
         # Config checks are not really relevant for this plugin right now
         pass
 
-    @staticmethod
-    def post_processing(
-        submission: Submission, values: JSONObject, schema: JSONObject
-    ) -> None:
-        """Post-processing of values and schema.
 
-        File components need special treatment, as we send the content of the file
-        encoded with base64, instead of the output from the serializer. Also, Radio,
-        Select, and SelectBoxes components need to be updated if their data source is
-        set to another form variable.
+def post_process(
+    values: JSONObject, schema: JSONObject, submission: Submission
+) -> None:
+    """Post-process the values and schema.
 
-        :param submission: Submission
-        :param values: JSONObject
-        :param schema: JSONObject
-        """
-        state = submission.load_submission_value_variables_state()
+    File components need special treatment, as we send the content of the file
+    encoded with base64, instead of the output from the serializer. Also, Radio,
+    Select, and SelectBoxes components need to be updated if their data source is
+    set to another form variable.
 
-        for key in values.keys():
-            variable = state.variables.get(key)
-            if (
-                variable is None
-                or variable.form_variable.source == FormVariableSources.user_defined
-            ):
-                # None for static variables, and processing user defined variables is
-                # not relevant here
-                continue
+    :param values: JSONObject
+    :param schema: JSONObject
+    :param submission: Submission
+    """
+    state = submission.load_submission_value_variables_state()
 
-            component = get_component(variable, submission)
-            assert component is not None
+    # Update config wrapper. This is necessary to update the options for Select,
+    # SelectBoxes, and Radio components that get their options from another form
+    # variable.
+    data = DataContainer(state=state)
+    configuration_wrapper = rewrite_formio_components(
+        submission.total_configuration_wrapper,
+        submission=submission,
+        data=data.data,
+    )
 
-            match component["type"]:
-                case "file":
-                    encoded_attachments = [
-                        {
-                            "file_name": attachment.original_name,
-                            "content": encode_attachment(attachment),
-                        }
-                        for attachment in submission.attachments
-                        if attachment.form_key == key
-                    ]
+    for key in values.keys():
+        variable = state.variables.get(key)
+        if (
+            variable is None
+            or variable.form_variable.source == FormVariableSources.user_defined
+        ):
+            # None for static variables, and processing user defined variables is
+            # not relevant here
+            continue
 
+        component = configuration_wrapper[key]
+        assert component is not None
+
+        match component:
+            case {"type": "file", "multiple": True}:
+                attachment_list, base_schema = get_attachments_and_base_schema(
+                    cast(FileComponent, component), submission
+                )
+                values[key] = attachment_list  # type: ignore
+                schema["properties"][key] = to_multiple(base_schema)  # type: ignore
+
+            case {"type": "file"}:  # multiple is False or missing
+                attachment_list, base_schema = get_attachments_and_base_schema(
+                    cast(FileComponent, component), submission
+                )
+
+                assert (n_attachments := len(attachment_list)) <= 1  # sanity check
+                if n_attachments == 0:
+                    value = None
+                    base_schema = {"title": component["label"], "type": "null"}
+                else:
+                    value = attachment_list[0]
+                values[key] = value
+                schema["properties"][key] = base_schema  # type: ignore
+
+            case {"type": "radio", "openForms": {"dataSrc": "variable"}}:
+                component = cast(RadioComponent, component)
+                choices = [options["value"] for options in component["values"]]
+                choices.append("")  # Take into account an unfilled field
+                schema["properties"][key]["enum"] = choices  # type: ignore
+
+            case {"type": "select", "openForms": {"dataSrc": "variable"}}:
+                component = cast(SelectComponent, component)
+                choices = [options["value"] for options in component["data"]["values"]]  # type: ignore[reportTypedDictNotRequiredAccess]
+                choices.append("")  # Take into account an unfilled field
+
+                if component.get("multiple", False):
+                    schema["properties"][key]["items"]["enum"] = choices  # type: ignore
+                else:
+                    schema["properties"][key]["enum"] = choices  # type: ignore
+
+            case {"type": "selectboxes"}:
+                component = cast(SelectBoxesComponent, component)
+                data_src = component.get("openForms", {}).get("dataSrc")
+
+                if data_src == "variable":
+                    properties = {
+                        options["value"]: {"type": "boolean"}
+                        for options in component["values"]
+                    }
                     base_schema = {
-                        "title": component["label"],
-                        "type": "object",
-                        "properties": {
-                            "file_name": {"type": "string"},
-                            "content": {"type": "string", "format": "base64"},
-                        },
-                        "required": (
-                            ["file_name", "content"]
-                            if len(encoded_attachments) != 0
-                            else []
-                            # No required properties when there are no attachments
-                        ),
+                        "properties": properties,
+                        "required": list(properties.keys()),
                         "additionalProperties": False,
                     }
+                    schema["properties"][key].update(base_schema)  # type: ignore
 
-                    match (
-                        component.get("multiple", False),
-                        n_attachments := len(encoded_attachments),
-                    ):
-                        case False, 0:
-                            value = None
+                # If the select boxes component was hidden, the submitted data of this
+                # component is an empty dict, so set the required to an empty list.
+                if not values[key]:
+                    schema["properties"][key]["required"] = []  # type: ignore
+            case _:
+                pass
 
-                            base_schema = {"title": component["label"], "type": "null"}
-                        case False, 1:
-                            value = encoded_attachments[0]
-                        case True, int():
-                            value = encoded_attachments
 
-                            base_schema = to_multiple(base_schema)
-                        case False, int():  # pragma: no cover
-                            raise ValueError(
-                                f"Cannot have multiple attachments ({n_attachments}) "
-                                "for a single file component."
-                            )
-                    values[key] = value
-                    schema["properties"][key] = base_schema
-                case "radio":
-                    data_src = component.get("openForms", {}).get("dataSrc")
-                    if data_src != "variable":
-                        # Only components where another variable is used as a data
-                        # source need to be processed, so skip this one
-                        continue
+def get_attachments_and_base_schema(
+    component: FileComponent, submission: Submission
+) -> tuple[list[JSONObject], JSONObject]:
+    """Return list of encoded attachments and the base schema.
 
-                    choices = [options["value"] for options in component["values"]]
-                    choices.append("")  # Take into account an unfilled field
-                    schema["properties"][key]["enum"] = choices
-                case "select":
-                    data_src = component.get("openForms", {}).get("dataSrc")
-                    if data_src != "variable":
-                        # Only components where another variable is used as a data
-                        # source need to be processed, so skip this one
-                        continue
+    :param component: FileComponent
+    :param submission: Submission
 
-                    choices = [
-                        options["value"] for options in component["data"]["values"]
-                    ]
-                    choices.append("")  # Take into account an unfilled field
+    :return encoded_attachments: list[JSONObject]
+    :return base_schema: JSONObject
+    """
+    encoded_attachments: list[JSONObject] = [
+        {
+            "file_name": attachment.original_name,
+            "content": encode_attachment(attachment),
+        }
+        for attachment in submission.attachments
+        if attachment.form_key == component["key"]
+    ]
 
-                    if component.get("multiple", False):
-                        schema["properties"][key]["items"]["enum"] = choices
-                    else:
-                        schema["properties"][key]["enum"] = choices
-                case "selectboxes":
-                    data_src = component.get("openForms", {}).get("dataSrc")
+    base_schema: JSONObject = {
+        "title": component["label"],
+        "type": "object",
+        "properties": {
+            "file_name": {"type": "string"},
+            "content": {"type": "string", "format": "base64"},
+        },
+        "required": (
+            ["file_name", "content"]
+            if len(encoded_attachments) != 0
+            else []
+            # No required properties when there are no attachments
+        ),
+        "additionalProperties": False,
+    }
 
-                    # Only components where another variable is used as a data
-                    # source need to be processed
-                    if data_src == "variable":
-                        properties = {
-                            options["value"]: {"type": "boolean"}
-                            for options in component["values"]
-                        }
-                        schema["properties"][key].update(
-                            {
-                                "properties": properties,
-                                "required": list(properties.keys()),
-                                "additionalProperties": False,
-                            }
-                        )
-
-                    # If the select boxes component is not filled, set required
-                    # properties to empty list
-                    if not values[key]:
-                        schema["properties"][key]["required"] = list()
-                case _:
-                    pass
+    return encoded_attachments, base_schema
 
 
 def encode_attachment(attachment: SubmissionFileAttachment) -> str:
@@ -208,28 +221,3 @@ def encode_attachment(attachment: SubmissionFileAttachment) -> str:
     with attachment.content.open("rb") as f:
         f.seek(0)
         return base64.b64encode(f.read()).decode()
-
-
-def get_component(
-    variable: SubmissionValueVariable, submission: Submission
-) -> Component | None:
-    """Get the component from a submission value variable.
-
-    :param variable: SubmissionValueVariable
-    :param submission: Submission
-    :return component: None if the form variable has no form definition
-    """
-    config_wrapper = variable.form_variable.form_definition.configuration_wrapper
-
-    # Update components. This is necessary to update the options for Select,
-    # SelectBoxes, and Radio components, which get their options from another form
-    # variable.
-    state = submission.load_submission_value_variables_state()
-    data = DataContainer(state=state)
-    config_wrapper = rewrite_formio_components(
-        config_wrapper, submission=submission, data=data.data
-    )
-
-    component = config_wrapper.component_map[variable.key]
-
-    return component
