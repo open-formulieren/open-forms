e68b7479fa28c9014f723c998ea029f8c65aae7f
Steven Bal <steven@maykinmedia.nl>
2025-01-24 16:41:30 +0100

    :sparkles: [#5016] Add Referentielijsten service select to builder
    and modify the services API endpoint to retrieve referentielijsten services based on a query parameter

--- src/openforms/services/api/viewsets.py
+++ src/openforms/services/api/viewsets.py
@@ -1,10 +1,11 @@
 from django.utils.translation import gettext_lazy as _
 
-from drf_spectacular.utils import extend_schema, extend_schema_view
+from drf_spectacular.utils import OpenApiParameter, extend_schema, extend_schema_view
 from rest_framework import authentication, permissions, viewsets
 from zgw_consumers.models import Service
 
 from openforms.api.utils import mark_experimental
+from openforms.config.models import GlobalConfiguration
 
 from . import serializers
 
@@ -17,7 +18,18 @@ from . import serializers
             "in the backend.\n\n"
             "Note that this endpoint is **EXPERIMENTAL**."
         ),
-    )
+        parameters=[
+            OpenApiParameter(
+                name="type",
+                type=str,
+                location=OpenApiParameter.QUERY,
+                description=_(
+                    "The type of Services to return, possible values: `referentielijsten`"
+                ),
+                required=False,
+            )
+        ],
+    ),
 )
 @mark_experimental
 class ServiceViewSet(viewsets.ReadOnlyModelViewSet):
@@ -30,3 +42,10 @@ class ServiceViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = serializers.ServiceSerializer
 
     queryset = Service.objects.all()
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.query_params.get("type") == "referentielijsten":
+            config = GlobalConfiguration.get_solo()
+            return config.referentielijsten_services
+        return qs
