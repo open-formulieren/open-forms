68831c9f1ae7b154956f8691caac93552d2de481
Sergei Maertens <sergei@maykinmedia.nl>
2025-01-27 15:06:39 +0100

    :technologist: Fix registration management command

--- src/openforms/registrations/management/commands/register_submission.py
+++ src/openforms/registrations/management/commands/register_submission.py
@@ -1,5 +1,8 @@
 from django.core.management import BaseCommand
 
+from openforms.submissions.constants import PostSubmissionEvents, RegistrationStatuses
+from openforms.submissions.models import Submission
+
 from ...tasks import pre_registration, register_submission
 
 
@@ -10,11 +13,22 @@ class Command(BaseCommand):
         parser.add_argument(
             "submission_id", type=int, help="ID of submission to register"
         )
+        parser.add_argument("--force", action="store_true")
 
     def handle(self, **options):
+        if options["force"]:
+            submission = Submission.objects.get(pk=options["submission_id"])
+            submission.pre_registration_completed = False
+            submission.registration_status = RegistrationStatuses.pending
+            submission.registration_attempts = 0
+            submission.registration_result = {}
+            submission.save()
+
         self.stdout.write("Pre-registering submission...")
         try:
-            pre_registration(options["submission_id"])
+            pre_registration(
+                options["submission_id"], PostSubmissionEvents.on_completion
+            )
         except Exception as exc:
             self.stderr.write(f"Pre-registration failed with error: {exc}")
         else:
@@ -24,7 +38,9 @@ class Command(BaseCommand):
 
         self.stdout.write("Registering submission...")
         try:
-            register_submission(options["submission_id"])
+            register_submission(
+                options["submission_id"], PostSubmissionEvents.on_completion
+            )
         except Exception as exc:
             self.stderr.write(f"Registration failed with error: {exc}")
         else:
