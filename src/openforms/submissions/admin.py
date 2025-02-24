31dda9be24d5bb4def147476878dddc61b53bcc2
robinvandermolen <robin@maykinmedia.nl>
2025-02-24 14:28:56 +0100

    :adhesive_bandage: [open-formulieren/security-issues#34] Added `Content-Disposition: attachment` headers to submission report
    When accessing submission reports from the admin, download them instead of opening in a new tab.

--- src/openforms/submissions/admin.py
+++ src/openforms/submissions/admin.py
@@ -500,6 +500,7 @@ class SubmissionReportAdmin(PrivateMediaMixin, admin.ModelAdmin):
     raw_id_fields = ("submission",)
 
     private_media_fields = ("content",)
+    private_media_view_options = {"attachment": True}
     readonly_fields = ("content",)
 
     def has_add_permission(self, request, obj=None):
