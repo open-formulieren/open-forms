a9ad884bfbf456755bd1b6c488be1e76c531b649
Sergei Maertens <sergei@maykinmedia.nl>
2025-03-17 16:55:52 +0100

    :green_heart: Remove test that crashes because of 2.8.x forward ported migration
    The test can't figure out how to roll back with merge migrations.
    
    Opted to remove it, since the migration logic itself has been
    battle tested now.

--- src/openforms/registrations/contrib/zgw_apis/tests/test_migrations.py
+++ src/openforms/registrations/contrib/zgw_apis/tests/test_migrations.py
@@ -3,61 +3,6 @@ from django.db.migrations.state import StateApps
 from openforms.utils.tests.test_migrations import TestMigrations
 
 
-class MigrateToExplicitObjectsAPIGroupsTests(TestMigrations):
-    app = "zgw_apis"
-    migrate_from = "0001_initial_to_v280"
-    migrate_to = "0015_explicit_objects_api_groups"
-
-    def setUpBeforeMigration(self, apps: StateApps):
-        ObjectsAPIGroupConfig = apps.get_model("objects_api", "ObjectsAPIGroupConfig")
-        Form = apps.get_model("forms", "Form")
-        FormRegistrationBackend = apps.get_model("forms", "FormRegistrationBackend")
-        Service = apps.get_model("zgw_consumers", "Service")
-
-        svc = Service.objects.create(
-            slug="dummy", api_type="orc", api_root="https://example.com"
-        )
-        form1 = Form.objects.create(name="form1")
-        form2 = Form.objects.create(name="form2")
-        self.objects_api_group = ObjectsAPIGroupConfig.objects.create(
-            identifier="group-1",
-            name="Group 1",
-            objects_service=svc,
-            objecttypes_service=svc,
-        )
-        ObjectsAPIGroupConfig.objects.create(
-            identifier="group-2",
-            name="Group 2",
-            objects_service=svc,
-            objecttypes_service=svc,
-        )
-        self.backend_without_api_group = FormRegistrationBackend.objects.create(
-            form=form1, backend="zgw-create-zaak", options={}
-        )
-        self.backend_with_api_group = FormRegistrationBackend.objects.create(
-            form=form2,
-            backend="zgw-create-zaak",
-            options={"objecttype": "https://objecttypes.com/foo/bar"},
-        )
-
-    def test_set_explicit_objects_api_groups_on_zgw_api_group_configs(self):
-        FormRegistrationBackend = self.apps.get_model(
-            "forms", "FormRegistrationBackend"
-        )
-        backend_without_api_group = FormRegistrationBackend.objects.get(
-            pk=self.backend_without_api_group.pk
-        )
-        backend_with_api_group = FormRegistrationBackend.objects.get(
-            pk=self.backend_with_api_group.pk
-        )
-
-        self.assertIsNone(backend_without_api_group.options["objects_api_group"])
-        self.assertEqual(
-            backend_with_api_group.options["objects_api_group"],
-            self.objects_api_group.pk,
-        )
-
-
 class AddZGWApiGroupConfigIdentifierTests(TestMigrations):
     app = "zgw_apis"
     migrate_from = "0015_explicit_objects_api_groups"
