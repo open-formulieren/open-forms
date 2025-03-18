9cacfe4dc5307bcf0f2f5620f1a1473c77bf539b
Viktor van Wijk <viktorvanwijk@hotmail.com>
2025-03-18 14:14:56 +0100

    :recycle: [#5139] Refactor evaluate_form_logic
    The amount of different data structures needed to evaluate form logic was too great. This severely impacted developer experience, but also
    performance, as we would generate lots of data structures repeatedly with little or no changes in data.
    
    This PR tries to tackle both things by erasing `DataContainer` from existence, tracking all data changes inside `evaluate_form_logic`, and
    updating the submission value variables state once at the end.
    
    To make comparing values easy, tracking of the data changes is performed in the Python-types domain (which includes time/date/datetime objects).
    This implies two things:
      - Data entering `evaluate_form_logic` is expected to be valid, because - for example - converting an invalid date '2025-13-32' to a date
        object will result in None, which *might* create unreliable data diffs. We can make this valid-data assumption as we perform validation
        in the frontend as well. The only way a user submit invalid data, is by manually accessing api endpoints, which they shouldn't be doing
        anyway.
      - Mutations returned by jsonLogic calls have to be converted to the python-type domain, as it returns json values by default. This can
        be done by using the variable type information.
    
    Building the data diff:
    Previously, a data diff was built up from comparing the initial (default) value of each variable with the value after evaluating all form
    logic. This doesn't accurately represent differences in data caused by logic evaluation, though. The initial value of a variable is never
    updated, so the comparison would always be made with default value set for the corresponding component. Now, the data diff is built up from
    comparing the initial data (before applying form logic) and final data (after applying form logic).
    
    Removing of function argument `dirty`:
    Previously, there was a difference between the data diff depending on the argument `dirty`. If `dirty` was True, the data diff would
    contain any changes that were made while handling the 'clearOnHide' property (step 6.2). Now, determining the data difference is moved to
    the end of the logic evaluation, which means there is no difference between the two data diffs, so we can remove the `dirty` argument. At
    the moment of writing this, all the tests pass, so we can be cautiously optimistic it won't break anything :grimacing:. Setting the data
    difference before handling the 'clearOnHide' property seems like a leftover change from 553a80d7, anyway, where all data updates would go
    through `SubmissionStep.data`. This is no longer the case, as we track all data changes locally and update the variables state at the end.

--- src/openforms/submissions/form_logic.py
+++ src/openforms/submissions/form_logic.py
@@ -13,7 +13,6 @@ from openforms.formio.utils import get_component_empty_value
 from openforms.typing import DataMapping
 
 from .logic.actions import ActionOperation
-from .logic.datastructures import DataContainer
 from .logic.rules import get_rules_to_evaluate, iter_evaluate_rules
 from .models.submission_step import DirtyData
 
@@ -26,7 +25,6 @@ def evaluate_form_logic(
     submission: "Submission",
     step: "SubmissionStep",
     data: DataMapping,
-    dirty=False,
     **context,
 ) -> DataMapping:
     """
@@ -39,26 +37,35 @@ def evaluate_form_logic(
     * setting/writing values for variables
     * dynamic Formio configuration updates, based on the variable values
     * dynamic Formio configuration updates, based on the logic actions
-    * dynamic Formio configuration based on the request context
 
     We achieve this by:
 
     1. Loading the submission and submission step (passed as function arguments)
     2. Prefilled data is saved at submission start and available in the variables
-    3. Loading the variable state, which populates non-saved values with their defaults.
+    3. Loading the variable state, which populates non-saved values with their defaults
     4. Apply any dirty data (unsaved) to the variable state
     5. Evaluate the logic rules in order, for each action in each triggered rule:
 
-       1. If a variable value is being updated, update the variable state
+       1. If a variable value is being updated, update the data structure
        2. Else record the action to perform to the configuration (but don't apply it yet!)
 
-    6. The variables state is now completely resolved and can be used as input for the
-       dynamic configuration.
-    7. Apply the dynamic configuration
+    6. Apply the dynamic configuration
 
-       1. Apply the component property mutations that were recorded in 5.
-       2. Interpolate the component configuration with the variables.
-       3. Handle custom formio types (which require variables as input!)
+       1. Apply the component property mutations that were recorded in 5
+       2. Handle the 'clearOnHide' property
+       3. Interpolate the component configuration with the variables
+       4. Handle custom formio types
+
+
+    7. Update relevant data structures
+
+       1. Update the variables state
+    2. Create a data difference between before and after applying form logic
+
+    :param submission: Submission instance.
+    :param step: Submission-step instance.
+    :param data: Submitted data. This data is assumed to be valid, as we perform a
+      conversion to the Python-type domain.
 
     """
     # grab the configuration that will be mutated
@@ -82,70 +89,60 @@ def evaluate_form_logic(
 
     # 4. Apply the (dirty) data to the variable state.
     submission_variables_state.set_values(data)
+    initial_data = submission_variables_state.to_python()
+    data_for_evaluation = submission_variables_state.to_python()
 
     rules = get_rules_to_evaluate(submission, step)
-    data_container = DataContainer(state=submission_variables_state)
 
     # 5. Evaluate the logic rules in order
     mutation_operations = []
 
-    # 5.1 - if the action type is to set a variable, update the variable state. This
-    # happens inside of iter_evaluate_rules. This is the ONLY operation that is allowed
-    # to execute while we're looping through the rules.
+    # 5.1 If the action type is to set a variable, update the data. This happens inside
+    # of iter_evaluate_rules. This is the ONLY operation that is allowed to execute
+    # while we're looping through the rules.
+    data_diff = FormioData()
     with elasticapm.capture_span(
         name="collect_logic_operations", span_type="app.submissions.logic"
     ):
-        for operation in iter_evaluate_rules(
+        for operation, mutations in iter_evaluate_rules(
             rules,
-            data_container,
+            data_for_evaluation,
             submission=submission,
         ):
             mutation_operations.append(operation)
+            if mutations:
+                data_diff.update(mutations)
 
-    # 6. The variable state is now completely resolved - we can start processing the
-    # dynamic configuration and side effects.
-
-    # Calculate which data has changed from the initial, for the step.
-    updated_step_data = data_container.get_updated_step_data(step)
-    step.data = DirtyData(updated_step_data.data)
-
-    # 7. finally, apply the dynamic configuration
+    # 6. Apply the dynamic configuration
 
     # we need to apply the context-specific configurations before we can apply
     # mutations based on logic, which is then in turn passed to the serializer(s)
-    # TODO: refactor this to rely on variables state
     config_wrapper = get_dynamic_configuration(
         config_wrapper,
-        # context is expected to contain request, as it's the default behaviour with
-        # DRF view(set)s and serializers.
-        # TODO: check if we can use context["request"] rather than .get - None is not
-        # expected, but that currently breaks a lot of tests...
-        request=context.get("request"),
+        request=None,
         submission=submission,
-        data=data_container.data,
+        data=data_for_evaluation.data,
     )
 
-    # 7.1 Apply the component mutation operations
+    # 6.1 Apply the component mutation operations
     for mutation in mutation_operations:
         mutation.apply(step, config_wrapper)
 
-    initial_data = FormioData(data_container.initial_data)
-
+    # 6.2 Handle 'clearOnHide' property
     # XXX: See #2340 and #2409 - we need to clear the values of components that are
     # (eventually) hidden BEFORE we do any further processing. This is only a bandaid
     # fix, as the (stale) data has potentially been input for other logic rules.
     # Note that only the dirty data logic check acts on these differences.
-
-    # only keep the changes in the data, so that old values do not overwrite otherwise
-    # debounced client-side data changes
-    data_diff = FormioData()
     for component in config_wrapper:
         key = component["key"]
-        is_visible = config_wrapper.is_visible_in_frontend(key, data_container.data)
+        is_visible = config_wrapper.is_visible_in_frontend(
+            key, data_for_evaluation.data
+        )
         if is_visible:
             continue
 
-        # Reset the value of any field that may have become hidden again after evaluating the logic
+        # Reset the value of any field that may have become hidden again after
+        # evaluating the logic
         original_value = initial_data.get(key, empty)
         empty_value = get_component_empty_value(component)
         if original_value is empty or original_value == empty_value:
@@ -155,32 +152,38 @@ def evaluate_form_logic(
             continue
 
         # clear the value
-        data_container.update({key: empty_value})
+        data_for_evaluation[key] = empty_value
         data_diff[key] = empty_value
 
-    # 7.2 Interpolate the component configuration with the variables.
-    inject_variables(config_wrapper, data_container.data)
+    # 6.3 Interpolate the component configuration with the variables.
+    inject_variables(config_wrapper, data_for_evaluation.data)
 
-    # 7.3 Handle custom formio types - TODO: this needs to be lifted out of
-    # :func:`get_dynamic_configuration` so that it can use variables.
+    # 6.4 Handle custom formio types
+    # TODO: this needs to be lifted out of :func:`get_dynamic_configuration` so that it
+    #  can use variables.
 
-    # process the output for logic checks with dirty data
-    if dirty:
-        # Iterate over all components instead of `step.data`, to take hidden fields into account (See: #1755)
-        for component in config_wrapper:
-            key = component["key"]
-            # already processed, don't process it again
-            if data_diff.get(key, default=empty) is not empty:
-                continue
+    # 7. Form evaluation completed
+    # 7.1 All the processing is now complete, so we can update the state.
+    submission_variables_state.set_values(data_diff)
 
-            new_value = updated_step_data.get(key, default=empty)
-            original_value = initial_data.get(key, default=empty)
-            if new_value is empty or new_value == original_value:
-                continue
-            data_diff[key] = new_value
+    # 7.2 Build a difference in data for the step. It is important that we only keep the
+    # changes in the data, so that old values do not overwrite otherwise debounced
+    # client-side data changes
 
-        # only return the 'overrides'
-        step.data = DirtyData(data_diff.data)
+    # Get relevant step variables
+    relevant_variables = submission_variables_state.get_variables_in_submission_step(
+        step, include_unsaved=True
+    )
+
+    # NOTE: we cannot use `data_diff` as it might contain mutations that are equal to
+    # the original value of the variable. For example, when the same variable was
+    # changed by two separate logic actions back to its original value, it would show
+    # up in the `data_diff` but it shouldn't be returned (set to `SubmissionStep.data`)
+    updated_step_data = FormioData()
+    for key, variable in relevant_variables.items():
+        if not variable.form_variable or initial_data[key] != data_for_evaluation[key]:
+            updated_step_data[key] = data_for_evaluation[key]
+    step.data = DirtyData(updated_step_data.data)
 
     step._form_logic_evaluated = True
 
@@ -206,11 +209,18 @@ def check_submission_logic(
     submission_variables_state = submission.load_submission_value_variables_state()
     if unsaved_data:
         submission_variables_state.set_values(unsaved_data)
-    data_container = DataContainer(state=submission_variables_state)
+    data_for_evaluation = submission_variables_state.to_python()
 
     mutation_operations: list[ActionOperation] = []
-    for operation in iter_evaluate_rules(rules, data_container, submission):
+    data_diff = FormioData({})
+    for operation, mutations in iter_evaluate_rules(
+        rules, data_for_evaluation, submission
+    ):
         mutation_operations.append(operation)
+        if mutations:
+            data_diff.update(mutations)
+
+    submission_variables_state.set_values(data_diff)
 
     # we loop over all steps because we have validations that ensure unique component
     # keys across multiple steps for the whole form.
