6e5552f89f3c4fc9f887e54356df6aa8fd7ad72a
Viktor van Wijk <viktorvanwijk@hotmail.com>
2025-04-04 16:45:30 +0200

    :sparkles: [#5027] Add GEO JSON schemas
    Support for drawing lines and polygons in a map component was added in GH-2177 and GH-2178. Now, the schema reflects these shapes

--- src/openforms/utils/json_schema.py
+++ src/openforms/utils/json_schema.py
@@ -1,4 +1,38 @@
+from openforms.api.geojson import GeoJsonGeometryTypes
 from openforms.typing import JSONObject
+
+GEO_JSON_COORDINATE_SCHEMA = {
+    "type": "array",
+    "prefixItems": [
+        {"title": "Longitude", "type": "number"},
+        {"title": "Latitude", "type": "number"},
+    ],
+    "items": False,
+    "minItems": 2,
+}
+
+GEO_JSON_LINE_COORDINATE_SCHEMA = {
+    "type": "array",
+    "minItems": 2,
+    "items": GEO_JSON_COORDINATE_SCHEMA,
+}
+
+GEO_JSON_POLYGON_COORDINATE_SCHEMA = {
+    "type": "array",
+    "minItems": 1,
+    "maxItems": 1,
+    "items": {
+        "type": "array",
+        "minItems": 4,
+        "items": GEO_JSON_COORDINATE_SCHEMA,
+    },
+}
+
+GEO_JSON_COORDINATE_SCHEMAS = {
+    GeoJsonGeometryTypes.point: GEO_JSON_COORDINATE_SCHEMA,
+    GeoJsonGeometryTypes.line_string: GEO_JSON_LINE_COORDINATE_SCHEMA,
+    GeoJsonGeometryTypes.polygon: GEO_JSON_POLYGON_COORDINATE_SCHEMA,
+}
 
 
 def to_multiple(schema: JSONObject) -> JSONObject:
