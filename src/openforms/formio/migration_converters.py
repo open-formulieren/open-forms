e6c92486c1b9f61ebff935814b845c7346e8478e
Viktor van Wijk <viktorvanwijk@hotmail.com>
2025-04-07 09:47:59 +0200

    :bug: [#5181] Fix default values of components
    Just like the fixes #4738 and #5123, the default value of these components (checkbox, edit grid, file, and signature) in the form builder was set to `null`. Because this does not match the default values set by the backend - specifically when the component is hidden and 'clearOnHide' is set (see `get_component_empty_value`) - it causes premature validation to be performed.

--- src/openforms/formio/migration_converters.py
+++ src/openforms/formio/migration_converters.py
@@ -17,6 +17,7 @@ from openforms.typing import JSONObject
 
 from .datastructures import FormioConfigurationWrapper
 from .typing import AddressNLComponent, Component, MapComponent
+from .utils import get_component_empty_value
 
 logger = logging.getLogger(__name__)
 
@@ -118,11 +119,19 @@ def fix_column_sizes(component: Component) -> bool:
 
 def fix_file_default_value(component: Component) -> bool:
     component = cast(FileComponent, component)
-    default_value = component.get("defaultValue")
+
+    if "defaultValue" not in component:
+        return False
+
+    default_value = component["defaultValue"]
+    empty_value = get_component_empty_value(component)
 
     match default_value:
         case list() if None in default_value:
-            component["defaultValue"] = None
+            component["defaultValue"] = empty_value
+            return True
+        case None:
+            component["defaultValue"] = empty_value
             return True
         case _:
             return False
@@ -302,22 +311,16 @@ def fix_empty_default_value(component: Component) -> bool:
     if "defaultValue" not in component:
         return False
 
-    default_value = component.get("defaultValue")
     changed = False
+    if component["defaultValue"] is None:
+        component["defaultValue"] = get_component_empty_value(component)
+        changed = True
 
     if component.get("multiple", False):
-        if default_value is None:
-            component["defaultValue"] = []
-            return True
-
-        for index, value in enumerate(default_value):
+        for index, value in enumerate(component["defaultValue"]):
             if value is None:
                 component["defaultValue"][index] = ""
                 changed = True
-
-    if default_value is None:
-        component["defaultValue"] = ""
-        changed = True
 
     return changed
 
@@ -389,6 +392,9 @@ CONVERTERS: dict[str, dict[str, ComponentConverter]] = {
         "set_openforms_datasrc": set_openforms_datasrc,
         "fix_empty_default_value": fix_empty_default_value,
     },
+    "checkbox": {
+        "fix_empty_default_value": fix_empty_default_value,
+    },
     # Special components
     "iban": {
         "fix_empty_validate_lengths": fix_empty_validate_lengths,
@@ -409,6 +415,12 @@ CONVERTERS: dict[str, dict[str, ComponentConverter]] = {
     },
     "addressNL": {
         "ensure_addressnl_has_deriveAddress": ensure_addressnl_has_deriveAddress
+    },
+    "editgrid": {
+        "fix_empty_default_value": fix_empty_default_value,
+    },
+    "signature": {
+        "fix_empty_default_value": fix_empty_default_value,
     },
     # Layout components
     "columns": {
