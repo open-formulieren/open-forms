9c52e60b5e912bc7cd52771ad074c099b7516060
Sergei Maertens <sergei@maykinmedia.nl>
2025-04-10 11:38:33 +0200

    :green_heart: Fix CI
    [skip: e2e]
    
    (cherry picked from commit ab9a87afffe5ee9df74c0e8be76486018b4e1ab4)

--- src/openforms/registrations/contrib/zgw_apis/tests/test_backend.py
+++ src/openforms/registrations/contrib/zgw_apis/tests/test_backend.py
@@ -590,6 +590,7 @@ class ZGWBackendTests(TestCase):
                 {
                     "handelsnaam": ["ACME"],
                     "vestigingsNummer": "87654321",
+                    "kvkNummer": "12345678",
                     "innNnpId": "12345678",
                     "statutaireNaam": "ACME",
                     "verblijfsadres": {
@@ -883,6 +884,7 @@ class ZGWBackendTests(TestCase):
                 {
                     "handelsnaam": ["ACME"],
                     "innNnpId": "12345678",
+                    "kvkNummer": "12345678",
                     "statutaireNaam": "ACME",
                     "verblijfsadres": {
                         "aoaPostcode": "1000 AA",
