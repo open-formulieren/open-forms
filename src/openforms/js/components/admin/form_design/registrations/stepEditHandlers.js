1b09b40bcbbf506504a866f7d84da6d28c39ab55
vasileios <zigras00@gmail.com>
2025-04-10 08:46:58 +0200

    [#5243] Fixed wrong arrays of variables in JSONDump and ObjectsAPI
    All the arrays in JSONDump (variables, fixedMetadataVariables etc)
    contained variable keys even if these variables were deleted or
    modified. This was happening for user defined variables as well.
    
    The same fix was needed for the transformToList array in JSONDump and in
    the ObjectsAPI.

--- src/openforms/js/components/admin/form_design/registrations/stepEditHandlers.js
+++ src/openforms/js/components/admin/form_design/registrations/stepEditHandlers.js
@@ -81,11 +81,24 @@ const onZGWStepEdit = (registrationBackendOptions, componentSchema, originalComp
  *                                             no changes need to be made.
  */
 const onObjectsAPIStepEdit = (registrationBackendOptions, componentSchema, originalComponent) => {
-  if (registrationBackendOptions.version !== 2) return;
+  const isSelectboxesComponentType = componentSchema.type === 'selectboxes';
+
+  if (registrationBackendOptions.version !== 2 && !isSelectboxesComponentType) return;
 
   const removed = originalComponent == null;
-
   if (removed) {
+    // selectboxes component type and the transformToList array
+    if (isSelectboxesComponentType) {
+      const matchingTransformToListVariableIndex =
+        registrationBackendOptions.transformToList.findIndex(
+          transformToListVariable => transformToListVariable === componentSchema.key
+        );
+      if (matchingTransformToListVariableIndex !== -1) {
+        registrationBackendOptions.transformToList.splice(matchingTransformToListVariableIndex, 1);
+      }
+    }
+
+    // Objects API mappings
     const matchingMappingIndex = registrationBackendOptions.variablesMapping.findIndex(
       mapping => mapping.variableKey === componentSchema.key
     );
@@ -102,8 +115,82 @@ const onObjectsAPIStepEdit = (registrationBackendOptions, componentSchema, origi
         mapping.variableKey = componentSchema.key;
       }
     }
+
+    if (isSelectboxesComponentType) {
+      registrationBackendOptions.transformToList.forEach((variableKey, index) => {
+        if (variableKey === originalComponent.key) {
+          registrationBackendOptions.transformToList[index] = componentSchema.key;
+        }
+      });
+    }
     return registrationBackendOptions;
   }
 };
 
-export {onCamundaStepEdit, onZGWStepEdit, onObjectsAPIStepEdit};
+/**
+ * Update the included variable keys after the form definitions change
+ * @param  {Object} registrationBackendOptions The currently configured backend options,
+ *                                             including the various variable keys. Note that this is
+ *                                             an immer draft which can be mutated.
+ * @param  {Object} componentSchema            The Formio component (schema) that was mutated in some way
+ * @param  {Object|null} originalComponent     The component before it was mutated, null if the component is removed.
+ * @return {Object|null}                       The updated registrationBackendOptions draft. Return null to indicate
+ *                                             no changes need to be made.
+ */
+const onJSONDumpStepEdit = (registrationBackendOptions, componentSchema, originalComponent) => {
+  const removed = originalComponent == null;
+  const optionsArrays = [
+    registrationBackendOptions.variables,
+    registrationBackendOptions.fixedMetadataVariables,
+    registrationBackendOptions.additionalMetadataVariables,
+    registrationBackendOptions.transformToList,
+  ];
+
+  if (removed) {
+    const matchingVariableIndex = registrationBackendOptions.variables.findIndex(
+      variable => variable === componentSchema.key
+    );
+    const matchingFixedMetadataVariableIndex =
+      registrationBackendOptions.fixedMetadataVariables.findIndex(
+        fixedMetadataVariable => fixedMetadataVariable === componentSchema.key
+      );
+    const matchingAdditionalMetadataVariableIndex =
+      registrationBackendOptions.additionalMetadataVariables.findIndex(
+        additionalMetadataVariable => additionalMetadataVariable === componentSchema.key
+      );
+    const matchingTransformToListVariableIndex =
+      registrationBackendOptions.transformToList.findIndex(
+        transformToListVariable => transformToListVariable === componentSchema.key
+      );
+
+    const matchingPropertiesIndexes = [
+      matchingVariableIndex,
+      matchingFixedMetadataVariableIndex,
+      matchingAdditionalMetadataVariableIndex,
+      matchingTransformToListVariableIndex,
+    ];
+
+    if (matchingPropertiesIndexes.every(index => index === -1)) return;
+
+    matchingPropertiesIndexes.forEach((index, i) => {
+      if (index !== -1) optionsArrays[i].splice(index, 1);
+    });
+
+    return registrationBackendOptions;
+  } else {
+    const keyChanged = componentSchema.key !== originalComponent.key;
+    if (!keyChanged) return null;
+
+    for (const optionArray of optionsArrays) {
+      optionArray.forEach((variableKey, index) => {
+        if (variableKey === originalComponent.key) {
+          optionArray[index] = componentSchema.key;
+        }
+      });
+    }
+  }
+
+  return registrationBackendOptions;
+};
+
+export {onCamundaStepEdit, onZGWStepEdit, onObjectsAPIStepEdit, onJSONDumpStepEdit};
