4d620ba316198a8550b4045ab64609716c5847ac
robinvandermolen <robin@maykinmedia.nl>
2025-05-12 12:32:51 +0200

    :label: [#5140] More specific DigiDOptionsForm component typing

--- src/openforms/js/components/admin/form_design/authentication/digid/DigidOptionsForm.js
+++ src/openforms/js/components/admin/form_design/authentication/digid/DigidOptionsForm.js
@@ -2,29 +2,40 @@ import PropTypes from 'prop-types';
 
 import LoAOverride from 'components/admin/form_design/authentication/LoAOverride';
 
-const DigidOptionsForm = ({name, plugin, authBackend, onChange}) => {
-  return (
-    <LoAOverride
-      name={`${name}.options.loa`}
-      plugin={plugin}
-      loa={authBackend.options.loa}
-      onChange={onChange}
-    />
-  );
-};
+const DigidOptionsForm = ({name, plugin, authBackend, onChange}) => (
+  <LoAOverride
+    name={`${name}.options.loa`}
+    plugin={plugin}
+    loa={authBackend.options.loa}
+    onChange={onChange}
+  />
+);
 
 DigidOptionsForm.propType = {
   name: PropTypes.string.isRequired,
   authBackend: PropTypes.shape({
     backend: PropTypes.string.isRequired, // Auth plugin id
-    // Options configuration shape is specific to plugin
-    options: PropTypes.object,
+    options: PropTypes.shape({
+      loa: PropTypes.string,
+    }),
   }).isRequired,
   plugin: PropTypes.shape({
-    id: PropTypes.string,
-    label: PropTypes.string,
-    providesAuth: PropTypes.string,
-  }),
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    providesAuth: PropTypes.oneOf(['bsn']).isRequired,
+    schema: PropTypes.exact({
+      type: PropTypes.oneOf(['object']).isRequired,
+      properties: PropTypes.shape({
+        loa: PropTypes.exact({
+          type: PropTypes.oneOf(['string']).isRequired,
+          title: PropTypes.string.isRequired,
+          description: PropTypes.string.isRequired,
+          enum: PropTypes.arrayOf(PropTypes.string).isRequired,
+          enumNames: PropTypes.arrayOf(PropTypes.string).isRequired,
+        }).isRequired,
+      }),
+    }).isRequired,
+  }).isRequired,
   onChange: PropTypes.func.isRequired,
 };
 
