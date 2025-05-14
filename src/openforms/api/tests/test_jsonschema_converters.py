cfbba61cd8e41b0e137e69f4d72d4985972d9f12
robinvandermolen <robin@maykinmedia.nl>
2025-05-14 14:21:04 +0200

    :sparkles: [#5132] Extend support of JsonSchemaSerializerMixin to PolymorphicSerializer classes
    Add polymorphic shapes, defined in/with drf_polymorphic PolymorphicSerializer, to the JsonSchemaSerializerMixin generate json schemas.
    
    The typical polymorphism of JSON schema is defined with "anyOf", which tells that an object *can* contain certain properties. But "anyOf" tells nothing about when a property/shape should be part of the whole.
    
    To add some relationship between polymorphic discriminator and object shape, a `discriminator` property will be included in the JSON schema. Note: `discriminator` isn't a supported property of JSON schema. The `discriminator` property contains a reference to the "discriminator" field and a mapping of which discriminator value corresponds to which partial schema.

--- src/openforms/api/tests/test_jsonschema_converters.py
+++ src/openforms/api/tests/test_jsonschema_converters.py
@@ -1,5 +1,7 @@
+from django.db import models
 from django.test import TestCase
 
+from drf_polymorphic.serializers import PolymorphicSerializer
 from rest_framework import serializers
 
 from openforms.accounts.models import User
@@ -31,3 +33,105 @@ class PrimaryKeyRelatedAsChoicesFieldConverterTests(TestCase):
             "enumNames": ["bob", "alice"],
         }
         self.assertEqual(schema["properties"]["user"], expected_schema)
+
+
+class PolymorphicSerializerConverterTests(TestCase):
+    def test_converted_polymorphic_object_contains_all_polymorphic_shapes(self):
+        class TestPolymorphicTypes(models.TextChoices):
+            a = "a", "A"
+            b = "b", "B"
+
+        class TestPolymorphicShapeASerializer(serializers.Serializer):
+            field_of_shape_a = serializers.CharField(
+                label="Shape A field", required=False
+            )
+
+        class TestPolymorphicShapeBSerializer(serializers.Serializer):
+            field_of_shape_b = serializers.CharField(
+                label="Shape B field", required=True
+            )
+
+        class TestPolymorphicSerializer(
+            JsonSchemaSerializerMixin, PolymorphicSerializer, serializers.Serializer
+        ):
+            field = serializers.CharField(label="Field", required=True)
+            type = serializers.ChoiceField(
+                label="Type", choices=TestPolymorphicTypes.choices, required=False
+            )
+
+            discriminator_field = "type"
+            serializer_mapping = {
+                TestPolymorphicTypes.a: TestPolymorphicShapeASerializer,
+                TestPolymorphicTypes.b: TestPolymorphicShapeBSerializer,
+            }
+
+        schema = TestPolymorphicSerializer.display_as_jsonschema()
+
+        expected_schema = {
+            "type": "object",
+            "properties": {
+                "field": {
+                    "title": "Field",
+                    "type": "string",
+                    "minLength": 1,
+                },
+                "type": {
+                    "title": "Type",
+                    "type": "string",
+                    "enum": ["a", "b"],
+                    "enumNames": ["A", "B"],
+                },
+            },
+            "required": ["field"],
+            "discriminator": {
+                "propertyName": "type",
+                "mappings": {
+                    "a": {
+                        "type": "object",
+                        "properties": {
+                            "field_of_shape_a": {
+                                "title": "Shape A field",
+                                "type": "string",
+                                "minLength": 1,
+                            },
+                        },
+                    },
+                    "b": {
+                        "type": "object",
+                        "properties": {
+                            "field_of_shape_b": {
+                                "title": "Shape B field",
+                                "type": "string",
+                                "minLength": 1,
+                            },
+                        },
+                        "required": ["fieldOfShapeB"],
+                    },
+                },
+            },
+            "anyOf": [
+                {
+                    "type": "object",
+                    "properties": {
+                        "field_of_shape_a": {
+                            "title": "Shape A field",
+                            "type": "string",
+                            "minLength": 1,
+                        },
+                    },
+                },
+                {
+                    "type": "object",
+                    "properties": {
+                        "field_of_shape_b": {
+                            "title": "Shape B field",
+                            "type": "string",
+                            "minLength": 1,
+                        },
+                    },
+                    "required": ["fieldOfShapeB"],
+                },
+            ],
+        }
+
+        self.assertEqual(schema, expected_schema)
--- src/openforms/utils/mixins.py
+++ src/openforms/utils/mixins.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
 
 from drf_jsonschema_serializer import to_jsonschema
+from drf_polymorphic.serializers import PolymorphicSerializer
 
 from openforms.api.utils import underscore_to_camel
 
@@ -31,8 +32,50 @@ def _camelize_required(schema: dict):
 
 class JsonSchemaSerializerMixin:
     @classmethod
+    def _add_polymorphic_items_to_jsonschema(cls, json_schema: dict):
+        """
+        Add polymorphic items to a jsonschema schema.
+
+        Polymorphic items (from drf_polymorphic) are statically not available, as they
+        rely on their "discriminator_field" to define its shape. To add the entire
+        polymorphic shape to a jsonschema, each polymorphic item needs to be manually
+        transformed to a json schema.
+
+        JSON schemas only support polymorphic items via "anyOf" definitions. This setup
+        lacks any connection between a discriminator and the polymorphic shape, making it
+        impossible to know which shape should be part of the schema at any time. To
+        create such a relationship, some custom definitions are required.
+
+        .. note:: The `discriminator` schema property, and its structure, are unknown by
+           JSON schema, so any regular JSON schema validation won't validate these parts.
+
+        :param json_schema: jsonschema schema to add polymorphic items to.
+        """
+        discriminator_field = cls().discriminator_field
+        polymorphic_mappings = {}
+
+        # Generate json schema for all polymorphic shapes
+        for key, PolymorphicItemSerializer in cls().serializer_mapping.items():
+            item_schema = to_jsonschema(PolymorphicItemSerializer())
+            item_schema = _camelize_required(item_schema)
+
+            polymorphic_mappings[key.value] = item_schema
+
+        # Add custom (not supported by JSON schema) discriminator mapping to json_schema
+        json_schema["discriminator"] = {
+            "propertyName": discriminator_field,
+            "mappings": polymorphic_mappings,
+        }
+        # Add polymorphic shapes as anyOf to json_schema, supported by JSON schema
+        json_schema["anyOf"] = list(polymorphic_mappings.values())
+
+    @classmethod
     def display_as_jsonschema(cls):
         json_schema = to_jsonschema(cls())
+
+        if issubclass(cls, PolymorphicSerializer):
+            cls()._add_polymorphic_items_to_jsonschema(json_schema)
+
         json_schema = _camelize_required(json_schema)
         return json_schema
 
