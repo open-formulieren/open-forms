1521e385a92e629696743cf0bd3c39fb7e51d313
vasileios <zigras00@gmail.com>
2025-05-16 15:46:13 +0200

    [#5254] Removed id from translatable texts

--- src/openforms/js/components/admin/form_design/variables/prefill/family_members/constants.js
+++ src/openforms/js/components/admin/form_design/variables/prefill/family_members/constants.js
@@ -2,13 +2,11 @@ import {defineMessages} from 'react-intl';
 
 export const PERSON_TYPE_CHOICES = defineMessages({
   partners: {
-    id: 'personType.partners',
-    defaultMessage: 'Partners',
     description: 'Label for partners option',
+    defaultMessage: 'Partners',
   },
   children: {
-    id: 'personType.children',
-    defaultMessage: 'Children',
     description: 'Label for children option',
+    defaultMessage: 'Children',
   },
 });
