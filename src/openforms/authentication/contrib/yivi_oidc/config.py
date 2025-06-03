b80ab647aa6069dd751bb2c72854c9e31daa078d
robinvandermolen <robin@maykinmedia.nl>
2025-06-03 16:05:02 +0200

    :sparkles: [#5132] Add Yivi options serializer
    Adding a Serializer for the Yivi plugin options. This is a first setup, containing LOA config for BSN and KVK, available user authentication methods and additional attribute groups.
    
    The choices for `attributes_groups` are retrieved from the Yivi AttributeGroup configuration.

---
+++ src/openforms/authentication/contrib/yivi_oidc/config.py
@@ -0,0 +1,76 @@
+from typing import List, Literal, TypedDict
+
+from django.utils.translation import gettext_lazy as _
+
+from digid_eherkenning.choices import AssuranceLevels, DigiDAssuranceLevels
+from rest_framework import serializers
+
+from openforms.utils.mixins import JsonSchemaSerializerMixin
+
+from .constants import YiviAuthenticationAttributes
+from .models import AttributeGroup
+
+
+class YiviOptions(TypedDict):
+    """
+    Shape of the DigiD authentication plugin options.
+    This describes the shape of :attr:`YiviOptionsSerializer.validated_data`, after
+    the input data has been cleaned/validated.
+    """
+
+    authentication_options: List[YiviAuthenticationAttributes]
+    additional_attributes_groups: List[str]
+    bsn_loa: DigiDAssuranceLevels | Literal[""] | None
+    kvk_loa: AssuranceLevels | Literal[""] | None
+
+
+class YiviOptionsSerializer(JsonSchemaSerializerMixin, serializers.Serializer):
+    authentication_options = serializers.ListField(
+        child=serializers.ChoiceField(choices=YiviAuthenticationAttributes.choices),
+        label=_("Authentication options"),
+        help_text=_(
+            "Available authentication options that the user can choice from. "
+            "The user must chose one of the options. "
+            "If no options are specified, a pseudo value will be used as identifier."
+        ),
+        default=[],
+        required=False,
+        allow_empty=True,
+    )
+    additional_attributes_groups = serializers.ListField(
+        child=serializers.ChoiceField(choices=[]),  # Choices are dynamically defined
+        label=_("Additional attributes groups"),
+        help_text=_(
+            "Additional attributes groups to use for authentication. "
+            "The user can choice per group whether they provide these attributes or not."
+        ),
+        default=[],
+        required=False,
+        allow_empty=True,
+    )
+
+    bsn_loa = serializers.ChoiceField(
+        label=_("options bsn LoA"),
+        help_text=_("The minimal LoA for bsn authentication."),
+        choices=DigiDAssuranceLevels.choices,
+        default=DigiDAssuranceLevels.middle,
+        required=False,
+        allow_blank=True,
+    )
+    kvk_loa = serializers.ChoiceField(
+        label=_("options kvk LoA"),
+        help_text=_("The minimal LoA for kvk authentication."),
+        choices=AssuranceLevels.choices,
+        default=AssuranceLevels.substantial,
+        required=False,
+        allow_blank=True,
+    )
+
+    def get_fields(self):
+        fields = super().get_fields()
+        fields[
+            "additional_attributes_groups"
+        ].child.choices = AttributeGroup.objects.all().values_list(
+            "name", "description"
+        )
+        return fields
