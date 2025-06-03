67ba5ca48ae49ecbc9c3482896a42aa018056226
robinvandermolen <robin@maykinmedia.nl>
2025-06-03 16:29:37 +0200

    :building_construction: [#5140] Authentication options forms using Formik
    The authentication options for DigiD have been moved from inline to a modal form. This creates a contained environment for auth plugin configuration. This also reduced the "noice" of the formbuild a bit.
    
    This allows us to use Formik for the auth options forms. Making it easier to support/maintain/develop the authentication configuration.

--- src/openforms/js/components/admin/form_design/authentication/LoAOverride.js
+++ src/openforms/js/components/admin/form_design/authentication/LoAOverride.js
@@ -4,55 +4,38 @@ import {FormattedMessage} from 'react-intl';
 
 import Field from 'components/admin/forms/Field';
 import FormRow from 'components/admin/forms/FormRow';
-import Select from 'components/admin/forms/Select';
+import ReactSelect from 'components/admin/forms/ReactSelect';
 
-const LoAOverride = ({name, plugin, loa, onChange}) => {
-  const {enum: enumValue, enumNames} = plugin.schema.properties.loa;
-  const LoaChoices = enumValue.map((value, index) => [value, enumNames[index] || '------']);
-  return (
-    <FormRow>
-      <Field
-        name={name}
-        label={
-          <FormattedMessage
-            description="Minimal levels of assurance label"
-            defaultMessage="Minimal levels of assurance"
-          />
-        }
-        helpText={
-          <FormattedMessage
-            defaultMessage="Override the minimum Level of Assurance. This is not supported by all authentication plugins."
-            description="Minimal LoA override help text"
-          />
-        }
-      >
-        <Select value={loa} onChange={onChange} choices={LoaChoices} />
-      </Field>
-    </FormRow>
-  );
-};
+const LoAOverride = ({name, options}) => (
+  <FormRow>
+    <Field
+      name={name}
+      label={
+        <FormattedMessage
+          description="Minimal levels of assurance label"
+          defaultMessage="Minimal levels of assurance"
+        />
+      }
+      helpText={
+        <FormattedMessage
+          defaultMessage="Override the minimum Level of Assurance. This is not supported by all authentication plugins."
+          description="Minimal LoA override help text"
+        />
+      }
+    >
+      <ReactSelect name={name} options={options} />
+    </Field>
+  </FormRow>
+);
 
 LoAOverride.propTypes = {
   name: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  plugin: PropTypes.shape({
-    id: PropTypes.string,
-    label: PropTypes.string,
-    providesAuth: PropTypes.string,
-    schema: PropTypes.exact({
-      type: PropTypes.oneOf(['object']).isRequired,
-      properties: PropTypes.shape({
-        loa: PropTypes.exact({
-          type: PropTypes.oneOf(['string']).isRequired,
-          title: PropTypes.string.isRequired,
-          description: PropTypes.string.isRequired,
-          enum: PropTypes.arrayOf(PropTypes.string).isRequired,
-          enumNames: PropTypes.arrayOf(PropTypes.string).isRequired,
-        }).isRequired,
-      }),
-    }),
-  }).isRequired,
-  loa: PropTypes.string,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default LoAOverride;
--- src/openforms/js/utils/json-schema.js
+++ src/openforms/js/utils/json-schema.js
@@ -1,8 +1,22 @@
-export const getChoicesFromSchema = (enums, enumNames) => {
+const createListFromSchema = (enums, enumNames, callback) => {
   const finalChoices = [];
   Object.keys(enums).forEach(key => {
-    finalChoices.push([enums[key], enumNames[key]]);
+    finalChoices.push(callback(enums[key], enumNames[key]));
   });
 
   return finalChoices;
+};
+
+export const getChoicesFromSchema = (enums, enumNames, blankChoiceTitle = '') => {
+  return createListFromSchema(enums, enumNames, (value, label) => [
+    value,
+    label || blankChoiceTitle,
+  ]);
+};
+
+export const getReactSelectOptionsFromSchema = (enums, enumNames, blankChoiceTitle = '') => {
+  return createListFromSchema(enums, enumNames, (value, label) => ({
+    value,
+    label: label || blankChoiceTitle,
+  }));
 };
