2c5be013df812809d7e4d9edbec831bb777e6290
SilviaAmAm <silvia@maykinmedia.nl>
2025-06-16 13:01:42 +0200

    :construction: Updated plugins to deal with optional claims

--- src/oidc_plugins/plugins.py
+++ src/oidc_plugins/plugins.py
@@ -1,4 +1,3 @@
-
 import warnings
 from abc import abstractmethod
 from typing import Any
@@ -38,7 +37,7 @@ from .schemas import (
     EHERKENNING_BEWINDVOERING_OPTIONS_SCHEMA,
     EHERKENNING_OPTIONS_SCHEMA,
 )
-from .types import ClaimProcessingInstructions, ErrorMessagesMap
+from .types import ClaimPathWithLegacy, ClaimProcessingInstructions, ErrorMessagesMap
 from .utils import process_claims
 from .views import anon_user_callback_view
 
@@ -68,8 +67,8 @@ class BaseDigiDeHerkenningPlugin(OIDCBasePlugin):
                 )
                 return self._get_legacy_callback()
             return "oidc_authentication_callback"
-        
-        return super().get_settings(attr, *args)
+
+        return super().get_setting(attr, *args)
 
     def verify_claims(self, claims: JSONObject) -> bool:
         """Verify the provided claims to decide if authentication should be allowed."""
@@ -91,7 +90,7 @@ class BaseDigiDeHerkenningPlugin(OIDCBasePlugin):
             return False
 
         return True
-    
+
     # TODO find a more elegant way of doing this...
     def create_user(self, claims: JSONObject) -> AnonymousUser:
         # This method is not called since we implemented get_or_create_user
@@ -100,18 +99,17 @@ class BaseDigiDeHerkenningPlugin(OIDCBasePlugin):
     def update_user(self, user: AnonymousUser, claims: JSONObject) -> AnonymousUser:
         # This method is not called since we implemented get_or_create_user
         pass
-    
+
     def filter_users_by_claims(self, claims: JSONObject):
         # This method is not called since we implemented get_or_create_user
         pass
-        
-    
+
     def get_or_create_user(
-        self, 
-        access_token: str, 
-        id_token: str, 
-        payload: JSONObject, 
-        request: HttpRequest
+        self,
+        access_token: str,
+        id_token: str,
+        payload: JSONObject,
+        request: HttpRequest,
     ) -> AnonymousUser:
         """
         Return a "fake" Django user.
@@ -121,7 +119,7 @@ class BaseDigiDeHerkenningPlugin(OIDCBasePlugin):
         """
         # Here we use the payload instead of the user_info, because the claims
         # configured in the OIDCClient options refer to the structure of the payload and not
-        # that of the user_info. 
+        # that of the user_info.
         claims_verified = self.verify_claims(payload)
         if not claims_verified:
             msg = "Claims verification failed"
@@ -134,20 +132,27 @@ class BaseDigiDeHerkenningPlugin(OIDCBasePlugin):
             # used for admin OIDC.
             raise PermissionDenied(msg)
 
-        procssed_claims = self._process_claims(payload)
-        request.session[self.identifier] = procssed_claims
+        processed_claims = self._process_claims(payload)
+        request.session[self.identifier] = processed_claims
 
         user = AnonymousUser()
         user.is_active = True  # type: ignore
         return user
-    
+
     def _process_claims(self, claims: JSONObject) -> JSONObject:
         config = self.get_config()
 
         strict_mode = flag_enabled("DIGID_EHERKENNING_OIDC_STRICT")
+        legacy_mode = True  # TODO make flag
         assert isinstance(strict_mode, bool)
-        return process_claims(claims, config, self.get_claim_processing_instructions(), strict_mode)
-    
+        return process_claims(
+            claims,
+            config,
+            self.get_claim_processing_instructions(),
+            strict_mode,
+            legacy_mode,
+        )
+
     def validate_settings(self) -> None:
         pass
 
@@ -155,60 +160,77 @@ class BaseDigiDeHerkenningPlugin(OIDCBasePlugin):
         return anon_user_callback_view(request)
 
 
-
 @register(OIDC_DIGID_IDENTIFIER)
 class OIDCDigidPlugin(BaseDigiDeHerkenningPlugin):
     def get_schema(self) -> JSONObject:
         return DIGID_OPTIONS_SCHEMA
-    
+
     def _get_legacy_callback(self) -> str:
         return "digid_oidc:callback"
-        
+
     def get_sensitive_claims(self) -> list[list[str]]:
         config = self.get_config()
 
         return [config.options["identity_settings"]["bsn_claim_path"]]
-    
+
     def get_claim_processing_instructions(self) -> ClaimProcessingInstructions:
         config = self.get_config()
 
         # TODO: check if this logic is correct:
-        # The claims in "always required" come from the models in OpenForms, 
+        # The claims in "always required" come from the models in OpenForms,
         # while the other ones come from the digid_eherkenning package
         return {
             "always_required_claims": [
-                {"path": config.options["identity_settings"]["bsn_claim_path"], "legacy": "bsn_claim"}
+                {
+                    "path": config.options["identity_settings"]["bsn_claim_path"],
+                    "legacy": "bsn_claim",
+                }
             ],
             "strict_required_claims": [],
+            "optional_claims": [],
         }
-    
+
     def get_error_messages(self) -> ErrorMessagesMap:
-        return {
-            "access_denied": (DIGID_MESSAGE_PARAMETER, LOGIN_CANCELLED)
-        }
-    
+        return {"access_denied": (DIGID_MESSAGE_PARAMETER, LOGIN_CANCELLED)}
+
 
 @register(OIDC_DIGID_MACHTIGEN_IDENTIFIER)
 class OIDCDigiDMachtigenPlugin(BaseDigiDeHerkenningPlugin):
     def get_schema(self) -> JSONObject:
         return DIGID_MACHTIGEN_OPTIONS_SCHEMA
-    
+
     def get_claim_processing_instructions(self) -> ClaimProcessingInstructions:
         config = self.get_config()
 
         return {
             "always_required_claims": [
-                {"path": config.options["identity_settings"]["representee_bsn_claim_path"], "legacy": "representee_bsn_claim"},
-                {"path": config.options["identity_settings"]["authorizee_bsn_claim_path"], "legacy": "authorizee_bsn_claim"}
+                {
+                    "path": config.options["identity_settings"][
+                        "representee_bsn_claim_path"
+                    ],
+                    "legacy": "representee_bsn_claim",
+                },
+                {
+                    "path": config.options["identity_settings"][
+                        "authorizee_bsn_claim_path"
+                    ],
+                    "legacy": "authorizee_bsn_claim",
+                },
             ],
             "strict_required_claims": [
-                {"path": config.options["identity_settings"]["mandate_service_id_claim_path"], "legacy": "mandate_service_id_claim"}
+                {
+                    "path": config.options["identity_settings"][
+                        "mandate_service_id_claim_path"
+                    ],
+                    "legacy": "mandate_service_id_claim",
+                }
             ],
+            "optional_claims": [],
         }
-    
+
     def _get_legacy_callback(self) -> str:
         return "digid_machtigen_oidc:callback"
-    
+
     def get_sensitive_claims(self) -> list[list[str]]:
         config = self.get_config()
 
@@ -216,95 +238,151 @@ class OIDCDigiDMachtigenPlugin(BaseDigiDeHerkenningPlugin):
             config.options["identity_settings"]["representee_bsn_claim_path"],
             config.options["identity_settings"]["authorizee_bsn_claim_path"],
         ]
-    
+
     def get_error_messages(self) -> ErrorMessagesMap:
-        return {
-            "access_denied": (DIGID_MESSAGE_PARAMETER, LOGIN_CANCELLED)
-        }
-    
+        return {"access_denied": (DIGID_MESSAGE_PARAMETER, LOGIN_CANCELLED)}
+
+
 @register(OIDC_EH_IDENTIFIER)
 class OIDCeHerkenningPlugin(BaseDigiDeHerkenningPlugin):
     def get_schema(self) -> JSONObject:
         return EHERKENNING_OPTIONS_SCHEMA
-    
+
     def get_claim_processing_instructions(self) -> ClaimProcessingInstructions:
         config = self.get_config()
 
+        optional_claims: list[ClaimPathWithLegacy] = []
+        if branch_number_claim_path := config.options["identity_settings"].get(
+            "branch_number_claim_path"
+        ):
+            optional_claims.append(
+                {"path": branch_number_claim_path, "legacy": "branch_number_claim"}
+            )
+
         return {
             "always_required_claims": [
-                {"path": config.options["identity_settings"]["legal_subject_claim_path"], "legacy": "legal_subject_claim"},
+                {
+                    "path": config.options["identity_settings"][
+                        "legal_subject_claim_path"
+                    ],
+                    "legacy": "legal_subject_claim",
+                },
             ],
             "strict_required_claims": [
-                {"path": config.options["identity_settings"]["acting_subject_claim_path"], "legacy": "acting_subject_claim"}
+                {
+                    "path": config.options["identity_settings"][
+                        "acting_subject_claim_path"
+                    ],
+                    "legacy": "acting_subject_claim",
+                }
             ],
+            "optional_claims": optional_claims,
         }
-    
+
     def _get_legacy_callback(self) -> str:
         return "eherkenning_oidc:callback"
-    
+
     def get_sensitive_claims(self) -> list[list[str]]:
         config = self.get_config()
 
-        return [
-            config.options["identity_settings"]["legal_subject_claim_path"],
-            config.options["identity_settings"]["branch_number_claim_path"],
+        sensitive_claims = [
+            config.options["identity_settings"]["legal_subject_claim_path"]
         ]
-    
+        if branch_number_claim_path := config.options["identity_settings"].get(
+            "branch_number_claim_path"
+        ):
+            sensitive_claims.append(branch_number_claim_path)
+
+        return sensitive_claims
+
     def get_error_messages(self) -> ErrorMessagesMap:
-        eh_message_parameter = EH_MESSAGE_PARAMETER % {
-            "plugin_id": self.identifier
-        }
-        return {
-            "access_denied": (eh_message_parameter, LOGIN_CANCELLED)
-        }
-    
+        eh_message_parameter = EH_MESSAGE_PARAMETER % {"plugin_id": self.identifier}
+        return {"access_denied": (eh_message_parameter, LOGIN_CANCELLED)}
+
+
 @register(OIDC_EH_BEWINDVOERING_IDENTIFIER)
 class OIDCeHerkenningBewindvoeringPlugin(BaseDigiDeHerkenningPlugin):
     def get_schema(self) -> JSONObject:
         return EHERKENNING_BEWINDVOERING_OPTIONS_SCHEMA
-    
+
     def get_claim_processing_instructions(self) -> ClaimProcessingInstructions:
         config = self.get_config()
 
+        optional_claims: list[ClaimPathWithLegacy] = []
+        if branch_number_claim_path := config.options["identity_settings"].get(
+            "branch_number_claim_path"
+        ):
+            optional_claims.append(
+                {"path": branch_number_claim_path, "legacy": "branch_number_claim"}
+            )
+
         return {
             "always_required_claims": [
-                {"path": config.options["identity_settings"]["legal_subject_claim_path"], "legacy": "legal_subject_claim"},
-                {"path": config.options["identity_settings"]["representee_claim_path"], "legacy": "representee_claim"},
+                {
+                    "path": config.options["identity_settings"][
+                        "legal_subject_claim_path"
+                    ],
+                    "legacy": "legal_subject_claim",
+                },
+                {
+                    "path": config.options["identity_settings"][
+                        "representee_claim_path"
+                    ],
+                    "legacy": "representee_claim",
+                },
             ],
             "strict_required_claims": [
-                {"path": config.options["identity_settings"]["acting_subject_claim_path"], "legacy": "acting_subject_claim"},
-                {"path": config.options["identity_settings"]["mandate_service_id_claim_path"], "legacy": "mandate_service_id_claim"},
-                {"path": config.options["identity_settings"]["mandate_service_uuid_claim_path"], "legacy": "mandate_service_uuid_claim"}
+                {
+                    "path": config.options["identity_settings"][
+                        "acting_subject_claim_path"
+                    ],
+                    "legacy": "acting_subject_claim",
+                },
+                {
+                    "path": config.options["identity_settings"][
+                        "mandate_service_id_claim_path"
+                    ],
+                    "legacy": "mandate_service_id_claim",
+                },
+                {
+                    "path": config.options["identity_settings"][
+                        "mandate_service_uuid_claim_path"
+                    ],
+                    "legacy": "mandate_service_uuid_claim",
+                },
             ],
+            "optional_claims": optional_claims,
         }
-    
+
     def _get_legacy_callback(self) -> str:
         return "eherkenning_bewindvoering_oidc:callback"
-    
+
     def get_sensitive_claims(self) -> list[list[str]]:
         config = self.get_config()
 
-        return [
+        sensitive_claims = [
             config.options["identity_settings"]["legal_subject_claim_path"],
-            config.options["identity_settings"]["branch_number_claim_path"],
             config.options["identity_settings"]["representee_claim_path"],
         ]
-    
+        if branch_number_claim_path := config.options["identity_settings"].get(
+            "branch_number_claim_path"
+        ):
+            sensitive_claims.append(branch_number_claim_path)
+
+        return sensitive_claims
+
     def get_error_messages(self) -> ErrorMessagesMap:
-        eh_message_parameter = EH_MESSAGE_PARAMETER % {
-            "plugin_id": self.identifier
-        }
-        return {
-            "access_denied": (eh_message_parameter, LOGIN_CANCELLED)
-        }
-    
+        eh_message_parameter = EH_MESSAGE_PARAMETER % {"plugin_id": self.identifier}
+        return {"access_denied": (eh_message_parameter, LOGIN_CANCELLED)}
+
+
 @register(OIDC_ORG_IDENTIFIER)
 class OIDCOrgPlugin(OIDCAdminPlugin):
     def get_schema(self) -> JSONObject:
         return ADMIN_OPTIONS_SCHEMA
-    
+
     def handle_callback(self, request: HttpRequest) -> HttpResponse:
         return callback_view(request)
-    
+
     def _get_legacy_callback(self) -> str:
         return "org-oidc-callback"
