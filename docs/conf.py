4f4d454e35c7cfdb96c91b3c662d582fe30f31d4
Sergei Maertens <sergei@maykinmedia.nl>
2025-06-23 11:16:24 +0200

    :alien: Update to new domain for requests docs
    Seems to have been moved to readthedocs instead of the custom
    domain it used to be on.
    
    (cherry picked from commit 4ae445273022d9525b80601f187a9c1ec7cc41fe)

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ source_suffix = [".rst", ".md"]
 
 intersphinx_mapping = {
     "requests": (
-        "https://docs.python-requests.org/en/latest/",
+        "https://requests.readthedocs.io/en/latest/",
         None,
     ),
     "ape_pie": (
