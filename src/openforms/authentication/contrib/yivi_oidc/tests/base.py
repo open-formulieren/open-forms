a5fd1a233b57a7300ae3216d7192d22899f81106
robinvandermolen <robin@maykinmedia.nl>
2025-07-03 12:01:37 +0200

    :white_check_mark: [#5132] Added tests for generic OIDC setup
    For the generic OIDC setup some functionality has shifted from the shared code to the plugins. These tests make sure that the new logic works as expected.

--- src/openforms/authentication/contrib/yivi_oidc/tests/base.py
+++ src/openforms/authentication/contrib/yivi_oidc/tests/base.py
@@ -1,6 +1,14 @@
 from functools import partial
+from pathlib import Path
+
+from django.test import override_settings
+
+from django_webtest import WebTest
 
 from openforms.utils.tests.keycloak import KEYCLOAK_BASE_URL, mock_oidc_db_config
+from openforms.utils.tests.vcr import OFVCRMixin
+
+TEST_FILES = (Path(__file__).parent / "data").resolve()
 
 
 def mock_config(model: str, **overrides):
@@ -13,3 +21,8 @@ mock_yivi_config = partial(
     model="YiviOpenIDConnectConfig",
     oidc_rp_scopes_list=["openid"],
 )
+
+
+@override_settings(CORS_ALLOW_ALL_ORIGINS=True, IS_HTTPS=True)
+class IntegrationTestsBase(OFVCRMixin, WebTest):
+    VCR_TEST_FILES = TEST_FILES
