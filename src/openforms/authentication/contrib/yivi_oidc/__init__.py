2f5bbf6cf3e26f19fbbc175143ec02dd0e61ef2f
robinvandermolen <robin@maykinmedia.nl>
2025-07-03 17:58:00 +0200

    :memo: [#5132] Update documentation

---
+++ src/openforms/authentication/contrib/yivi_oidc/__init__.py
@@ -0,0 +1,10 @@
+"""
+Plugin for Yivi authentication
+
+Website: https://yivi.nl/
+
+For development, we can use the Yivi demo attribute sets:
+https://attribute-index.yivi.app/en/irma-demo.html.
+
+See the project documentation for more information about Yivi authentication.
+"""
