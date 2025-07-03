935c92243dbb3d3e239040d04b6e9daa19a8ef9a
robinvandermolen <robin@maykinmedia.nl>
2025-07-03 12:01:37 +0200

    :white_check_mark: [#5132] Added tests for generic OIDC setup
    For the generic OIDC setup some functionality has shifted from the shared code to the plugins. These tests make sure that the new logic works as expected.

--- src/openforms/authentication/contrib/yivi_oidc/tests/test_plugin.py
+++ src/openforms/authentication/contrib/yivi_oidc/tests/test_plugin.py
@@ -1,110 +1,38 @@
 from django.test import TestCase
 
+from openforms.authentication.constants import AuthAttribute
 from openforms.authentication.contrib.yivi_oidc.config import YiviOptions
-from openforms.authentication.contrib.yivi_oidc.constants import (
-    YiviAuthenticationAttributes,
+from openforms.authentication.contrib.yivi_oidc.models import (
+    YiviOpenIDConnectConfig,
 )
+from openforms.authentication.contrib.yivi_oidc.tests.base import mock_yivi_config
 from openforms.authentication.registry import register
-from openforms.authentication.tests.factories import AttributeGroupFactory
+from openforms.authentication.tests.factories import (
+    AttributeGroupFactory,
+)
 
 plugin = register["yivi_oidc"]
 
 
-class YiviPluginTest(TestCase):
+class YiviPluginTransformClaimsTest(TestCase):
     """
-    Test functionality of the Yivi plugin.
+    Testing the Yivi plugin ``transform_claims`` function.
     """
-
-    def test_get_user_chosen_authentication_attribute_when_all_authentication_options_are_allowed(
-        self,
-    ):
-        claims_expected_result_map = (
-            (
-                {
-                    "bsn_claim": "123456789",
-                    "kvk_claim": "12345678",
-                    "pseudo_claim": "hash",
-                },
-                YiviAuthenticationAttributes.bsn,
-            ),
-            (
-                {"bsn_claim": "123456789", "kvk_claim": "12345678"},
-                YiviAuthenticationAttributes.bsn,
-            ),
-            ({"bsn_claim": "123456789"}, YiviAuthenticationAttributes.bsn),
-            (
-                {"kvk_claim": "12345678", "pseudo_claim": "hash"},
-                YiviAuthenticationAttributes.kvk,
-            ),
-            ({"kvk_claim": "12345678"}, YiviAuthenticationAttributes.kvk),
-            ({"pseudo_claim": "hash"}, YiviAuthenticationAttributes.pseudo),
-            ({}, YiviAuthenticationAttributes.pseudo),
-        )
-        for claims, expected_result in claims_expected_result_map:
-            with self.subTest(data=claims, expected_result=expected_result):
-                result = plugin._get_user_chosen_authentication_attribute(
-                    YiviAuthenticationAttributes.values, claims
-                )
-
-                self.assertEqual(result, expected_result)
-
-    def test_get_user_chosen_authentication_attribute_when_only_bsn_authentication_is_allowed(
-        self,
-    ):
-        claims_expected_result_map = (
-            ({"bsn_claim": "123456789"}, YiviAuthenticationAttributes.bsn),
-            ({"kvk_claim": "12345678"}, YiviAuthenticationAttributes.pseudo),
-            ({"pseudo_claim": "hash"}, YiviAuthenticationAttributes.pseudo),
-            ({}, YiviAuthenticationAttributes.pseudo),
-        )
-        for claims, expected_result in claims_expected_result_map:
-            with self.subTest(data=claims, expected_result=expected_result):
-                result = plugin._get_user_chosen_authentication_attribute(
-                    [YiviAuthenticationAttributes.bsn], claims
-                )
-
-                self.assertEqual(result, expected_result)
-
-    def test_get_user_chosen_authentication_attribute_when_only_kvk_authentication_is_allowed(
-        self,
-    ):
-        claims_expected_result_map = (
-            ({"bsn_claim": "123456789"}, YiviAuthenticationAttributes.pseudo),
-            ({"kvk_claim": "12345678"}, YiviAuthenticationAttributes.kvk),
-            ({"pseudo_claim": "hash"}, YiviAuthenticationAttributes.pseudo),
-            ({}, YiviAuthenticationAttributes.pseudo),
-        )
-        for claims, expected_result in claims_expected_result_map:
-            with self.subTest(data=claims, expected_result=expected_result):
-                result = plugin._get_user_chosen_authentication_attribute(
-                    [YiviAuthenticationAttributes.kvk], claims
-                )
-
-                self.assertEqual(result, expected_result)
-
-    def test_get_user_chosen_authentication_attribute_when_no_authentication_options_are_used(
-        self,
-    ):
-        claims_expected_result_map = (
-            ({"bsn_claim": "123456789"}, YiviAuthenticationAttributes.pseudo),
-            ({"kvk_claim": "12345678"}, YiviAuthenticationAttributes.pseudo),
-            ({"pseudo_claim": "hash"}, YiviAuthenticationAttributes.pseudo),
-            ({}, YiviAuthenticationAttributes.pseudo),
-        )
-        for claims, expected_result in claims_expected_result_map:
-            with self.subTest(data=claims, expected_result=expected_result):
-                result = plugin._get_user_chosen_authentication_attribute([], claims)
-
-                self.assertEqual(result, expected_result)
 
     def test_transform_claims_for_bsn_without_additional_attributes_claims(self):
         plugin_options: YiviOptions = {
-            "authentication_options": YiviAuthenticationAttributes.values,
+            "authentication_options": AuthAttribute.values,
             "additional_attributes_groups": [],
             "bsn_loa": "",
             "kvk_loa": "",
         }
-        form_auth = plugin.transform_claims(plugin_options, {"bsn_claim": "123456789"})
+        form_auth = plugin.transform_claims(
+            plugin_options,
+            {
+                "bsn_claim": "123456789",
+                "loa_claim": "urn:oasis:names:tc:SAML:2.0:ac:classes:Smartcard",
+            },
+        )
 
         self.assertEqual(
             form_auth,
@@ -113,35 +41,13 @@ class YiviPluginTest(TestCase):
                 "additional_claims": {},
                 "attribute": "bsn",
                 "value": "123456789",
-                "loa": "",
+                "loa": "urn:oasis:names:tc:SAML:2.0:ac:classes:Smartcard",
             },
         )
 
     def test_transform_claims_for_kvk_without_additional_attributes_claims(self):
         plugin_options: YiviOptions = {
-            "authentication_options": YiviAuthenticationAttributes.values,
-            "additional_attributes_groups": [],
-            "bsn_loa": "",
-            "kvk_loa": "",
-        }
-        form_auth = plugin.transform_claims(plugin_options, {"kvk_claim": "12345678"})
-
-        self.assertEqual(
-            form_auth,
-            {
-                "plugin": "yivi_oidc",
-                "additional_claims": {},
-                "attribute": "kvk",
-                "value": "12345678",
-                "loa": "",
-            },
-        )
-
-    def test_transform_claims_for_kvk_extended_without_additional_attributes_claims(
-        self,
-    ):
-        plugin_options: YiviOptions = {
-            "authentication_options": YiviAuthenticationAttributes.values,
+            "authentication_options": AuthAttribute.values,
             "additional_attributes_groups": [],
             "bsn_loa": "",
             "kvk_loa": "",
@@ -166,25 +72,6 @@ class YiviPluginTest(TestCase):
             },
         )
 
-    def test_transform_claims_for_pseudo_without_additional_attributes_claims(self):
-        plugin_options: YiviOptions = {
-            "authentication_options": YiviAuthenticationAttributes.values,
-            "additional_attributes_groups": [],
-            "bsn_loa": "",
-            "kvk_loa": "",
-        }
-        form_auth = plugin.transform_claims(plugin_options, {})
-
-        self.assertEqual(
-            form_auth,
-            {
-                "plugin": "yivi_oidc",
-                "additional_claims": {},
-                "attribute": "pseudo",
-                "value": "dummy-set-by@openforms",
-            },
-        )
-
     def test_transform_claims_without_authentication_or_additional_attributes(self):
         plugin_options: YiviOptions = {
             "authentication_options": [],
@@ -205,23 +92,174 @@ class YiviPluginTest(TestCase):
         )
 
     def test_transform_claims_with_additional_attributes_claims(self):
-        AttributeGroupFactory(name="foo_attribute", attributes=["foo"])
+        AttributeGroupFactory(name="foo_attribute", attributes=["attribute_name"])
         plugin_options: YiviOptions = {
-            "authentication_options": YiviAuthenticationAttributes.values,
+            "authentication_options": AuthAttribute.values,
             "additional_attributes_groups": ["foo_attribute"],
             "bsn_loa": "",
             "kvk_loa": "",
         }
         form_auth = plugin.transform_claims(
-            plugin_options, {"additional_claims": {"foo": "bar"}}
+            plugin_options, {"additional_claims": {"attribute_name": "attribute_value"}}
         )
 
+        # Assert that pseudo is used as fallback identifying value
         self.assertEqual(
             form_auth,
             {
                 "plugin": "yivi_oidc",
-                "additional_claims": {"foo": "bar"},
+                "additional_claims": {"attribute_name": "attribute_value"},
                 "attribute": "pseudo",
                 "value": "dummy-set-by@openforms",
             },
         )
+
+
+class YiviPluginBeforeProcessClaimsTest(TestCase):
+    """
+    Testing the Yivi plugin ``before_process_claims`` function.
+
+    The Yivi implementation of ``before_process_claims`` checks the claims and updates
+    the config to make sure the correct loa configuration is used. Because Yivi has loa
+    config for bsn and kvk, we need to define the default loa dynamically based on the
+    received claims.
+    """
+
+    @mock_yivi_config(
+        bsn_claim=["test.attribute.bsn"],
+        bsn_loa_claim=["test.attribute.loa.bsn"],
+        bsn_default_loa="urn:oasis:names:tc:SAML:2.0:ac:classes:Smartcard",
+        bsn_loa_value_mapping={"mapping bsn": "schema definition"},
+    )
+    def test_before_process_claims_with_bsn_loa_config(self):
+        config = YiviOpenIDConnectConfig.get_solo()
+        plugin.before_process_claims(
+            config,
+            {
+                "test.attribute.bsn": "123456789",
+                "test.attribute.loa.bsn": "urn:oasis:names:tc:SAML:2.0:ac:classes:SmartcardPKI",
+            },
+        )
+
+        # When, for some reason, both bsn and kvk claims are received, we treat bsn as
+        # identification.
+        self.assertEqual(config.loa_claim, ["test.attribute.loa.bsn"])
+        self.assertEqual(
+            config.default_loa, "urn:oasis:names:tc:SAML:2.0:ac:classes:Smartcard"
+        )
+        self.assertEqual(config.loa_value_mapping, {"mapping bsn": "schema definition"})
+
+    @mock_yivi_config(
+        kvk_claim=["test.attribute.kvk"],
+        kvk_loa_claim=["test.attribute.loa.kvk"],
+        kvk_default_loa="urn:etoegang:core:assurance-class:loa2",
+        kvk_loa_value_mapping={"mapping kvk": "schema definition"},
+    )
+    def test_before_process_claims_with_kvk_loa_config(self):
+        config = YiviOpenIDConnectConfig.get_solo()
+        plugin.before_process_claims(
+            config,
+            {
+                "test.attribute.kvk": "12345678",
+                "test.attribute.loa.kvk": "urn:etoegang:core:assurance-class:loa3",
+            },
+        )
+
+        # When, for some reason, both bsn and kvk claims are received, we treat bsn as
+        # identification.
+        self.assertEqual(config.loa_claim, ["test.attribute.loa.kvk"])
+        self.assertEqual(config.default_loa, "urn:etoegang:core:assurance-class:loa2")
+        self.assertEqual(config.loa_value_mapping, {"mapping kvk": "schema definition"})
+
+    @mock_yivi_config(
+        bsn_claim=["test.attribute.bsn"],
+        bsn_loa_claim=["test.attribute.loa.bsn"],
+        bsn_default_loa="urn:oasis:names:tc:SAML:2.0:ac:classes:Smartcard",
+        bsn_loa_value_mapping={"mapping bsn": "schema definition"},
+        kvk_claim=["test.attribute.kvk"],
+        kvk_loa_claim=["test.attribute.loa.kvk"],
+        kvk_default_loa="urn:etoegang:core:assurance-class:loa2",
+        kvk_loa_value_mapping={"mapping kvk": "schema definition"},
+    )
+    def test_before_process_claims_with_bsn_and_kvk_loa_config(self):
+        config = YiviOpenIDConnectConfig.get_solo()
+        plugin.before_process_claims(
+            config,
+            {
+                "test.attribute.bsn": "123456789",
+                "test.attribute.loa.bsn": "urn:oasis:names:tc:SAML:2.0:ac:classes:SmartcardPKI",
+                "test.attribute.kvk": "12345678",
+                "test.attribute.loa.kvk": "urn:etoegang:core:assurance-class:loa3",
+            },
+        )
+
+        # When, for some reason, both bsn and kvk claims are received, we treat bsn as
+        # the primary identification.
+        self.assertEqual(config.loa_claim, ["test.attribute.loa.bsn"])
+        self.assertEqual(
+            config.default_loa, "urn:oasis:names:tc:SAML:2.0:ac:classes:Smartcard"
+        )
+        self.assertEqual(config.loa_value_mapping, {"mapping bsn": "schema definition"})
+
+    @mock_yivi_config()
+    def test_before_process_claims_without_bsn_and_kvk_loa_config(self):
+        config = YiviOpenIDConnectConfig.get_solo()
+        plugin.before_process_claims(config, {})
+
+        # When, for some reason, both bsn and kvk claims are received, we treat bsn as
+        # identification.
+        self.assertEqual(config.loa_claim, [""])
+        self.assertEqual(config.default_loa, None)
+        self.assertEqual(config.loa_value_mapping, None)
+
+
+class YiviPluginExtractAdditionalClaimsTest(TestCase):
+    """
+    Testing the Yivi plugin ``extract_additional_claims`` function.
+    """
+
+    def test_extract_additional_claims_with_known_attributes(self):
+        AttributeGroupFactory(
+            name="know_attributes", attributes=["firstname", "lastname"]
+        )
+        AttributeGroupFactory(name="know_attributes_2", attributes=["dob"])
+        plugin_options: YiviOptions = {
+            "authentication_options": [],
+            "additional_attributes_groups": ["know_attributes", "know_attributes_2"],
+            "bsn_loa": "",
+            "kvk_loa": "",
+        }
+        data = {"firstname": "bob", "lastname": "joe", "dob": "21-01-1999"}
+
+        extracted_claims = plugin.extract_additional_claims(plugin_options, data)
+        self.assertEqual(
+            extracted_claims,
+            {"firstname": "bob", "lastname": "joe", "dob": "21-01-1999"},
+        )
+
+    def test_extract_additional_claims_with_unknown_attributes(self):
+        plugin_options: YiviOptions = {
+            "authentication_options": [],
+            "additional_attributes_groups": ["unknow_attributes"],
+            "bsn_loa": "",
+            "kvk_loa": "",
+        }
+        data = {"firstname": "bob"}
+
+        extracted_claims = plugin.extract_additional_claims(plugin_options, data)
+        self.assertEqual(extracted_claims, {})
+
+    def test_extract_additional_claims_with_missing_claims(self):
+        AttributeGroupFactory(
+            name="know_attributes", attributes=["firstname", "lastname"]
+        )
+        plugin_options: YiviOptions = {
+            "authentication_options": [],
+            "additional_attributes_groups": ["know_attributes"],
+            "bsn_loa": "",
+            "kvk_loa": "",
+        }
+        data = {"firstname": "bob"}
+
+        extracted_claims = plugin.extract_additional_claims(plugin_options, data)
+        self.assertEqual(extracted_claims, {"firstname": "bob"})
