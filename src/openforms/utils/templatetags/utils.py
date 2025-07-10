20a814b4d306e84f0d1bca276674845d4675e75c
Sergei Maertens <sergei@maykinmedia.nl>
2025-07-10 12:49:11 +0200

    :fire: Replace admin env info with maykin-common utility

--- src/openforms/utils/templatetags/utils.py
+++ src/openforms/utils/templatetags/utils.py
@@ -5,8 +5,6 @@ Any template tag added to this file will automatically be added to the 'sandboxe
 """
 
 from django import template
-from django.conf import settings
-from django.utils.html import format_html
 
 register = template.Library()
 
@@ -17,22 +15,3 @@ def get_value(value: dict, value_key: str) -> str:
         return ""
 
     return str(value.get(value_key, ""))
-
-
-@register.simple_tag(takes_context=True)
-def environment_info(context) -> str:
-    if not settings.SHOW_ENVIRONMENT:
-        return ""
-    if (user := context.get("user")) is None or not user.is_authenticated:
-        return ""
-
-    return format_html(
-        """
-        <div class="env-info" style="--of-env-info-background-color: {bg_color}; --of-env-info-color: {color}">
-            {label}
-        </div>
-        """,
-        label=settings.ENVIRONMENT_LABEL,
-        bg_color=settings.ENVIRONMENT_BACKGROUND_COLOR,
-        color=settings.ENVIRONMENT_FOREGROUND_COLOR,
-    )
