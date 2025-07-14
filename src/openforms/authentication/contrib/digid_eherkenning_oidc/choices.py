c99aa1a362521c1e34da632d39a164ceb3dc7e07
robinvandermolen <robin@maykinmedia.nl>
2025-07-14 15:31:50 +0200

    :bento: [#5428] Update available eIDAS LoA-levels in admin
    As mentioned on the issue, we suspect that eIDAS (in the Netherlands) uses the same URN values as eHerkenning for the LoA-levels. We think that the eIDAS LoA-levels `Low`, `Substantial` and `High` are used only on european scale, to connect the different nation-specific eIDAS LoA-values.
    
    Because LoA-level 1 isn't used/allowed/doesn't exist, it isn't available as a eIDAS (OIDC) LoA-level. LoA 2 and LoA 2+ are very closely related (and by eIDAS regarded as the same level), yet we cannot drop support for LoA 2 as its apparently still being used in certain situations.
    
    The new LoA-levels for eIDAS are:
    low = urn:etoegang:core:assurance-class:loa2
    low_plus = urn:etoegang:core:assurance-class:loa2plus
    substantial = urn:etoegang:core:assurance-class:loa3
    high = urn:etoegang:core:assurance-class:loa4

--- src/openforms/authentication/contrib/digid_eherkenning_oidc/choices.py
+++ src/openforms/authentication/contrib/digid_eherkenning_oidc/choices.py
@@ -1,9 +1,14 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+
+from digid_eherkenning.choices import AssuranceLevels
 
 
-# ref: https://ec.europa.eu/digital-building-blocks/sites/display/DIGITAL/eIDAS+Levels+of+Assurance
+# ref: https://afsprakenstelsel.etoegang.nl/Startpagina/as/level-of-assurance
+# LoA 1 is no-longer used. LoA 2 and LoA 2+ both translate to eIDAS LoA "Low".
+# Because of the slightly worst security, we should drop LoA 2 in favor of LoA 2+.
+# But, apparently LoA 2 is (in certain situations) still used, so we must support it.
 class EIDASAssuranceLevels(models.TextChoices):
-    low = ("low", _("Laag"))
-    substantial = ("substantial", _("Substantieel"))
-    high = ("high", _("Hoog"))
+    low = AssuranceLevels.low
+    low_plus = AssuranceLevels.low_plus
+    substantial = AssuranceLevels.substantial
+    high = AssuranceLevels.high
