738e740109f989211761d4e1c7094d981e7eb705
Sonny Bakker <sonny871@hotmail.com>
2025-07-18 12:35:26 +0200

    [#5382] mark field as translatable

--- src/openforms/forms/models/form.py
+++ src/openforms/forms/models/form.py
@@ -69,8 +69,8 @@ class Form(models.Model):
         help_text=_("internal name for management purposes"),
     )
     internal_remarks = models.TextField(
-        "internal remarks",
-        help_text=(
+        _("internal remarks"),
+        help_text=_(
             "Remarks or intentions about the form. Can also be used to save notes"
             " for later use or for another admin user."
         ),
