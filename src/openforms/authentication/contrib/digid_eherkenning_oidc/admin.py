66944c8d189c420b81803c9caef41a9145c56844
SilviaAmAm <silvia@maykinmedia.nl>
2025-07-18 17:34:34 +0200

    :recycle: Refactor OF to use new OIDC models

--- src/openforms/authentication/contrib/digid_eherkenning_oidc/admin.py
+++ src/openforms/authentication/contrib/digid_eherkenning_oidc/admin.py
@@ -1,101 +1,64 @@
+from django import forms
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-from digid_eherkenning.oidc.admin import (
-    DigiDConfigAdmin as _DigiDConfigAdmin,
-    DigiDMachtigenConfigAdmin as _DigiDMachtigenConfigAdmin,
-    EHerkenningBewindvoeringConfigAdmin as _EHerkenningBewindvoeringConfigAdmin,
-    EHerkenningConfigAdmin as _EHerkenningConfigAdmin,
-    admin_modelform_factory,
-    fieldsets_factory,
-)
-from digid_eherkenning.oidc.models import (
-    DigiDConfig,
-    DigiDMachtigenConfig,
-    EHerkenningBewindvoeringConfig,
-    EHerkenningConfig,
-)
-from solo.admin import SingletonModelAdmin
+from mozilla_django_oidc_db.admin import OIDCClientAdmin
+from mozilla_django_oidc_db.models import OIDCClient
 
-from openforms.contrib.auth_oidc.admin import OIDCConfigForm
+from openforms.forms.models import Form
 
-from .models import (
-    OFDigiDConfig,
-    OFDigiDMachtigenConfig,
-    OFEHerkenningBewindvoeringConfig,
-    OFEHerkenningConfig,
-    OFEIDASCompanyConfig,
-    OFEIDASConfig,
-)
+from ...registry import register as auth_registry
 
-# unregister the default app admins so we can add in our own behaviour
-admin.site.unregister(DigiDMachtigenConfig)
-admin.site.unregister(EHerkenningBewindvoeringConfig)
-admin.site.unregister(EHerkenningConfig)
-admin.site.unregister(DigiDConfig)
+# unregister mozilla_django_oidc_db admin so we can add in our own behaviour
+admin.site.unregister(OIDCClient)
 
 
-@admin.register(OFDigiDConfig)
-class DigiDConfigAdmin(_DigiDConfigAdmin):
-    form = admin_modelform_factory(OFDigiDConfig, form=OIDCConfigForm)
+@admin.register(OIDCClient)
+class OFOIDCClientAdmin(OIDCClientAdmin):
+    def get_form(self, request, obj=None, change=False, **kwargs):
+        """Add custom cleaning behaviour to the model form
 
+        This is cleaning behaviour specific to Open Form. We don't want to overwrite the
+        model form, otherwise we loose the fieldsets in the parent admin class.
+        """
+        result_form = super().get_form(request, obj, change, **kwargs)
 
-@admin.register(OFDigiDMachtigenConfig)
-class DigiDMachtigenConfigAdmin(_DigiDMachtigenConfigAdmin):
-    form = admin_modelform_factory(OFDigiDMachtigenConfig, form=OIDCConfigForm)
+        def clean_enabled(form: forms.Form) -> bool:
+            """
+            Scan the (live) forms to see if any might be using this backend.
+            Disabling a backend while it is being used as a plugin on a live form would
+            break this form, so we warn the users for this.
+            """
+            enabled = form.cleaned_data["enabled"]
+            # Nothing to do if it is being or stays enabled
+            if enabled:
+                return enabled
 
+            # Find the Auth plugin corresponding to the OIDC
+            # client being modified. If it doesn't correspond to any OIDC
+            # auth plugin, just return.
+            for _identifier, plugin in auth_registry.items():
+                if (
+                    getattr(plugin, "oidc_plugin_identifier", "")
+                    == form.instance.identifier
+                ):
+                    matching_plugin = plugin
+                    break
+            else:
+                return enabled
 
-@admin.register(OFEHerkenningConfig)
-class EHerkenningConfigAdmin(_EHerkenningConfigAdmin):
-    form = admin_modelform_factory(OFEHerkenningConfig, form=OIDCConfigForm)
+            forms_with_backend = Form.objects.live().filter(
+                auth_backends__backend__exact=matching_plugin.identifier,
+            )
+            if forms_with_backend.exists():
+                raise forms.ValidationError(
+                    _(
+                        "{plugin_name} is selected as authentication backend "
+                        "for one or more forms, please remove this backend from these "
+                        "forms before disabling this authentication backend."
+                    ).format(plugin_name=matching_plugin.verbose_name)
+                )
+            return enabled
 
-
-@admin.register(OFEHerkenningBewindvoeringConfig)
-class EHerkenningBewindvoeringConfigAdmin(_EHerkenningBewindvoeringConfigAdmin):
-    form = admin_modelform_factory(
-        OFEHerkenningBewindvoeringConfig, form=OIDCConfigForm
-    )
-
-
-@admin.register(OFEIDASConfig)
-class EIDASConfigAdmin(SingletonModelAdmin):
-    """
-    Configuration for eIDAS authentication via OpenID connect.
-    """
-
-    form = admin_modelform_factory(OFEIDASConfig, form=OIDCConfigForm)
-    fieldsets = fieldsets_factory(
-        claim_mapping_fields=[
-            "legal_subject_identifier_claim",
-            "legal_subject_identifier_type_claim",
-            "legal_subject_first_name_claim",
-            "legal_subject_family_name_claim",
-            "legal_subject_date_of_birth_claim",
-            "loa_claim",
-            "default_loa",
-            "loa_value_mapping",
-        ]
-    )
-
-
-@admin.register(OFEIDASCompanyConfig)
-class EIDASCompanyConfigAdmin(SingletonModelAdmin):
-    """
-    Configuration for eIDAS authentication for companies via OpenID connect.
-    """
-
-    form = admin_modelform_factory(OFEIDASCompanyConfig, form=OIDCConfigForm)
-    fieldsets = fieldsets_factory(
-        claim_mapping_fields=[
-            "legal_subject_identifier_claim",
-            "legal_subject_name_claim",
-            "acting_subject_identifier_claim",
-            "acting_subject_identifier_type_claim",
-            "acting_subject_first_name_claim",
-            "acting_subject_family_name_claim",
-            "acting_subject_date_of_birth_claim",
-            "mandate_service_id_claim",
-            "loa_claim",
-            "default_loa",
-            "loa_value_mapping",
-        ]
-    )
+        result_form.clean_enabled = clean_enabled
+        return result_form
