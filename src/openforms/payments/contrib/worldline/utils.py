adcfea3d4c5dcfc5daeedb9f0b78109f23e2f1a5
Sonny Bakker <sonny871@hotmail.com>
2025-08-01 10:38:00 +0200

    [#4879] add docstrings for payment status conversion function

--- src/openforms/payments/contrib/worldline/utils.py
+++ src/openforms/payments/contrib/worldline/utils.py
@@ -6,6 +6,12 @@ logger = structlog.stdlib.get_logger(__name__)
 
 
 def get_payment_status(worldline_status: str, checkout_status: str = "") -> str:
+    # The worldline_status here represents the status of the payment. It could be
+    # possible that no payment was created yet for a given checkout and we therefore
+    # receive an empty string value here. In order to do have an indication what
+    # the payment status is we can derive it from the checkout_status. See the
+    # worldline documentation for more information about statuses.
+    # https://apireference.connect.worldline-solutions.com/s2sapi/v1/en_US/java/statuses.html?paymentPlatform=ALL
     if not worldline_status and checkout_status:
         return HostedCheckoutStatus.to_of_status(checkout_status)
 
