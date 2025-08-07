3596fab2d8e524ef7510eb0ccf070becaf2c4a11
Sonny Bakker <sonny@maykinmedia.nl>
2025-08-07 15:05:17 +0200

    [#4879] add string representation test for webhook entry

--- src/openforms/payments/contrib/worldline/tests/test_models.py
+++ src/openforms/payments/contrib/worldline/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from .factories import WorldlineMerchantFactory
+from .factories import WorldlineMerchantFactory, WorldlineWebhookEntryFactory
 
 
 class WorldlineMerchantTests(TestCase):
@@ -8,3 +8,10 @@ class WorldlineMerchantTests(TestCase):
         merchant = WorldlineMerchantFactory(label="Maykin Media")
 
         self.assertEqual(str(merchant), "Maykin Media")
+
+
+class WorldlineWebhookEntryTests(TestCase):
+    def test_string_representation(self):
+        webhook_entry = WorldlineWebhookEntryFactory()
+
+        self.assertEqual(str(webhook_entry), webhook_entry.webhook_key_id)
