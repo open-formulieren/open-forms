1038f9bc62ae98ce347021677c7b3f25946249c9
robinvandermolen <robin@maykinmedia.nl>
2025-08-13 17:40:55 +0200

    :sparkles: [#5515] Fetch attribute groups from api
    Adding a new api endpoint for fetching the attribute groups. This is used in the form-builder, when configuring the additional attributes groups on Yivi authentication plugins.

--- src/openforms/js/components/admin/form_design/constants.js
+++ src/openforms/js/components/admin/form_design/constants.js
@@ -7,6 +7,8 @@ export const REGISTRATION_OBJECTS_TARGET_PATHS =
 export const REGISTRATION_GENERIC_JSON_FIXED_METADATA_VARIABLES =
   '/api/v2/registration/plugins/generic-json/fixed-metadata-variables';
 export const AUTH_PLUGINS_ENDPOINT = '/api/v2/authentication/plugins';
+export const YIVI_ATTRIBUTE_GROUPS_ENDPOINT =
+  '/api/v2/authentication/plugins/yivi/attribute-groups';
 export const PREFILL_PLUGINS_ENDPOINT = '/api/v2/prefill/plugins';
 export const DMN_PLUGINS_ENDPOINT = '/api/v2/dmn/plugins';
 export const PAYMENT_PLUGINS_ENDPOINT = '/api/v2/payment/plugins';
--- src/openforms/js/components/admin/form_design/form-creation-form.js
+++ src/openforms/js/components/admin/form_design/form-creation-form.js
@@ -50,6 +50,7 @@ import {
   REGISTRATION_VARIABLES_ENDPOINT,
   STATIC_VARIABLES_ENDPOINT,
   THEMES_ENDPOINT,
+  YIVI_ATTRIBUTE_GROUPS_ENDPOINT,
 } from './constants';
 import {loadForm, loadFromBackend, saveCompleteForm} from './data';
 import {updateWarningsValidationError} from './logic/utils';
@@ -130,6 +131,7 @@ const initialFormState = {
   reusableFormDefinitionsLoaded: false,
   availableRegistrationBackends: [],
   availableAuthPlugins: [],
+  availableYiviAttributeGroups: [],
   availablePrefillPlugins: [],
   availableDMNPlugins: [],
   selectedAuthPlugins: [],
@@ -919,6 +921,7 @@ const FormCreationForm = ({formUuid, formUrl, formHistoryUrl, outgoingRequestsUr
     {endpoint: PAYMENT_PLUGINS_ENDPOINT, stateVar: 'availablePaymentBackends'},
     {endpoint: REGISTRATION_BACKENDS_ENDPOINT, stateVar: 'availableRegistrationBackends'},
     {endpoint: AUTH_PLUGINS_ENDPOINT, stateVar: 'availableAuthPlugins'},
+    {endpoint: YIVI_ATTRIBUTE_GROUPS_ENDPOINT, stateVar: 'availableYiviAttributeGroups'},
     {endpoint: CATEGORIES_ENDPOINT, stateVar: 'availableCategories'},
     {endpoint: THEMES_ENDPOINT, stateVar: 'availableThemes'},
     {endpoint: PREFILL_PLUGINS_ENDPOINT, stateVar: 'availablePrefillPlugins'},
@@ -1224,6 +1227,7 @@ const FormCreationForm = ({formUuid, formUrl, formHistoryUrl, outgoingRequestsUr
           formVariables: state.formVariables,
           staticVariables: state.staticVariables,
           registrationPluginsVariables: state.registrationPluginsVariables,
+          availableYiviAttributeGroups: state.availableYiviAttributeGroups,
           plugins: {
             availableAuthPlugins: state.availableAuthPlugins,
             selectedAuthPlugins: state.selectedAuthPlugins,
