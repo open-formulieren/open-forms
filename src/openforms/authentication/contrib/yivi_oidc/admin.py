c4f336887c8d90ce97540fcbf4faa748cc3e5a05
robinvandermolen <robin@maykinmedia.nl>
2025-08-14 10:39:59 +0200

    :children_crossing: [#5515] Add import and export functionality for AttributeGroups
    Implemented django-import-export for AttributeGroups, allowing attribute configuration to be easily shared between OF environments.
    
    The import/export process handles the `uuid`, `name`, `description` and `attributes` fields, using `uuid` as identifier.

--- src/openforms/authentication/contrib/yivi_oidc/admin.py
+++ src/openforms/authentication/contrib/yivi_oidc/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
+from import_export.admin import ImportExportModelAdmin
+
 from .models import AttributeGroup
+from .resources import AttributeGroupResource
 
 
 @admin.register(AttributeGroup)
-class AttributeGroupAdmin(admin.ModelAdmin):
+class AttributeGroupAdmin(ImportExportModelAdmin):
     fields = (
         "name",
         "uuid",
@@ -19,3 +22,6 @@ class AttributeGroupAdmin(admin.ModelAdmin):
         "attributes",
     )
     search_fields = ("uuid", "name")
+
+    # Import and export options:
+    resource_classes = (AttributeGroupResource,)
