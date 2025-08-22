fa9bc39e4a9499040feefa3baa70a760f2bd095b
Sergei Maertens <sergei@maykinmedia.nl>
2025-08-22 15:17:02 +0200

    :chart_with_upwards_trend: [#3999] Report appointment plugin usage counts

--- src/openforms/appointments/registry.py
+++ src/openforms/appointments/registry.py
@@ -1,9 +1,12 @@
+from __future__ import annotations
+
+from collections.abc import Iterable
 from typing import TYPE_CHECKING
 
 from openforms.plugins.registry import BaseRegistry
 
 if TYPE_CHECKING:
-    from .base import BasePlugin  # noqa: F401
+    from .base import BasePlugin
 
 
 class Registry(BaseRegistry["BasePlugin"]):
@@ -13,5 +16,17 @@ class Registry(BaseRegistry["BasePlugin"]):
 
     module = "appointments"
 
+    def report_plugin_usage(self) -> Iterable[tuple[BasePlugin, int]]:
+        from openforms.forms.models import Form
+
+        from .models import AppointmentsConfig
+
+        config = AppointmentsConfig.get_solo()
+        num_appointment_forms = Form.objects.live().filter(is_appointment=True).count()
+        for plugin in self:
+            in_use = num_appointment_forms > 0 and config.plugin == plugin.identifier
+            yield plugin, num_appointment_forms if in_use else 0
+
 
 register = Registry()
+register.set_as_metric_reporter()
