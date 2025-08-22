883ee9afac00dfd868ee1c61c1e1e47ef7edfc56
Sergei Maertens <sergei@maykinmedia.nl>
2025-08-22 16:39:44 +0200

    :chart_with_upwards_trend: [#3999] Report registration plugin usage

--- src/openforms/registrations/registry.py
+++ src/openforms/registrations/registry.py
@@ -1,3 +1,9 @@
+from __future__ import annotations
+
+from collections.abc import Iterable
+
+from django.db.models import Count, F
+
 from openforms.plugins.registry import BaseRegistry
 
 from .base import BasePlugin
@@ -16,7 +22,21 @@ class Registry(BaseRegistry[BasePlugin]):
                 "Please specify 'configuration_options' attribute for plugin class."
             )
 
+    def report_plugin_usage(self) -> Iterable[tuple[BasePlugin, int]]:
+        from openforms.forms.models import Form
+
+        qs = (
+            Form.objects.live()
+            .values(plugin=F("registration_backends__backend"))
+            .values("plugin")
+            .annotate(count=Count("*"))
+        )
+        usage_counts: dict[str, int] = {item["plugin"]: item["count"] for item in qs}
+        for plugin in self:
+            yield plugin, usage_counts.get(plugin.identifier, 0)
+
 
 # Sentinel to provide the default registry. You an easily instantiate another
 # :class:`Registry` object to use as dependency injection in tests.
 register = Registry()
+register.set_as_metric_reporter()
