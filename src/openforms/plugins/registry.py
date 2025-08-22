fe4abe888e7cca06e10cf71079e6779b4b0fac74
Sergei Maertens <sergei@maykinmedia.nl>
2025-08-22 15:04:14 +0200

    :building_construction: [#3999] Set up base mechanism for registers/plugins to report their usage counts
    This generic approach inverts control and defers to each specialized
    plugin registry to determine what 'usage' means and how to extract the
    requested statistics.

--- src/openforms/plugins/registry.py
+++ src/openforms/plugins/registry.py
@@ -1,16 +1,20 @@
 from __future__ import annotations
 
-from collections.abc import Callable, Iterator
+from collections.abc import Callable, Iterable, Iterator, MutableMapping
 from typing import TYPE_CHECKING
 
 from django.db import OperationalError
 
 from flags.state import flag_enabled
+from opentelemetry import metrics
 
 from .constants import UNIQUE_ID_MAX_LENGTH
 
 if TYPE_CHECKING:
     from .plugin import AbstractBasePlugin
+
+
+meter = metrics.get_meter("openforms.plugins")
 
 
 class BaseRegistry[PluginT: AbstractBasePlugin]:
@@ -88,3 +92,57 @@ class BaseRegistry[PluginT: AbstractBasePlugin]:
 
     def get_choices(self):
         return [(p.identifier, p.get_label()) for p in self.iter_enabled_plugins()]
+
+    def set_as_metric_reporter(self) -> None:
+        """
+        Mark the instance as the one that will produce metrics for telemetry.
+
+        Only one instance (the singleton, typically) can be used to report metrics
+        without affecting other registry instances created for testing.
+        """
+        assert self.module
+        if self.module in module_registers:
+            raise ValueError(
+                f"Registry {module_registers[self.module]!r} is already configured "
+                "as metrics producer."
+            )
+        module_registers[self.module] = self
+
+    def report_plugin_usage(self) -> Iterable[tuple[AbstractBasePlugin, int]]:
+        """
+        Introspect the registered plugins and report how often each one is used.
+
+        This is called by the plugin usage metric exporter to get insight in how often
+        plugins are used.
+
+        .. note:: This method will be invoked periodically in a background thread. Pay
+           attention to DB query optimization to minimize system load.
+        """
+        return ()
+
+
+module_registers: MutableMapping[str, BaseRegistry] = {}
+
+
+def record_plugin_usage(
+    options: metrics.CallbackOptions,
+) -> Iterator[metrics.Observation]:
+    for module, register in module_registers.items():
+        for plugin, times_used in register.report_plugin_usage():
+            yield metrics.Observation(
+                value=times_used,
+                attributes={
+                    "scope": "global",
+                    "plugin.module": module,
+                    "plugin.identifier": plugin.identifier,
+                    "plugin.is_enabled": plugin.is_enabled,
+                },
+            )
+
+
+meter.create_observable_gauge(
+    name="plugin_usage_count",
+    description="The usage counts of module plugins.",
+    unit="",  # no unit so that the _ratio suffix is not added
+    callbacks=[record_plugin_usage],
+)
