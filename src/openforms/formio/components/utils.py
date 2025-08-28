c96f65fc92447c9b0bfc87a0902e795e3f07e126
Anna Shamray <anna.shamray@maykinmedia.nl>
2025-08-28 17:04:20 +0200

    :bug: [#5557] replace regex pattern for filenames since it's not used for 'originalName' in formio with just soft hyphen removal
    :recycle: [#5557] sanitize "originalName" attribute
    
    Formio doesn't change the names of uploaded files. Django on the other hand does change them.
    When the file is being uploaded "temporary-file-upload" endpoint is used, which has
    MultiPartParser, which changes the file name in the 'sanitize_file_name' method,
    including the removal of soft-hyphens.
    Formio isn't aware of change, so its "originalName" attribute may differ from the file name
    stored in django. Here we apply the same changes to the "originalName" attribute
    
    Backport-of: #5560

--- src/openforms/formio/components/utils.py
+++ src/openforms/formio/components/utils.py
@@ -1,3 +1,5 @@
+import html
+
 from django.utils.crypto import salted_hmac
 
 
@@ -19,3 +21,16 @@ def salt_location_message(message_bits: dict[str, str]) -> str:
     computed_message = f"{message_bits['postcode']}/{message_bits['number']}/{message_bits['city']}/{message_bits['street_name']}"
     computed_hmac = salted_hmac("location_check", value=computed_message).hexdigest()
     return computed_hmac
+
+
+def sanitize_file_name(file_name: str) -> str:
+    """
+    copy of MultiPartParser.sanitize_file_name method
+    """
+    file_name = html.unescape(file_name)
+    file_name = file_name.rsplit("/")[-1]
+    file_name = file_name.rsplit("\\")[-1]
+    # Remove non-printable characters.
+    file_name = "".join([char for char in file_name if char.isprintable()])
+
+    return file_name
