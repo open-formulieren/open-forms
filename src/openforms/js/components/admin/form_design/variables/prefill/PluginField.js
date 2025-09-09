0b05a591586510da00b3f98dc2d924c2781987bc
Sergei Maertens <sergei@maykinmedia.nl>
2025-09-09 14:05:42 +0200

    :recycle: [#5419] Ensure the same prefill plugin warnings are displayed in various contexts
    The prefill configuration from components was already being checked and
    displayed on the form definition panel, but for similiar prefill config
    on user defined variables, no such warnings are displayed.
    
    This is even more relevant for the eidas prefill options, which allows
    us to now use the generic/default edit modal to select a plugin +
    attribute, removing the need for the custom configuration forms
    with their own warnings.

--- src/openforms/js/components/admin/form_design/variables/prefill/PluginField.js
+++ src/openforms/js/components/admin/form_design/variables/prefill/PluginField.js
@@ -1,15 +1,18 @@
 import {useField, useFormikContext} from 'formik';
 import {useContext} from 'react';
+import {FormattedMessage} from 'react-intl';
 import useUpdateEffect from 'react-use/esm/useUpdateEffect';
 
+import MessageList from 'components/admin/MessageList';
 import {FormContext} from 'components/admin/form_design/Context';
+import {getPluginWarnings} from 'components/admin/form_design/PluginWarning';
 import Select from 'components/admin/forms/Select';
 
 const PluginField = () => {
   const [fieldProps] = useField('plugin');
   const {setFieldValue} = useFormikContext();
   const {
-    plugins: {availablePrefillPlugins},
+    plugins: {availablePrefillPlugins, availableAuthPlugins, selectedAuthPlugins},
   } = useContext(FormContext);
 
   const {value} = fieldProps;
@@ -19,8 +22,50 @@ const PluginField = () => {
     setFieldValue('attribute', '');
   }, [setFieldValue, value]);
 
+  // check if we need to display any warnings
+  const prefillPlugin = availablePrefillPlugins.find(plugin => plugin.id === value);
+  const authWarnings = getPluginWarnings(prefillPlugin, availableAuthPlugins, selectedAuthPlugins);
+
+  const warnings = authWarnings.map(({type, ...rest}) => {
+    switch (type) {
+      case 'authAttribute': {
+        const {requiresAuth} = rest;
+        return (
+          <FormattedMessage
+            description="Prefill plugin requires unavailable auth attribute warning"
+            defaultMessage={`{plugin} requires one of the "{requiredAuthAttribute}" attributes.
+              Please select one or more authentication plugins that provide such an attribute.`}
+            values={{
+              plugin: prefillPlugin.label,
+              requiredAuthAttribute: requiresAuth.join(', '),
+            }}
+          />
+        );
+      }
+      case 'authPlugin': {
+        const {requiredPlugin} = rest;
+        return (
+          <FormattedMessage
+            description="Prefill plugin requires unavailable auth plugin warning"
+            defaultMessage={`{plugin} requires one of the "{requiredPlugin}"
+              login options. Please select one or remove the prefill plugin.`}
+            values={{
+              plugin: prefillPlugin.label,
+              requiredPlugin: requiredPlugin.join(', '),
+            }}
+          />
+        );
+      }
+    }
+  });
+
   const choices = availablePrefillPlugins.map(plugin => [plugin.id, plugin.label]);
-  return <Select allowBlank choices={choices} id="id_plugin" {...fieldProps} />;
+  return (
+    <div>
+      <Select allowBlank choices={choices} id="id_plugin" {...fieldProps} />
+      <MessageList messages={warnings.map(warning => ({level: 'warning', message: warning}))} />
+    </div>
+  );
 };
 
 export default PluginField;
