b3c964bed18f4bdc0002a6e5029ce045481fe6f7
Sergei Maertens <sergei@maykinmedia.nl>
2025-09-10 14:52:44 +0200

    :sparkles: [#5419] Implement performing the Yivi prefill
    The fixed/hardcoded attributes are treated separately with their own
    context applied, while the remaining attributes from attribute groups
    are sourced from the additional claims JSON data.

--- src/openforms/prefill/contrib/yivi/plugin.py
+++ src/openforms/prefill/contrib/yivi/plugin.py
@@ -1,15 +1,25 @@
+from collections.abc import Iterable
 from typing import TypedDict
 
 from django.utils.translation import gettext, gettext_lazy as _
+
+import structlog
+from glom import Coalesce, glom
 
 from openforms.authentication.constants import AuthAttribute
 from openforms.authentication.contrib.yivi_oidc.constants import (
     PLUGIN_ID as AUTH_PLUGIN_ID,
 )
-from openforms.typing import JSONObject
+from openforms.prefill.constants import IdentifierRoles
+from openforms.submissions.models import Submission
+from openforms.typing import JSONEncodable, JSONObject
 
 from ...base import BasePlugin
 from ...registry import register
+
+logger = structlog.stdlib.get_logger(__name__)
+
+MISSING = object()
 
 
 class YiviOptions(TypedDict):
@@ -23,6 +33,54 @@ class YiviPrefill(BasePlugin[YiviOptions]):
     requires_auth_plugin = (AUTH_PLUGIN_ID,)
     # do not expose the plugin to the 'prefill' tab of components
     for_components = ()
+
+    @staticmethod
+    def get_available_attributes() -> Iterable[tuple[str, str]]:
+        # Yivi attributes are dynamic because they depend on the selected attribute
+        # groups in the authentication plugin options. A custom UI component handles
+        # this.
+        return ()
+
+    @classmethod
+    def get_prefill_values(
+        cls,
+        submission: Submission,
+        attributes: list[str],
+        identifier_role: IdentifierRoles = IdentifierRoles.main,
+    ) -> dict[str, JSONEncodable]:
+        if identifier_role != IdentifierRoles.main:
+            return {}
+
+        glom_spec: dict[str, str | Coalesce] = {}
+
+        for attribute in attributes:
+            match attribute:
+                # generic identifier attribute -> always read it from the auth info
+                case "_internal.auth_info.value":
+                    glom_spec[attribute] = "value"
+                # only expose AuthAttribute.foo if that's what was actually provided
+                # during auth
+                case str(x) if x.startswith("_internal."):
+                    requested_attr = x.removeprefix("_internal.")
+                    if submission.auth_info.attribute == requested_attr:
+                        glom_spec[attribute] = "value"
+                # unknown fixed names -> look them up in the additional_claims JSON
+                # struct
+                case _:
+                    glom_spec[attribute] = Coalesce(
+                        f"additional_claims.{attribute}", default=MISSING
+                    )
+
+        values = glom(submission.auth_info, glom_spec)
+
+        # remove the `None` defaults. we deliberately don't log these missing keys, as
+        # the end-user decides if they expose the attribute(s) or not
+        for key in list(values.keys()):
+            if values[key] is not MISSING:
+                continue
+            del values[key]
+
+        return values
 
     @classmethod
     def configuration_context(cls) -> JSONObject | None:
--- src/openforms/prefill/contrib/yivi/tests/test_plugin.py
+++ src/openforms/prefill/contrib/yivi/tests/test_plugin.py
@@ -1,4 +1,11 @@
-from django.test import SimpleTestCase, override_settings
+from django.test import SimpleTestCase, TestCase, override_settings
+
+from openforms.authentication.constants import AuthAttribute
+from openforms.authentication.contrib.yivi_oidc.constants import (
+    PLUGIN_ID as AUTH_PLUGIN_ID,
+)
+from openforms.prefill.constants import IdentifierRoles
+from openforms.submissions.tests.factories import SubmissionFactory
 
 from ..plugin import YiviPrefill
 
@@ -35,3 +42,175 @@ class ConfigurationTests(SimpleTestCase):
             ],
         }
         self.assertEqual(configuration_context, expected)
+
+    def test_no_static_attributes_available(self):
+        attributes = YiviPrefill.get_available_attributes()
+
+        self.assertEqual(attributes, ())
+
+
+class YiviPluginTests(TestCase):
+    maxDiff = None
+
+    def test_populate_fixed_attribute_identifier(self):
+        plugin = YiviPrefill("yivi")
+
+        for auth_attr in (AuthAttribute.bsn, AuthAttribute.kvk, AuthAttribute.pseudo):
+            with self.subTest(auth_info__attribute=auth_attr):
+                submission = SubmissionFactory.create(
+                    auth_info__value="anything",
+                    auth_info__plugin=AUTH_PLUGIN_ID,
+                    auth_info__attribute=auth_attr,
+                )
+
+                values = plugin.get_prefill_values(
+                    submission, attributes=["_internal.auth_info.value"]
+                )
+
+                self.assertEqual(
+                    values,
+                    {"_internal.auth_info.value": "anything"},
+                )
+
+    def test_populate_fixed_bsn_attribute(self):
+        plugin = YiviPrefill("yivi")
+
+        with self.subTest("with matching auth attribute provided"):
+            submission = SubmissionFactory.create(
+                auth_info__value="anything",
+                auth_info__plugin=AUTH_PLUGIN_ID,
+                auth_info__attribute=AuthAttribute.bsn,
+            )
+
+            values = plugin.get_prefill_values(
+                submission, attributes=[f"_internal.{AuthAttribute.bsn}"]
+            )
+
+            self.assertEqual(values, {"_internal.bsn": "anything"})
+
+        with self.subTest("without matching auth attribute provided"):
+            submission = SubmissionFactory.create(
+                auth_info__value="anything",
+                auth_info__plugin=AUTH_PLUGIN_ID,
+                auth_info__attribute=AuthAttribute.pseudo,
+            )
+
+            values = plugin.get_prefill_values(
+                submission, attributes=[f"_internal.{AuthAttribute.bsn}"]
+            )
+
+            self.assertEqual(values, {})
+
+    def test_populate_fixed_kvk_attribute(self):
+        plugin = YiviPrefill("yivi")
+
+        with self.subTest("with matching auth attribute provided"):
+            submission = SubmissionFactory.create(
+                auth_info__value="anything",
+                auth_info__plugin=AUTH_PLUGIN_ID,
+                auth_info__attribute=AuthAttribute.kvk,
+            )
+
+            values = plugin.get_prefill_values(
+                submission, attributes=[f"_internal.{AuthAttribute.kvk}"]
+            )
+
+            self.assertEqual(values, {"_internal.kvk": "anything"})
+
+        with self.subTest("without matching auth attribute provided"):
+            submission = SubmissionFactory.create(
+                auth_info__value="anything",
+                auth_info__plugin=AUTH_PLUGIN_ID,
+                auth_info__attribute=AuthAttribute.pseudo,
+            )
+
+            values = plugin.get_prefill_values(
+                submission, attributes=[f"_internal.{AuthAttribute.kvk}"]
+            )
+
+            self.assertEqual(values, {})
+
+    def test_populate_fixed_pseudo_attribute(self):
+        plugin = YiviPrefill("yivi")
+
+        with self.subTest("with matching auth attribute provided"):
+            submission = SubmissionFactory.create(
+                auth_info__value="anything",
+                auth_info__plugin=AUTH_PLUGIN_ID,
+                auth_info__attribute=AuthAttribute.pseudo,
+            )
+
+            values = plugin.get_prefill_values(
+                submission, attributes=[f"_internal.{AuthAttribute.pseudo}"]
+            )
+
+            self.assertEqual(values, {"_internal.pseudo": "anything"})
+
+        with self.subTest("without matching auth attribute provided"):
+            submission = SubmissionFactory.create(
+                auth_info__value="anything",
+                auth_info__plugin=AUTH_PLUGIN_ID,
+                auth_info__attribute=AuthAttribute.bsn,
+            )
+
+            values = plugin.get_prefill_values(
+                submission, attributes=[f"_internal.{AuthAttribute.pseudo}"]
+            )
+
+            self.assertEqual(values, {})
+
+    def test_arbitrary_attributes_taken_from_additional_claims(self):
+        plugin = YiviPrefill("yivi")
+
+        submission = SubmissionFactory.create(
+            auth_info__value="anything",
+            auth_info__plugin=AUTH_PLUGIN_ID,
+            auth_info__attribute=AuthAttribute.bsn,
+            auth_info__additional_claims={
+                "irma": {"gemeente": {"personalData": {"firstName": "Bob"}}}
+            },
+        )
+
+        values = plugin.get_prefill_values(
+            submission,
+            attributes=[
+                f"_internal.{AuthAttribute.bsn}",
+                "irma.gemeente.personalData.firstName",
+                "irma.gemeente.personalData.__missing__",
+            ],
+        )
+
+        self.assertEqual(
+            values,
+            {
+                "_internal.bsn": "anything",
+                "irma.gemeente.personalData.firstName": "Bob",
+            },
+        )
+
+    def test_only_support_main_identifier_role(self):
+        """
+        Yivi authentication/prefill does not support mandates.
+        """
+        plugin = YiviPrefill("yivi")
+
+        submission = SubmissionFactory.create(
+            auth_info__value="anything",
+            auth_info__plugin=AUTH_PLUGIN_ID,
+            auth_info__attribute=AuthAttribute.bsn,
+            auth_info__additional_claims={
+                "irma": {"gemeente": {"personalData": {"firstName": "Bob"}}}
+            },
+        )
+
+        values = plugin.get_prefill_values(
+            submission,
+            attributes=[
+                f"_internal.{AuthAttribute.bsn}",
+                "irma.gemeente.personalData.firstName",
+                "irma.gemeente.personalData.__missing__",
+            ],
+            identifier_role=IdentifierRoles.authorizee,
+        )
+
+        self.assertEqual(values, {})
