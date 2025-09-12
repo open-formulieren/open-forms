b0d81deb46426379f767444853702721109d1762
Sonny Bakker <sonny@maykinmedia.nl>
2025-09-12 12:57:24 +0200

    [#4879] add payment migration tests

--- src/openforms/forms/tests/admin/test_views.py
+++ src/openforms/forms/tests/admin/test_views.py
@@ -6,6 +6,7 @@ from zipfile import ZipFile
 from django.core.files import File
 from django.test import TestCase, override_settings
 from django.urls import reverse
+from django.utils.translation import gettext as _
 
 from django_webtest import WebTest
 from maykin_2fa.test import disable_admin_mfa
@@ -19,6 +20,13 @@ from openforms.accounts.tests.factories import (
 from openforms.forms.admin.tasks import process_forms_export
 from openforms.forms.models.form import FormsExport
 from openforms.forms.tests.factories import FormFactory
+from openforms.payments.contrib.ogone.constants import OgoneEndpoints
+from openforms.payments.contrib.ogone.tests.factories import OgoneMerchantFactory
+from openforms.payments.contrib.worldline.constants import WorldlineEndpoints
+from openforms.payments.contrib.worldline.models import WorldlineMerchant
+from openforms.payments.contrib.worldline.tests.factories import (
+    WorldlineMerchantFactory,
+)
 from openforms.utils.urls import build_absolute_uri
 
 
@@ -304,4 +312,277 @@ class TestImportView(WebTest):
         self.assertEqual(
             "Form successfully imported!",
             messages[0].message,
+        )
+
+
+@disable_admin_mfa()
+class TestMigrationForm(WebTest):
+    csrf_checks = False
+
+    def test_single_form(self):
+        user = SuperUserFactory.create()
+        ogone_merchant = OgoneMerchantFactory(
+            label="Merchant Y", pspid="merchant-y", endpoint_preset=OgoneEndpoints.test
+        )
+        form_to_migrate = FormFactory(
+            name="Ogone form 1",
+            payment_backend="ogone-legacy",
+            payment_backend_options={"merchant_id": ogone_merchant.pk},
+        )
+
+        response = self.app.post(
+            reverse("admin:forms_payment_migration"),
+            {"forms_to_migrate": [form_to_migrate.pk]},
+            user=user,
+        )
+
+        self.assertRedirects(response, reverse("admin:forms_form_changelist"))
+
+        worldline_merchant = WorldlineMerchant.objects.get()
+
+        self.assertEqual(worldline_merchant.label, "Merchant Y")
+        self.assertEqual(worldline_merchant.pspid, "merchant-y")
+        self.assertEqual(worldline_merchant.endpoint, WorldlineEndpoints.test)
+
+        form_to_migrate.refresh_from_db()
+
+        self.assertEqual(form_to_migrate.payment_backend, "worldline")
+        self.assertEqual(
+            form_to_migrate.payment_backend_options, {"merchant": "merchant-y"}
+        )
+
+    def test_multiple_forms(self):
+        user = SuperUserFactory.create()
+        ogone_merchant_1 = OgoneMerchantFactory(
+            label="Merchant Y", pspid="merchant-y", endpoint_preset=OgoneEndpoints.test
+        )
+        form_to_migrate_1 = FormFactory(
+            name="Ogone form 1",
+            payment_backend="ogone-legacy",
+            payment_backend_options={"merchant_id": ogone_merchant_1.pk},
+        )
+        ogone_merchant_2 = OgoneMerchantFactory(
+            label="Merchant Z", pspid="merchant-z", endpoint_preset=OgoneEndpoints.live
+        )
+        form_to_migrate_2 = FormFactory(
+            name="Ogone form 2",
+            payment_backend="ogone-legacy",
+            payment_backend_options={"merchant_id": ogone_merchant_2.pk},
+        )
+
+        response = self.app.post(
+            reverse("admin:forms_payment_migration"),
+            {"forms_to_migrate": [form_to_migrate_1.pk, form_to_migrate_2.pk]},
+            user=user,
+        )
+
+        self.assertRedirects(response, reverse("admin:forms_form_changelist"))
+
+        worldline_merchants = WorldlineMerchant.objects.order_by("pspid")
+
+        self.assertEqual(worldline_merchants[0].label, "Merchant Y")
+        self.assertEqual(worldline_merchants[0].pspid, "merchant-y")
+        self.assertEqual(worldline_merchants[0].endpoint, WorldlineEndpoints.test)
+        self.assertEqual(worldline_merchants[1].label, "Merchant Z")
+        self.assertEqual(worldline_merchants[1].pspid, "merchant-z")
+        self.assertEqual(worldline_merchants[1].endpoint, WorldlineEndpoints.live)
+
+        form_to_migrate_1.refresh_from_db()
+        form_to_migrate_2.refresh_from_db()
+
+        self.assertEqual(form_to_migrate_1.payment_backend, "worldline")
+        self.assertEqual(
+            form_to_migrate_1.payment_backend_options, {"merchant": "merchant-y"}
+        )
+        self.assertEqual(form_to_migrate_2.payment_backend, "worldline")
+        self.assertEqual(
+            form_to_migrate_2.payment_backend_options, {"merchant": "merchant-z"}
+        )
+
+    def test_reuse_merchants(self):
+        user = SuperUserFactory.create()
+        ogone_merchant_1 = OgoneMerchantFactory(
+            label="Merchant Y", pspid="merchant-y", endpoint_preset=OgoneEndpoints.test
+        )
+        form_to_migrate_1 = FormFactory(
+            name="Ogone form 1",
+            payment_backend="ogone-legacy",
+            payment_backend_options={"merchant_id": ogone_merchant_1.pk},
+        )
+        ogone_merchant_2 = OgoneMerchantFactory(
+            label="Merchant Z", pspid="merchant-z", endpoint_preset=OgoneEndpoints.live
+        )
+        form_to_migrate_2 = FormFactory(
+            name="Ogone form 2",
+            payment_backend="ogone-legacy",
+            payment_backend_options={"merchant_id": ogone_merchant_2.pk},
+        )
+        worldline_merchant = WorldlineMerchantFactory(
+            label="Merchant A",
+            pspid="merchant-z",
+            endpoint=WorldlineEndpoints.test,
+        )
+
+        response = self.app.post(
+            reverse("admin:forms_payment_migration"),
+            {"forms_to_migrate": [form_to_migrate_1.pk, form_to_migrate_2.pk]},
+            user=user,
+        )
+
+        self.assertRedirects(response, reverse("admin:forms_form_changelist"))
+
+        new_merchant = WorldlineMerchant.objects.order_by("pspid").first()
+        worldline_merchant.refresh_from_db()
+
+        self.assertEqual(new_merchant.label, "Merchant Y")
+        self.assertEqual(new_merchant.pspid, "merchant-y")
+        self.assertEqual(new_merchant.endpoint, WorldlineEndpoints.test)
+        self.assertEqual(worldline_merchant.label, "Merchant Z")
+        self.assertEqual(worldline_merchant.pspid, "merchant-z")
+        self.assertEqual(worldline_merchant.endpoint, WorldlineEndpoints.live)
+
+        form_to_migrate_1.refresh_from_db()
+        form_to_migrate_2.refresh_from_db()
+
+        self.assertEqual(form_to_migrate_1.payment_backend, "worldline")
+        self.assertEqual(
+            form_to_migrate_1.payment_backend_options, {"merchant": "merchant-y"}
+        )
+        self.assertEqual(form_to_migrate_2.payment_backend, "worldline")
+        self.assertEqual(
+            form_to_migrate_2.payment_backend_options, {"merchant": "merchant-z"}
+        )
+
+    def test_non_existent_merchant(self):
+        user = SuperUserFactory.create()
+        ogone_merchant_1 = OgoneMerchantFactory(
+            label="Merchant Y", pspid="merchant-y", endpoint_preset=OgoneEndpoints.test
+        )
+        form_to_migrate_1 = FormFactory(
+            name="Ogone form 1",
+            payment_backend="ogone-legacy",
+            payment_backend_options={"merchant_id": ogone_merchant_1.pk},
+        )
+        form_to_migrate_2 = FormFactory(
+            name="Ogone form 2",
+            payment_backend="ogone-legacy",
+            payment_backend_options={"merchant_id": 1000000000},
+        )
+
+        response = self.app.post(
+            reverse("admin:forms_payment_migration"),
+            {"forms_to_migrate": [form_to_migrate_1.pk, form_to_migrate_2.pk]},
+            user=user,
+        )
+
+        self.assertContains(response, _("Please select a new merchant for these forms"))
+
+        self.assertEqual(WorldlineMerchant.objects.count(), 0)
+
+        form_to_migrate_1.refresh_from_db()
+        form_to_migrate_2.refresh_from_db()
+
+        self.assertEqual(form_to_migrate_1.payment_backend, "ogone-legacy")
+        self.assertEqual(
+            form_to_migrate_1.payment_backend_options,
+            {"merchant_id": ogone_merchant_1.pk},
+        )
+        self.assertEqual(form_to_migrate_2.payment_backend, "ogone-legacy")
+        self.assertEqual(
+            form_to_migrate_2.payment_backend_options,
+            {"merchant_id": 1000000000},
+        )
+
+    def test_migrate_credentials(self):
+        user = SuperUserFactory.create()
+        ogone_merchant_1 = OgoneMerchantFactory(
+            label="Merchant Y",
+            pspid="merchant-y",
+            endpoint_preset=OgoneEndpoints.test,
+            api_key="api-key-1",
+            api_secret="api-secret-1",
+        )
+        form_to_migrate_1 = FormFactory(
+            name="Ogone form 1",
+            payment_backend="ogone-legacy",
+            payment_backend_options={"merchant_id": ogone_merchant_1.pk},
+        )
+        ogone_merchant_2 = OgoneMerchantFactory(
+            label="Merchant Z",
+            pspid="merchant-z",
+            endpoint_preset=OgoneEndpoints.live,
+            api_key="api-key-2",
+            api_secret="api-secret-2",
+        )
+        form_to_migrate_2 = FormFactory(
+            name="Ogone form 2",
+            payment_backend="ogone-legacy",
+            payment_backend_options={"merchant_id": ogone_merchant_2.pk},
+        )
+        worldline_merchant = WorldlineMerchantFactory(
+            label="Merchant A",
+            pspid="merchant-z",
+            endpoint=WorldlineEndpoints.test,
+            api_key="existing-key",
+            api_secret="existing-secret",
+        )
+
+        response = self.app.post(
+            reverse("admin:forms_payment_migration"),
+            {"forms_to_migrate": [form_to_migrate_1.pk, form_to_migrate_2.pk]},
+            user=user,
+        )
+
+        self.assertRedirects(response, reverse("admin:forms_form_changelist"))
+
+        new_merchant = WorldlineMerchant.objects.order_by("pspid").first()
+        worldline_merchant.refresh_from_db()
+
+        self.assertEqual(new_merchant.api_key, "api-key-1")
+        self.assertEqual(new_merchant.api_secret, "api-secret-1")
+        self.assertEqual(worldline_merchant.label, "Merchant Z")
+        self.assertEqual(worldline_merchant.api_key, "api-key-2")
+        self.assertEqual(worldline_merchant.api_secret, "api-secret-2")
+
+    def test_migrate_com_attribute(self):
+        user = SuperUserFactory.create()
+        ogone_merchant_1 = OgoneMerchantFactory(
+            label="Merchant Y", pspid="merchant-y", endpoint_preset=OgoneEndpoints.test
+        )
+        form_to_migrate_1 = FormFactory(
+            name="Ogone form 1",
+            payment_backend="ogone-legacy",
+            payment_backend_options={
+                "merchant_id": ogone_merchant_1.pk,
+                "com_template": "{{ foo }}",
+            },
+        )
+        ogone_merchant_2 = OgoneMerchantFactory(
+            label="Merchant Z", pspid="merchant-z", endpoint_preset=OgoneEndpoints.live
+        )
+        form_to_migrate_2 = FormFactory(
+            name="Ogone form 2",
+            payment_backend="ogone-legacy",
+            payment_backend_options={"merchant_id": ogone_merchant_2.pk},
+        )
+
+        response = self.app.post(
+            reverse("admin:forms_payment_migration"),
+            {"forms_to_migrate": [form_to_migrate_1.pk, form_to_migrate_2.pk]},
+            user=user,
+        )
+
+        self.assertRedirects(response, reverse("admin:forms_form_changelist"))
+
+        form_to_migrate_1.refresh_from_db()
+        form_to_migrate_2.refresh_from_db()
+
+        self.assertEqual(form_to_migrate_1.payment_backend, "worldline")
+        self.assertEqual(
+            form_to_migrate_1.payment_backend_options,
+            {"merchant": "merchant-y", "descriptor_template": "{{ foo }}"},
+        )
+        self.assertEqual(form_to_migrate_2.payment_backend, "worldline")
+        self.assertEqual(
+            form_to_migrate_2.payment_backend_options, {"merchant": "merchant-z"}
         )
