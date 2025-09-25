41cea1f5e11d4e6119b1071e9e04c8e2921d7026
Sergei Maertens <sergei@maykinmedia.nl>
2025-09-25 10:46:29 +0200

    :globe_with_meridians: [#5325] Localize children/parterns component builder preview

--- src/openforms/js/components/formio_builder/templates/children.js
+++ src/openforms/js/components/formio_builder/templates/children.js
@@ -1,12 +1,12 @@
 const TEMPLATE = `
 <div ref="children">
-  <label for="bsn">BSN:</label>
+  <label for="bsn">{{ ctx.t('BSN') }}</label>
   <input type="text" name="bsn" id="bsn" disabled class="form-control"/>
 
-  <label for="first-names">Firstnames:</label>
+  <label for="first-names">{{ ctx.t('Firstnames') }}</label>
   <input type="text" name="first-names" id="first-names" disabled class="form-control"/>
 
-  <label for="date-of-birth">Date of birth:</label>
+  <label for="date-of-birth">{{ ctx.t('Date of birth') }}</label>
   <input type="text" name="date-of-birth" id="date-of-birth" disabled class="form-control"/>
 </div>
 `;
