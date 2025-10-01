acc632d0933ce6e022b1b21226e6ae2be7c32c36
Anna Shamray <anna.shamray@maykinmedia.nl>
2025-10-01 17:21:29 +0200

    :lipstick: [#5546] move 'print page' button to the footer

--- src/openforms/js/public.js
+++ src/openforms/js/public.js
@@ -5,7 +5,7 @@ import {onLoaded} from 'utils/dom';
 
 // connect the "Print this page" link element to a handler
 const registerPrintHandler = () => {
-  const nodes = document.querySelectorAll('.a11y-toolbar__window-print-action');
+  const nodes = document.querySelectorAll('.utrecht-link__window-print-action');
   nodes.forEach(node => {
     node.addEventListener('click', event => {
       event.preventDefault();
