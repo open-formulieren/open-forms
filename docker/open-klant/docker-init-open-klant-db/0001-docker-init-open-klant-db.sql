ff4b39c83e2f118439eeb3bc30f3963d98a95088
annashamray <anna.shamray@maykinmedia.nl>
2025-10-27 09:58:20 +0100

    :whale: [#5707] add docker config for Open Klant

---
+++ docker/open-klant/docker-init-open-klant-db/0001-docker-init-open-klant-db.sql
@@ -0,0 +1,5 @@
+CREATE USER openklant;
+CREATE DATABASE openklant;
+GRANT ALL PRIVILEGES ON DATABASE openklant TO openklant;
+GRANT ALL ON SCHEMA public TO openklant;
+ALTER DATABASE openklant OWNER TO openklant;
