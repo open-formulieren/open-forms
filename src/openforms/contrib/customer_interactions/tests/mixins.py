072b7f89c72938c1cc9bb5ceb527ca655046ba56
Sergei Maertens <sergei@maykinmedia.nl>
2025-12-23 16:48:16 +0100

    :bento: [#5830] Re-record the Open Klant VCR cassettes

--- src/openforms/contrib/customer_interactions/tests/mixins.py
+++ src/openforms/contrib/customer_interactions/tests/mixins.py
@@ -18,6 +18,7 @@ class CustomerInteractionsMixin:
     def assertAddressPresent(self, results, expected_address: ExpectedDigitalAddress):
         adres = expected_address["adres"]
         found_address = next((da for da in results if da["adres"] == adres), None)
+        assert found_address is not None
         for property, expected_value in expected_address.items():
             self.assertEqual(
                 expected_value, found_address[property], f"for address {adres}"
