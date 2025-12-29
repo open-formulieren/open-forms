12349ae62dbe5eb7725417911a80aee611d8d551
annashamray <anna.shamray@maykinmedia.nl>
2025-12-29 16:45:26 +0100

    :test_tube: [#5872] add regression test for Communication preferences prefill if the profile variable is not found

---
+++ src/openforms/prefill/contrib/customer_interactions/tests/test_options_validation.py
@@ -0,0 +1,45 @@
+from django.test import TestCase, tag
+
+from openforms.contrib.customer_interactions.tests.factories import (
+    CustomerInteractionsAPIGroupConfigFactory,
+)
+from openforms.forms.tests.factories import FormFactory
+
+from ..config import CommunicationPreferencesSerializer
+
+
+class CommunicationPreferencesOptionsTests(TestCase):
+    @tag("gh-5872")
+    def test_prefill_options_incorrect_profile_variable(self):
+        customer_interactions_config = (
+            CustomerInteractionsAPIGroupConfigFactory.create()
+        )
+        form = FormFactory.create(
+            generate_minimal_setup=True,
+            formstep__form_definition__configuration={
+                "components": [
+                    {
+                        "key": "profileNew",
+                        "type": "customerProfile",
+                        "label": "Profile",
+                        "digitalAddressTypes": ["email"],
+                        "shouldUpdateCustomerData": True,
+                    }
+                ],
+            },
+        )
+        data = {
+            "customer_interactions_api_group": customer_interactions_config.identifier,
+            "profile_form_variable": "profileOld",
+        }
+        serializer = CommunicationPreferencesSerializer(
+            data=data, context={"form": form}
+        )
+
+        is_valid = serializer.is_valid()
+
+        self.assertFalse(is_valid)
+
+        error = serializer.errors["profile_form_variable"][0]
+        self.assertEqual(error.code, "invalid")
+        self.assertEqual(error, "Form variable 'profileOld' is not found.")
