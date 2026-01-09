cccb10944ab0b5ccf1b644d611b543962a5d2995
Sergei Maertens <sergei@maykinmedia.nl>
2026-01-09 17:57:21 +0100

    :alembic: [#5287] Experiment with beat monitoring based on signals
    The core job of beat is to schedule tasks to the broker,
    for workers to pick up. We can verify this behaviour by
    confirming via the signals that a task is scheduled
    regularly.
    
    Failure to publish tasks can have multiple root causes -
    it may be because the broker is down, or because beat
    itself stopped working somehow.
    
    Beat does not have timers or an event loop like workers,
    so tapping into the bootstreps is not an option. This
    may lead to beat restarts that do not solve the issue,
    if the broker is down, but that't the tradeoff we need
    to work with. And, if the broker is down, you have
    bigger issues anyway...

--- src/openforms/celery/probes.py
+++ src/openforms/celery/probes.py
@@ -1,9 +1,14 @@
+import atexit
 from pathlib import Path
 
 from django.conf import settings
 
+import structlog
 from celery import bootsteps
-from celery.signals import worker_ready, worker_shutdown
+from celery.beat import Service as BeatService
+from celery.signals import after_task_publish, beat_init, worker_ready, worker_shutdown
+
+logger = structlog.stdlib.get_logger()
 
 HEARTBEAT_FILE = Path(settings.BASE_DIR) / "tmp" / "celery_worker_heartbeat"
 READINESS_FILE = Path(settings.BASE_DIR) / "tmp" / "celery_worker_ready"
@@ -42,3 +47,40 @@ def on_worker_ready(**_):
 @worker_shutdown.connect
 def on_worker_shutdown(**_):
     READINESS_FILE.unlink(missing_ok=True)
+
+
+#
+# Utilities for checking the health of celery beat
+#
+
+_RUNNING_IN_BEAT = False
+
+BEAT_LIVENESS_FILE = Path(settings.BASE_DIR) / "tmp" / "celery_beat_live"
+
+
+@beat_init.connect(dispatch_uid="probes.on_beat_init")
+def on_beat_init(*, sender: BeatService, **kwargs):
+    global _RUNNING_IN_BEAT
+    _RUNNING_IN_BEAT = True
+    logger.debug("beat_process_marked")
+    # on shutdown, clear up the liveness file
+    atexit.register(BEAT_LIVENESS_FILE.unlink, missing_ok=True)
+
+
+@after_task_publish.connect(dispatch_uid="probes.on_beat_task_published")
+def on_beat_task_published(*, sender: str, routing_key: str, **kwargs):
+    """
+    Update the celery beat liveness every time a task is successfully published.
+
+    ``after_task_publish`` fires in the process that sent the task, so we must discern
+    between the regular Django app that schedules tasks, and celery beat that also
+    schedules tasks. We do this by tapping into the ``beat_init`` signal to mark the
+    process as a beat process, and only touch the liveness file when running in beat.
+    """
+    if not _RUNNING_IN_BEAT:
+        return
+
+    logger.debug("beat_task_published", task=sender, routing_key=routing_key)
+    # touching the file updates the last modified timestamp, which can be checked by
+    # the health-check command
+    BEAT_LIVENESS_FILE.touch()
