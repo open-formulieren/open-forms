e52ae242ca58a6651108637103e8206308d2d49b
Sonny Bakker <sonny@maykinmedia.nl>
2026-01-20 12:10:29 +0100

    Fix task

--- src/openforms/products/tasks.py
+++ src/openforms/products/tasks.py
@@ -4,6 +4,8 @@ from ..contrib.open_producten.client import get_open_producten_client
 from ..contrib.open_producten.types import ActuelePrijsItem
 from .models import PriceOption, Product
 
+from openforms.celery import app
+
 logger = structlog.stdlib.get_logger(__name__)
 
 
@@ -16,19 +18,18 @@ def update_product_prices(product: Product, current_price_item: ActuelePrijsItem
     # Update/create price options based on current price item
     updated_price_option_uuids = []
     for current_price_option in current_price_item.actuele_prijs.prijsopties:
-        price_option = PriceOption.objects.get(
-            product=product, price_option_uuid=current_price_option.uuid
+        price_option, created = PriceOption.objects.get_or_create(
+            product=product,
+            price_option_uuid=current_price_option.uuid,
+            defaults={
+                "price": current_price_option.bedrag,
+                "name": current_price_option.beschrijving,
+            },
         )
-        if price_option is not None:
+        if not created:
             price_option.price = current_price_option.bedrag
             price_option.save()
-        else:
-            PriceOption.objects.create(
-                product=product,
-                price_option_uuid=current_price_option.uuid,
-                price=current_price_option.bedrag,
-                name=current_price_option.beschrijving,
-            )
+
         updated_price_option_uuids.append(current_price_option.uuid)
 
     # Remove stale price options
@@ -37,13 +38,13 @@ def update_product_prices(product: Product, current_price_item: ActuelePrijsItem
     ).delete()
 
 
-# @app.task(ignore_result=True)
+@app.task(ignore_result=True)
 def update_products():
     """Activate all the forms that should be activated by the specific date and time."""
     with get_open_producten_client() as client:
         client_product_types = client.get_product_types()
         for product_type in client_product_types:
-            created, product = Product.objects.get_or_create(
+            product, created = Product.objects.get_or_create(
                 product_type_uuid=str(product_type.uuid),
                 defaults={
                     "name": product_type.naam,
@@ -52,10 +53,10 @@ def update_products():
             )
             current_price = client.get_current_price(product_type.uuid)
 
-            # if product:
-            #     logger.info("update_product_type", product_type_uuid=product_type.uuid)
-            #     product.name = product_type.naam
-            #     product.information = product_type.samenvatting
+            if not created:
+                logger.info("update_product_type", product_type_uuid=product_type.uuid)
+                product.name = product_type.naam
+                # TODO: product.information = product_type.naam
+                product.save()
 
-            # update_product_prices(product, current_price)
-            # product.save()
+            update_product_prices(product, current_price)
