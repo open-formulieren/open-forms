b0dd72616d0e6b60451a6b02df22390401590512
vasileios <zigras00@gmail.com>
2026-01-28 16:07:06 +0100

    [#5820] Added endpoint for retrieving the customized translations

--- src/openforms/translations/api/views.py
+++ src/openforms/translations/api/views.py
@@ -1,10 +1,11 @@
+import json
 from pathlib import Path
 
 from django.conf import settings
 from django.http import FileResponse
 from django.utils.translation import activate, get_language, gettext_lazy as _
 
-from drf_spectacular.plumbing import build_object_type
+from drf_spectacular.plumbing import build_array_type, build_object_type
 from drf_spectacular.utils import OpenApiExample, OpenApiParameter, extend_schema
 from rest_framework import permissions, status
 from rest_framework.exceptions import NotFound
@@ -16,8 +17,13 @@ from openforms.api.drf_spectacular.functional import lazy_enum
 from openforms.api.serializers import ExceptionSerializer, ValidationErrorSerializer
 from openforms.api.views.mixins import SerializerContextMixin
 from openforms.translations.utils import set_language_cookie
+from openforms.typing import JSONObject
 
-from ..utils import get_language_codes, get_supported_languages
+from ..models import TranslationsMetaData
+from ..utils import (
+    get_language_codes,
+    get_supported_languages,
+)
 from .serializers import LanguageCodeSerializer, LanguageInfoSerializer
 
 
@@ -78,7 +84,7 @@ class SetLanguageView(SerializerContextMixin, APIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        language_code = serializer.data["code"]
+        language_code = serializer.data["code"]  # pyright: ignore[reportCallIssue,reportArgumentType]
         activate(language_code)
         response = Response(status=status.HTTP_204_NO_CONTENT)
         set_language_cookie(response, language_code)
@@ -95,7 +101,7 @@ class SetLanguageView(SerializerContextMixin, APIView):
             location=OpenApiParameter.PATH,
             description=_("Language code to retrieve the messages for."),
             type=str,
-            enum=lazy_enum(get_language_codes),
+            enum=lazy_enum(get_language_codes),  # pyright: ignore[reportArgumentType]
         ),
     ],
     responses={
@@ -126,3 +132,84 @@ class FormioTranslationsView(APIView):
             / f"{language}.json"
         )
         return FileResponse(filepath.open("rb"))
+
+
+@extend_schema(
+    summary=_("Get customized (compiled) translations"),
+    description=_(
+        "Retrieve the new customized (compiled) translations after changes have been "
+        "applied. This is done based on the language code in the path."
+    ),
+    tags=["translations"],
+    parameters=[
+        OpenApiParameter(
+            name="language_code",
+            location=OpenApiParameter.PATH,
+            description=_("Language code to retrieve the messages for."),
+            type=str,
+            enum=lazy_enum(get_language_codes),  # pyright: ignore[reportArgumentType]
+        ),
+    ],
+    responses={
+        ("200", "application/json"): build_object_type(
+            additionalProperties=build_array_type(
+                build_object_type(additionalProperties=True)
+            ),
+            description=_("Compiled FormatJS messages keyed by translation id"),
+        ),
+        "404": ExceptionSerializer,
+        "5XX": ExceptionSerializer,
+    },
+    examples=[
+        OpenApiExample(
+            "Compiled FormatJS example",
+            value={
+                "skjd8uh": [{"type": 0, "value": "A translated text"}],
+                "abc123": [
+                    {
+                        "type": 6,
+                        "options": {
+                            "one": [{"type": 0, "value": "1 item"}],
+                            "other": [{"type": 0, "value": "{count} items"}],
+                        },
+                    }
+                ],
+            },
+        )
+    ],
+)
+class CustomizedCompiledTranslations(APIView):
+    authentication_classes = ()
+    permission_classes = (permissions.AllowAny,)
+
+    def _get_custom_translation_data(self, language_code: str) -> JSONObject | None:
+        # filter the objects by the received language code and the most recent and
+        # successfully processed upload
+        translations_metadata = (
+            TranslationsMetaData.objects.filter(
+                language_code=language_code,
+                last_updated__isnull=False,
+            )
+            .order_by("-last_updated")
+            .first()
+        )
+        if not translations_metadata:
+            return None
+
+        with open(translations_metadata.compiled_asset.path, encoding="utf-8") as f:
+            # no further checks at this point since the file has been already checked concerning
+            # JSON format during the processing/save
+            compiled_json: str = f.read()
+            return json.loads(compiled_json)
+
+    def get(self, request, language_code: str):
+        _valid_codes = get_language_codes()
+        if language_code not in _valid_codes:
+            raise NotFound(
+                _("The language code {language_code} is not supported.").format(
+                    language_code=language_code
+                )
+            )
+
+        custom_translations = self._get_custom_translation_data(language_code)
+        return Response(custom_translations)
--- src/openforms/translations/tests/test_api.py
+++ src/openforms/translations/tests/test_api.py
@@ -1,9 +1,15 @@
+import json
+
 from django.conf import settings
 from django.test import override_settings
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
+from rest_framework import status
 from rest_framework.test import APITestCase
+
+from ..constants import StatusChoices
+from .factories import TranslationsMetaDataFactory
 
 
 @override_settings(
@@ -57,7 +63,7 @@ class I18NAPITests(APITestCase):
             url, data={"code": "en"}, HTTP_ACCEPT_LANGUAGE="nl-NL, nl;q=0.9, en;q=0.8"
         )
 
-        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(response.content, b"")
         # assert current has changed
         current = self.client.get(
@@ -72,8 +78,40 @@ class I18NAPITests(APITestCase):
         # ትግርኛ is not available
         response = self.client.put(url, data={"code": "ti"})
 
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         error = response.json()
         self.assertEqual(error["code"], "invalid")
         self.assertEqual(error["invalidParams"][0]["code"], "invalid_choice")
         self.assertEqual(error["invalidParams"][0]["name"], "code")
+
+
+class CustomizedCompiledTranslationsViewTests(APITestCase):
+    def test_view_returns_compiled_file_json_data(self):
+        translations_metadata = TranslationsMetaDataFactory.create(
+            with_compiled_asset=True
+        )
+        file_content = json.load(translations_metadata.compiled_asset.file)
+
+        endpoint = reverse("api:i18n:customized-translations", args=["en"])
+        response = self.client.get(endpoint)
+
+        self.assertEqual(response.json(), file_content)
+
+    def test_view_when_language_unsupported(self):
+        endpoint = reverse("api:i18n:customized-translations", args=["gr"])
+        response = self.client.get(endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_view_with_not_compiled_asset(self):
+        translations_metadata = TranslationsMetaDataFactory.create()
+
+        # make sure no compiled asset is present
+        self.assertEqual(translations_metadata.processing_status, StatusChoices.pending)
+        self.assertIsNone(translations_metadata.last_updated)
+
+        endpoint = reverse("api:i18n:customized-translations", args=["en"])
+        response = self.client.get(endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsNone(response.data)  # pyright: ignore [reportAttributeAccessIssue]
