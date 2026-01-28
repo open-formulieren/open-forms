141b6f2c63fa7fbddbc5d59f7bb31210a8fd0c58
vasileios <zigras00@gmail.com>
2026-01-28 09:20:45 +0100

    [#5820] Added celery task for processing custom translations input file, triggered by the admin page

--- src/openforms/translations/admin.py
+++ src/openforms/translations/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
+from django.db import transaction
+from django.forms import ModelForm
+from django.http import HttpRequest
 from django.templatetags.static import static
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 
+from privates.admin import PrivateMediaMixin
+
 from .models import TranslationsMetaData
+from .tasks import process_custom_translation_assets
 
 
 @admin.register(TranslationsMetaData)
-class TranslationsMetaDataAdmin(admin.ModelAdmin):
+class TranslationsMetaDataAdmin(PrivateMediaMixin, admin.ModelAdmin):
     list_display = (
         "language_code",
         "processing_status",
@@ -52,6 +58,8 @@ class TranslationsMetaDataAdmin(admin.ModelAdmin):
         "messages_count",
         "app_release",
     )
+    private_media_fields = ("compiled_asset",)
+    private_media_view_options = {"attachment": True}
 
     @admin.display(description=_("Default messages"))
     def default_source_messages_link(self, obj: TranslationsMetaData) -> str:
@@ -65,3 +73,15 @@ class TranslationsMetaDataAdmin(admin.ModelAdmin):
             url,
             _("Download"),
         )
+
+    def save_model(
+        self,
+        request: HttpRequest,
+        obj: TranslationsMetaData,
+        form: ModelForm,
+        change: bool,
+    ) -> None:
+        super().save_model(request, obj, form, change)
+
+        if "messages_file" in form.changed_data:
+            transaction.on_commit(lambda: process_custom_translation_assets(obj.pk))
