d7d99e6f2428b180d08112a70bae8546d7221119
Sergei Maertens <sergei@maykinmedia.nl>
2026-02-20 17:04:53 +0100

    :bookmark: Bump version to 3.3.12 and update changelog

--- README.rst
+++ README.rst
@@ -2,7 +2,7 @@
 Open Forms
 ==========
 
-:Version: 3.3.11
+:Version: 3.3.12
 :Source: https://github.com/open-formulieren/open-forms
 :Keywords: e-Formulieren, Common Ground, FormIO, API
 
--- src/openforms/__init__.py
+++ src/openforms/__init__.py
@@ -1,6 +1,6 @@
 from .celery import app as celery_app
 
 __all__ = ("celery_app",)
-__version__ = "3.3.11"
+__version__ = "3.3.12"
 __author__ = "Maykin Media"
 __homepage__ = "https://github.com/open-formulieren/open-forms"
