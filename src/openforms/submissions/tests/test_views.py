932910bdccc1a57f913d7c4d0c87c5c2f0220954
Sergei Maertens <sergei@maykinmedia.nl>
2026-03-03 12:01:09 +0100

    :white_check_mark: [open-formulieren/security-issues#40] Add/update tests for cosign flow
    * Add explicit access control tests for the OTP-part of the flow
    * Update lookup tests now that the frontend-redirect part is no longer
      relevant

--- src/openforms/submissions/tests/test_views.py
+++ src/openforms/submissions/tests/test_views.py
@@ -5,17 +5,20 @@ from django.test import override_settings, tag
 from django.urls import reverse
 from django.utils.translation import gettext as _
 
+from digid_eherkenning.choices import DigiDAssuranceLevels
 from django_webtest import WebTest
 
 from openforms.authentication.constants import FORM_AUTH_SESSION_KEY
 from openforms.authentication.contrib.digid.constants import DIGID_DEFAULT_LOA
+from openforms.forms.tests.factories import FormFactory
 from openforms.frontend.tests import FrontendRedirectMixin
 
 from ..constants import COSIGN_VERIFICATION_SESSION_KEY
+from ..models import CosignOTP
 from .factories import SubmissionFactory
 
 
-class SearchSubmissionForCosignViewTests(FrontendRedirectMixin, WebTest):
+class SearchSubmissionForCosignViewTests(WebTest):
     def setUp(self) -> None:
         super().setUp()
 
@@ -75,16 +78,6 @@ class SearchSubmissionForCosignViewTests(FrontendRedirectMixin, WebTest):
         # expect OTP email
         self.assertEqual(len(mail.outbox), 1)
 
-        # self.assertRedirectsToFrontend(
-        #     submission_response,
-        #     frontend_base_url="http://url-to-form.nl/",
-        #     action="cosign",
-        #     action_params={
-        #         "submission_uuid": str(submission.uuid),
-        #     },
-        #     fetch_redirect_response=False,
-        # )
-
     def test_successfully_resolve_code_from_GET_params(self):
         submission = SubmissionFactory.from_components(
             form__authentication_backend="digid",
@@ -132,16 +125,6 @@ class SearchSubmissionForCosignViewTests(FrontendRedirectMixin, WebTest):
         )
         # expect OTP email
         self.assertEqual(len(mail.outbox), 1)
-
-        # self.assertRedirectsToFrontend(
-        #     response,
-        #     frontend_base_url="http://url-to-form.nl/",
-        #     action="cosign",
-        #     action_params={
-        #         "submission_uuid": str(submission.uuid),
-        #     },
-        #     fetch_redirect_response=False,
-        # )
 
     def test_user_no_auth_details_in_session(self):
         SubmissionFactory.from_components(
@@ -526,4 +509,363 @@ class SearchSubmissionForCosignViewTests(FrontendRedirectMixin, WebTest):
         response2 = response.click(description=_("Go back"))
 
         self.assertEqual(response2.status_code, 200)
+        self.assertNotIn(COSIGN_VERIFICATION_SESSION_KEY, self.app.session)
+
+
+class CosignOTPViewTests(FrontendRedirectMixin, WebTest):
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.addCleanup(cache.clear)
+
+        # Needed so that when we get self.app.session we get a session object and not a dict
+        self.app.get("/", status=403)
+
+    def test_cannot_access_view_without_being_authenticated(self):
+        SubmissionFactory.from_components(
+            components_list=[
+                {
+                    "key": "cosign",
+                    "type": "cosign",
+                    "label": "Cosign component",
+                    "validate": {"required": True},
+                    "authPlugin": "digid",
+                },
+            ],
+            submitted_data={"cosign": "test@test.nl"},
+            completed=True,
+            cosign_complete=False,
+            form__slug="form-to-cosign",
+            form__authentication_backend="digid",
+            form__authentication_backend_options={"loa": DIGID_DEFAULT_LOA},
+            form_url="http://url-to-form.nl/startpagina",
+        )
+
+        self.app.get(
+            reverse(
+                "submissions:otp-for-cosign", kwargs={"form_slug": "form-to-cosign"}
+            ),
+            status=403,
+        )
+
+    def test_cannot_access_view_with_incorrect_auth_plugin(self):
+        SubmissionFactory.from_components(
+            components_list=[
+                {
+                    "key": "cosign",
+                    "type": "cosign",
+                    "label": "Cosign component",
+                    "validate": {"required": True},
+                    "authPlugin": "digid",
+                },
+            ],
+            submitted_data={"cosign": "test@test.nl"},
+            completed=True,
+            cosign_complete=False,
+            form__slug="form-to-cosign",
+            form__authentication_backend="digid",
+            form__authentication_backend_options={"loa": DIGID_DEFAULT_LOA},
+            form_url="http://url-to-form.nl/startpagina",
+        )
+        session = self.app.session
+        assert isinstance(session, SessionBase)
+        session[FORM_AUTH_SESSION_KEY] = {
+            "plugin": "not-digid",
+            "attribute": "bsn",
+            "value": "123456782",
+        }
+        session.save()
+
+        self.app.get(
+            reverse(
+                "submissions:otp-for-cosign", kwargs={"form_slug": "form-to-cosign"}
+            ),
+            status=403,
+        )
+
+    def test_cannot_access_view_with_insufficient_loa_auth_plugin(self):
+        SubmissionFactory.from_components(
+            components_list=[
+                {
+                    "key": "cosign",
+                    "type": "cosign",
+                    "label": "Cosign component",
+                    "validate": {"required": True},
+                    "authPlugin": "digid",
+                },
+            ],
+            submitted_data={"cosign": "test@test.nl"},
+            completed=True,
+            cosign_complete=False,
+            form__slug="form-to-cosign",
+            form__authentication_backend="digid",
+            form__authentication_backend_options={"loa": DIGID_DEFAULT_LOA},
+            form_url="http://url-to-form.nl/startpagina",
+        )
+        session = self.app.session
+        assert isinstance(session, SessionBase)
+        session[FORM_AUTH_SESSION_KEY] = {
+            "plugin": "digid",
+            "attribute": "bsn",
+            "value": "123456782",
+            "loa": DIGID_DEFAULT_LOA,
+        }
+        session.save()
+
+        self.app.get(
+            reverse(
+                "submissions:otp-for-cosign", kwargs={"form_slug": "form-to-cosign"}
+            ),
+            status=403,
+        )
+
+    def test_wrong_form_slug_results_in_404(self):
+        SubmissionFactory.from_components(
+            components_list=[
+                {
+                    "key": "cosign",
+                    "type": "cosign",
+                    "label": "Cosign component",
+                    "validate": {"required": True},
+                },
+            ],
+            submitted_data={"cosign": "test@test.nl"},
+            completed=True,
+            cosign_complete=False,
+            form__slug="form-to-cosign",
+            form__authentication_backend="digid",
+            form__authentication_backend_options={"loa": DIGID_DEFAULT_LOA},
+            form_url="http://url-to-form.nl/startpagina",
+        )
+        session = self.app.session
+        assert isinstance(session, SessionBase)
+        session[FORM_AUTH_SESSION_KEY] = {
+            "plugin": "digid",
+            "attribute": "bsn",
+            "value": "123456782",
+            "loa": DIGID_DEFAULT_LOA,
+        }
+        session.save()
+
+        self.app.get(
+            reverse("submissions:otp-for-cosign", kwargs={"form_slug": "wrong-slug"}),
+            status=404,
+        )
+
+    def test_requires_submission_reference_in_session(self):
+        SubmissionFactory.from_components(
+            components_list=[
+                {
+                    "key": "cosign",
+                    "type": "cosign",
+                    "label": "Cosign component",
+                    "validate": {"required": True},
+                },
+            ],
+            submitted_data={"cosign": "test@test.nl"},
+            completed=True,
+            cosign_complete=False,
+            form__slug="form-to-cosign",
+            form__authentication_backend="digid",
+            form__authentication_backend_options={"loa": DigiDAssuranceLevels.high},
+            form_url="http://url-to-form.nl/startpagina",
+        )
+        session = self.app.session
+        assert isinstance(session, SessionBase)
+        session[FORM_AUTH_SESSION_KEY] = {
+            "plugin": "digid",
+            "attribute": "bsn",
+            "value": "123456782",
+            "loa": DigiDAssuranceLevels.base,
+        }
+        assert COSIGN_VERIFICATION_SESSION_KEY not in session
+        session.save()
+
+        self.app.get(
+            reverse(
+                "submissions:otp-for-cosign", kwargs={"form_slug": "form-to-cosign"}
+            ),
+            status=403,
+        )
+
+    def test_requires_submission_form_matches_form_slug_url_kwarg(self):
+        submission = SubmissionFactory.from_components(
+            components_list=[
+                {
+                    "key": "cosign",
+                    "type": "cosign",
+                    "label": "Cosign component",
+                    "validate": {"required": True},
+                },
+            ],
+            submitted_data={"cosign": "test@test.nl"},
+            completed=True,
+            cosign_complete=False,
+            form__slug="form-to-cosign",
+            form__authentication_backend="digid",
+            form__authentication_backend_options={"loa": DIGID_DEFAULT_LOA},
+            form_url="http://url-to-form.nl/startpagina",
+        )
+        FormFactory.create(
+            slug="other-form",
+            authentication_backend="digid",
+            authentication_backend_options={"loa": DIGID_DEFAULT_LOA},
+        )
+        session = self.app.session
+        assert isinstance(session, SessionBase)
+        session[FORM_AUTH_SESSION_KEY] = {
+            "plugin": "digid",
+            "attribute": "bsn",
+            "value": "123456782",
+            "loa": DIGID_DEFAULT_LOA,
+        }
+        session[COSIGN_VERIFICATION_SESSION_KEY] = submission.pk
+        session.save()
+
+        self.app.get(
+            reverse("submissions:otp-for-cosign", kwargs={"form_slug": "other-form"}),
+            status=404,
+        )
+
+    def test_requires_submission_waiting_for_cosign(self):
+        submission = SubmissionFactory.from_components(
+            components_list=[
+                {
+                    "key": "cosign",
+                    "type": "cosign",
+                    "label": "Cosign component",
+                    "validate": {"required": True},
+                },
+            ],
+            submitted_data={"cosign": "test@test.nl"},
+            form__slug="form-to-cosign",
+            form__authentication_backend="digid",
+            form__authentication_backend_options={"loa": DIGID_DEFAULT_LOA},
+            form_url="http://url-to-form.nl/startpagina",
+            cosigned=True,
+            co_sign_data__plugin="digid",
+        )
+        assert not submission.cosign_state.is_waiting
+        session = self.app.session
+        assert isinstance(session, SessionBase)
+        session[FORM_AUTH_SESSION_KEY] = {
+            "plugin": "digid",
+            "attribute": "bsn",
+            "value": "123456782",
+            "loa": DIGID_DEFAULT_LOA,
+        }
+        session[COSIGN_VERIFICATION_SESSION_KEY] = submission.pk
+        session.save()
+
+        self.app.get(
+            reverse(
+                "submissions:otp-for-cosign", kwargs={"form_slug": "form-to-cosign"}
+            ),
+            status=403,
+        )
+
+    @tag("security-40", "GHSA-2g49-rfm6-5qj5")
+    def test_rate_limiting_on_cosign_otp(self):
+        submission = SubmissionFactory.from_components(
+            form__authentication_backend="digid",
+            form__authentication_backend_options={"loa": DIGID_DEFAULT_LOA},
+            components_list=[
+                {
+                    "key": "cosign",
+                    "type": "cosign",
+                    "label": "Cosign component",
+                    "validate": {"required": True},
+                },
+            ],
+            submitted_data={"cosign": "test@test.nl"},
+            completed=True,
+            cosign_complete=False,
+            form__slug="form-to-cosign",
+            form_url="http://url-to-form.nl/startpagina",
+            public_registration_reference="OF-IMAREFERENCE",
+        )
+        session = self.client.session
+        assert isinstance(session, SessionBase)
+        session[FORM_AUTH_SESSION_KEY] = {
+            "plugin": "digid",
+            "attribute": "bsn",
+            "value": "123456782",
+            "loa": DIGID_DEFAULT_LOA,
+        }
+        session[COSIGN_VERIFICATION_SESSION_KEY] = submission.pk
+        session.save()
+        url = reverse(
+            "submissions:otp-for-cosign", kwargs={"form_slug": "form-to-cosign"}
+        )
+
+        num_attempts = 0
+
+        def _attempt_otp_bruteforce():
+            nonlocal num_attempts
+            num_attempts += 1
+            return self.client.post(url, data={"otp": f" 000-{num_attempts:0>3} "})
+
+        # do 30 requests in a short time - this exceeds the configured rate limit
+        last_response = _attempt_otp_bruteforce()
+        for __ in range(30):
+            last_response = _attempt_otp_bruteforce()
+
+        self.assertEqual(last_response.status_code, 429)
+
+    def test_successfully_start_cosign_process(self):
+        """
+        Assert that the entire flow leads to a redirect to the frontend.
+        """
+        submission = SubmissionFactory.from_components(
+            form__authentication_backend="digid",
+            form__authentication_backend_options={"loa": DIGID_DEFAULT_LOA},
+            components_list=[
+                {
+                    "key": "cosign",
+                    "type": "cosign",
+                    "label": "Cosign component",
+                    "validate": {"required": True},
+                },
+            ],
+            submitted_data={"cosign": "test@test.nl"},
+            completed=True,
+            cosign_complete=False,
+            form__slug="form-to-cosign",
+            form_url="http://url-to-form.nl/startpagina",
+            public_registration_reference="OF-IMAREFERENCE",
+        )
+        session = self.app.session
+        assert isinstance(session, SessionBase)
+        session[FORM_AUTH_SESSION_KEY] = {
+            "plugin": "digid",
+            "attribute": "bsn",
+            "value": "123456782",
+            "loa": DIGID_DEFAULT_LOA,
+        }
+        session.save()
+
+        # start with lookup by code -> navigate to OTP page
+        otp_response = self.app.get(
+            reverse(
+                "submissions:find-submission-for-cosign",
+                kwargs={"form_slug": "form-to-cosign"},
+            ),
+            {"code": submission.public_registration_reference},
+        ).follow()
+        # a OTP record must be created, look it up to grab the code
+        otp_code = CosignOTP.objects.get().verification_code
+
+        form = otp_response.forms[0]
+        form["otp"] = otp_code
+        submission_response = form.submit()
+
+        self.assertRedirectsToFrontend(
+            submission_response,
+            frontend_base_url="http://url-to-form.nl/",
+            action="cosign",
+            action_params={
+                "submission_uuid": str(submission.uuid),
+            },
+            fetch_redirect_response=False,
+        )
         self.assertNotIn(COSIGN_VERIFICATION_SESSION_KEY, self.app.session)
