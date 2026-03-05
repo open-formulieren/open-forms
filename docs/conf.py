dc5d092bbf656afce1ccc0ac36180a370d9b27e0
Sergei Maertens <sergei@maykinmedia.nl>
2026-03-05 11:15:00 +0100

    :green_heart: Ignore storybook links in link check
    They time out frequently despite being available. Probably there's some
    cold starts/cold caches that cause this, nothing we can do about it.

--- docs/conf.py
+++ docs/conf.py
@@ -129,6 +129,7 @@ linkcheck_ignore = [
     r"https://www\.gnu\.org/software/gettext/",
     r"https://opentelemetry\.io/docs/.*",  # times out frequently, even with 30s timeouts
     r"https://(www\.)?npmjs\.com.*",  # IP/UA blocking...
+    r"https://storybook\.js\.org.*",  # frequently times out despite being available
 ]
 
 linkcheck_anchors_ignore_for_url = [
