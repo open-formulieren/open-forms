6ddf4bea388c7ff729a14decc997beb5544cfd79
Viktor van Wijk <viktorvanwijk@hotmail.com>
2026-03-31 16:06:31 +0200

    :bug: [#6140] Fix crash with conditional logic inside a fieldset inside an editgrid
    If a component is inside a fieldset inside an editgrid, the trigger component key inside the conditional could be "editgrid.trigger",
    because a fieldset does not hold any data. Therefore, we also need to include nested components to the component map of the
    configuration wrapper.
    
    Note that the renderer creates a custom component map and uses this to process the visibility of its children. This would be the
    ideal solution here as well, instead of adding it to the overall configuration wrapper, but I'm afraid this will take too much
    time. There is another post-it on the wall that addresses problems with the configuration, and I think this is one of them.
    
    Backport-of: #6145

--- src/openforms/submissions/tests/form_logic/test_conditional_logic.py
+++ src/openforms/submissions/tests/form_logic/test_conditional_logic.py
@@ -757,3 +757,78 @@ class ConditionalLogicTests(TestCase):
             },
         )
         self.assertEqual(step.unsaved_data, {})
+
+    @tag("gh-6140")
+    def test_fieldset_inside_editgrid(self):
+        """
+        Ensure that items inside a fieldset inside an editgrid are properly handled.
+        """
+        submission = SubmissionFactory.from_components(
+            components_list=[
+                {
+                    "type": "editgrid",
+                    "key": "editgrid",
+                    "label": "Edit grid",
+                    "components": [
+                        {
+                            "type": "fieldset",
+                            "key": "fieldset",
+                            "label": "Fieldset",
+                            "components": [
+                                {
+                                    "type": "selectboxes",
+                                    "key": "trigger",
+                                    "label": "Trigger",
+                                    "values": [
+                                        {"label": "foo", "value": "foo"},
+                                        {"label": "bar", "value": "bar"},
+                                    ],
+                                },
+                                {
+                                    "type": "textfield",
+                                    "key": "follower",
+                                    "label": "Follower",
+                                    "clearOnHide": True,
+                                    "conditional": {
+                                        "show": True,
+                                        "when": "editgrid.trigger",
+                                        "eq": "foo",
+                                    },
+                                },
+                            ],
+                        },
+                    ],
+                }
+            ],
+            form__new_renderer_enabled=True,
+        )
+        step = submission.submissionstep_set.get()
+
+        evaluate_form_logic(
+            submission,
+            step,
+            FormioData(
+                {
+                    "editgrid": [
+                        {"trigger": {"foo": True, "bar": False}, "follower": "keep me"},
+                        {
+                            "trigger": {"foo": False, "bar": False},
+                            "follower": "clear me",
+                        },
+                    ]
+                }
+            ),
+        )
+
+        state = submission.variables_state
+        data = state.get_data(include_static_variables=False, include_unsaved=True)
+        self.assertEqual(
+            data,
+            {
+                "editgrid": [
+                    {"trigger": {"foo": True, "bar": False}, "follower": "keep me"},
+                    {"trigger": {"foo": False, "bar": False}, "follower": ""},
+                ]
+            },
+        )
+        self.assertEqual(step.unsaved_data, {})
