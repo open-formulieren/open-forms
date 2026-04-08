646e9de31f4abcbbee3d3efcbac8f9ed963ce5af
vasileios <zigras00@gmail.com>
2026-04-08 18:53:34 +0200

    [#6106] Updated backend according to the new name of the form type field

--- src/openforms/forms/admin/mixins.py
+++ src/openforms/forms/admin/mixins.py
@@ -54,7 +54,7 @@ class FormioConfigMixin:
                     {"label": label, "value": value}
                     for value, label in VertrouwelijkheidsAanduidingen.choices
                 ],
-                "form_mode": "default",
+                "form_type": "regular",
             }
         )
 
