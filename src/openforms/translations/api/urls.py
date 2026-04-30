5c719b672f76617e6a052e20dbc95b1283b89a2b
Sergei Maertens <sergei@maykinmedia.nl>
2026-04-30 12:48:32 +0200

    :fire: [#6164] Delete obsoleted formio translation endpoints
    Our new renderer does not use these assets but instead has all the
    translations backed in through react-intl.
    
    The frontend code for the backend includes the translations assets
    directly rather than reading them from an API endpoint, so the API
    endpoints are safe to delete.

--- src/openforms/translations/api/urls.py
+++ src/openforms/translations/api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from .views import (
     CustomizedCompiledTranslations,
-    FormioTranslationsView,
     LanguageInfoView,
     SetLanguageView,
 )
@@ -12,11 +11,6 @@ app_name = "i18n"
 urlpatterns = [
     path("info", LanguageInfoView.as_view(), name="info"),
     path("language", SetLanguageView.as_view(), name="language"),
-    path(
-        "formio/<str:language>",
-        FormioTranslationsView.as_view(),
-        name="formio-translations",
-    ),
     path(
         "compiled-messages/<str:language_code>.json",
         CustomizedCompiledTranslations.as_view(),
