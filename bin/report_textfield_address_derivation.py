cc9d866a3f8b39f3b59e10bf88b20ed36a09fbfa
Sergei Maertens <sergei@maykinmedia.nl>
2026-05-01 13:39:12 +0200

    :monocle_face: [#6239] Analyze logic usage of address fields

--- bin/report_textfield_address_derivation.py
+++ bin/report_textfield_address_derivation.py
@@ -21,7 +21,9 @@ def report_address_derivation_usage():
     Address derivation in textfields in the new renderer is not supported. Users must
     convert their forms to use addressNL which now handles this feature.
     """
+    from openforms.forms.constants import LogicActionTypes
     from openforms.forms.models import Form, FormDefinition
+    from openforms.utils.json_logic import introspect_json_logic
 
     # Ignore form definitions that aren't used in any form
     form_definitions = (
@@ -32,6 +34,7 @@ def report_address_derivation_usage():
 
     for form_definition in form_definitions.iterator():
         components: list[str] = []
+        component_keys: set[str] = set()
 
         for component in form_definition.configuration_wrapper:
             if component["type"] != "textfield":
@@ -58,6 +61,9 @@ def report_address_derivation_usage():
                         else ""
                     )
                     components.append(f"{identification}{registration}")
+                    component_keys.add(component["key"])
+                    component_keys.add(house_number_ref)
+                    component_keys.add(postcode_ref)
                 case _:
                     continue
 
@@ -77,13 +83,69 @@ def report_address_derivation_usage():
                 return ""
             return f", registration: {backends}"
 
+        def _get_logic_usage(form: Form) -> str:
+            for rule in form.formlogic_set.all():
+                trigger = introspect_json_logic(rule.json_logic_trigger)
+                has_usage = component_keys.intersection(  # noqa: B023
+                    {input_var.key for input_var in trigger.get_input_keys()}
+                )
+                if has_usage:
+                    return ", used in logic trigger"
+
+                for action in rule.actions:
+                    match action:
+                        case {
+                            "action": {"type": LogicActionTypes.property},
+                            "component": str(key),
+                        } if key in component_keys:  # noqa: B023
+                            return ", used in logic action (property)"
+                        case {
+                            "action": {"type": LogicActionTypes.variable},
+                            "variable": str(key),
+                        }:
+                            if key in component_keys:  # noqa: B023
+                                return ", used in logic action (variable)"
+
+                            value = introspect_json_logic(action["action"]["value"])
+                            _has_usage = component_keys.intersection(  # noqa: B023
+                                {input_var.key for input_var in value.get_input_keys()}
+                            )
+                            if _has_usage:
+                                return (
+                                    ", used in logic action (variable value expression)"
+                                )
+                        case {"action": {"type": LogicActionTypes.evaluate_dmn}}:
+                            dmn_config = action["action"]["config"]
+                            mappings = (
+                                dmn_config["input_mapping"]
+                                + dmn_config["output_mapping"]
+                            )
+
+                            if component_keys.intersection(  # noqa: B023
+                                {mapping["form_variable"] for mapping in mappings}
+                            ):
+                                return ", used in logic action (evaluate_dmn)"
+
+                        case {
+                            "action": {
+                                "type": LogicActionTypes.disable_next
+                                | LogicActionTypes.step_not_applicable
+                                | LogicActionTypes.step_applicable
+                                | LogicActionTypes.synchronize_variables
+                                | LogicActionTypes.fetch_from_service
+                                | LogicActionTypes.set_registration_backend
+                            }
+                        }:
+                            pass
+            return ""
+
         problems.append(
             (
                 form_definition.admin_name,
                 form_definition.pk,
                 "\n".join(components),
                 "\n".join(
-                    f"{form.admin_name} (ID: {form.pk}{_get_form_registration_backends(form)})"
+                    f"{form.admin_name} (ID: {form.pk}{_get_form_registration_backends(form)}{_get_logic_usage(form)})"
                     for form in used_in_forms
                 ),
             )
