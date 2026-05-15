835c6b501e43d36dca2ef88e18d34a1bc6fa65dd
Viktor van Wijk <viktorvanwijk@hotmail.com>
2026-05-15 14:09:49 +0200

    :wrench: [#6164] Add upgrade check for 4.0
    We also want to include the invalid form logic script as an additional constraint.
    
    I have revised the script to block the upgrade if there are forms (with logic rules) that have not been converted
    to the new logic evaluation. Note that these will also include forms with cycles, as a conversion is illegal if
    cycles are present
    
    4.0.0 has not been released officially, but we are tricking it to run the upgrade check already :)

--- bin/report_invalid_form_logic.py
+++ bin/report_invalid_form_logic.py
@@ -2,12 +2,9 @@
 from __future__ import annotations
 
 import sys
-from collections.abc import Iterator
 from pathlib import Path
-from traceback import format_exc
 
 import django
-from django.db import transaction
 
 from tabulate import tabulate
 
@@ -21,8 +18,10 @@ def report_invalid_forms() -> bool:
     """
     Query forms that have new logic evaluation enabled but inconsistent logic rule
     configuration.
+
+    In 4.0, this script will be used as an upgrade check, which means we can also
+    report forms that are not yet converted to new logic evalution at all.
     """
-    from openforms.forms.logic_analysis import CyclesDetected
     from openforms.forms.models import Form
 
     invalid_rules_detected = False
@@ -45,52 +44,28 @@ def report_invalid_forms() -> bool:
             )
         )
 
-    # check each individual form for logic rule cycles. Run this in a transaction and
-    # roll back, because apply_logic_analysis() saves the result to the DB.
+    # check if any forms still have new logic evaluation disabled
     forms_to_check = Form.objects.filter(
         _is_deleted=False,
         new_logic_evaluation_enabled=False,
         formlogic__isnull=False,
     ).distinct()
 
-    transaction.set_autocommit(False)
-    cycles_detected = False
-
-    def _detect_cycles() -> Iterator[tuple[int, str, bool, str]]:
-        nonlocal cycles_detected
-
-        for form in forms_to_check.iterator():
-            form.new_logic_evaluation_enabled = True
-            try:
-                form.apply_logic_analysis()
-            except CyclesDetected as exc:
-                variables = {var for cycle in exc.cycles for var in cycle.variables}
-                cycles_detected = True
-                yield (form.pk, form.admin_name, form.active, ", ".join(variables))
-            except Exception:
-                print(
-                    f"Unexpected error in form {form.admin_name} ({form.pk}): "
-                    f"{format_exc()}"
-                )
-
-    try:
+    non_converted_forms_detected = False
+    if forms_to_check.exists():
+        non_converted_forms_detected = True
+        print(
+            "The following forms have have not been converted to the new logic "
+            "evaluation. Please review and change them to upgrade to Open Forms 4.0."
+        )
         print(
             tabulate(
-                _detect_cycles(),
-                headers=("Form ID", "Form name", "Active", "Variables in cycles"),
+                ((form.pk, form.admin_name) for form in forms_to_check.iterator()),
+                headers=("Form ID", "Form name"),
             )
         )
-    finally:
-        transaction.rollback()
 
-    if cycles_detected:
-        print(
-            "\nThe table above lists all the forms where cycles in logic rules were "
-            "detected. Please review and fix them as soon as possible. Open Forms 4.0 "
-            "will require this to be resolved before you can upgrade."
-        )
-
-    return not invalid_rules_detected and not cycles_detected
+    return not invalid_rules_detected and not non_converted_forms_detected
 
 
 def main(skip_setup=False) -> bool:
