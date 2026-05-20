40449a8b6e02c852d68451f3f7c71306859d3e8b
Sergei Maertens <sergei@maykinmedia.nl>
2026-05-20 18:23:07 +0200

    :white_check_mark: [#4939] Test desired outcome of data migration(s)

--- src/openforms/forms/tests/test_migrations.py
+++ src/openforms/forms/tests/test_migrations.py
@@ -1,5 +1,8 @@
 from django_test_migrations.contrib.unittest_case import MigratorTestCase
 
+from openforms.registrations.contrib.objects_api.constants import (
+    PLUGIN_IDENTIFIER as OBJECTS_API_PLUGIN_IDENTIFIER,
+)
 from openforms.variables.constants import FormVariableDataTypes, FormVariableSources
 
 from ..constants import FormTypeChoices
@@ -571,3 +574,133 @@ class AddFormTypeFieldReverseMigrationTests(MigratorTestCase):
         self.assertTrue(forms[0].is_appointment)
         self.assertFalse(forms[1].is_appointment)
         self.assertFalse(forms[2].is_appointment)
+
+
+class MoveObjectsAPIGroupConfigurationToBackendTests(MigratorTestCase):
+    migrate_from = [
+        ("objects_api", "0006_alter_objectsapigroupconfig_catalogue_domain"),
+        ("forms", "0129_remove_form_new_renderer_enabled"),
+    ]
+    migrate_to = (
+        "forms",
+        "0131_move_api_group_configuration_to_backends",
+    )
+
+    def prepare(self):
+        # uses real model, but that shouldn't matter
+
+        apps = self.old_state.apps
+        Service = apps.get_model("zgw_consumers", "Service")
+        ObjectsAPIGroupConfig = apps.get_model("objects_api", "ObjectsAPIGroupConfig")
+        Form = apps.get_model("forms", "Form")
+        FormRegistrationBackend = apps.get_model("forms", "FormRegistrationBackend")
+
+        # create API groups
+        dummy_service = Service.objects.create(
+            slug="dummy", api_type="orc", api_root="https://example.com/api/v1/"
+        )
+        group_1 = ObjectsAPIGroupConfig.objects.create(
+            name="Ignore - no catalogue set up",
+            identifier="group_1",
+            objects_service=dummy_service,
+            objecttypes_service=dummy_service,
+            drc_service=dummy_service,
+            catalogi_service=dummy_service,
+            catalogue_domain="",
+            catalogue_rsin="",
+        )
+        group_2 = ObjectsAPIGroupConfig.objects.create(
+            name="Copy over",
+            identifier="group_2",
+            objects_service=dummy_service,
+            objecttypes_service=dummy_service,
+            drc_service=dummy_service,
+            catalogi_service=dummy_service,
+            catalogue_domain="ABCDE",
+            catalogue_rsin="000111222",
+            iot_submission_report="PDF",
+            iot_submission_csv="",
+            iot_attachment="",
+        )
+
+        form = Form.objects.create(name="Test")
+        FormRegistrationBackend.objects.create(
+            form=form,
+            key="group-to-ignore",
+            name="Should be ignored because of the group",
+            backend=OBJECTS_API_PLUGIN_IDENTIFIER,
+            # incomplete according to the types/serializer, but shouldn't break the
+            # migration!
+            options={
+                "objects_api_group": group_1.identifier,
+            },
+        )
+        FormRegistrationBackend.objects.create(
+            form=form,
+            key="group-to-update",
+            name="Should be updated and gain all the defaults",
+            backend=OBJECTS_API_PLUGIN_IDENTIFIER,
+            # incomplete according to the types/serializer, but shouldn't break the
+            # migration!
+            options={
+                "objects_api_group": group_2.identifier,
+            },
+        )
+        FormRegistrationBackend.objects.create(
+            form=form,
+            key="has-local-config-already",
+            name="May not be updated despite group qualifying",
+            backend=OBJECTS_API_PLUGIN_IDENTIFIER,
+            # incomplete according to the types/serializer, but shouldn't break the
+            # migration!
+            options={
+                "objects_api_group": group_2.identifier,
+                "catalogue": {
+                    "domain": "notouch",
+                    "rsin": "notouch",
+                },
+            },
+        )
+
+    def test_migration_processes_backends_correctly(self):
+        FormRegistrationBackend = self.new_state.apps.get_model(
+            "forms", "FormRegistrationBackend"
+        )
+
+        with self.subTest("group to ignore is unchanged"):
+            backend_1 = FormRegistrationBackend.objects.get(key="group-to-ignore")
+
+            self.assertEqual(backend_1.options, {"objects_api_group": "group_1"})
+
+        with self.subTest("group to update has catalogue and document types"):
+            backend_2 = FormRegistrationBackend.objects.get(key="group-to-update")
+
+            self.assertEqual(
+                backend_2.options,
+                {
+                    "objects_api_group": "group_2",
+                    "catalogue": {
+                        "domain": "ABCDE",
+                        "rsin": "000111222",
+                    },
+                    "iot_submission_report": "PDF",
+                    "iot_submission_csv": "",
+                    "iot_attachment": "",
+                },
+            )
+
+        with self.subTest("group with local config is not updated"):
+            backend_3 = FormRegistrationBackend.objects.get(
+                key="has-local-config-already"
+            )
+
+            self.assertEqual(
+                backend_3.options,
+                {
+                    "objects_api_group": "group_2",
+                    "catalogue": {
+                        "domain": "notouch",
+                        "rsin": "notouch",
+                    },
+                },
+            )
