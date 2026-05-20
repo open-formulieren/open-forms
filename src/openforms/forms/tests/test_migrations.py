2dce02f3c7c7a271f306ddc92ba21f6f57513d02
Sergei Maertens <sergei@maykinmedia.nl>
2026-05-20 19:22:35 +0200

    :white_check_mark: [#4939] Add tests for ZGW APIs counterpart
    This is simpler because there are no case type or document type defaults
    at the group level, only the catalogue is relevant to be copied over.

--- src/openforms/forms/tests/test_migrations.py
+++ src/openforms/forms/tests/test_migrations.py
@@ -704,3 +704,130 @@ class MoveObjectsAPIGroupConfigurationToBackendTests(MigratorTestCase):
                     },
                 },
             )
+
+
+class MoveZGWAPIGroupConfigurationToBackendTests(MigratorTestCase):
+    migrate_from = [
+        ("zgw_apis", "0003_alter_zgwapigroupconfig_use_generated_zaaknummer"),
+        ("forms", "0129_remove_form_new_renderer_enabled"),
+    ]
+    migrate_to = (
+        "forms",
+        "0131_move_api_group_configuration_to_backends",
+    )
+
+    def prepare(self):
+        # uses real model, but that shouldn't matter
+
+        apps = self.old_state.apps
+        Service = apps.get_model("zgw_consumers", "Service")
+        ZGWApiGroupConfig = apps.get_model("zgw_apis", "ZGWApiGroupConfig")
+        Form = apps.get_model("forms", "Form")
+        FormRegistrationBackend = apps.get_model("forms", "FormRegistrationBackend")
+
+        # create API groups
+        dummy_service = Service.objects.create(
+            slug="dummy", api_type="orc", api_root="https://example.com/api/v1/"
+        )
+        group_1 = ZGWApiGroupConfig.objects.create(
+            name="Ignore - no catalogue set up",
+            identifier="group_1",
+            zrc_service=dummy_service,
+            drc_service=dummy_service,
+            ztc_service=dummy_service,
+            catalogue_domain="",
+            catalogue_rsin="",
+        )
+        group_2 = ZGWApiGroupConfig.objects.create(
+            name="Copy over",
+            identifier="group_2",
+            zrc_service=dummy_service,
+            drc_service=dummy_service,
+            ztc_service=dummy_service,
+            catalogue_domain="ABCDE",
+            catalogue_rsin="000111222",
+        )
+
+        form = Form.objects.create(name="Test")
+        FormRegistrationBackend.objects.create(
+            form=form,
+            key="group-to-ignore",
+            name="Should be ignored because of the group",
+            backend="zgw-create-zaak",
+            # incomplete according to the types/serializer, but shouldn't break the
+            # migration!
+            options={
+                "zgw_api_group": group_1.pk,
+            },
+        )
+        FormRegistrationBackend.objects.create(
+            form=form,
+            key="group-to-update",
+            name="Should be updated",
+            backend="zgw-create-zaak",
+            # incomplete according to the types/serializer, but shouldn't break the
+            # migration!
+            options={
+                "zgw_api_group": group_2.pk,
+            },
+        )
+        FormRegistrationBackend.objects.create(
+            form=form,
+            key="has-local-config-already",
+            name="May not be updated despite group qualifying",
+            backend="zgw-create-zaak",
+            # incomplete according to the types/serializer, but shouldn't break the
+            # migration!
+            options={
+                "zgw_api_group": group_2.pk,
+                "catalogue": {
+                    "domain": "notouch",
+                    "rsin": "notouch",
+                },
+            },
+        )
+
+    def test_migration_processes_backends_correctly(self):
+        FormRegistrationBackend = self.new_state.apps.get_model(
+            "forms", "FormRegistrationBackend"
+        )
+        ZGWApiGroupConfig = self.new_state.apps.get_model(
+            "zgw_apis", "ZGWApiGroupConfig"
+        )
+        group_1 = ZGWApiGroupConfig.objects.get(identifier="group_1")
+        group_2 = ZGWApiGroupConfig.objects.get(identifier="group_2")
+
+        with self.subTest("group to ignore is unchanged"):
+            backend_1 = FormRegistrationBackend.objects.get(key="group-to-ignore")
+
+            self.assertEqual(backend_1.options, {"zgw_api_group": group_1.pk})
+
+        with self.subTest("group to update has catalogue"):
+            backend_2 = FormRegistrationBackend.objects.get(key="group-to-update")
+
+            self.assertEqual(
+                backend_2.options,
+                {
+                    "zgw_api_group": group_2.pk,
+                    "catalogue": {
+                        "domain": "ABCDE",
+                        "rsin": "000111222",
+                    },
+                },
+            )
+
+        with self.subTest("group with local config is not updated"):
+            backend_3 = FormRegistrationBackend.objects.get(
+                key="has-local-config-already"
+            )
+
+            self.assertEqual(
+                backend_3.options,
+                {
+                    "zgw_api_group": group_2.pk,
+                    "catalogue": {
+                        "domain": "notouch",
+                        "rsin": "notouch",
+                    },
+                },
+            )
