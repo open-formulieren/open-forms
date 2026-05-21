66aa7188a8027108218eadbd21fd1b1bec558f27
Sergei Maertens <sergei@maykinmedia.nl>
2026-05-21 08:58:22 +0200

    :rewind: [#4939] Restore DB field removal for check script
    The 4.0 upgrades will run a modified version of the migration tool as
    an upgrade check. To be able to detect unmigrated backends and API
    groups, we must still be able to query the legacy fields using the
    real models, which requires the field definitions to still be on the
    model.

--- src/openforms/contrib/objects_api/models.py
+++ src/openforms/contrib/objects_api/models.py
@@ -3,7 +3,12 @@ from django.utils.translation import gettext_lazy as _
 
 from zgw_consumers.constants import APITypes
 
-from openforms.utils.validators import validate_rsin
+from openforms.utils.validators import RSINValidator, validate_rsin
+
+# no catalogus specified, requires both RSIN and domain to be unspecified
+_CATALOGUE_NOT_SET = models.Q(catalogue_domain="", catalogue_rsin="")
+# catalogus specified, requires both RSIN and domain to be set
+_CATALOGUE_SET = ~models.Q(catalogue_domain="") & ~models.Q(catalogue_rsin="")
 
 
 class ObjectsAPIGroupConfig(models.Model):
@@ -57,6 +62,32 @@ class ObjectsAPIGroupConfig(models.Model):
     #
     # Overridable defaults
     #
+
+    # Specify which catalogus to use to look up document types. See
+    # https://catalogi-api.vng.cloud/ for the API specification. A catalogus is uniquely
+    # identified by the combination (domein, rsin)
+    catalogue_domain = models.CharField(
+        _("catalogus domain"),
+        # blank because: opt-in to new config pattern & may be specified on form-level
+        # options instead of here.
+        blank=True,
+        max_length=5,
+        help_text=_(
+            "The 'domein' attribute for the Catalogus resource in the Catalogi API."
+        ),
+    )
+    catalogue_rsin = models.CharField(
+        _("catalogus RSIN"),
+        # blank because: opt-in to new config pattern & may be specified on form-level
+        # options instead of here.
+        blank=True,
+        max_length=9,
+        help_text=_(
+            "The 'rsin' attribute for the Catalogus resource in the Catalogi API."
+        ),
+        validators=[RSINValidator()],
+    )
+
     organisatie_rsin = models.CharField(
         _("organisation RSIN"),
         max_length=9,
@@ -65,9 +96,125 @@ class ObjectsAPIGroupConfig(models.Model):
         help_text=_("Default RSIN of organization, which creates the INFORMATIEOBJECT"),
     )
 
+    # Replaces informatiobjecttype_submission_report
+    iot_submission_report = models.CharField(
+        _("submission report document type description"),
+        # InformatieObjecttype.omschrijving resource attribute in Catalogi API spec
+        max_length=80,
+        blank=True,
+        help_text=_(
+            "Description of the document type in the Catalogi API to be used for the "
+            "submission report PDF (i.e. the INFORMATIEOBJECTTYPE.omschrijving "
+            "attribute). The appropriate version will automatically be selected based "
+            "on the submission timestamp and validity dates of the "
+            "document type versions."
+        ),
+    )
+    # Replaces informatiobjecttype_submission_csv
+    iot_submission_csv = models.CharField(
+        _("submission report CSV document type description"),
+        # InformatieObjecttype.omschrijving resource attribute in Catalogi API spec
+        max_length=80,
+        blank=True,
+        help_text=_(
+            "Description of the document type in the Catalogi API to be used for the "
+            "submission report CSV (i.e. the INFORMATIEOBJECTTYPE.omschrijving "
+            "attribute). The appropriate version will automatically be selected based "
+            "on the submission timestamp and validity dates of the "
+            "document type versions."
+        ),
+    )
+    # Replaces informatiobjecttype_attachment
+    iot_attachment = models.CharField(
+        _("attachment document type description"),
+        # InformatieObjecttype.omschrijving resource attribute in Catalogi API spec
+        max_length=80,
+        blank=True,
+        help_text=_(
+            "Description of the document type in the Catalogi API to be used for the "
+            "submission attachments (i.e. the INFORMATIEOBJECTTYPE.omschrijving "
+            "attribute). The appropriate version will automatically be selected based "
+            "on the submission timestamp and validity dates of the "
+            "document type versions."
+        ),
+    )
+
+    # XXX: the URLFields are to be replaced with charfields storing the omschrijving.
+    # DeprecationWarning: remove in OF 4.0
+    informatieobjecttype_submission_report = models.URLField(
+        _("submission report informatieobjecttype"),
+        max_length=1000,
+        blank=True,
+        help_text=_(
+            "Default URL that points to the INFORMATIEOBJECTTYPE in the Catalogi API "
+            "to be used for the submission report PDF"
+        ),
+    )
+    informatieobjecttype_submission_csv = models.URLField(
+        _("submission report CSV informatieobjecttype"),
+        max_length=1000,
+        blank=True,
+        help_text=_(
+            "Default URL that points to the INFORMATIEOBJECTTYPE in the Catalogi API "
+            "to be used for the submission report CSV"
+        ),
+    )
+    informatieobjecttype_attachment = models.URLField(
+        _("attachment informatieobjecttype"),
+        max_length=1000,
+        blank=True,
+        help_text=_(
+            "Default URL that points to the INFORMATIEOBJECTTYPE in the Catalogi API "
+            "to be used for the submission attachments"
+        ),
+    )
+
     class Meta:
         verbose_name = _("Objects API group")
         verbose_name_plural = _("Objects API groups")
+        constraints = [
+            models.CheckConstraint(
+                check=_CATALOGUE_NOT_SET | _CATALOGUE_SET,
+                name="catalogue_composite_key",
+                violation_error_message=_(
+                    "You must specify both domain and RSIN to uniquely identify a "
+                    "catalogue.",
+                ),
+            ),
+            models.CheckConstraint(
+                check=(
+                    models.Q(iot_submission_report="")
+                    | (~models.Q(iot_submission_report="") & _CATALOGUE_SET)
+                ),
+                name="iot_report_requires_catalogue",
+                violation_error_message=_(
+                    "You must specify a catalogue when specifying the submission "
+                    "report PDF document type.",
+                ),
+            ),
+            models.CheckConstraint(
+                check=(
+                    models.Q(iot_submission_csv="")
+                    | (~models.Q(iot_submission_csv="") & _CATALOGUE_SET)
+                ),
+                name="iot_csv_requires_catalogue",
+                violation_error_message=_(
+                    "You must specify a catalogue when specifying the submission "
+                    "report CSV document type.",
+                ),
+            ),
+            models.CheckConstraint(
+                check=(
+                    models.Q(iot_attachment="")
+                    | (~models.Q(iot_attachment="") & _CATALOGUE_SET)
+                ),
+                name="iot_attachment_requires_catalogue",
+                violation_error_message=_(
+                    "You must specify a catalogue when specifying the submission "
+                    "attachment document type.",
+                ),
+            ),
+        ]
 
     def __str__(self) -> str:
         return self.name
--- src/openforms/registrations/contrib/zgw_apis/models.py
+++ src/openforms/registrations/contrib/zgw_apis/models.py
@@ -6,11 +6,17 @@ from zgw_consumers.api_models.constants import VertrouwelijkheidsAanduidingen
 from zgw_consumers.constants import APITypes
 
 from openforms.template.validators import DjangoTemplateValidator
-from openforms.utils.validators import validate_rsin
+from openforms.utils.validators import RSINValidator, validate_rsin
 
 
 def get_content_text() -> str:
     return render_to_string("registrations/contrib/zgw_apis/content_json.txt").strip()
+
+
+# no catalogus specified, requires both RSIN and domain to be unspecified
+_CATALOGUE_NOT_SET = models.Q(catalogue_domain="", catalogue_rsin="")
+# catalogus specified, requires both RSIN and domain to be set
+_CATALOGUE_SET = ~models.Q(catalogue_domain="") & ~models.Q(catalogue_rsin="")
 
 
 class ZGWApiGroupConfig(models.Model):
@@ -54,6 +60,28 @@ class ZGWApiGroupConfig(models.Model):
     #
     # Overridable defaults
     #
+    catalogue_domain = models.CharField(
+        _("catalogus domain"),
+        # blank because: opt-in to new config pattern & may be specified on form-level
+        # options instead of here.
+        blank=True,
+        max_length=5,
+        help_text=_(
+            "The 'domein' attribute for the Catalogus resource in the Catalogi API."
+        ),
+    )
+    catalogue_rsin = models.CharField(
+        _("catalogus RSIN"),
+        # blank because: opt-in to new config pattern & may be specified on form-level
+        # options instead of here.
+        blank=True,
+        max_length=9,
+        help_text=_(
+            "The 'rsin' attribute for the Catalogus resource in the Catalogi API."
+        ),
+        validators=[RSINValidator()],
+    )
+
     organisatie_rsin = models.CharField(
         _("organisation RSIN"),
         max_length=9,
@@ -119,6 +147,16 @@ class ZGWApiGroupConfig(models.Model):
     class Meta:
         verbose_name = _("ZGW API set")
         verbose_name_plural = _("ZGW API sets")
+        constraints = [
+            models.CheckConstraint(
+                check=_CATALOGUE_NOT_SET | _CATALOGUE_SET,
+                name="registrations_zgw_apis_catalogue_composite_key",
+                violation_error_message=_(
+                    "You must specify both domain and RSIN to uniquely identify a "
+                    "catalogue.",
+                ),
+            ),
+        ]
 
     def __str__(self):
         return self.name
