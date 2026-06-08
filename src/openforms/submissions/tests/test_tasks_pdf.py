dac42b8cdfa217367145339ecd872b8e5abc96b2
Viktor van Wijk <viktorvanwijk@hotmail.com>
2026-06-08 16:27:43 +0200

    :rewind: [#6275] Revert previous bugfix for #5980
    The fix for #5980 to include all unsaved variables was not the root cause of the problem. The actual root cause was the
    trigger component value not defaulting to an empty string when a component cannot be found in the submission data - mimicking
    what our renderer does.
    
    With `is_visible_in_frontend` gone, and `is_hidden` using the "correct" trigger component value if a component is missing from
    the data now, including all unsaved variables is no longer necessary, and we can revert to the old behavior. Note that the added
    test for this still passes (SubmissionSummaryRendererTests.test_with_conditional_logic).
    
    I have updated the tests in `ExportTests` to reflect this, but also updated the test setup to be representative of what happens in
    the frontend: hidden fields with clearOnHide disabled will still have submission value variables created.
    
    Note on test_date_object_is_converted_to_str_when_it_comes_from_logic_rule: the value would be set correctly by the logic rule,
    but it just wouldn't be included in the report because it wasn't saved previously.

--- src/openforms/submissions/tests/test_tasks_pdf.py
+++ src/openforms/submissions/tests/test_tasks_pdf.py
@@ -119,13 +119,18 @@ class SubmissionReportGenerationTests(TestCase):
                 {
                     "type": "date",
                     "key": "date1",
+                    "label": "Date",
                 },
                 {
                     "type": "date",
                     "key": "updatedDate",
+                    "label": "Updated date",
                 },
             ],
-            submitted_data={"date1": "2025-01-01"},
+            # Note: this submission data is only possible by modifying the submission
+            # step put call, because the logic rule would have already been executed
+            # while filling out the step -> value should be "2025-02-01" already.
+            submitted_data={"date1": "2025-01-01", "updatedDate": ""},
             completed=True,
             with_report=True,
         )
