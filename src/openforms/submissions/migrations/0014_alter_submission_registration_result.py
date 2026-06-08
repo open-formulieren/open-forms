492e35d3f4d5e53221b721b16f89423362a5bc3c
Sergei Maertens <sergei@maykinmedia.nl>
2026-06-08 09:46:02 +0200

    :card_file_box: [#6217] Rename migration and remove merge migration
    From PR feedback.

--- src/openforms/submissions/migrations/0014_alter_submission_registration_result.py
+++ src/openforms/submissions/migrations/0014_alter_submission_registration_result.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("submissions", "0011_merge_20260304_1610"),
+        ("submissions", "0013_submissionstep_completed_on"),
     ]
 
     operations = [
