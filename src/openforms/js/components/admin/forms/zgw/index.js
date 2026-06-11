7063d31e47367f5c5aa90765740ddde69cc70686
Sergei Maertens <sergei@maykinmedia.nl>
2026-06-11 13:02:36 +0200

    :fire: [#6281] Remove CopyDocumentTypesConfig from zgw/objects registration
    The copy functionality was a temporary solution to copy the backend
    configuration (catalogue, document type) to the individual file
    components, relevant only for 3.5.
    
    The migration tool added in 3.5 takes care of resolving specific doc
    type URLs specified on the file components. Our (future) data migration
    in 4.0 will copy the configuration from the component to the
    registration backend options (for each file component), obsoleting this
    bulk-copy functionality.
    
    The variables table makes it possible to configure the file overrides
    for each relevant backend, so this table/copy tool is obsoleted and
    now removed.

--- src/openforms/js/components/admin/forms/zgw/index.js
+++ src/openforms/js/components/admin/forms/zgw/index.js
@@ -7,5 +7,4 @@ export {
   extractValue as getCatalogueOption,
   groupAndSortOptions as groupAndSortCatalogueOptions,
 } from './CatalogueSelect';
-export {default as CopyDocumentTypesConfig} from './CopyDocumentTypesConfig';
 export {DocumentTypeSelect, useGetDocumentTypes} from './DocumentTypeSelect';
