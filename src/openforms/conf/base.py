20af9151a97c2ef77dbc35f26f205bf951a33fd3
robinvandermolen <robin@maykinmedia.nl>
2026-07-20 16:49:14 +0200

    :bookmark: Bump API version

--- src/openforms/conf/base.py
+++ src/openforms/conf/base.py
@@ -1070,7 +1070,7 @@ Open Forms fits in the [Common Ground](https://commonground.nl) vision and archi
 and it plays nice with other available components.
 """
 
-API_VERSION = "4.0.0-alpha.1"
+API_VERSION = "4.0.0-alpha.2"
 
 SPECTACULAR_SETTINGS = {
     "SCHEMA_PATH_PREFIX": "/api/v2",
