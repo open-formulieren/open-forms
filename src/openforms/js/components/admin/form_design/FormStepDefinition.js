28a06a5430d224ee67f49ce0b8fbd2477c63fb60
Sergei Maertens <sergei@maykinmedia.nl>
2026-08-13 11:06:32 +0200

    :art: Clean up warnings
    * Remove unused imports
    * Add missing property to fix proptypes warning
    * Strict slug check for form definition collapse state

--- src/openforms/js/components/admin/form_design/FormStepDefinition.js
+++ src/openforms/js/components/admin/form_design/FormStepDefinition.js
@@ -170,7 +170,7 @@ const FormStepDefinition = ({
           />
         }
         collapsible
-        initialCollapsed={hasName && slug && !errors.length}
+        initialCollapsed={hasName && slug !== '' && !errors.length}
       >
         <LanguageTabs haveErrors={[...erroredLanguages]}>
           {(langCode, defaultLang) => (
