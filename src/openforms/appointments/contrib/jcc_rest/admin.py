a446f06ca89b8274b0848baf8209789768d01536
Sergei Maertens <sergei@maykinmedia.nl>
2026-08-25 12:02:54 +0200

    :bug: [#6570] Ensure global validate.required default configuration is not ignored
    This got accidentally nuked in PR #6455 (commit
    a666d4da7d5452cecb22739b1a8db14059c399da) when swapping out formiojs'
    builder for our own.

--- src/openforms/appointments/contrib/jcc_rest/admin.py
+++ src/openforms/appointments/contrib/jcc_rest/admin.py
@@ -10,3 +10,23 @@ from .models import JccRestConfig
 class JccRestConfigAdmin(SingletonModelAdmin):
     form = JccRestConfigForm
     change_form_template = "admin/jcc_rest/jccrestconfig/change_form.html"
+
+    def render_change_form(
+        self, request, context, add=False, change=False, form_url="", obj=None
+    ):
+        # Can't use `openforms.forms.admin.FormioConfigMixin` because the appointment
+        # configuration has different semantics - `required_default` is controlled by
+        # the upstream API, while file uploads/map components are irrelevant.
+        context.update(
+            {
+                "required_default": False,
+                "rich_text_colors": [],
+                "map_tile_layers": [],
+                "wms_layers": [],
+                "upload_filetypes": [],
+                "feature_flags": {},
+                "confidentiality_levels": [],
+                "component_empty_values": [],
+            }
+        )
+        return super().render_change_form(request, context, add, change, form_url, obj)
