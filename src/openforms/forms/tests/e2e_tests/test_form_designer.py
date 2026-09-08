3778d7f9f96e3b068211188a5f30b584eae52405
Sergei Maertens <sergei@maykinmedia.nl>
2026-09-08 15:53:53 +0200

    :green_heart: [#3941] Update end to end test
    The previous test passed because the save actions were slow enough for
    the tab to still be visible, but now with the single PUT call you
    immediately get the loader and success/error feedback.

--- src/openforms/forms/tests/e2e_tests/test_form_designer.py
+++ src/openforms/forms/tests/e2e_tests/test_form_designer.py
@@ -366,10 +366,10 @@ class FormDesignerRegressionTests(E2ETestCase):
             # Save form
             await page.locator('[name="_save"]', has_text="Save").click()
 
-            await page.get_by_role("tab", name="Steps and fields").click()
-
-            error_node = page.locator("css=.error")
-            await expect(error_node).not_to_be_visible()
+            # check that we get the success message
+            await expect(
+                page.get_by_text('The form "Playwright test" was changed successfully.')
+            ).to_be_visible()
 
     @tag("gh-3921", "gh-4061")
     async def test_all_components_are_visible_in_component_select_dropdown(self):
