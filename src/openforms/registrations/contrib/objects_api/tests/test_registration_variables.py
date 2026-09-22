dab56e7ec4d6e19a7070e783adfb5802f1faef19
Sergei Maertens <sergei@maykinmedia.nl>
2026-09-22 14:39:48 +0200

    :white_check_mark: [#5301] Add tests for desired output of registrator variable

--- src/openforms/registrations/contrib/objects_api/tests/test_registration_variables.py
+++ src/openforms/registrations/contrib/objects_api/tests/test_registration_variables.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 
+from openforms.authentication.constants import AuthAttribute
+from openforms.authentication.tests.factories import RegistratorInfoFactory
+from openforms.submissions.tests.factories import SubmissionFactory
+
 from ..registration_variables import register
 
 
@@ -13,3 +17,32 @@ class RegistrationVariableTests(TestCase):
                     raise self.failureException(
                         "Unexpected crash on None value"
                     ) from exc
+
+    def test_registrator_variable_without_registrator(self):
+        submission = SubmissionFactory.create()
+        assert submission.has_registrator is False
+
+        variable = register["registrator"].get_initial_value(submission=submission)
+
+        self.assertIsNone(variable)
+
+    def test_registrator_variable_with_registrator(self):
+        submission = SubmissionFactory.create()
+        RegistratorInfoFactory.create(
+            submission=submission,
+            plugin="org-oidc",
+            attribute=AuthAttribute.employee_id,
+            value="some-employee@example.com",
+        )
+
+        variable = register["registrator"].get_initial_value(submission=submission)
+
+        self.assertIsNotNone(variable)
+        self.assertEqual(
+            variable,
+            {
+                "plugin": "org-oidc",
+                "attribute": "employee_id",
+                "value": "some-employee@example.com",
+            },
+        )
