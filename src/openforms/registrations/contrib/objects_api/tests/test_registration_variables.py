862111668ff1da6a03004c4a46b046d2be77ab4a
Sergei Maertens <sergei@maykinmedia.nl>
2026-09-23 13:47:28 +0200

    :white_check_mark: [#5301] Extend tests with expected included OIDC claims
    We expect the claims from the user model to be included in the
    registration variable.

--- src/openforms/registrations/contrib/objects_api/tests/test_registration_variables.py
+++ src/openforms/registrations/contrib/objects_api/tests/test_registration_variables.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+from openforms.accounts.tests.factories import UserFactory
 from openforms.authentication.constants import AuthAttribute
 from openforms.authentication.tests.factories import RegistratorInfoFactory
 from openforms.submissions.tests.factories import SubmissionFactory
@@ -22,27 +23,84 @@ class RegistrationVariableTests(TestCase):
         submission = SubmissionFactory.create()
         assert submission.has_registrator is False
 
-        variable = register["registrator"].get_initial_value(submission=submission)
+        value = register["registrator"].get_initial_value(submission=submission)
 
-        self.assertIsNone(variable)
+        self.assertIsNone(value)
 
-    def test_registrator_variable_with_registrator(self):
+    def test_registrator_variable_with_registrator_but_no_oidc_claims(self):
+        user = UserFactory.create(is_staff=True, raw_oidc_claims={}, employee_id="9999")
         submission = SubmissionFactory.create()
         RegistratorInfoFactory.create(
             submission=submission,
             plugin="org-oidc",
-            attribute=AuthAttribute.employee_id,
-            value="some-employee@example.com",
+            attribute=AuthAttribute.local_user_id,
+            value=str(user.pk),
         )
 
-        variable = register["registrator"].get_initial_value(submission=submission)
+        value = register["registrator"].get_initial_value(submission=submission)
 
-        self.assertIsNotNone(variable)
+        self.assertIsNotNone(value)
         self.assertEqual(
-            variable,
+            value,
             {
                 "plugin": "org-oidc",
                 "attribute": "employee_id",
-                "value": "some-employee@example.com",
+                "value": "9999",
+                "oidc_claims": None,
+            },
+        )
+
+    def test_registrator_variable_with_registrator_and_oidc_claims(self):
+        user = UserFactory.create(
+            is_staff=True,
+            employee_id="9999",
+            raw_oidc_claims={
+                "aud": "testid",
+                "email": "admin@example.com",
+                "email_verified": True,
+                "employeeId": "9999",
+                "groups": [
+                    "Registreerders",
+                    "default-roles-test",
+                    "offline_access",
+                    "uma_authorization",
+                ],
+                "iss": "http://localhost:8080/realms/test",
+                "preferred_username": "admin",
+                "sub": "6db2db87-de31-4e30-9f25-cefe5da8b154",
+            },
+        )
+        submission = SubmissionFactory.create()
+        RegistratorInfoFactory.create(
+            submission=submission,
+            plugin="org-oidc",
+            attribute=AuthAttribute.local_user_id,
+            value=str(user.pk),
+        )
+
+        value = register["registrator"].get_initial_value(submission=submission)
+
+        self.assertIsNotNone(value)
+        self.assertEqual(
+            value,
+            {
+                "plugin": "org-oidc",
+                "attribute": "employee_id",
+                "value": "9999",
+                "oidc_claims": {
+                    "aud": "testid",
+                    "email": "admin@example.com",
+                    "email_verified": True,
+                    "employeeId": "9999",
+                    "groups": [
+                        "Registreerders",
+                        "default-roles-test",
+                        "offline_access",
+                        "uma_authorization",
+                    ],
+                    "iss": "http://localhost:8080/realms/test",
+                    "preferred_username": "admin",
+                    "sub": "6db2db87-de31-4e30-9f25-cefe5da8b154",
+                },
             },
         )
